f772447452879571318fbd3cedd2a497b0277a8a
Stanislaw Maciag <stanislaw.maciag@intel.com>
2018-06-12 10:30:55 +0100

    Fix dimensions in serializer

--- include/fathom/deployer/serializer.hpp
+++ include/fathom/deployer/serializer.hpp
@@ -231,7 +231,7 @@ class Blob_buffer : public WBuffer
                 
             }
 
-            blob_stats.output_size = cm.getLast()->getInput(0)->getShape()[1] * cm.getLast()->getInput(0)->getShape()[2] ;
+            blob_stats.output_size = cm.getLast()->getInput(0)->getShape()[0] * cm.getLast()->getInput(0)->getShape()[1] ;
             blob_stats.stage_section_size = align(blob_stats.stage_section_size, 16) ;
             blob_stats.buffer_data_size = blob_stats.weights_region_size + blob_stats.bias_region_size + blob_stats.params_region_size ;
 
@@ -391,17 +391,17 @@ class Blob_buffer : public WBuffer
                     AddBytes(4, it->getInput(0)->getShape()[2]);  // input Z-dimension size   (0x90)
                     AddBytes(4, number_size*it->getInput(0)->getShape()[2]);    // InputStrideX
                     AddBytes(4, number_size*it->getInput(0)->getShape()[2]*it->getInput(0)->getShape()[0]);  // InputStrideY
-                    AddBytes(4, number_size*it->getInput(0)->getShape()[0]);   // InputStrideZ
+                    AddBytes(4, number_size); // InputStrideZ
             AddBytes(4, test_conv_stage.InputOffset);     //  0xa0
             AddBytes(4, test_conv_stage.InputLocation);
             AddBytes(4, test_conv_stage.InputDataType);
             AddBytes(4, test_conv_stage.InputOrder);
 
-                    AddBytes(4, it->getOutput()->getShape()[1]);  // output X-dimension size  (0xb0)
-                    AddBytes(4, it->getOutput()->getShape()[2]);  // output Y-dimension size
-                    AddBytes(4, it->getOutput()->getShape()[0]);  // output Z-dimension size
+                    AddBytes(4, it->getOutput()->getShape()[0]);  // output X-dimension size  (0xb0)
+                    AddBytes(4, it->getOutput()->getShape()[1]);  // output Y-dimension size
+                    AddBytes(4, it->getOutput()->getShape()[2]);  // output Z-dimension size
             AddBytes(4, test_conv_stage.OutputStrideX);
-                    AddBytes(4, number_size*it->getOutput()->getShape()[1]);   // 0xc0
+                    AddBytes(4, number_size*it->getOutput()->getShape()[0]);   // 0xc0
             AddBytes(4, test_conv_stage.OutputStrideZ);
             AddBytes(4, test_conv_stage.OutputOffset);
             AddBytes(4, test_conv_stage.OutputLocation);
