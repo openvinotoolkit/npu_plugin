67e302043975130c79a044de628aae5277000321
Stanislaw Maciag <stanislaw.maciag@intel.com>
2018-07-19 19:03:01 +0100

    Document ResNet18 example

--- examples/cm_resnet18.cpp
+++ examples/cm_resnet18.cpp
@@ -1,23 +1,50 @@
+/**
+ * @brief Example presenting composition of the ResNet18 network and its compilation
+ * @file cm_resnet18.cpp
+ * @author Stanislaw Maciag
+ * @date 2018-07-19
+ */
+
 #include "include/mcm/compiler/compilation_unit.hpp"
 #include "include/mcm/utils/data_generator.hpp"
 
+/**
+ * @brief Helper function creates a chain of conv2D and batchnorm attached to the selected input tensor
+ * 
+ * @param model Master compositional model
+ * @param input Tensor that is an input data for the conv2D
+ * @param kernelShape Shape of conv2D kernel
+ * @param stride Stride of conv2D
+ * @param padding Padding of conv2D
+ * @return mv::Data::TensorIterator Iterator referencing the created batchnorm output 
+ */
 mv::Data::TensorIterator convBatchNormBlock(mv::CompositionalModel& model, mv::Data::TensorIterator input,  mv::Shape kernelShape, mv::UnsignedVector2D stride, mv::UnsignedVector4D padding)
 {
+    
     mv::dynamic_vector<mv::float_type> weightsData = mv::utils::generateSequence<mv::float_type>(kernelShape.totalSize());
     auto weights = model.constant(weightsData, kernelShape, mv::DType::Float, mv::Order::LastDimMajor);
     auto conv = model.conv2D(input, weights, stride, padding);
 
+    // For debugging purpose weights are initialized as sequences of numbers, to be replaced with actual weights
     mv::dynamic_vector<mv::float_type> meanData = mv::utils::generateSequence<mv::float_type>(conv->getShape()[-1]);
     mv::dynamic_vector<mv::float_type> varianceData = mv::utils::generateSequence<mv::float_type>(conv->getShape()[-1]);
     mv::dynamic_vector<mv::float_type> offsetData = mv::utils::generateSequence<mv::float_type>(conv->getShape()[-1]);
     mv::dynamic_vector<mv::float_type> scaleData = mv::utils::generateSequence<mv::float_type>(conv->getShape()[-1]);
+   
     auto bnmean = model.constant(meanData, conv->getShape()[-1], mv::DType::Float, mv::Order::LastDimMajor);
     auto bnvariance = model.constant(varianceData, conv->getShape()[-1], mv::DType::Float, mv::Order::LastDimMajor);
     auto bnoffset = model.constant(offsetData, conv->getShape()[-1], mv::DType::Float, mv::Order::LastDimMajor);
     auto bnscale = model.constant(scaleData, conv->getShape()[-1], mv::DType::Float, mv::Order::LastDimMajor);
     return model.batchNorm(conv, bnmean, bnvariance, bnoffset, bnscale, 1e-6);
+
 }
 
+/**
+ * @brief Helper function that attaches a residual block to the selected input tensor
+ * @param model Master compositional model
+ * @param input Tensor that is an input data for first stages of residual block 
+ * @return mv::Data::TensorIterator Iterator referencing the created residual block output
+ */
 mv::Data::TensorIterator residualBlock(mv::CompositionalModel& model, mv::Data::TensorIterator input)
 {
 
@@ -31,6 +58,12 @@ mv::Data::TensorIterator residualBlock(mv::CompositionalModel& model, mv::Data::
 
 }
 
+/**
+ * @brief Helper function that attaches a residual block (with conv2d on branch b) to the selected input tensor
+ * @param model Master compositional model
+ * @param input Tensor that is an input data for first stages of residual block 
+ * @return mv::Data::TensorIterator Iterator referencing the created residual block output
+ */
 mv::Data::TensorIterator residualConvBlock(mv::CompositionalModel& model, mv::Data::TensorIterator input, mv::unsigned_type outputDepth, mv::UnsignedVector2D stride)
 {
 
@@ -48,10 +81,14 @@ mv::Data::TensorIterator residualConvBlock(mv::CompositionalModel& model, mv::Da
 int main()
 {
 
+    // Define the primary compilation unit
     mv::CompilationUnit unit(mv::Logger::VerboseLevel::VerboseInfo);
-    mv::CompositionalModel& cm = unit.model();
-    auto input = cm.input(mv::Shape(224, 224, 3), mv::DType::Float, mv::Order::LastDimMajor);
 
+    // Obtain compositional model from the compilation unit
+    mv::CompositionalModel& cm = unit.model();
+
+    // Compose the model for ResNet18
+    auto input = cm.input(mv::Shape(224, 224, 3), mv::DType::Float, mv::Order::LastDimMajor);
     auto conv1 = convBatchNormBlock(cm, input, mv::Shape(7, 7, 3, 64), {2, 2}, {3, 3, 3, 3});
     conv1 = cm.relu(conv1);
     auto pool1 = cm.maxpool2D(conv1, {3, 3}, {2, 2}, {1, 1, 1, 1});
@@ -68,21 +105,26 @@ int main()
     auto weights = cm.constant(weightsData, mv::Shape(pool5->getShape().totalSize(), 1000), mv::DType::Float, mv::Order::LastDimMajor);
     auto fc1000 = cm.fullyConnected(pool5, weights);
     auto softmax = cm.softmax(fc1000);
-
     cm.output(softmax);
 
+    // Load target descriptor for the selected target to the compilation unit
     std::string targetDescPath = std::getenv("MCM_HOME") + std::string("/config/target/ma2480.json");
     unit.targetDescriptor().load(targetDescPath);
+    
+    // Define the manadatory arguments for passes using compilation descriptor obtained from compilation unit
     unit.compilationDescriptor()["GenerateDot"]["output"] = std::string("cm_resnet18.dot");
     unit.compilationDescriptor()["GenerateDot"]["scope"] = std::string("ExecOpControlModel");
     unit.compilationDescriptor()["GenerateDot"]["content"] = std::string("full");
     unit.compilationDescriptor()["GenerateDot"]["html"] = true;
     
+    // Initialize compilation 
     unit.initialize();
+
+    // Run all passes
     unit.run();
 
-    system("dot -Tsvg cm_resnet18.dot -o cm_resnet18.svg");
-    system("dot -Tsvg cm_resnet18_adapt.dot -o cm_resnet18_adapt.svg");
+    //system("dot -Tsvg cm_resnet18.dot -o cm_resnet18.svg");
+    //system("dot -Tsvg cm_resnet18_adapt.dot -o cm_resnet18_adapt.svg");
 
     return 0;
 
