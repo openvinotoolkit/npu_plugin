9f2324d9fd2beea196056083f7614dcaf0de1b26
Ian Hunter <ian.hunter@intel.com>
2018-07-31 11:07:19 +0100

    fix the python unit tests after the master_rc merge

--- python/tests/convolution_composition_test.py
+++ python/tests/convolution_composition_test.py
@@ -17,267 +17,292 @@ class TestComposition(unittest.TestCase):
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_empty_om(self):
-        g = ca.getOM()
-        self.assertFalse(g.isValid())
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
+        self.assertFalse(om.isValid())
 
     def test_SWIG_connection(self):
         self.assertEqual(1, ca.testSWIG())
 
     def test_minimal_om(self):
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(32, 32, 3)
 
-        in_ = ca.input(g, shape)
-        ca.output(g, in_)
+        in_ = ca.input(om, shape)
+        ca.output(om, in_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_convolution(self):
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
 
         shape = ca.getShape(32, 32, 3)
 
         weightData = ca.getData(np.arange(3 * 3 * 3 * 3).astype(np.float32))
-        weights = ca.constant(g, weightData, ca.getShape(3, 3, 3, 3))
+        weights = ca.constant(om, weightData, ca.getShape(3, 3, 3, 3))
 
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
 
-        c_ = ca.conv2D(g, in_, weights, 4, 4, 1, 1)
-        cOp_ = ca.getSourceOp(g, c_)
-        ca.output(g, c_)
+        c_ = ca.conv2D(om, in_, weights, 4, 4, 1, 1)
+        cOp_ = ca.getSourceOp(om, c_)
+        ca.output(om, c_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
         self.assertEqual(ca.testConv(cOp_, 4, 4, 1, 1), 0)
 
     def test_maxpool(self):
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(32, 32, 3)
 
-        in_ = ca.input(g, shape)
-        mx_ = ca.maxpool2D(g, in_, 2, 2, 2, 2, 0, 0)
-        ca.output(g, mx_)
+        in_ = ca.input(om, shape)
+        mx_ = ca.maxpool2D(om, in_, 2, 2, 2, 2, 0, 0)
+        ca.output(om, mx_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_concat(self):
 
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(256, 256, 1)
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
 
         k1data = [ 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2 ]
         k1blurData = ca.getData(np.array(k1data).astype(np.float32))
-        bweights = ca.constant(g, k1blurData, ca.getShape(3, 3, 1, 1))
+        bweights = ca.constant(om, k1blurData, ca.getShape(3, 3, 1, 1))
 
 
         k2data = [ 65504.0,65504.0,65504.0,65504.0,65504.0,65504.0,65504.0,65504.0,65504.0 ]
         k2edgeData = ca.getData(np.array(k2data).astype(np.float32))
-        eweights = ca.constant(g, k2edgeData, ca.getShape(3, 3, 1, 1))
+        eweights = ca.constant(om, k2edgeData, ca.getShape(3, 3, 1, 1))
 
-        c1_ = ca.conv2D(g, in_, bweights, 1, 1, 0, 0)
-        c2_ = ca.conv2D(g, in_, eweights, 1, 1, 0, 0)
-        cc_ = ca.concat(g, c1_, c2_)
-        ca.output(g, cc_)
+        c1_ = ca.conv2D(om, in_, bweights, 1, 1, 0, 0)
+        c2_ = ca.conv2D(om, in_, eweights, 1, 1, 0, 0)
+        cc_ = ca.concat(om, c1_, c2_)
+        ca.output(om, cc_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_avgpool(self):
 
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(32, 32, 3)
 
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
         kernels = ca.get2DVector(7, 7)
         stride = ca.get2DVector(1, 1)
         padding = ca.get4DVector(0, 0, 0, 0)
-        mx_ = ca.avgpool2D(g, in_, kernels, stride, padding)
-        ca.output(g, mx_)
+        mx_ = ca.avgpool2D(om, in_, kernels, stride, padding)
+        ca.output(om, mx_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_matMul(self):
 
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(4, 100)
 
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
         weightData = ca.getData(np.arange(100 * 100).astype(np.float32))
-        weights_ = ca.constant(g, weightData, ca.getShape(100, 100))
-        mx_ = ca.matMul(g, in_, weights_)
-        ca.output(g, mx_)
+        weights_ = ca.constant(om, weightData, ca.getShape(100, 100))
+        mx_ = ca.matMul(om, in_, weights_)
+        ca.output(om, mx_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_fullyConnected(self):
 
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(1, 1, 100)
 
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
         weightData = ca.getData(np.arange(100 * 100).astype(np.float32))
-        weights_ = ca.constant(g, weightData, ca.getShape(100, 100))
-        mx_ = ca.fullyConnected(g, in_, weights_)
-        ca.output(g, mx_)
+        weights_ = ca.constant(om, weightData, ca.getShape(100, 100))
+        mx_ = ca.fullyConnected(om, in_, weights_)
+        ca.output(om, mx_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_batchNorm(self):
 
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(28, 28, 4)
 
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
         meanData = ca.getData(np.arange(28 * 28 * 4).astype(np.float32))
-        mean_ = ca.constant(g, meanData, ca.getShape(28, 28, 4))
+        mean_ = ca.constant(om, meanData, ca.getShape(28, 28, 4))
 
         varData = ca.getData(np.arange(28 * 28 * 4).astype(np.float32))
-        variance_ = ca.constant(g, varData, ca.getShape(28, 28, 4))
+        variance_ = ca.constant(om, varData, ca.getShape(28, 28, 4))
 
         offsetData = ca.getData(np.arange(28 * 28 * 4).astype(np.float32))
-        offset_ = ca.constant(g, offsetData, ca.getShape(28, 28, 4))
+        offset_ = ca.constant(om, offsetData, ca.getShape(28, 28, 4))
 
         scaleData = ca.getData(np.arange(28 * 28 * 4).astype(np.float32))
-        scale_ = ca.constant(g, scaleData, ca.getShape(28, 28, 4))
+        scale_ = ca.constant(om, scaleData, ca.getShape(28, 28, 4))
 
         eps_ = 2
 
-        mx_ = ca.batchNorm(g, in_, mean_, variance_, offset_, scale_, eps_)
-        ca.output(g, mx_)
+        mx_ = ca.batchNorm(om, in_, mean_, variance_, offset_, scale_, eps_)
+        ca.output(om, mx_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_scale(self):
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(32, 32, 3)
 
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
         weightData = ca.getData(np.arange(32 * 32 * 3).astype(np.float32))
 
-        scaleMatrix_ = ca.constant(g, weightData, ca.getShape(32, 32, 3))
-        mx_ = ca.scale(g, in_, scaleMatrix_)
-        ca.output(g, mx_)
+        scaleMatrix_ = ca.constant(om, weightData, ca.getShape(32, 32, 3))
+        mx_ = ca.scale(om, in_, scaleMatrix_)
+        ca.output(om, mx_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_relu(self):
 
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(32, 32, 3)
 
-        in_ = ca.input(g, shape)
-        mx_ = ca.relu(g, in_)
-        ca.output(g, mx_)
+        in_ = ca.input(om, shape)
+        mx_ = ca.relu(om, in_)
+        ca.output(om, mx_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_softmax(self):
 
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(32, 32, 3)
 
-        in_ = ca.input(g, shape)
-        mx_ = ca.softmax(g, in_)
-        ca.output(g, mx_)
+        in_ = ca.input(om, shape)
+        mx_ = ca.softmax(om, in_)
+        ca.output(om, mx_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_add(self):
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
+
         shape = ca.getShape(32, 32, 3)
 
-        in_ = ca.input(g, shape)
-        mx_ = ca.maxpool2D(g, in_, 1, 1, 1, 1, 0, 0)
-        ad_ = ca.add(g, in_, mx_)
-        ca.output(g, ad_)
+        in_ = ca.input(om, shape)
+        mx_ = ca.maxpool2D(om, in_, 1, 1, 1, 1, 0, 0)
+        ad_ = ca.add(om, in_, mx_)
+        ca.output(om, ad_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_bias(self):
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
+
         shape = ca.getShape(32, 32, 3)
 
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
 
         data = ca.getData(np.arange(3).astype(np.float32))
-        bias = ca.constant(g, data, ca.getShape(3))
-        bi_ = ca.bias(g, in_, bias)
-        ca.output(g, bi_)
+        bias = ca.constant(om, data, ca.getShape(3))
+        bi_ = ca.bias(om, in_, bias)
+        ca.output(om, bi_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_subtract(self):
-        g = ca.getOM()
+
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(32, 32, 3)
 
-        in_ = ca.input(g, shape)
-        mx_ = ca.maxpool2D(g, in_, 1, 1, 1, 1, 0, 0)
+        in_ = ca.input(om, shape)
+        mx_ = ca.maxpool2D(om, in_, 1, 1, 1, 1, 0, 0)
 
-        sb_ = ca.subtract(g, in_, mx_)
-        ca.output(g, sb_)
+        sb_ = ca.subtract(om, in_, mx_)
+        ca.output(om, sb_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_multiply(self):
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
+
         shape = ca.getShape(32, 32, 3)
 
-        in_ = ca.input(g, shape)
-        mx_ = ca.maxpool2D(g, in_, 1, 1, 1, 1, 0, 0)
+        in_ = ca.input(om, shape)
+        mx_ = ca.maxpool2D(om, in_, 1, 1, 1, 1, 0, 0)
 
-        mu_ = ca.multiply(g, in_, mx_)
-        ca.output(g, mu_)
+        mu_ = ca.multiply(om, in_, mx_)
+        ca.output(om, mu_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_divide(self):
-        g = ca.getOM()
+
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(32, 32, 3)
 
-        in_ = ca.input(g, shape)
-        mx_ = ca.maxpool2D(g, in_, 1, 1, 1, 1, 0, 0)
-        mu_ = ca.divide(g, in_, mx_)
+        in_ = ca.input(om, shape)
+        mx_ = ca.maxpool2D(om, in_, 1, 1, 1, 1, 0, 0)
+        mu_ = ca.divide(om, in_, mx_)
 
-        ca.output(g, mu_)
+        ca.output(om, mu_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
     def test_reshape(self):
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(32, 32, 3)
 
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
         new_shape = ca.getShape(32, 32, 3)
-        mx_ = ca.reshape(g, in_, new_shape)
-        ca.output(g, mx_)
+        mx_ = ca.reshape(om, in_, new_shape)
+        ca.output(om, mx_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
 
     def testDOT(self):
 
-        g = ca.getOM()
+        cm = ca.getCompilationUnit()
+        om = ca.getModel(cm)
         shape = ca.getShape(256, 256, 1)
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
 
         k1data = [ 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2 ]
         k1blurData = ca.getData(np.array(k1data).astype(np.float32))
-        bweights = ca.constant(g, k1blurData, ca.getShape(3, 3, 1, 1))
+        bweights = ca.constant(om, k1blurData, ca.getShape(3, 3, 1, 1))
 
 
         k2data = [ 65504.0,65504.0,65504.0,65504.0,65504.0,65504.0,65504.0,65504.0,65504.0 ]
         k2edgeData = ca.getData(np.array(k2data).astype(np.float32))
-        eweights = ca.constant(g, k2edgeData, ca.getShape(3, 3, 1, 1))
+        eweights = ca.constant(om, k2edgeData, ca.getShape(3, 3, 1, 1))
 
-        c1_ = ca.conv2D(g, in_, bweights, 1, 1, 0, 0)
-        c2_ = ca.conv2D(g, in_, eweights, 1, 1, 0, 0)
-        cc_ = ca.concat(g, c1_, c2_)
-        ca.output(g, cc_)
+        c1_ = ca.conv2D(om, in_, bweights, 1, 1, 0, 0)
+        c2_ = ca.conv2D(om, in_, eweights, 1, 1, 0, 0)
+        cc_ = ca.concat(om, c1_, c2_)
+        ca.output(om, cc_)
 
-        ca.produceDOT(g)
+        # ca.produceDOT(om)
+        ca.compile(cm)
         self.assertTrue(os.path.isfile("pycm.dot"))
         self.assertTrue(os.path.isfile("pycm.svg"))
 
-    def test_serialize_convolution_01(self):
+    def test_compile_convolution_01(self):
         """
             32x32x1
                 |
@@ -287,7 +312,8 @@ class TestComposition(unittest.TestCase):
 
         """
 
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(32, 32, 1)
 
         arr = [
@@ -295,22 +321,22 @@ class TestComposition(unittest.TestCase):
         ]
         weightData = ca.getData(np.array(arr).astype(np.float32))
 
-        weights = ca.constant(g, weightData, ca.getShape(3, 3, 1, 1))
+        weights = ca.constant(om, weightData, ca.getShape(3, 3, 1, 1))
 
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
 
-        c_ = ca.conv2D(g, in_, weights, 4, 4, 0, 0)
-        cOp_ = ca.getSourceOp(g, c_)
-        ca.output(g, c_)
+        c_ = ca.conv2D(om, in_, weights, 4, 4, 0, 0)
+        cOp_ = ca.getSourceOp(om, c_)
+        ca.output(om, c_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
         self.assertEqual(ca.testConv(cOp_, 4, 4, 0, 0), 0)
 
-        fs = ca.serialize(g)
+        fs = ca.compile(cu)
 
         self.assertTrue(filecmp.cmp("cpp.blob", "../../tests/data/gold_01.blob"))
 
-    def test_serialize_convolution_02(self):
+    def test_compile_convolution_02(self):
         """
             32x32x3
                 |
@@ -319,28 +345,29 @@ class TestComposition(unittest.TestCase):
             32x32x3
 
         """
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
 
-        g = ca.getOM()
         shape = ca.getShape(32, 32, 3)
 
         weightData = ca.getData(np.linspace(0.101, 0.101 + (3 * 3 * 3 * 3 - 1) * 0.001, 3 * 3 * 3 * 3).astype(np.float32))
 
-        weights = ca.constant(g, weightData, ca.getShape(3, 3, 3, 3))
+        weights = ca.constant(om, weightData, ca.getShape(3, 3, 3, 3))
 
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
 
-        c_ = ca.conv2D(g, in_, weights, 4, 4, 0, 0)
-        cOp_ = ca.getSourceOp(g, c_)
-        out_ = ca.output(g, c_)
+        c_ = ca.conv2D(om, in_, weights, 4, 4, 0, 0)
+        cOp_ = ca.getSourceOp(om, c_)
+        out_ = ca.output(om, c_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
         self.assertEqual(ca.testConv(cOp_, 4, 4, 0, 0), 0)
 
-        fs = ca.serialize(g)
+        fs = ca.compile(cu)
         self.assertTrue(filecmp.cmp("cpp.blob", "../../tests/data/gold_02.blob"))
 
 
-    def test_serialize_convolution_03(self):
+    def test_compile_convolution_03(self):
         """
             32x32x3
                 |
@@ -350,27 +377,29 @@ class TestComposition(unittest.TestCase):
 
         """
 
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
+
         shape = ca.getShape(256, 256, 3)
 
         weightData = ca.getData(np.linspace(0.101, 0.101 + (3 * 3 * 3 * 3 - 1) * 0.001, 3 * 3 * 3 * 3).astype(np.float32))
 
-        weights = ca.constant(g, weightData, ca.getShape(3, 3, 3, 3))
+        weights = ca.constant(om, weightData, ca.getShape(3, 3, 3, 3))
 
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
 
-        c_ = ca.conv2D(g, in_, weights, 2, 2, 0, 0)
-        cOp_ = ca.getSourceOp(g, c_)
-        out_ = ca.output(g, c_)
+        c_ = ca.conv2D(om, in_, weights, 2, 2, 0, 0)
+        cOp_ = ca.getSourceOp(om, c_)
+        out_ = ca.output(om, c_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
         self.assertEqual(ca.testConv(cOp_, 2, 2, 0, 0), 0)
 
-        fs = ca.serialize(g)
+        fs = ca.compile(cu)
 
         self.assertTrue(filecmp.cmp("cpp.blob", "../../tests/data/gold_03.blob"))
 
-    def test_serialize_convolution_04(self):
+    def test_compile_convolution_04(self):
         """
             32x32x3
                 |
@@ -380,28 +409,29 @@ class TestComposition(unittest.TestCase):
 
         """
 
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(256, 256, 3)
 
         weightData = ca.getData(np.linspace(0.101, 0.101 + (5 * 5 * 3 * 3 - 1) * 0.001, 5 * 5 * 3 * 3).astype(np.float32))
 
-        weights = ca.constant(g, weightData, ca.getShape(5, 5, 3, 3))
+        weights = ca.constant(om, weightData, ca.getShape(5, 5, 3, 3))
 
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
 
-        c_ = ca.conv2D(g, in_, weights, 2, 2, 0, 0)
-        cOp_ = ca.getSourceOp(g, c_)
-        ca.output(g, c_)
+        c_ = ca.conv2D(om, in_, weights, 2, 2, 0, 0)
+        cOp_ = ca.getSourceOp(om, c_)
+        ca.output(om, c_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
         self.assertEqual(ca.testConv(cOp_, 2, 2, 0, 0), 0)
 
-        fs = ca.serialize(g)
+        fs = ca.compile(cu)
 
         self.assertTrue(filecmp.cmp("cpp.blob", "../../tests/data/gold_04.blob"))
 
 
-    def test_serialize_convolution_05(self):
+    def test_compile_convolution_05(self):
         """
             256x256x1
                 |
@@ -413,26 +443,27 @@ class TestComposition(unittest.TestCase):
 
         """
 
-        g = ca.getOM()
+        cu = ca.getCompilationUnit()
+        om = ca.getModel(cu)
         shape = ca.getShape(256, 256, 1)
-        in_ = ca.input(g, shape)
+        in_ = ca.input(om, shape)
 
         k1data = [ 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2 ]
         k1blurData = ca.getData(np.array(k1data).astype(np.float32))
-        bweights = ca.constant(g, k1blurData, ca.getShape(3, 3, 1, 1))
+        bweights = ca.constant(om, k1blurData, ca.getShape(3, 3, 1, 1))
 
 
         k2data = [ 65504.0,65504.0,65504.0,65504.0,65504.0,65504.0,65504.0,65504.0,65504.0 ]
         k2edgeData = ca.getData(np.array(k2data).astype(np.float32))
-        eweights = ca.constant(g, k2edgeData, ca.getShape(3, 3, 1, 1))
+        eweights = ca.constant(om, k2edgeData, ca.getShape(3, 3, 1, 1))
 
-        c1_ = ca.conv2D(g, in_, bweights, 1, 1, 0, 0)
-        c2_ = ca.conv2D(g, c1_, eweights, 1, 1, 0, 0)
-        ca.output(g, c2_)
+        c1_ = ca.conv2D(om, in_, bweights, 1, 1, 0, 0)
+        c2_ = ca.conv2D(om, c1_, eweights, 1, 1, 0, 0)
+        ca.output(om, c2_)
 
-        self.assertTrue(g.isValid())
+        self.assertTrue(om.isValid())
 
-        ca.serialize(g)
+        ca.compile(cu)
 
         self.assertTrue(filecmp.cmp("cpp.blob", "../../tests/data/gold_05.blob"))
 
