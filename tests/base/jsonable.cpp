1e68a84423fb8fe032f39205750446fb25e7e146
Marco Mecchia <marco.mecchia@intel.com>
2018-08-01 09:14:33 +0100

    Added gtest for computational model.

--- tests/base/jsonable.cpp
+++ tests/base/jsonable.cpp
@@ -160,4 +160,58 @@ TEST(jsonable, tensor)
 }
 
 
+TEST(jsonable, computation_model)
+{
+    // Define blank computation model (op view)
+    mv::OpModel om(mv::Logger::VerboseLevel::VerboseInfo);
+
+    // Initialize weights data
+    mv::dynamic_vector<mv::float_type> weights1Data = mv::utils::generateSequence<mv::float_type>(3u * 3u * 3u * 8u);
+    mv::dynamic_vector<mv::float_type> weights2Data = mv::utils::generateSequence<mv::float_type>(5u * 5u * 8u * 16u);
+    mv::dynamic_vector<mv::float_type> weights3Data = mv::utils::generateSequence<mv::float_type>(4u * 4u * 16u * 32u);
+
+    // Compose model - use Composition API to create ops and obtain tensors
+    auto input = om.input(mv::Shape(128, 128, 3), mv::DType::Float, mv::Order::ColumnMajor);
+    auto weights1 = om.constant(weights1Data, mv::Shape(3, 3, 3, 8), mv::DType::Float, mv::Order::ColumnMajor);
+    auto conv1 = om.conv2D(input, weights1, {2, 2}, {1, 1, 1, 1});
+    auto pool1 = om.maxpool2D(conv1, {3, 3}, {2, 2}, {1, 1, 1, 1});
+    auto weights2 = om.constant(weights2Data, mv::Shape(5, 5, 8, 16), mv::DType::Float, mv::Order::ColumnMajor);
+    auto conv2 = om.conv2D(pool1, weights2, {2, 2}, {2, 2, 2, 2});
+    auto pool2 = om.maxpool2D(conv2, {5, 5}, {4, 4}, {2, 2, 2, 2});
+    auto weights3 = om.constant(weights3Data, mv::Shape(4, 4, 16, 32), mv::DType::Float, mv::Order::ColumnMajor);
+    auto conv3 = om.conv2D(pool2, weights3, {1, 1}, {0, 0, 0, 0});
+    om.output(conv3);
+
+    // Obtain ops from tensors and add them to groups
+    auto pool1Op = om.getSourceOp(pool1);
+    auto pool2Op = om.getSourceOp(pool2);
+    auto group1It = om.addGroup("pools");
+    om.addGroupElement(pool1Op, group1It);
+    om.addGroupElement(pool2Op, group1It);
+
+    auto group2It = om.addGroup("convs");
+    auto conv1Op = om.getSourceOp(conv1);
+    auto conv2Op = om.getSourceOp(conv2);
+    auto conv3Op = om.getSourceOp(conv3);
+    om.addGroupElement(conv1Op, group2It);
+    om.addGroupElement(conv2Op, group2It);
+    om.addGroupElement(conv3Op, group2It);
+
+    // Add groups to another group
+    auto group3It = om.addGroup("ops");
+    om.addGroupElement(group1It, group3It);
+    om.addGroupElement(group2It, group3It);
+
+    // Add ops that are already in some group to another group
+    auto group4It = om.addGroup("first");
+    om.addGroupElement(conv1Op, group4It);
+    om.addGroupElement(pool1Op, group4It);
+
+    mv::json::Value v = om.toJsonValue();
+    mv::OpModel om2(v);
+    mv::json::Value v2 = om2.toJsonValue();
+    ASSERT_EQ(v.stringify(), v2.stringify());
+}
+
+
 
