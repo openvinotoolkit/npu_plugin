53754f893675d9d8c9806542e4e5a0a77d79f30e
Ian Hunter <ian.hunter@intel.com>
2018-09-13 10:56:54 +0100

    Update prelu.cpp

--- tests/op/prelu.cpp
+++ tests/op/prelu.cpp
@@ -9,14 +9,9 @@ TEST(ops, prelu)
     mv::dynamic_vector<mv::float_type> data = mv::utils::generateSequence<mv::float_type>(3);
     auto slope = om.constant(data, mv::Shape(3), mv::DType::Float, mv::Order::ColumnMajor);
 
-    std::cout << "CREATE THE PRELU" << std::endl;
     auto prelu = om.prelu(input, slope);
-    std::cout << "GET tHE sOURCEOP" << std::endl;
     auto preluOp = om.getSourceOp(prelu);
-    std::cout << "Add output" << std::endl;
     auto output = om.output(prelu);
-
-    std::cout << "Output shape:" << mv::Printable::toString(output->getShape()) << std::endl;
 
     ASSERT_EQ(output->getShape(), mv::Shape(32, 32, 3));
     ASSERT_EQ(preluOp->getOpType(), mv::OpType::PReLU);
