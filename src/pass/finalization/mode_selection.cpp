53d322bb6b20aadb81a9c0bc5a8f698de5fa39ca
Marco Mecchia <marco.mecchia@intel.com>
2018-09-19 11:53:27 +0100

    Handling simple case of optimization routine: MX Pre Padding pass takes care of all paddings in a safe way.

--- src/pass/finalization/mode_selection.cpp
+++ src/pass/finalization/mode_selection.cpp
@@ -22,37 +22,9 @@ namespace mv
     }
 }
 
-mv::ModeSelectionResult optimize_convolution_nce1(mv::Nce1& nce, mv::Data::OpListIterator convIterator)
-{
-    mv::ModeSelectionNode source;
-    auto weigth_tensor = convIterator->getInputTensor(1);
-    auto input_tensor = convIterator->getInputTensor(0);
-    auto output_tensor = convIterator->getOutputTensor(0);
-
-    auto kernel_dimensions = weigth_tensor->getShape();
-    auto input_dimensions = input_tensor->getShape();
-    auto output_dimensions = output_tensor->getShape();
-
-    source.parameters.kernel_x = kernel_dimensions[0];
-    source.parameters.kernel_y = kernel_dimensions[1];
-    source.parameters.input_width = input_dimensions[0];
-    source.parameters.input_height = input_dimensions[1];
-    source.parameters.input_channels = input_dimensions[2];
-    source.parameters.output_width = output_dimensions[0];
-    source.parameters.output_height = output_dimensions[1];
-    source.parameters.output_channels = output_dimensions[2];
-
-    auto strides = convIterator->getAttr("stride").getContent<mv::UnsignedVector2D>();
-    source.parameters.stride_x = strides.e0;
-    source.parameters.stride_y = strides.e1;
-
-    source.remaining_output_channels = source.parameters.output_channels;
-    return nce.optimize_convolution(source);
-}
 
 void write_hardware_attributes(mv::OpModel& om, mv::Data::OpListIterator convIterator, mv::ModeSelectionResult& modes_to_use, mv::Nce1& nce)
 {
-    mv::DataModel dm(om);
     // ASSUMPTION: all the splits in modes_to_use are are equal (This assumption is true now, and will be routines to assure it's trueness in the future)
 
     // Non vector attributes first
@@ -85,61 +57,43 @@ void write_hardware_attributes(mv::OpModel& om, mv::Data::OpListIterator convIte
     unsigned output_channels = output_tensor_dimensions[2];
     unsigned kernel_height = weight_tensor_dimensions[0];
 
-    // Computing actual dimensions
-    unsigned actual_input_width = nce.getActualInputWidth(input_width);
-    unsigned actual_input_height = input_height;
-    unsigned actual_input_channels = nce.getActualInputChannels(input_channels, max_mode);
-    unsigned actual_output_width = output_width;
-    unsigned actual_output_height = output_height;
-    unsigned actual_output_channels = nce.getActualOutputChannels(output_channels);
+    //TEMPORARY HACK: Prepass takes care of everything
+    if(input_tensor->hasAttr("NCE1_Paddings"))
+    {
+        mv::dynamic_vector<size_t> input_tensor_paddings = input_tensor->getAttr("NCE1_Paddings").getContent<mv::dynamic_vector<size_t>>();
+        input_width += input_tensor_paddings[0];
+        input_height += input_tensor_paddings[1];
+        input_channels += input_tensor_paddings[2];
+    }
 
-    // Check if any split is needed
-    unsigned actual_total_tensor_size = actual_input_height * actual_input_width * actual_input_channels + actual_output_width * actual_output_height * actual_output_channels;
-    unsigned splits_over_height = nce.getSplitsOverH(actual_total_tensor_size);
+    if(output_tensor->hasAttr("NCE1_Paddings"))
+    {
+        mv::dynamic_vector<size_t> output_tensor_paddings = output_tensor->getAttr("NCE1_Paddings").getContent<mv::dynamic_vector<size_t>>();
+        output_width += output_tensor_paddings[0];
+        output_height += output_tensor_paddings[1];
+        output_channels += output_tensor_paddings[2];
+    }
+
+    unsigned total_tensor_size = input_width * input_height * input_channels + output_width * output_height * output_channels;
+
+    // Check if any split over input channel is needed
     unsigned splits_over_input_channels = modes_to_use.distances[0].num_splits; //num_splits must be equal for every mode, see above assumption
-    unsigned splitted_input_channels = actual_input_channels / splits_over_input_channels;
-
-    // Handling top output junk and bottom output junk
-    if(splits_over_height > 1)
-    {
-        om.addAttr(convIterator, "NCE1_TopOutputJunk", mv::Attribute(mv::AttrType::IntegerType, 0));
-        om.addAttr(convIterator, "NCE1_BottomOutputJunk", mv::Attribute(mv::AttrType::IntegerType, 0));
-    }
-    else // Should be like this
-    {
-        om.addAttr(convIterator, "NCE1_TopOutputJunk", mv::Attribute(mv::AttrType::IntegerType, kernel_height-1));
-        om.addAttr(convIterator, "NCE1_BottomOutputJunk", mv::Attribute(mv::AttrType::IntegerType, kernel_height-1));
-    }
-
-    // Compute paddings for input and output tensors using actual dimensions
-    mv::UnsignedVector3D paddingsInputTensor;
-    mv::UnsignedVector3D paddingsOutputTensor;
-
-    paddingsInputTensor.e0 = actual_input_width - input_width;
-    paddingsInputTensor.e1 = actual_input_height - input_height;
-    paddingsInputTensor.e2 = actual_input_channels - input_channels;
-
-    paddingsOutputTensor.e0 = actual_output_width - output_width;
-    paddingsOutputTensor.e1 = actual_output_height - output_height;
-    paddingsOutputTensor.e2 = actual_output_channels - output_channels;
-
-    dm.addAttr(input_tensor, "NCE1_Paddings", mv::Attribute(mv::AttrType::UnsignedVec3DType, paddingsInputTensor));
-    dm.addAttr(output_tensor, "NCE1_Paddings", mv::Attribute(mv::AttrType::UnsignedVec3DType, paddingsOutputTensor));
+    unsigned splitted_input_channels = input_channels / splits_over_input_channels;
 
     // Compute local line stride
-    unsigned local_line_stride = nce.computeLocalLineStride(actual_input_width);
+    unsigned local_line_stride = nce.computeLocalLineStride(input_width);
     om.addAttr(convIterator, "NCE1_LocalLineStride", mv::Attribute(mv::AttrType::IntegerType, local_line_stride));
-
-    // Compute DescriptorsSplits
-    int descriptor_splits = nce.computeDescriptorSplits(splits_over_height, splits_over_input_channels, actual_output_channels, modes);
-    om.addAttr(convIterator, "NCE1_DescriptorSplits", mv::Attribute(mv::AttrType::IntegerType, descriptor_splits));
 
     // TODO: Streaming mask
     int streaming_mask = 0; // For DDR streaming
     om.addAttr(convIterator, "NCE1_StreamingMask", mv::Attribute(mv::AttrType::IntegerType, streaming_mask));
 
-    // Vector attributes
+    // Compute DescriptorsSplits
+    unsigned splits_over_height = nce.getSplitsOverH(total_tensor_size);
+    int descriptor_splits = nce.computeDescriptorSplits(splits_over_height, splits_over_input_channels, output_channels, modes);
+    om.addAttr(convIterator, "NCE1_DescriptorSplits", mv::Attribute(mv::AttrType::IntegerType, descriptor_splits));
 
+    // -------------------VECTOR ATTRIBUTES----------------
     std::vector<unsigned> input_channels_per_ram_block(num_modes_to_use);
     std::vector<unsigned> lines_per_channel(num_modes_to_use);
     std::vector<unsigned> local_channel_stride(num_modes_to_use);
@@ -157,24 +111,75 @@ void write_hardware_attributes(mv::OpModel& om, mv::Data::OpListIterator convIte
         else
             min_lines[i] = std::min(kernel_height+1, lines_per_channel[i]);
     }
-    om.addAttr(convIterator, "NCE1_InputChannelsRamBlock", mv::Attribute(mv::AttrType::FloatVecType, local_line_stride));
-    om.addAttr(convIterator, "NCE1_LinesPerChannel", mv::Attribute(mv::AttrType::FloatVecType, lines_per_channel));
-    om.addAttr(convIterator, "NCE1_LocalChannelStride", mv::Attribute(mv::AttrType::FloatVecType, local_channel_stride));
-    om.addAttr(convIterator, "NCE1_MinLines", mv::Attribute(mv::AttrType::FloatVecType, min_lines));
+    om.addAttr(convIterator, "NCE1_InputChannelsRamBlock", mv::Attribute(mv::AttrType::UnsignedVecType, input_channels_per_ram_block));
+    om.addAttr(convIterator, "NCE1_LinesPerChannel", mv::Attribute(mv::AttrType::UnsignedVecType, lines_per_channel));
+    om.addAttr(convIterator, "NCE1_LocalChannelStride", mv::Attribute(mv::AttrType::UnsignedVecType, local_channel_stride));
+    om.addAttr(convIterator, "NCE1_MinLines", mv::Attribute(mv::AttrType::UnsignedVecType, min_lines));
+}
+
+void optimize_convolution_nce1(mv::Nce1& nce, mv::Data::OpListIterator convIterator, mv::OpModel& om)
+{
+    mv::ModeSelectionNode source;
+    auto weigth_tensor = convIterator->getInputTensor(1);
+    auto input_tensor = convIterator->getInputTensor(0);
+    auto output_tensor = convIterator->getOutputTensor(0);
+
+    auto kernel_dimensions = weigth_tensor->getShape();
+    auto input_dimensions = input_tensor->getShape();
+    auto output_dimensions = output_tensor->getShape();
+
+    source.parameters.kernel_x = kernel_dimensions[0];
+    source.parameters.kernel_y = kernel_dimensions[1];
+    source.parameters.input_width = input_dimensions[0];
+    source.parameters.input_height = input_dimensions[1];
+    source.parameters.input_channels = input_dimensions[2];
+    source.parameters.output_width = output_dimensions[0];
+    source.parameters.output_height = output_dimensions[1];
+    source.parameters.output_channels = output_dimensions[2];
+
+    if(input_tensor->hasAttr("NCE1_Paddings")) //The input tensor involved in this convolution has already been padded (probably as output tensor of some other convolution)
+    {
+        mv::dynamic_vector<unsigned> paddings = input_tensor->getAttr("NCE1_Paddings").getContent<mv::dynamic_vector<unsigned>>();
+        source.parameters.input_width += paddings[0];
+        source.parameters.input_height += paddings[1];
+        source.parameters.input_channels += paddings[2];
+    }
+
+    if(output_tensor->hasAttr("NCE1_Paddings"))
+    //The output tensor involved in this convolution has already been padded (probably as input tensor of some other convolution)
+    //NOTE: Maybe this shouldn't happen at all, but it's better to play safe
+    {
+        mv::dynamic_vector<unsigned> paddings = output_tensor->getAttr("NCE1_Paddings").getContent<mv::dynamic_vector<unsigned>>();
+        source.parameters.output_width += paddings[0];
+        source.parameters.output_height += paddings[1];
+        source.parameters.output_channels += paddings[2];
+    }
+
+    auto strides = convIterator->getAttr("stride").getContent<mv::UnsignedVector2D>();
+    source.parameters.stride_x = strides.e0;
+    source.parameters.stride_y = strides.e1;
+
+    source.remaining_output_channels = source.parameters.output_channels;
+    mv::ModeSelectionResult modes = nce.optimize_convolution(source);
+    write_hardware_attributes(om, convIterator, modes, nce);
 }
 
 void modeSelection(mv::ComputationModel& model, mv::TargetDescriptor&, mv::json::Object& pobj, mv::json::Object&)
 {
     mv::OpModel om(model);
     mv::Nce1 nce;
+    std::queue<mv::Data::OpListIterator> to_be_optimized;
 
     for(auto opIterator = om.opBegin(); opIterator != om.opEnd(); ++opIterator)
     {
         switch(opIterator->getOpType())
         {
             case mv::OpType::Conv2D:
-                mv::ModeSelectionResult modes = optimize_convolution_nce1(nce, opIterator);
-                write_hardware_attributes(om, opIterator, modes, nce);
+                if(!opIterator->hasAttr("NCE1_Compatible"))
+                    continue;
+                if(!opIterator->getAttr("NCE1_Compatible").getContent<int>())
+                    continue;
+                to_be_optimized.push(opIterator);
                 break;
             /*
             case mv::OpType::FullyConnected:
@@ -186,5 +191,12 @@ void modeSelection(mv::ComputationModel& model, mv::TargetDescriptor&, mv::json:
                 break;
             */
         }
+    }
+
+    while(to_be_optimized.size() > 0)
+    {
+        auto opIterator = to_be_optimized.front();
+        to_be_optimized.pop();
+        optimize_convolution_nce1(nce, opIterator, om);
     }
 }
