1c68d1cc728b3c2c236ed01394b7434b3c1d8395
Stanislaw Maciag <stanislaw.maciag@intel.com>
2018-09-21 18:33:23 +0100

    Change cm_two_branch example

--- examples/cm_two_branch.cpp
+++ examples/cm_two_branch.cpp
@@ -1,50 +1,45 @@
-#include "include/mcm/computation/model/op_model.hpp"
-#include "include/mcm/computation/model/data_model.hpp"
-#include "include/mcm/computation/model/control_model.hpp"
+#include "include/mcm/compiler/compilation_unit.hpp"
 #include "include/mcm/utils/data_generator.hpp"
 
 int main()
 {
 
-    mv::OpModel om("model1");
-    auto input = om.input({128, 128, 3}, mv::DTypeType::Float16, mv::OrderType::ColumnMajor);
+    // Define the primary compilation unit
+    mv::CompilationUnit unit("cm_two_branch");
 
-    std::vector<double> conv1WeightsData = mv::utils::generateSequence<double>(3u * 3u * 3u * 8u);
-    std::vector<double> conv2WeightsData = mv::utils::generateSequence<double>(3u * 3u * 3u * 8u);
-    std::vector<double> conv3WeightsData = mv::utils::generateSequence<double>(5u * 5u * 16u * 32u);
-    std::vector<double> conv4WeightsData = mv::utils::generateSequence<double>(6u * 6u * 32u * 64u);
+    // Obtain compositional model from the compilation unit
+    mv::CompositionalModel& cm = unit.model();
 
-    auto conv1WeightsIt = om.constant(conv1WeightsData, {3, 3, 3, 8}, mv::DTypeType::Float16, mv::OrderType::ColumnMajor);
-    auto conv1It = om.conv2D(input, conv1WeightsIt, {2, 2}, {1, 1, 1, 1});
-    auto pool1It = om.maxpool2D(conv1It, {3, 3}, {2, 2}, {1, 1, 1, 1});
-    auto conv2WeightsIt = om.constant(conv2WeightsData, {3, 3, 3, 8}, mv::DTypeType::Float16, mv::OrderType::ColumnMajor);
-    auto conv2It = om.conv2D(input, conv2WeightsIt, {2, 2}, {1, 1, 1, 1});
-    auto pool2It = om.maxpool2D(conv2It, {3, 3}, {2, 2}, {1, 1, 1, 1});
-    auto concat1It = om.concat(pool1It, pool2It);
-    auto conv3WeightsIt = om.constant(conv3WeightsData, {5, 5, 16, 32}, mv::DTypeType::Float16, mv::OrderType::ColumnMajor);
-    auto conv3It = om.conv2D(concat1It, conv3WeightsIt, {2, 2}, {2, 2, 2, 2});
-    auto pool3It = om.maxpool2D(conv3It, {5, 5}, {3, 3}, {2, 2, 2, 2});
-    auto conv4WeightsIt = om.constant(conv4WeightsData, {6, 6, 32, 64}, mv::DTypeType::Float16, mv::OrderType::ColumnMajor);
-    auto conv4It = om.conv2D(pool3It, conv4WeightsIt, {1, 1}, {0, 0, 0, 0});
-    om.output(conv4It);
+    auto input = cm.input({24, 24, 20}, mv::DTypeType::Float16, mv::OrderType::ColumnMajor);
+    auto pool1It = cm.maxpool2D(input, {1, 1}, {1, 1}, {0, 0, 0, 0});
+    auto pool2It = cm.maxpool2D(pool1It, {1, 1}, {1, 1}, {0, 0, 0, 0});
+    auto pool3It = cm.maxpool2D(pool1It, {1, 1}, {1, 1}, {0, 0, 0, 0});
+    auto concat1It = cm.concat(pool3It, pool2It);
+    auto pool4It = cm.maxpool2D(concat1It, {1, 1}, {1, 1}, {0, 0, 0, 0});
+    cm.output(pool4It);
 
-    auto msgType = mv::Logger::MessageType::MessageInfo;
-    mv::DataModel dm(om);
-
-    mv::Logger::instance().log(msgType, "cm_two_branch", "Input op: " + om.getInput()->getName());
-    mv::Logger::instance().log(msgType, "cm_two_branch", "Input tensor (output tensor of the input op): " + dm.getInputFlow()->getTensor()->getName());
-    mv::Logger::instance().log(msgType, "cm_two_branch", "Output op: " + om.getOutput()->getName());
-    mv::Logger::instance().log(msgType, "cm_two_branch", "Output tensor (input tensor of the output op): " + dm.getOutputFlow()->getTensor()->getName());
-
-    mv::ControlModel cm(om);
-
-    std::size_t i = 0;
-    for (auto it = cm.getFirst(); it != cm.opEnd(); ++it)
-    {
-        mv::Logger::instance().log(msgType, "cm_two_branch", "Op " + std::to_string(i) + ": " + it->getName());
-        ++i;
-    }
+    // Load target descriptor for the selected target to the compilation unit
+    if (!unit.loadTargetDescriptor(mv::Target::ma2480))
+        exit(1);
     
+    // Define the manadatory arguments for passes using compilation descriptor obtained from compilation unit
+    unit.compilationDescriptor()["GenerateDot"]["output"] = std::string("cm_two_branch.dot");
+    unit.compilationDescriptor()["GenerateDot"]["scope"] = std::string("OpControlModel");
+    unit.compilationDescriptor()["GenerateDot"]["content"] = std::string("full");
+    unit.compilationDescriptor()["GenerateDot"]["html"] = true;
+    unit.compilationDescriptor()["GenerateBlob"]["output"] = std::string("cm_two_branch.blob");
+    
+    // Initialize compilation 
+    unit.initialize();
+    //unit.passManager().disablePass(mv::PassGenre::Serialization);
+    //unit.passManager().disablePass(mv::PassGenre::Adaptation);
+
+    // Run all passes
+    unit.run();
+
+    system("dot -Tsvg cm_two_branch.dot -o cm_two_branch.svg");
+    system("dot -Tsvg cm_two_branch_adapt.dot -o cm_two_branch_adapt.svg");
+    system("dot -Tsvg cm_two_branch_final.dot -o cm_two_branch_final.svg");
     return 0;
 
 }
