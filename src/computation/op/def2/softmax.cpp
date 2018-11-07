750d446adda77a43bf432a06f4445c3d1ecb708e
john.brady@intel.com <john.brady@intel.com>
2018-11-07 12:53:36 +0000

    added softmax

---
+++ src/computation/op/def2/softmax.cpp
@@ -0,0 +1,37 @@
+#include "include/mcm/computation/op/op_registry.hpp"
+
+namespace mv
+{
+
+    namespace op
+    {
+
+        static std::function<std::pair<bool, std::size_t>(const std::vector<Data::TensorIterator>&,
+            const std::map<std::string, Attribute>&, std::string&)> inputCheckFcn =
+            [](const std::vector<Data::TensorIterator>& inputs, const std::map<std::string, Attribute>&,
+            std::string& errMsg) -> std::pair<bool, std::size_t>
+        {
+
+            return {true, 0};
+
+        };
+                
+        static std::function<void(const std::vector<Data::TensorIterator>&, const std::map<std::string, Attribute>&, 
+            std::vector<Tensor>&)> outputDefFcn =
+            [](const std::vector<Data::TensorIterator>& inputs, const std::map<std::string, Attribute>&, std::vector<Tensor>& outputs)
+        {
+
+            outputs.push_back(mv::Tensor(":0", inputs[0]->getShape(), inputs[0]->getDType(), inputs[0]->getOrder()));
+
+        };
+    
+        MV_REGISTER_OP(Softmax)
+        .setInputs({"data"})
+        .setOutputs({"output"})
+        .setInputCheck(inputCheckFcn)
+        .setOutputDef(outputDefFcn)
+        .setTypeTrait({"executable", "exposed"});
+
+    }
+
+}
