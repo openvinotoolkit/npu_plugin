309d1fddc0721e559c73307966bde782895a7e05
Marco Mecchia <marco.mecchia@intel.com>
2018-11-20 15:39:06 +0000

    Using mcmCheck for python vs cpp comparison.

--- src/utils/hardware_tests.cpp
+++ src/utils/hardware_tests.cpp
@@ -14,21 +14,21 @@ void mv::HWTest(mv::CompilationUnit& unit, std::string outputName)
     std::cout << command0 << std::endl;
     system(command0.c_str());
 
-    //1) Compile generated prototext with fathom
+    //1) Compile generated prototxt with fathom
     std::cout << "COMPILING GENERATED PROTOTXT WITH FATHOM" << std::endl;
     std::string command1("python3 " + mv::utils::mdkRootPath() + "/projects/Fathom/src2/mvNCCompile.py " + outputName + ".prototxt -w " + outputName + ".caffemodel ");
     std::cout << command1 << std::endl;
     system(command1.c_str());
 
-    //3) Compare python and caffe
+    //2) Compare python and caffe
     std::cout << "COMPARING PYTHON AND CAFFE" << std::endl;
     std::string command3("python3 " + mv::utils::mdkRootPath() + "/projects/Fathom/src2/mvNCCheck.py " + outputName + ".prototxt -w " + outputName + ".caffemodel ");
     std::cout << command3 << std::endl;
     system(command3.c_str());
 
-    //2) Compare python and cpp
+    //3) Compare python and cpp
     std::cout << "COMPARING FATHOM AND MCMCOMPILER BLOBS" << std::endl;
-    std::string command2("python3 " + mv::utils::projectRootPath() + "/python/tools/compare_blobs.py " + outputName + ".blob graph");
+    std::string command2(mv::utils::projectRootPath() + "/python/tools/mcmCheck.sh -b " + outputName + ".blob -b graph -i " + mv::utils::projectRootPath() + "/mug.png");
     std::cout << command2 << std::endl;
     system(command2.c_str());
 
