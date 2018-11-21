e9fafad7fce7183d406703b65864975d1f3a836a
Marco Mecchia <marco.mecchia@intel.com>
2018-11-21 10:41:04 +0000

    Added other standalone op examples

---
+++ examples/standalone_sigmoid.cpp
@@ -0,0 +1,37 @@
+#include "include/mcm/compiler/compilation_unit.hpp"
+#include "include/mcm/utils/data_generator.hpp"
+#include "include/mcm/utils/serializer/Fp16Convert.h"
+#include "meta/include/mcm/op_model.hpp"
+#include "include/mcm/utils/hardware_tests.hpp"
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    mv::CompilationUnit unit("sigmoid");
+    mv::CompositionalModel& om = unit.model();
+
+    auto input = om.input({32, 32, 3}, mv::DTypeType::Float16, mv::Order("CHW"));
+    std::vector<double> data = mv::utils::generateSequence<double>(3);
+    auto sigmoid = om.sigmoid(input);
+    auto output = om.output(sigmoid);
+
+    std::string outputName = "test_sigmoid";
+    unit.compilationDescriptor()["GenerateBlob"]["fileName"] = std::string(outputName+".blob");
+    unit.compilationDescriptor()["GenerateBlob"]["enableFileOutput"] = true;
+    unit.compilationDescriptor()["GenerateBlob"]["enableRAMOutput"] = false;
+    unit.compilationDescriptor()["GenerateDot"]["output"] = std::string(outputName+".dot");
+    unit.compilationDescriptor()["GenerateDot"]["scope"] = std::string("OpControlModel");
+    unit.compilationDescriptor()["GenerateDot"]["content"] = std::string("full");
+    unit.compilationDescriptor()["GenerateDot"]["html"] = true;
+    unit.compilationDescriptor()["GenerateCaffe"]["outputPrototxt"] = std::string(outputName + ".prototxt");
+    unit.compilationDescriptor()["GenerateCaffe"]["outputCaffeModel"] = std::string(outputName + ".caffemodel");
+    unit.compilationDescriptor()["MarkHardwareOperations"]["disableHardware"] = true;
+
+    unit.loadTargetDescriptor(mv::Target::ma2480);
+    unit.initialize();
+
+    mv::HWTest(unit, outputName);
+
+}
