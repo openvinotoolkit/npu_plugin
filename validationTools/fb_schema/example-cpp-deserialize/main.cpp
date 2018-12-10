76944eddda53c1737ca5a47e3568d34a90df1b97
john.brady@intel.com <john.brady@intel.com>
2018-12-10 13:36:24 +0000

    updates to blob comapre tool

--- validationTools/fb_schema/example-cpp-deserialize/main.cpp
+++ validationTools/fb_schema/example-cpp-deserialize/main.cpp
@@ -11,153 +11,224 @@
 #include "flatbuffers/util.h"
 #include <gtest/gtest.h>
 
-TEST(graphFile, header_SummaryHeader_version_Version)	
+//need to break up
+// TEST(graphFile, header_SummaryHeader_version_Version)	
+// {
+//     Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
+//     Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
+//     const auto graph1 = GetGraphFile(blob_1.get_ptr());
+//     const auto graph2 = GetGraphFile(blob_2.get_ptr());
+    
+//     EXPECT_EQ(graph1->header()->version()->majorV(),graph2->header()->version()->majorV());
+//     EXPECT_EQ(graph1->header()->version()->minorV(),graph2->header()->version()->minorV());
+//     EXPECT_EQ(graph1->header()->version()->patchV(),graph2->header()->version()->patchV());
+//     EXPECT_EQ(graph1->header()->version()->hash()->c_str(),graph2->header()->version()->hash()->c_str());
+// }
+
+TEST(graphFile, header_SummaryHeader_task_count)	
 {
     Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
     Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
     const auto graph1 = GetGraphFile(blob_1.get_ptr());
     const auto graph2 = GetGraphFile(blob_2.get_ptr());
-    
-    EXPECT_EQ(graph1->header()->version()->majorV(),graph2->header()->version()->majorV());
-    EXPECT_EQ(graph1->header()->version()->minorV(),graph2->header()->version()->minorV());
-    EXPECT_EQ(graph1->header()->version()->patchV(),graph2->header()->version()->patchV());
-    EXPECT_EQ(graph1->header()->version()->hash()->c_str(),graph2->header()->version()->hash()->c_str());
+
+    //Is task_cout present in graph1
+    auto taskCountPresentGraph1 = flatbuffers::IsFieldPresent(graph1->header(), SummaryHeader::VT_TASK_COUNT);
+
+    if(taskCountPresentGraph1) {
+        ASSERT_TRUE(flatbuffers::IsFieldPresent(graph2->header(), SummaryHeader::VT_TASK_COUNT)); //Check if present in graph2
+        EXPECT_EQ(graph1->header()->task_count(),graph2->header()->task_count()); //Test if equal
+    }
+    else
+        SUCCEED();
 }
 
-
-TEST(DISABLE_graphFile, header_SummaryHeader_task_count)	
+TEST(graphFile, header_SummaryHeader_layer_count)	
 {
     Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
     Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
     const auto graph1 = GetGraphFile(blob_1.get_ptr());
     const auto graph2 = GetGraphFile(blob_2.get_ptr());
+
+    //Is layer_cout present in graph1
+    auto layerCountPresentGraph1 = flatbuffers::IsFieldPresent(graph1->header(), SummaryHeader::VT_LAYER_COUNT);
     
-    EXPECT_EQ(graph1->header()->task_count(),graph2->header()->task_count());
+    if(layerCountPresentGraph1) {
+        ASSERT_TRUE(flatbuffers::IsFieldPresent(graph2->header(), SummaryHeader::VT_LAYER_COUNT)); //Check if present in graph2
+        EXPECT_EQ(graph1->header()->layer_count(),graph2->header()->layer_count()); //Then test if equal
+    }
+    else
+        SUCCEED();
 }
 
-TEST(DISABLE_graphFile, header_SummaryHeader_layer_count)	
+//Need to update to check if TensorReference table (contains dimension field) is present
+TEST(graphFile, header_SummaryHeader_net_input_TensorReference_dimensions)	
 {
     Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
     Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
     const auto graph1 = GetGraphFile(blob_1.get_ptr());
     const auto graph2 = GetGraphFile(blob_2.get_ptr());
-    
-    EXPECT_EQ(graph1->header()->layer_count(),graph2->header()->layer_count());
+
+    //Is net_input present in graph1
+    auto netInputPresentGraph1 = flatbuffers::IsFieldPresent(graph1->header(), SummaryHeader::VT_NET_INPUT);
+
+     if(netInputPresentGraph1) {
+        ASSERT_TRUE(flatbuffers::IsFieldPresent(graph2->header(), SummaryHeader::VT_NET_INPUT)); //Check if present in graph2
+
+        for (flatbuffers::uoffset_t i = 0; i < graph1->header()->net_input()->size(); i++) { //No. of TensorReference tables
+
+            for (flatbuffers::uoffset_t j = 0; j < graph1->header()->net_input()->Get(i)->dimensions()->size(); j++) { //For each dimension check if equal
+            
+                EXPECT_EQ(graph1->header()->net_input()->Get(i)->dimensions()->Get(j),graph2->header()->net_input()->Get(i)->dimensions()->Get(j));
+            }
+        }  
+    } 
+    else
+        SUCCEED();
 }
 
-//Need to fix
-TEST(DISABLE_graphFile, header_SummaryHeader_net_input_TensorReference_dimensions)	
+//Need to update to check if TensorReference table (contains dimension field) is present
+TEST(graphFile, header_SummaryHeader_net_output_TensorReference_dimensions)	
 {
     Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
     Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
     const auto graph1 = GetGraphFile(blob_1.get_ptr());
     const auto graph2 = GetGraphFile(blob_2.get_ptr());
-    
 
-    auto net_input_graph1 = graph1->header()->net_input();
-    auto net_input_graph2 = graph2->header()->net_input();
+    //Is net_output present in graph1
+    auto netOutputPresentGraph1 = flatbuffers::IsFieldPresent(graph1->header(), SummaryHeader::VT_NET_OUTPUT);
 
-    
-    EXPECT_EQ(net_input_graph1->Get(0)->dimensions()->Get(0),net_input_graph2->Get(0)->dimensions()->Get(0));
-    EXPECT_EQ(net_input_graph1->Get(0)->dimensions()->Get(0),net_input_graph2->Get(0)->dimensions()->Get(0));
-    EXPECT_EQ(net_input_graph1->Get(0)->dimensions()->Get(0),net_input_graph2->Get(0)->dimensions()->Get(0));
-    EXPECT_EQ(net_input_graph1->Get(0)->dimensions()->Get(0),net_input_graph2->Get(0)->dimensions()->Get(0));
+     if(netOutputPresentGraph1) {
+        ASSERT_TRUE(flatbuffers::IsFieldPresent(graph2->header(), SummaryHeader::VT_NET_OUTPUT)); //Check if present in graph2
 
+        for (flatbuffers::uoffset_t i = 0; i < graph1->header()->net_output()->size(); i++) { //No. of TensorReference tables
+
+            for (flatbuffers::uoffset_t j = 0; j < graph1->header()->net_output()->Get(i)->dimensions()->size(); j++) { //For each dimension check if equal
+            
+                EXPECT_EQ(graph1->header()->net_output()->Get(i)->dimensions()->Get(j),graph2->header()->net_output()->Get(i)->dimensions()->Get(j));
+            }
+        }  
+    } 
+    else
+        SUCCEED();
 }
 
-// TEST(DISABLE_graphFile, summaryHeader_net_output)	
-// {
-//     Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
-//     Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
-//     const auto graph1 = GetGraphFile(blob_1.get_ptr());
-//     const auto graph2 = GetGraphFile(blob_2.get_ptr());
+TEST(graphFile, header_SummaryHeader_resources_Resources_shave_mask)	
+{
+    Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
+    Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
+    const auto graph1 = GetGraphFile(blob_1.get_ptr());
+    const auto graph2 = GetGraphFile(blob_2.get_ptr());
+
+    //Is shave_mask present in graph1
+    auto shaveMaskPresentGraph1 = flatbuffers::IsFieldPresent(graph1->header()->resources(), Resources::VT_SHAVE_MASK);
     
+    if(shaveMaskPresentGraph1) {
+        ASSERT_TRUE(flatbuffers::IsFieldPresent(graph2->header()->resources(), Resources::VT_SHAVE_MASK)); //Check if present in graph2
+        EXPECT_EQ(graph1->header()->resources()->shave_mask(),graph2->header()->resources()->shave_mask()); //Then test if equal
+    }
+    else
+        SUCCEED();
+}
 
-//     auto net_output_graph1 = graph1->header()->net_output();
-//     auto net_output_graph2 = graph2->header()->net_output();
+TEST(graphFile, header_SummaryHeader_resources_Resources_nce1_mask)	
+{
+    Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
+    Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
+    const auto graph1 = GetGraphFile(blob_1.get_ptr());
+    const auto graph2 = GetGraphFile(blob_2.get_ptr());
 
+    //Is nce1_mask present in graph1
+    auto nce1MaskPresentGraph1 = flatbuffers::IsFieldPresent(graph1->header()->resources(), Resources::VT_NCE1_MASK);
     
-//     EXPECT_EQ(net_output_graph1->Get(0)->dimensions()->Get(0),net_output_graph2->Get(0)->dimensions()->Get(0));
-//     EXPECT_EQ(net_output_graph1->Get(0)->dimensions()->Get(0),net_output_graph2->Get(0)->dimensions()->Get(0));
-//     EXPECT_EQ(net_output_graph1->Get(0)->dimensions()->Get(0),net_output_graph2->Get(0)->dimensions()->Get(0));
-//     EXPECT_EQ(net_output_graph1->Get(0)->dimensions()->Get(0),net_output_graph2->Get(0)->dimensions()->Get(0));
+    if(nce1MaskPresentGraph1) {
+        ASSERT_TRUE(flatbuffers::IsFieldPresent(graph2->header()->resources(),Resources::VT_NCE1_MASK)); //Check if present in graph2
+        EXPECT_EQ(graph1->header()->resources()->nce1_mask(),graph2->header()->resources()->nce1_mask()); //Then test if equal
+    }
+    else
+        SUCCEED();
+}
 
-// }
+TEST(graphFile, header_SummaryHeader_resources_Resources_dpu_mask)	
+{
+    Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
+    Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
+    const auto graph1 = GetGraphFile(blob_1.get_ptr());
+    const auto graph2 = GetGraphFile(blob_2.get_ptr());
 
-// TEST(DISABLE_graphFile, summaryHeader_resources)	
-// {
-//     Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
-//     Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
-//     const auto graph1 = GetGraphFile(blob_1.get_ptr());
-//     const auto graph2 = GetGraphFile(blob_2.get_ptr());
+    //Is dpu_mask present in graph1
+    auto dpuMaskPresentGraph1 = flatbuffers::IsFieldPresent(graph1->header()->resources(), Resources::VT_DPU_MASK);
     
+    if(dpuMaskPresentGraph1) {
+        ASSERT_TRUE(flatbuffers::IsFieldPresent(graph2->header()->resources(),Resources::VT_DPU_MASK)); //Check if present in graph2
+        EXPECT_EQ(graph1->header()->resources()->dpu_mask(),graph2->header()->resources()->dpu_mask()); //Then test if equal
+    }
+    else
+        SUCCEED();
+}
 
-//     auto resources_graph1 = graph1->header()->resources();
-//     auto resources_graph2 = graph2->header()->resources();
+TEST(graphFile, header_SummaryHeader_resources_Resources_leon_cmx)	
+{
+    Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
+    Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
+    const auto graph1 = GetGraphFile(blob_1.get_ptr());
+    const auto graph2 = GetGraphFile(blob_2.get_ptr());
 
+    //Is leon_cmx present in graph1
+    auto leonCmxPresentGraph1 = flatbuffers::IsFieldPresent(graph1->header()->resources(), Resources::VT_LEON_CMX);
     
+    if(leonCmxPresentGraph1) {
+        ASSERT_TRUE(flatbuffers::IsFieldPresent(graph2->header()->resources(),Resources::VT_LEON_CMX)); //Check if present in graph2
+        EXPECT_EQ(graph1->header()->resources()->leon_cmx(),graph2->header()->resources()->leon_cmx()); //Then test if equal
+    }
+    else
+        SUCCEED();
+}
+
+TEST(graphFile, header_SummaryHeader_resources_Resources_nn_cmx)	
+{
+    Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
+    Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
+    const auto graph1 = GetGraphFile(blob_1.get_ptr());
+    const auto graph2 = GetGraphFile(blob_2.get_ptr());
+
+    //Is nn_cmx present in graph1
+    auto nnCmxPresentGraph1 = flatbuffers::IsFieldPresent(graph1->header()->resources(), Resources::VT_NN_CMX);
     
-//     EXPECT_EQ(resources_graph1->nce1_mask(),resources_graph1->nce1_mask());
-//     EXPECT_EQ(resources_graph1->dpu_mask(),resources_graph1->dpu_mask());
-//     EXPECT_EQ(resources_graph1->leon_cmx(),resources_graph1->leon_cmx());
-//     EXPECT_EQ(resources_graph1->nn_cmx(),resources_graph1->nn_cmx());
-//     EXPECT_EQ(resources_graph1->ddr_scratch(),resources_graph1->ddr_scratch());
-// }
+    if(nnCmxPresentGraph1) {
+        ASSERT_TRUE(flatbuffers::IsFieldPresent(graph2->header()->resources(),Resources::VT_NN_CMX)); //Check if present in graph2
+        EXPECT_EQ(graph1->header()->resources()->nn_cmx(),graph2->header()->resources()->nn_cmx()); //Then test if equal
+    }
+    else
+        SUCCEED();
+}
 
-// TEST(DISABLE_graphFile, summaryHeader_original_strucure)	
-// {
-//     Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
-//     Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
-//     const auto graph1 = GetGraphFile(blob_1.get_ptr());
-//     const auto graph2 = GetGraphFile(blob_2.get_ptr());
+TEST(graphFile, header_SummaryHeader_resources_Resources_ddr_scratch)	
+{
+    Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
+    Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
+    const auto graph1 = GetGraphFile(blob_1.get_ptr());
+    const auto graph2 = GetGraphFile(blob_2.get_ptr());
 
-//     auto original_structure_graph1 = graph1->header()->original_structure();
-//     auto original_structure_graph2 = graph2->header()->original_structure();
-
-// }
-
-// TEST(DISABLE_graphFile, task_lists_nodeID)	// TEST(DISABLE_graphFile, task_lists_sourceTaskIDs)	
-// {
-//     Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
-//     Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
-//     const auto graph1 = GetGraphFile(blob_1.get_ptr());
-//     const auto graph2 = GetGraphFile(blob_2.get_ptr());
-
-//     auto task_lists_graph1 = graph1->task_lists();
-//     auto task_lists_graph2 = graph2->task_lists();
+    //Is ddr_scratch present in graph1
+    auto ddrScratchPresentGraph1 = flatbuffers::IsFieldPresent(graph1->header()->resources(), Resources::VT_NN_CMX);
+    
+    if(ddrScratchPresentGraph1) {
+        ASSERT_TRUE(flatbuffers::IsFieldPresent(graph2->header()->resources(),Resources::VT_DDR_SCRATCH)); //Check if present in graph2
+        EXPECT_EQ(graph1->header()->resources()->ddr_scratch(),graph2->header()->resources()->ddr_scratch()); //Then test if equal
+    }
+    else
+        SUCCEED();
+}
 
 
-//     std::cout << "task_lists_size  " << task_lists_graph1->size() << std::endl; 
+//Not written in POC blob
+TEST(DISABLE_graphFile, task_lists_TaskList_content_Task_nodeID)		
+{
+   SUCCEED();
+}
 
-
-//      for (flatbuffers::uoffset_t i = 0; i < task_lists_graph1->size(); i++) {
-//       auto sourceTaskIDs_graph1 = task_lists_graph1->Get(i)->content()->Get(i)->sourceTaskIDs()->Get(i);
-//       auto sourceTaskIDs_graph2 = task_lists_graph2->Get(i)->content()->Get(i)->sourceTaskIDs()->Get(i);
-//       EXPECT_EQ(sourceTaskIDs_graph1,sourceTaskIDs_graph2);
-//     }
-
-// }
-// {
-//     Blob blob_1("/home/john/blobv3/vpu_3_1.bin");
-//     Blob blob_2("/home/john/blobv3/vpu_3_1.bin");
-//     const auto graph1 = GetGraphFile(blob_1.get_ptr());
-//     const auto graph2 = GetGraphFile(blob_2.get_ptr());
-
-//     auto task_lists_graph1 = graph1->task_lists();
-//     auto task_lists_graph2 = graph2->task_lists();
-
-
-//     std::cout << "task_lists_size  " << task_lists_graph1->size() << std::endl; 
-
-//     for (flatbuffers::uoffset_t i = 0; i < task_lists_graph1->size() - 1; i++) {
-//       auto nodeID_graph1 = task_lists_graph1->Get(i)->content()->Get(i)->nodeID();
-//       auto nodeID_graph2 = task_lists_graph2->Get(i)->content()->Get(i)->nodeID();
-//       EXPECT_EQ(nodeID_graph1,nodeID_graph1);
-//     }
-
-
-// }
-
+//Need to check if Task table is present (contains sourceTaskIDs field)
 TEST(graphFile, task_lists_TaskList_content_Task_sourceTaskIDs)	
 {
     Blob blob_1("/home/john/blobv3/try4.blob");
@@ -181,13 +252,13 @@ TEST(graphFile, task_lists_TaskList_content_Task_sourceTaskIDs)
                 auto sourceTaskIDs_graph1 = TaskLists_graph1->Get(j)->content()->Get(i)->sourceTaskIDs()->Get(k);
                 auto sourceTaskIDs_graph2 = TaskLists_graph2->Get(j)->content()->Get(i)->sourceTaskIDs()->Get(k);
       
-            EXPECT_EQ(sourceTaskIDs_graph1, sourceTaskIDs_graph1);
+                EXPECT_EQ(sourceTaskIDs_graph1, sourceTaskIDs_graph2);
             }
         }
     }
 }
 
-
+//Add ASSERT
 TEST(graphFile, task_lists_TaskList_content_Task_associated_barriers_BarrierReference_wait_barriers)	
 {
     Blob blob_1("/home/john/blobv3/try4.blob");
@@ -202,16 +273,57 @@ TEST(graphFile, task_lists_TaskList_content_Task_associated_barriers_BarrierRefe
         
         auto content_size = TaskLists_graph1->Get(j)->content()->size(); //No. of Task tables
        
-        for (flatbuffers::uoffset_t i = 0; i < content_size; i++) {
+        for (flatbuffers::uoffset_t i = 0; i < content_size; i++) { //For each Task Table
+
             auto wait_barrier_graph1 = TaskLists_graph1->Get(j)->content()->Get(i)->associated_barriers()->wait_barrier(); //get wait_barrier
             auto wait_barrier_graph2 = TaskLists_graph2->Get(j)->content()->Get(i)->associated_barriers()->wait_barrier(); //get wait_barrier
       
-            std::cout << "wait_barrier_graph1 " << wait_barrier_graph1 << std::endl;
-            std::cout << "wait_barrier_graph2 " << wait_barrier_graph2 << std::endl;
-
             EXPECT_EQ(wait_barrier_graph1,wait_barrier_graph2);
         }
     }
+}
+
+//Add ASSERT
+TEST(graphFile, task_lists_TaskList_content_Task_associated_barriers_BarrierReference_update_barriers)	
+{
+    Blob blob_1("/home/john/blobv3/try4.blob");
+    Blob blob_2("/home/john/blobv3/try5.blob");
+    const auto graph1 = GetGraphFile(blob_1.get_ptr());
+    const auto graph2 = GetGraphFile(blob_2.get_ptr());
+
+    auto task_lists_graph1 = graph1->task_lists(); 
+    auto task_lists_graph2 = graph2->task_lists();
+
+    //Is task_lists present in graph1
+    auto taskListPresentGraph1 = flatbuffers::IsFieldPresent(graph1, GraphFile::VT_TASK_LISTS);
+
+    //if present in graph1
+    if(taskListPresentGraph1) {
+        for (flatbuffers::uoffset_t j = 0; j < task_lists_graph1->size(); j++) { //No. of TaskList tables
+        
+            auto task_tables_size = task_lists_graph1->Get(j)->content()->size(); //No. of Task tables
+       
+            for (flatbuffers::uoffset_t i = 0; i < task_tables_size; i++) { //For each Task Table
+
+                auto update_barriers_present = flatbuffers::IsFieldPresent(task_lists_graph1->Get(j)->content()->Get(i)->associated_barriers(),BarrierReference::VT_UPDATE_BARRIERS);
+
+                if(update_barriers_present) { //If present
+                    auto update_barriers_size = task_lists_graph1->Get(j)->content()->Get(i)->associated_barriers()->update_barriers()->size(); //Get Size of update_barriers vector
+                    for (flatbuffers::uoffset_t k = 0; k < update_barriers_size ; k++) { //For each update_barriers element check if equal
+            
+                        auto update_barriers_graph1 = task_lists_graph1->Get(j)->content()->Get(i)->associated_barriers()->update_barriers()->Get(k); //get update_barriers
+                        auto update_barriers_graph2 = task_lists_graph2->Get(j)->content()->Get(i)->associated_barriers()->update_barriers()->Get(k); //get update_barriers
+
+                        EXPECT_EQ(update_barriers_graph1,update_barriers_graph2);
+                    }
+                }
+                else
+                    SUCCEED();
+            }         
+        }
+    }
+    else
+        SUCCEED();
 }
 
 int main(int argc, char **argv) {
