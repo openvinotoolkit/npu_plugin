89916be49a039528cf855a249a20d3147fad384b
Marco Mecchia <marco.mecchia@intel.com>
2019-01-02 13:33:31 +0000

    Set up hw pooling example

---
+++ examples/hw_pooling.cpp
@@ -0,0 +1,36 @@
+#include "include/mcm/compiler/compilation_unit.hpp"
+#include "include/mcm/utils/data_generator.hpp"
+#include "include/mcm/utils/serializer/Fp16Convert.h"
+#include "meta/include/mcm/op_model.hpp"
+#include "include/mcm/utils/hardware_tests.hpp"
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    mv::CompilationUnit unit("testModel");
+    mv::CompositionalModel& test_cm = unit.model();
+
+    auto input1 = test_cm.input({225, 225, 3}, mv::DTypeType::Float16, mv::Order("CHW"));
+    auto pool1 = test_cm.maxPool(input1, {3, 3}, {2, 2}, {1, 1, 1, 1});
+    auto output = test_cm.output(pool1);
+
+    std::string outputName = "wddm_pool1";
+    unit.compilationDescriptor()["GenerateBlob"]["fileName"] = outputName + ".blob";
+    unit.compilationDescriptor()["GenerateBlob"]["enableFileOutput"] = true;
+    unit.compilationDescriptor()["GenerateBlob"]["enableRAMOutput"] = false;
+    unit.compilationDescriptor()["GenerateDot"]["output"] = std::string(outputName + ".dot");
+    unit.compilationDescriptor()["GenerateDot"]["scope"] = std::string("OpControlModel");
+    unit.compilationDescriptor()["GenerateDot"]["content"] = std::string("full");
+    unit.compilationDescriptor()["GenerateDot"]["html"] = true;
+    unit.compilationDescriptor()["GenerateCaffe"]["outputPrototxt"] = std::string(outputName + ".prototxt");
+    unit.compilationDescriptor()["GenerateCaffe"]["outputCaffeModel"] = std::string(outputName + ".caffemodel");
+    unit.compilationDescriptor()["MarkHardwareOperations"]["disableHardware"] = false;
+
+    unit.loadTargetDescriptor(mv::Target::ma2480);
+    unit.initialize();
+
+    auto returnValue = mv::HWTest(unit, outputName, true);
+    printReport(returnValue, std::cout);
+}
