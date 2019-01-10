361f5326fab15369d32fe83e81bbc9fad2ab7467
Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>
2019-01-10 11:58:00 -0800

    VPUNND-10: Flesh out details of the CompilationDescriptor
    Signed-off-by: Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>

--- include/mcm/compiler/compilation_descriptor.hpp
+++ include/mcm/compiler/compilation_descriptor.hpp
@@ -15,8 +15,7 @@ namespace mv
         {
 
             Singular,
-            GroupWise,
-            PassWise
+            Recurrent
 
         };
 
@@ -24,9 +23,22 @@ namespace mv
 
         const static unsigned jsonParserBufferLength_ = 128;
 
+        /**
+         * The map containing arguments for different passes.
+         */
         std::map<std::string, Element> passesArgs_;
-        std::map<std::string, std::vector<std::string>> passesGroups_;
-        std::vector<std::pair<std::string, Recurrence>> groupsOrder_;        
+
+        /**
+         * The map representing groups of passes. The first argument is the group id and the
+         * second argument stores the list of groups/passes in that group. The recurrence of
+         * a pass must be specified. XXX: do we need this kind of an enforcement?
+         */
+        std::map<std::string, std::pair<Recurrence, std::vector<std::string>> > groups_;
+
+        /**
+         * The root group containing the precise order of execution of groups.
+         */
+        std::vector<std::string> rootGroup_;
 
         static std::string toString(Recurrence rec);
         static Recurrence fromString(const std::string& str);
@@ -37,6 +49,30 @@ namespace mv
         CompilationDescriptor(const std::string& path);
 
         void load(const std::string& path);
+
+        /**
+         * Adds group to groups list. This just creates an entry for a group, without specifying
+         * the passes in the group.
+         */
+        bool addGroup(const std::string& group);
+
+        /**
+         * Adds pass to groups list. If the group doesn't exist, create one. This method also
+         * sets group recurrence -- "Singular" for a pass that executes only once; "Recurrent" for a
+         * pass that recurs.
+         */
+        bool addPassToGroup(const std::string& pass, const std::string& group, const std::string& recurrence);
+
+        /**
+         * Explicitly set the root group. This is needed to specify the order in which the groups
+         * will execute.
+         */
+        bool defineRootGroup(const std::vector<std::string>& rootGroup);
+
+        /**
+         * Add argument to a pass.
+         */
+        bool addArgToPass(const std::string& pass, const std::string& arg, const std::string& value);
 
     };
 
