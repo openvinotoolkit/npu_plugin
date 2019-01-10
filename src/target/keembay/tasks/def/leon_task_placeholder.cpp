cc07fad4f8c139a2d327368b1d667809cd808aa9
Marco Mecchia <marco.mecchia@intel.com>
2019-01-10 13:58:33 +0000

    Moved leon_task_placeholder to the appropriate place

--- src/target/keembay/tasks/def/leon_task_placeholder.cpp
+++ src/target/keembay/tasks/def/leon_task_placeholder.cpp
@@ -33,7 +33,7 @@ namespace mv
         .setArg<mv::Order>("order")
         .setInputCheck(inputCheckFcn)
         .setOutputDef(outputDefFcn)
-        .setTypeTrait({"exposed"});
+        .setTypeTrait({"automatic_api"});
 
     }
 
