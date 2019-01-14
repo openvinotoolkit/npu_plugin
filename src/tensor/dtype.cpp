3cd72b2d5b4dedb509dbaf424e7a3437d6186bf1
Eman Copty <eman.copty@intel.com>
2019-01-14 15:46:49 -0800

    Add conversion for the new dtypetypes and test
    - still missing Fp8, i2x, i4x, bin, log

--- src/tensor/dtype.cpp
+++ src/tensor/dtype.cpp
@@ -27,8 +27,21 @@ const std::unordered_map<mv::DTypeType, std::string, mv::DTypeTypeHash> mv::DTyp
 const std::unordered_map<mv::DTypeType,std::function<mv::BinaryData(const std::vector<double>&)>,
     mv::DTypeTypeHash> mv::DType::dTypeConvertors_=
 {
-    {DTypeType::Float16, [](const std::vector<double> & vals)->mv::BinaryData {
-
+    {DTypeType::Float64, [](const std::vector<double> & vals)->mv::BinaryData
+    {
+        mv::BinaryData bdata(mv::DTypeType::Float64);
+        bdata.setFp64(vals);
+        return bdata;
+    }},
+    {DTypeType::Float32, [](const std::vector<double> & vals)->mv::BinaryData
+    {
+        std::vector<float> res(vals.begin(), vals.end());
+        mv::BinaryData bdata(mv::DTypeType::Float32);
+        bdata.setFp32(res);
+        return bdata;
+    }},
+    {DTypeType::Float16, [](const std::vector<double> & vals)->mv::BinaryData
+    {
         std::vector<int16_t> res;
         mv_num_convert cvtr;
         for_each(vals.begin(), vals.end(), [&](double  val)
@@ -38,7 +51,139 @@ const std::unordered_map<mv::DTypeType,std::function<mv::BinaryData(const std::v
         mv::BinaryData bdata(mv::DTypeType::Float16);
         bdata.setFp16(res);
         return bdata;
+    }},
+    //TODO add F8 conversion
+    {DTypeType::UInt64, [](const std::vector<double> & vals)->mv::BinaryData
+    {
+        std::vector<uint64_t> res(vals.begin(), vals.end());
+        mv::BinaryData bdata(mv::DTypeType::UInt64);
+        bdata.setU64(res);
+        return bdata;
+    }},
+    {DTypeType::UInt32, [](const std::vector<double> & vals)->mv::BinaryData
+    {
+        std::vector<uint32_t> res(vals.begin(), vals.end());
+        mv::BinaryData bdata(mv::DTypeType::UInt32);
+        bdata.setU32(res);
+        return bdata;
+    }},
+    {DTypeType::UInt16, [](const std::vector<double> & vals)->mv::BinaryData
+    {
+        std::vector<uint16_t> res(vals.begin(), vals.end());
+        mv::BinaryData bdata(mv::DTypeType::UInt16);
+        bdata.setU16(res);
+        return bdata;
+    }},
+    {DTypeType::UInt8, [](const std::vector<double> & vals)->mv::BinaryData
+    {
+        std::vector<uint8_t> res(vals.begin(), vals.end());
+        mv::BinaryData bdata(mv::DTypeType::UInt8);
+        bdata.setU8(res);
+        return bdata;
+    }},
+    {DTypeType::Int64, [](const std::vector<double> & vals)->mv::BinaryData
+    {
+        std::vector<uint64_t> res(vals.begin(), vals.end());
+        mv::BinaryData bdata(mv::DTypeType::Int64);
+        bdata.setI64(res);
+        return bdata;
+    }},
+    {DTypeType::Int32, [](const std::vector<double> & vals)->mv::BinaryData
+    {
+        std::vector<int32_t> res(vals.begin(), vals.end());
+        mv::BinaryData bdata(mv::DTypeType::Int32);
+        bdata.setI32(res);
+        return bdata;
+    }},
+    {DTypeType::Int16, [](const std::vector<double> & vals)->mv::BinaryData
+    {
+        std::vector<int16_t> res(vals.begin(), vals.end());
+        mv::BinaryData bdata(mv::DTypeType::Int16);
+        bdata.setI16(res);
+        return bdata;
+    }},
+    {DTypeType::Int8, [](const std::vector<double> & vals)->mv::BinaryData
+    {
+        std::vector<int8_t> res(vals.begin(), vals.end());
+        mv::BinaryData bdata(mv::DTypeType::Int8);
+        bdata.setI8(res);
+        return bdata;
+    }},
+    {DTypeType::Int4, [](const std::vector<double> & vals)->mv::BinaryData
+    {
+        std::vector<int8_t> res;
+
+        union
+        {
+            struct bits {
+                uint8_t lb : 4;
+                uint8_t hb : 4;
+            } b;
+            uint8_t data;
+        } temp;
+
+        for(size_t i=0; i< vals.size(); i++)
+        {
+            if (i%2 == 0)
+            {
+                temp.b.hb = 0;
+                temp.b.lb = vals[i];
+            }
+            if (i%2 == 1)
+            {
+                temp.b.hb = vals[i];
+                res.push_back(temp.data);
+            }
+        }
+
+        if (vals.size() % 2 != 0)
+            res.push_back(temp.data);
+
+        mv::BinaryData bdata(mv::DTypeType::Int4);
+        bdata.setI4(res);
+        return bdata;
+    }},
+    {DTypeType::Int2, [](const std::vector<double> & vals)->mv::BinaryData
+    {
+        std::vector<int8_t> res;
+
+        union
+        {
+            struct bits {
+                uint8_t llb : 2;
+                uint8_t lhb : 2;
+                uint8_t hlb : 2;
+                uint8_t hhb : 2;
+            } b;
+            uint8_t data;
+        } temp;
+
+        for(size_t i=0; i< vals.size(); i++)
+        {
+            if (i%4 == 0)
+            {
+                temp.data = 0;
+                temp.b.llb = vals[i];
+            }
+            if (i%4 == 1)
+                temp.b.lhb = vals[i];
+            if (i%4 == 2)
+                temp.b.hlb = vals[i];
+            if (i%4 == 3)
+            {
+                temp.b.hhb = vals[i];
+                res.push_back(temp.data);
+            }
+        }
+
+        if (vals.size() % 4 != 0)
+            res.push_back(temp.data);
+
+        mv::BinaryData bdata(mv::DTypeType::Int2);
+        bdata.setI2(res);
+        return bdata;
     }}
+    //TODO add Int2x,4x, Bin,Log
 };
 
 mv::DType::DType(DTypeType value) :
--- tests/tensor/tensor.cpp
+++ tests/tensor/tensor.cpp
@@ -623,7 +623,7 @@ TEST(tensor, get_data)
 
 }
 
-TEST(tensor, to_binary)
+TEST(tensor, to_binary_fp16)
 {
 
     mv::Shape tShape({3, 3, 3, 3});
@@ -647,5 +647,330 @@ TEST(tensor, to_binary)
     const std::vector<int16_t>& fp16_data = bdata.fp16();
     for (unsigned i = 0; i < fp16_data.size(); i++)
         EXPECT_EQ(fp16_data[i], cvtr.fp32_to_fp16(data[i]));
+}
 
+TEST(tensor, to_binary_fp64)
+{
+
+    mv::Shape tShape({3, 3, 3, 3});
+
+    std::vector<double> data = {
+        0.0f, 27.0f, 54.0f, 9.0f, 36.0f, 63.0f, 18.0f, 45.0f, 72.0f,
+        1.0f, 28.0f, 55.0f, 10.0f, 37.0f, 64.0f, 19.0f, 46.0f, 73.0f,
+        2.0f, 29.0f, 56.0f, 11.0f, 38.0f, 65.0f, 20.0f, 47.0f, 74.0f,
+        3.0f, 30.0f, 57.0f, 12.0f, 39.0f, 66.0f, 21.0f, 48.0f, 75.0f,
+        4.0f, 31.0f, 58.0f, 13.0f, 40.0f, 67.0f, 22.0f, 49.0f, 76.0f,
+        5.0f, 32.0f, 59.0f, 14.0f, 41.0f, 68.0f, 23.0f, 50.0f, 77.0f,
+        6.0f, 33.0f, 60.0f, 15.0f, 42.0f, 69.0f, 24.0f, 51.0f, 78.0f,
+        7.0f, 34.0f, 61.0f, 16.0f, 43.0f, 70.0f, 25.0f, 52.0f, 79.0f,
+        8.0f, 35.0f, 62.0f, 17.0f, 44.0f, 71.0f, 26.0f, 53.0f, 80.0f
+    };
+
+    mv::Tensor t("t", tShape, mv::DTypeType::Float64, mv::Order("HWCN"));
+    t.populate(data);
+    mv::BinaryData bdata = t.toBinary();
+    const std::vector<double>& fp64_data = bdata.fp64();
+    for (unsigned i = 0; i < fp64_data.size(); i++)
+        EXPECT_EQ(fp64_data[i], data[i]);
+}
+
+TEST(tensor, to_binary_fp32)
+{
+
+    mv::Shape tShape({3, 3, 3, 3});
+
+    std::vector<double> data = {
+        0.0f, 27.0f, 54.0f, 9.0f, 36.0f, 63.0f, 18.0f, 45.0f, 72.0f,
+        1.0f, 28.0f, 55.0f, 10.0f, 37.0f, 64.0f, 19.0f, 46.0f, 73.0f,
+        2.0f, 29.0f, 56.0f, 11.0f, 38.0f, 65.0f, 20.0f, 47.0f, 74.0f,
+        3.0f, 30.0f, 57.0f, 12.0f, 39.0f, 66.0f, 21.0f, 48.0f, 75.0f,
+        4.0f, 31.0f, 58.0f, 13.0f, 40.0f, 67.0f, 22.0f, 49.0f, 76.0f,
+        5.0f, 32.0f, 59.0f, 14.0f, 41.0f, 68.0f, 23.0f, 50.0f, 77.0f,
+        6.0f, 33.0f, 60.0f, 15.0f, 42.0f, 69.0f, 24.0f, 51.0f, 78.0f,
+        7.0f, 34.0f, 61.0f, 16.0f, 43.0f, 70.0f, 25.0f, 52.0f, 79.0f,
+        8.0f, 35.0f, 62.0f, 17.0f, 44.0f, 71.0f, 26.0f, 53.0f, 80.0f
+    };
+
+    mv::Tensor t("t", tShape, mv::DTypeType::Float32, mv::Order("HWCN"));
+    t.populate(data);
+    mv::BinaryData bdata = t.toBinary();
+    const std::vector<float>& fp32_data = bdata.fp32();
+    for (unsigned i = 0; i < fp32_data.size(); i++)
+        EXPECT_EQ(fp32_data[i], data[i]);
+}
+
+TEST(tensor, to_binary_u64)
+{
+
+    mv::Shape tShape({3, 3, 3, 3});
+
+    std::vector<double> data = {
+        0.0f, 27.3f, 54.5f, 9.0f, 36.0f, 63.0f, 18.7f, 45.0f, 72.0f,
+        1.0f, 28.3f, 55.5f, 10.0f, 37.0f, 64.0f, 19.7f, 46.0f, 73.0f,
+        2.0f, 29.3f, 56.5f, 11.0f, 38.0f, 65.0f, 20.7f, 47.0f, 74.0f,
+        3.0f, 30.3f, 57.5f, 12.0f, 39.0f, 66.0f, 21.7f, 48.0f, 75.0f,
+        4.0f, 31.3f, 58.5f, 13.0f, 40.0f, 67.0f, 22.7f, 49.0f, 76.0f,
+        5.0f, 32.3f, 59.5f, 14.0f, 41.0f, 68.0f, 23.7f, 50.0f, 77.0f,
+        6.0f, 33.3f, 60.5f, 15.0f, 42.0f, 69.0f, 24.7f, 51.0f, 78.0f,
+        7.0f, 34.3f, 61.5f, 16.0f, 43.0f, 70.0f, 25.7f, 52.0f, 79.0f,
+        8.0f, 35.3f, 62.5f, 17.0f, 44.0f, 71.0f, 26.7f, 53.0f, 80.0f
+    };
+
+    mv::Tensor t("t", tShape, mv::DTypeType::UInt64, mv::Order("HWCN"));
+    t.populate(data);
+    mv::BinaryData bdata = t.toBinary();
+    const std::vector<uint64_t>& u64_data = bdata.u64();
+    for (unsigned i = 0; i < u64_data.size(); i++)
+        EXPECT_EQ(u64_data[i], (uint64_t) data[i]);
+}
+
+TEST(tensor, to_binary_u32)
+{
+
+    mv::Shape tShape({3, 3, 3, 3});
+
+    std::vector<double> data = {
+        0.0f, 27.3f, 54.5f, 9.0f, 36.0f, 63.0f, 18.7f, 45.0f, 72.0f,
+        1.0f, 28.3f, 55.5f, 10.0f, 37.0f, 64.0f, 19.7f, 46.0f, 73.0f,
+        2.0f, 29.3f, 56.5f, 11.0f, 38.0f, 65.0f, 20.7f, 47.0f, 74.0f,
+        3.0f, 30.3f, 57.5f, 12.0f, 39.0f, 66.0f, 21.7f, 48.0f, 75.0f,
+        4.0f, 31.3f, 58.5f, 13.0f, 40.0f, 67.0f, 22.7f, 49.0f, 76.0f,
+        5.0f, 32.3f, 59.5f, 14.0f, 41.0f, 68.0f, 23.7f, 50.0f, 77.0f,
+        6.0f, 33.3f, 60.5f, 15.0f, 42.0f, 69.0f, 24.7f, 51.0f, 78.0f,
+        7.0f, 34.3f, 61.5f, 16.0f, 43.0f, 70.0f, 25.7f, 52.0f, 79.0f,
+        8.0f, 35.3f, 62.5f, 17.0f, 44.0f, 71.0f, 26.7f, 53.0f, 80.0f
+    };
+
+    mv::Tensor t("t", tShape, mv::DTypeType::UInt32, mv::Order("HWCN"));
+    t.populate(data);
+    mv::BinaryData bdata = t.toBinary();
+    const std::vector<uint32_t>& u32_data = bdata.u32();
+    for (unsigned i = 0; i < u32_data.size(); i++)
+        EXPECT_EQ(u32_data[i], (uint32_t)data[i]);
+}
+
+TEST(tensor, to_binary_u16)
+{
+
+    mv::Shape tShape({3, 3, 3, 3});
+
+    std::vector<double> data = {
+        0.0f, 27.3f, 54.5f, 9.0f, 36.0f, 63.0f, 18.7f, 45.0f, 72.0f,
+        1.0f, 28.3f, 55.5f, 10.0f, 37.0f, 64.0f, 19.7f, 46.0f, 73.0f,
+        2.0f, 29.3f, 56.5f, 11.0f, 38.0f, 65.0f, 20.7f, 47.0f, 74.0f,
+        3.0f, 30.3f, 57.5f, 12.0f, 39.0f, 66.0f, 21.7f, 48.0f, 75.0f,
+        4.0f, 31.3f, 58.5f, 13.0f, 40.0f, 67.0f, 22.7f, 49.0f, 76.0f,
+        5.0f, 32.3f, 59.5f, 14.0f, 41.0f, 68.0f, 23.7f, 50.0f, 77.0f,
+        6.0f, 33.3f, 60.5f, 15.0f, 42.0f, 69.0f, 24.7f, 51.0f, 78.0f,
+        7.0f, 34.3f, 61.5f, 16.0f, 43.0f, 70.0f, 25.7f, 52.0f, 79.0f,
+        8.0f, 35.3f, 62.5f, 17.0f, 44.0f, 71.0f, 26.7f, 53.0f, 80.0f
+    };
+
+    mv::Tensor t("t", tShape, mv::DTypeType::UInt16, mv::Order("HWCN"));
+    t.populate(data);
+    mv::BinaryData bdata = t.toBinary();
+    const std::vector<uint16_t>& u16_data = bdata.u16();
+    for (unsigned i = 0; i < u16_data.size(); i++)
+        EXPECT_EQ(u16_data[i], (uint16_t) data[i]);
+}
+
+TEST(tensor, to_binary_u8)
+{
+
+    mv::Shape tShape({3, 3, 3, 3});
+
+    std::vector<double> data = {
+        0.0f, 27.3f, 54.5f, 9.0f, 36.0f, 63.0f, 18.7f, 45.0f, 72.0f,
+        1.0f, 28.3f, 55.5f, 10.0f, 37.0f, 64.0f, 19.7f, 46.0f, 73.0f,
+        2.0f, 29.3f, 56.5f, 11.0f, 38.0f, 65.0f, 20.7f, 47.0f, 74.0f,
+        3.0f, 30.3f, 57.5f, 12.0f, 39.0f, 66.0f, 21.7f, 48.0f, 75.0f,
+        4.0f, 31.3f, 58.5f, 13.0f, 40.0f, 67.0f, 22.7f, 49.0f, 76.0f,
+        5.0f, 32.3f, 59.5f, 14.0f, 41.0f, 68.0f, 23.7f, 50.0f, 77.0f,
+        6.0f, 33.3f, 60.5f, 15.0f, 42.0f, 69.0f, 24.7f, 51.0f, 78.0f,
+        7.0f, 34.3f, 61.5f, 16.0f, 43.0f, 70.0f, 25.7f, 52.0f, 79.0f,
+        8.0f, 35.3f, 62.5f, 17.0f, 44.0f, 71.0f, 26.7f, 53.0f, 80.0f
+    };
+
+    mv::Tensor t("t", tShape, mv::DTypeType::UInt8, mv::Order("HWCN"));
+    t.populate(data);
+    mv::BinaryData bdata = t.toBinary();
+    const std::vector<uint8_t>& u8_data = bdata.u8();
+    for (unsigned i = 0; i < u8_data.size(); i++)
+        EXPECT_EQ(u8_data[i], (uint8_t) data[i]);
+}
+
+TEST(tensor, to_binary_i64)
+{
+
+    mv::Shape tShape({3, 3, 3, 3});
+
+    std::vector<double> data = {
+        0.0f, 27.3f, 54.5f, 9.0f, 36.0f, 63.0f, 18.7f, 45.0f, 72.0f,
+        1.0f, 28.3f, 55.5f, 10.0f, 37.0f, 64.0f, 19.7f, 46.0f, 73.0f,
+        2.0f, 29.3f, 56.5f, 11.0f, 38.0f, 65.0f, 20.7f, 47.0f, 74.0f,
+        3.0f, 30.3f, 57.5f, 12.0f, 39.0f, 66.0f, 21.7f, 48.0f, 75.0f,
+        4.0f, 31.3f, 58.5f, 13.0f, 40.0f, 67.0f, 22.7f, 49.0f, 76.0f,
+        5.0f, 32.3f, 59.5f, 14.0f, 41.0f, 68.0f, 23.7f, 50.0f, 77.0f,
+        6.0f, 33.3f, 60.5f, 15.0f, 42.0f, 69.0f, 24.7f, 51.0f, 78.0f,
+        7.0f, 34.3f, 61.5f, 16.0f, 43.0f, 70.0f, 25.7f, 52.0f, 79.0f,
+        8.0f, 35.3f, 62.5f, 17.0f, 44.0f, 71.0f, 26.7f, 53.0f, 80.0f
+    };
+
+    mv::Tensor t("t", tShape, mv::DTypeType::Int64, mv::Order("HWCN"));
+    t.populate(data);
+    mv::BinaryData bdata = t.toBinary();
+    const std::vector<uint64_t>& i64_data = bdata.i64();
+    for (unsigned i = 0; i < i64_data.size(); i++)
+        EXPECT_EQ(i64_data[i], (uint64_t) data[i]);
+}
+
+TEST(tensor, to_binary_i32)
+{
+
+    mv::Shape tShape({3, 3, 3, 3});
+
+    std::vector<double> data = {
+        0.0f, 27.3f, -54.5f, -9.0f, 36.0f, 63.0f, 18.7f, 45.0f, 72.0f,
+        1.0f, 28.3f, -55.5f, -10.0f, 37.0f, 64.0f, 19.7f, 46.0f, 73.0f,
+        2.0f, 29.3f, -56.5f, -11.0f, 38.0f, 65.0f, 20.7f, 47.0f, 74.0f,
+        3.0f, 30.3f, -57.5f, -12.0f, 39.0f, 66.0f, 21.7f, 48.0f, 75.0f,
+        4.0f, 31.3f, -58.5f, -13.0f, 40.0f, 67.0f, 22.7f, 49.0f, 76.0f,
+        5.0f, 32.3f, -59.5f, -14.0f, 41.0f, 68.0f, 23.7f, 50.0f, 77.0f,
+        6.0f, 33.3f, -60.5f, -15.0f, 42.0f, 69.0f, 24.7f, 51.0f, 78.0f,
+        7.0f, 34.3f, -61.5f, -16.0f, 43.0f, 70.0f, 25.7f, 52.0f, 79.0f,
+        8.0f, 35.3f, -62.5f, -17.0f, 44.0f, 71.0f, 26.7f, 53.0f, 80.0f
+    };
+
+    mv::Tensor t("t", tShape, mv::DTypeType::Int32, mv::Order("HWCN"));
+    t.populate(data);
+    mv::BinaryData bdata = t.toBinary();
+    const std::vector<int32_t>& i32_data = bdata.i32();
+    for (unsigned i = 0; i < i32_data.size(); i++)
+        EXPECT_EQ(i32_data[i], (int32_t) data[i]);
+}
+
+TEST(tensor, to_binary_i16)
+{
+
+    mv::Shape tShape({3, 3, 3, 3});
+
+    std::vector<double> data = {
+        0.0f, 27.3f, -54.5f, -9.0f, 36.0f, 63.0f, 18.7f, 45.0f, 72.0f,
+        1.0f, 28.3f, -55.5f, -10.0f, 37.0f, 64.0f, 19.7f, 46.0f, 73.0f,
+        2.0f, 29.3f, -56.5f, -11.0f, 38.0f, 65.0f, 20.7f, 47.0f, 74.0f,
+        3.0f, 30.3f, -57.5f, -12.0f, 39.0f, 66.0f, 21.7f, 48.0f, 75.0f,
+        4.0f, 31.3f, -58.5f, -13.0f, 40.0f, 67.0f, 22.7f, 49.0f, 76.0f,
+        5.0f, 32.3f, -59.5f, -14.0f, 41.0f, 68.0f, 23.7f, 50.0f, 77.0f,
+        6.0f, 33.3f, -60.5f, -15.0f, 42.0f, 69.0f, 24.7f, 51.0f, 78.0f,
+        7.0f, 34.3f, -61.5f, -16.0f, 43.0f, 70.0f, 25.7f, 52.0f, 79.0f,
+        8.0f, 35.3f, -62.5f, -17.0f, 44.0f, 71.0f, 26.7f, 53.0f, 80.0f
+    };
+
+    mv::Tensor t("t", tShape, mv::DTypeType::Int16, mv::Order("HWCN"));
+    t.populate(data);
+    mv::BinaryData bdata = t.toBinary();
+    const std::vector<int16_t>& i16_data = bdata.i16();
+    for (unsigned i = 0; i < i16_data.size(); i++)
+        EXPECT_EQ(i16_data[i], (int16_t) data[i]);
+}
+
+TEST(tensor, to_binary_i8)
+{
+
+    mv::Shape tShape({3, 3, 3, 3});
+
+    std::vector<double> data = {
+        0.0f, 27.3f, -54.5f, -9.0f, 36.0f, 63.0f, 18.7f, 45.0f, 72.0f,
+        1.0f, 28.3f, -55.5f, -10.0f, 37.0f, 64.0f, 19.7f, 46.0f, 73.0f,
+        2.0f, 29.3f, -56.5f, -11.0f, 38.0f, 65.0f, 20.7f, 47.0f, 74.0f,
+        3.0f, 30.3f, -57.5f, -12.0f, 39.0f, 66.0f, 21.7f, 48.0f, 75.0f,
+        4.0f, 31.3f, -58.5f, -13.0f, 40.0f, 67.0f, 22.7f, 49.0f, 76.0f,
+        5.0f, 32.3f, -59.5f, -14.0f, 41.0f, 68.0f, 23.7f, 50.0f, 77.0f,
+        6.0f, 33.3f, -60.5f, -15.0f, 42.0f, 69.0f, 24.7f, 51.0f, 78.0f,
+        7.0f, 34.3f, -61.5f, -16.0f, 43.0f, 70.0f, 25.7f, 52.0f, 79.0f,
+        8.0f, 35.3f, -62.5f, -17.0f, 44.0f, 71.0f, 26.7f, 53.0f, 80.0f
+    };
+
+    mv::Tensor t("t", tShape, mv::DTypeType::Int8, mv::Order("HWCN"));
+    t.populate(data);
+    mv::BinaryData bdata = t.toBinary();
+    const std::vector<int8_t>& i8_data = bdata.i8();
+    for (unsigned i = 0; i < i8_data.size(); i++)
+        EXPECT_EQ(i8_data[i], (int8_t) data[i]);
+}
+
+TEST(tensor, to_binary_i4)
+{
+
+    mv::Shape tShape({1, 1, 1, 16});
+
+    std::vector<double> data = {
+        0.0f,
+        1.0f,
+        2.0f,
+        3.0f,
+        4.0f,
+        5.0f,
+        6.0f,
+        7.0f,
+        8.0f,
+        9.0f,
+        10.0f,
+        11.0f,
+        12.0f,
+        13.0f,
+        14.0f,
+        15.0f
+    };
+
+    mv::Tensor t("t", tShape, mv::DTypeType::Int4, mv::Order("HWCN"));
+    t.populate(data);
+    mv::BinaryData bdata = t.toBinary();
+    const std::vector<int8_t>& i4_data = bdata.i4();
+    EXPECT_EQ(i4_data.size(), 8);
+    int idx = 0;
+    for (unsigned i = 0; i < i4_data.size(); i++)
+    {
+        EXPECT_EQ((i4_data[i] & 0xF), (int8_t) data[idx++]);
+        EXPECT_EQ((i4_data[i] & 0xF0) >> 4, (int8_t) data[idx++]);
+    }
+}
+
+TEST(tensor, to_binary_i2)
+{
+
+    mv::Shape tShape({1, 1, 1, 16});
+
+    std::vector<double> data = {
+        0.0f,
+        1.0f,
+        2.0f,
+        3.0f,
+        0.0f,
+        1.0f,
+        1.0f,
+        3.0f,
+        0.0f,
+        2.0f,
+        2.0f,
+        1.0f,
+        3.0f,
+        2.0f,
+        1.0f,
+        0.0f
+    };
+
+    mv::Tensor t("t", tShape, mv::DTypeType::Int2, mv::Order("HWCN"));
+    t.populate(data);
+    mv::BinaryData bdata = t.toBinary();
+    const std::vector<int8_t>& i2_data = bdata.i2();
+    EXPECT_EQ(i2_data.size(), 4);
+    int idx = 0;
+    for (unsigned i = 0; i < i2_data.size(); i++)
+    {
+        EXPECT_EQ((i2_data[i] & 0x3), (int8_t) data[idx++]);
+        EXPECT_EQ((i2_data[i] & 0xC) >> 2, (int8_t) data[idx++]);
+        EXPECT_EQ((i2_data[i] & 0x30) >> 4, (int8_t) data[idx++]);
+        EXPECT_EQ((i2_data[i] & 0xC0) >> 6, (int8_t) data[idx++]);
+    }
 }
