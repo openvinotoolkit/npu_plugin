b5a3fbf443413a198d739c7874528b2fe2e6f692
Marco Mecchia <marco.mecchia@intel.com>
2019-01-15 12:52:10 +0000

    Barrier Task definition (forgot two/three commits ago

---
+++ src/target/keembay/tasks/def/barrier_task.cpp
@@ -0,0 +1,32 @@
+#include "include/mcm/computation/op/op_registry.hpp"
+#include "include/mcm/computation/op/op.hpp"
+
+namespace mv
+{
+
+    namespace op
+    {
+
+        static std::function<std::pair<bool, std::size_t>(const std::vector<Data::TensorIterator>&,
+            const std::map<std::string, Attribute>&, std::string&)> inputCheckFcn =
+            [](const std::vector<Data::TensorIterator>& inputs, const std::map<std::string, Attribute>& args,
+            std::string&) -> std::pair<bool, std::size_t>
+        {
+            return {true, 0};
+        };
+                
+        static std::function<void(const std::vector<Data::TensorIterator>&, const std::map<std::string, Attribute>&, 
+            std::vector<Tensor>&)> outputDefFcn =
+            [](const std::vector<Data::TensorIterator>& inputs, const std::map<std::string, Attribute>& args, std::vector<Tensor>& outputs)
+        {
+
+        };
+    
+        MV_REGISTER_OP(BarrierTask)
+        .setInputCheck(inputCheckFcn)
+        .setOutputDef(outputDefFcn)
+        .setTypeTrait({"executable"});
+
+    }
+
+}
