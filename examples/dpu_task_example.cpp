4b775824c4661d5ce7d4b813e128934ff83de133
Marco Mecchia <marco.mecchia@intel.com>
2019-01-22 13:33:58 +0000

    Added de allocation task

--- examples/dpu_task_example.cpp
+++ examples/dpu_task_example.cpp
@@ -14,12 +14,17 @@ int main()
     mv::ControlModel cm(test_cm);
 
     auto input1 = test_cm.input({225, 225, 3}, mv::DTypeType::Float16, mv::Order("CHW"));
+    auto input1dmaIN = test_cm.dMATask(input1, mv::DmaDirectionEnum::DDR2CMX);
+    auto input1dmaOUT = test_cm.deAllocate(input1dmaIN);
     std::vector<double> weights1Data = mv::utils::generateSequence<double>(3*3*3);
     auto weights1 = test_cm.constant(weights1Data, {3, 3, 3, 1}, mv::DTypeType::Float16, mv::Order("NCWH"));
-
-    auto dpuconv1 = test_cm.dPUTaskConv({input1, weights1}, {2,2}, {0,0,0,0});
+    auto dmaINweights1 = test_cm.dMATask(weights1, mv::DmaDirectionEnum::DDR2CMX);
+    auto dmaOUTweights2 = test_cm.deAllocate(dmaINweights1);
+    auto dpuconv1 = test_cm.dPUTaskConv({input1dmaIN, dmaINweights1}, {2,2}, {0,0,0,0});
     auto dpupool1 = test_cm.dPUTaskMaxPool({dpuconv1}, {3, 3}, {3, 3}, {0,0,0,0});
-    auto output = test_cm.output(dpupool1);
+    auto dmaOutput = test_cm.dMATask(dpupool1, mv::DmaDirectionEnum::CMX2DDR);
+
+    auto output = test_cm.output(dmaOutput);
 
     std::string outputName("dpu_task");
 
---
+++ src/target/keembay/tasks/def/deallocate_task.cpp
@@ -0,0 +1,33 @@
+#include "include/mcm/computation/op/op_registry.hpp"
+#include "include/mcm/computation/op/op.hpp"
+
+namespace mv
+{
+
+    namespace op
+    {
+
+        static std::function<std::pair<bool, std::size_t>(const std::vector<Data::TensorIterator>&,
+            const std::map<std::string, Attribute>&, std::string&)> inputCheckFcn =
+            [](const std::vector<Data::TensorIterator>&, const std::map<std::string, Attribute>&,
+            std::string&) -> std::pair<bool, std::size_t>
+        {
+            return {true, 0};
+        };
+                
+        static std::function<void(const std::vector<Data::TensorIterator>&, const std::map<std::string, Attribute>&, 
+            std::vector<Tensor>&)> outputDefFcn =
+            [](const std::vector<Data::TensorIterator>&, const std::map<std::string, Attribute>&, std::vector<Tensor>& )
+        {
+
+        };
+    
+        MV_REGISTER_OP(DeAllocate)
+        .setInputs({"inputs"})
+        .setInputCheck(inputCheckFcn)
+        .setOutputDef(outputDefFcn)
+        .setTypeTrait({"executable"});
+
+    }
+
+}
