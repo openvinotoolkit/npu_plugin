60fcda6d379dcb9e3811f8fc87ce556e21631631
Marco Mecchia <marco.mecchia@intel.com>
2019-01-24 15:35:47 +0000

    Added methods to RuntimeModel for conversion of:
    -TensorIterator + MemoryAllocator -> TensorReference
    -DType -> DType
    -AllocatorName -> MemoryLocation

--- include/mcm/target/keembay/runtime_model/runtime_model.hpp
+++ include/mcm/target/keembay/runtime_model/runtime_model.hpp
@@ -2,6 +2,9 @@
 #define MV_RUNTIME_MODEL_
 
 #include "meta/schema/graphfile/graphfile_generated.h"
+#include "meta/schema/graphfile/memoryManagement_generated.h"
+#include "include/mcm/computation/resource/memory_allocator.hpp"
+#include "include/mcm/computation/model/data_model.hpp"
 
 namespace mv
 {
@@ -9,9 +12,16 @@ namespace mv
     {
         private:
             MVCNN::GraphFileT graphFile_;
+            static const std::unordered_map<DTypeType, MVCNN::DType> dTypeMapping_;
+            static const std::unordered_map<std::string, MVCNN::MemoryLocation> memoryLocationMapping_;
+
         public:
             RuntimeModel();
             ~RuntimeModel();
+
+            static MVCNN::MemoryLocation convertAllocatorToMemoryLocale(const std::string& allocatorName);
+            static MVCNN::TensorReferenceT convertTensorRepresentation(MemoryAllocator &allocator, Data::TensorIterator t);
+            static MVCNN::DType convertDtype(const DType& dtype);
 
             void serialize(const std::string& path);
             char * serialize(int& bufferSize);
--- src/target/keembay/runtime_model/runtime_model.cpp
+++ src/target/keembay/runtime_model/runtime_model.cpp
@@ -14,6 +14,77 @@ mv::RuntimeModel::~RuntimeModel()
 
 }
 
+const std::unordered_map<mv::DTypeType, MVCNN::DType> mv::RuntimeModel::dTypeMapping_ =
+{
+    {mv::DTypeType::Float64, MVCNN::DType::DType_FP64},
+    {mv::DTypeType::Float32, MVCNN::DType::DType_FP32},
+    {mv::DTypeType::Float16, MVCNN::DType::DType_FP16},
+    {mv::DTypeType::Float8, MVCNN::DType::DType_FP8},
+    {mv::DTypeType::UInt64, MVCNN::DType::DType_U64},
+    {mv::DTypeType::UInt32, MVCNN::DType::DType_U32},
+    {mv::DTypeType::UInt16, MVCNN::DType::DType_U16},
+    {mv::DTypeType::UInt8, MVCNN::DType::DType_U8},
+    {mv::DTypeType::Int64, MVCNN::DType::DType_I64},
+    {mv::DTypeType::Int32, MVCNN::DType::DType_I32},
+    {mv::DTypeType::Int16, MVCNN::DType::DType_I16},
+    {mv::DTypeType::Int8, MVCNN::DType::DType_I8},
+    {mv::DTypeType::Int4, MVCNN::DType::DType_I4},
+    {mv::DTypeType::Int2, MVCNN::DType::DType_I2},
+    {mv::DTypeType::Int2X, MVCNN::DType::DType_I2X},
+    {mv::DTypeType::Int4X, MVCNN::DType::DType_I4X},
+    {mv::DTypeType::Bin, MVCNN::DType::DType_BIN},
+    {mv::DTypeType::Log, MVCNN::DType::DType_LOG}
+};
+
+const std::unordered_map<std::string, MVCNN::MemoryLocation> mv::RuntimeModel::memoryLocationMapping_ =
+{
+    {"ProgrammableInput", MVCNN::MemoryLocation::MemoryLocation_ProgrammableInput},
+    {"ProgrammableOutput", MVCNN::MemoryLocation::MemoryLocation_ProgrammableOutput},
+    {"VPU_DDR_Heap", MVCNN::MemoryLocation::MemoryLocation_VPU_DDR_Heap},
+    {"GraphFile", MVCNN::MemoryLocation::MemoryLocation_GraphFile},
+    {"VPU_CMX_NN", MVCNN::MemoryLocation::MemoryLocation_VPU_CMX_NN},
+    {"VPU_CMX_UPA", MVCNN::MemoryLocation::MemoryLocation_VPU_CMX_UPA},
+    {"VPU_DDR_BSS", MVCNN::MemoryLocation::MemoryLocation_VPU_DDR_BSS}
+};
+
+
+MVCNN::DType mv::RuntimeModel::convertDtype(const mv::DType& dtype)
+{
+    return dTypeMapping_.at(static_cast<mv::DTypeType>(dtype));
+}
+
+MVCNN::MemoryLocation mv::RuntimeModel::convertAllocatorToMemoryLocale(const std::string& allocatorName)
+{
+    return memoryLocationMapping_.at(allocatorName);
+}
+
+MVCNN::TensorReferenceT mv::RuntimeModel::convertTensorRepresentation(MemoryAllocator &allocator, mv::Data::TensorIterator t)
+{
+    MVCNN::TensorReferenceT toReturn;
+
+    mv::Data::BufferIterator it = allocator.getBuffer(0, t);
+
+    toReturn.dimensions = it->getData()->getShape(); // Padded or not?
+    toReturn.strides = it->getData()->computeStrides(); //Maybe directly it->computeStrides() in the future std::vector<uint32_t> strides;
+
+    auto strides = it->getStrides();
+    toReturn.leading_offset = strides[0];//    uint32_t leading_offset;
+    toReturn.trailing_offset = strides[strides.size()-1] + it->getPostAlign();//    uint32_t trailing_offset;
+
+    toReturn.data->data_index = it->getOffset();//    std::unique_ptr<IndirectDataReferenceT> data;
+    toReturn.locale = convertAllocatorToMemoryLocale(allocator.getAllocatorName());//    MemoryLocation locale;
+    toReturn.data_dtype = convertDtype(it->getData()->getDType());//    DType data_dtype;
+
+    //UNSUPPORTED FOR NOW
+    //toReturn.quant_scale;//    std::vector<int8_t> quant_scale;
+    //toReturn.quant_zero;//    std::vector<int8_t> quant_zero;
+    //toReturn.quant_shift;//    std::vector<int8_t> quant_shift;
+
+    return toReturn;
+}
+
+
+
 char * mv::RuntimeModel::serialize(int& bufferSize)
 {
     flatbuffers::FlatBufferBuilder fbb;
