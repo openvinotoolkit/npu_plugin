eaf1ea71cd764b87cc957ed08a5772fc3a1fe448
Marco Mecchia <marco.mecchia@intel.com>
2019-01-24 18:13:14 +0000

    Refined dpu_task_example to make it more similar to slides presented by John

--- examples/dpu_task_example.cpp
+++ examples/dpu_task_example.cpp
@@ -15,18 +15,42 @@ int main()
 
     auto input1 = test_cm.input({225, 225, 3}, mv::DTypeType::Float16, mv::Order("CHW"));
     auto input1dmaIN = test_cm.dMATask(input1, mv::DmaDirectionEnum::DDR2CMX);
-    auto input1dmaOUT = test_cm.deAllocate(input1dmaIN);
+    test_cm.deAllocate(input1dmaIN);
     std::vector<double> weights1Data = mv::utils::generateSequence<double>(3*3*3);
     auto weights1 = test_cm.constant(weights1Data, {3, 3, 3, 1}, mv::DTypeType::Float16, mv::Order("NCWH"));
     auto dmaINweights1 = test_cm.dMATask(weights1, mv::DmaDirectionEnum::DDR2CMX);
-    auto dmaOUTweights2 = test_cm.deAllocate(dmaINweights1);
+    test_cm.deAllocate(dmaINweights1);
     auto dpuconv1 = test_cm.dPUTaskConv({input1dmaIN, dmaINweights1}, {2,2}, {0,0,0,0});
-    auto dpupool1 = test_cm.dPUTaskMaxPool({dpuconv1}, {3, 3}, {3, 3}, {0,0,0,0});
-    auto dmaOutput = test_cm.dMATask(dpupool1, mv::DmaDirectionEnum::CMX2DDR);
+    auto dmaOutput = test_cm.dMATask(dpuconv1, mv::DmaDirectionEnum::CMX2DDR);
+    test_cm.output(dmaOutput);
 
-    auto output = test_cm.output(dmaOutput);
+
+    auto inputOp = test_cm.getSourceOp(input1);
+    auto input1dmaINOp = test_cm.getSourceOp(input1dmaIN);
+    auto input1dmaOutOp = test_cm.getOp("DeAllocate_0");
+    auto weightsOp = test_cm.getSourceOp(weights1);
+    auto dmaINweights1Op = test_cm.getSourceOp(dmaINweights1);
+    auto dmaOUTWeights1Op = test_cm.getOp("DeAllocate_1");
+    auto dpuconv1Op = test_cm.getSourceOp(dpuconv1);
+    auto dmaOutputOp = test_cm.getSourceOp(dmaOutput);
+    auto outputOp = test_cm.getOp("Output_0");
 
     std::string outputName("dpu_task");
+    cm.defineFlow(inputOp, input1dmaINOp);
+    cm.defineFlow(inputOp, dmaINweights1Op);
+
+    cm.defineFlow(input1dmaINOp, dpuconv1Op);
+    cm.defineFlow(dmaINweights1Op, dpuconv1Op);
+
+    cm.defineFlow(input1dmaINOp, input1dmaOutOp);
+    cm.defineFlow(dmaINweights1Op, dmaOUTWeights1Op);
+
+    cm.defineFlow(input1dmaOutOp, dmaOutputOp);
+    cm.defineFlow(dmaOUTWeights1Op, dmaOutputOp);
+    cm.defineFlow(dpuconv1Op, dmaOutputOp);
+    cm.defineFlow(dpuconv1Op, input1dmaOutOp);
+    cm.defineFlow(dpuconv1Op, dmaOUTWeights1Op);
+
 
     unit.compilationDescriptor()["GenerateDot"]["output"] = std::string(outputName + ".dot");
     unit.compilationDescriptor()["GenerateDot"]["scope"] = std::string("OpControlModel");
@@ -38,5 +62,7 @@ int main()
     unit.passManager().disablePass(mv::PassGenre::Serialization);
     unit.run();
 
-    system("dot -Tsvg dpu_task.dot -o dpu_task.svg");
+    system("dot -Tsvg dpu_task.dot -o dpu_task.png");
+    system("dot -Tsvg dpu_task_final.dot -o dpu_task_final.png");
+
 }
