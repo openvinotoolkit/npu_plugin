eda51b440ab93a78208f829eb7c9fab2b1077cce
Marco Mecchia <marco.mecchia@intel.com>
2019-01-30 15:09:21 +0000

    Added base passes for KB header serialization.

---
+++ src/pass/adaptation/assign_unique_op_id.cpp
@@ -0,0 +1,33 @@
+#include "include/mcm/pass/pass_registry.hpp"
+#include "meta/include/mcm/op_model.hpp"
+#include "include/mcm/computation/model/control_model.hpp"
+#include "include/mcm/computation/model/data_model.hpp"
+#include "include/mcm/target/myriadx/nce1.hpp"
+
+static void assignUniqueOpId(const mv::pass::PassEntry& pass, mv::ComputationModel& model, mv::TargetDescriptor&, mv::json::Object&, mv::json::Object&);
+
+namespace mv
+{
+
+    namespace pass
+    {
+
+        MV_REGISTER_PASS(AssignUniqueOpId)
+        .setFunc(assignUniqueOpId)
+        .setGenre(PassGenre::Adaptation)
+        .setDescription(
+            "This pass assigns an unique ID to each op in the graph."
+        );
+    }
+}
+
+void assignUniqueOpId(const mv::pass::PassEntry&, mv::ComputationModel& model, mv::TargetDescriptor&, mv::json::Object&, mv::json::Object&)
+{
+    mv::OpModel om(model);
+
+    unsigned currentId = 0;
+    std::string currentIdLabel("opId");
+
+    for(auto operationIt = om.opBegin(); operationIt != om.opEnd(); ++operationIt)
+        operationIt->set<unsigned>(currentIdLabel, currentId++);
+}
---
+++ src/pass/adaptation/convert_to_task_graph.cpp
@@ -0,0 +1,27 @@
+#include "include/mcm/pass/pass_registry.hpp"
+#include "meta/include/mcm/op_model.hpp"
+#include "include/mcm/computation/model/control_model.hpp"
+#include "include/mcm/computation/model/data_model.hpp"
+#include "include/mcm/target/myriadx/nce1.hpp"
+
+static void convertToTaskGraph(const mv::pass::PassEntry& pass, mv::ComputationModel& model, mv::TargetDescriptor&, mv::json::Object&, mv::json::Object&);
+
+namespace mv
+{
+
+    namespace pass
+    {
+
+        MV_REGISTER_PASS(ConvertToTaskGraph)
+        .setFunc(convertToTaskGraph)
+        .setGenre(PassGenre::Adaptation)
+        .setDescription(
+            "This pass converts a graph of operations into a graph of tasks."
+        );
+    }
+}
+
+void convertToTaskGraph(const mv::pass::PassEntry&, mv::ComputationModel& model, mv::TargetDescriptor&, mv::json::Object&, mv::json::Object&)
+{
+    // TODO
+}
