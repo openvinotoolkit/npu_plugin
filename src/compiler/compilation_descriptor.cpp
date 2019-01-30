0ffdc9378e6662c6c0e098b30a324d81a250b90a
Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>
2019-01-29 16:03:52 -0800

    VPUNND-11: Prework for PassManager changes to use CompilationDescriptor
    The following changes are made to the CompilationDescriptor in
    anticipation of changes to the PassManager.
    
    	- Return passes as Element after serialization. PassManager
    	needs to be able to access pass arguments, so
    	make serialized pass list a list of Element objects
    	instead of strings.
    	- Use lambdas to shorten some repetitive code.
    	- Make serializedPassList a list of Elements, and not pointers
    	- Make compilation profile an optional argument
    
    Signed-off-by: Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>

--- src/compiler/compilation_descriptor.cpp
+++ src/compiler/compilation_descriptor.cpp
@@ -16,38 +16,21 @@ profile_(profile)
 
 bool mv::CompilationDescriptor::validPass(const std::string& passStr)
 {
-    bool pass_entry = mv::pass::PassRegistry::instance().find(passStr);
-
-    if (!pass_entry) {
-        return false;
-    }
-
-    return true;
-}
-
-void mv::CompilationDescriptor::addElemAttribute(const std::string& elem, bool isGroup)
-{
-    if (!hasAttr(elem))
-    {
-        Element e = Element(elem);
-
-        if (isGroup)
-            e.set<bool>("isGroup", true);
-        else
-            e.set<bool>("isGroup", false);
-
-        set<Element>(elem, e);
-    }
+    return mv::pass::PassRegistry::instance().find(passStr) ? true : false;
 }
 
 void mv::CompilationDescriptor::addGroup(const std::string& group)
 {
-    addElemAttribute(group, true);
-}
+    if (!hasAttr(group))
+    {
+        Element g = Element(group);
+        g.set<bool>("isGroup", true);
+        set<Element>(group, g);
+    }
 
-void mv::CompilationDescriptor::addPass(const std::string& pass)
-{
-    addElemAttribute(pass, false);
+    auto it = std::find(groups_.begin(), groups_.end(), group);
+    if (it == groups_.end())
+        groups_.push_back(group);
 }
 
 void mv::CompilationDescriptor::addToGroup(const std::string& group, const std::string& elem, const std::string& recurrence, bool elemIsGroup)
@@ -71,73 +54,219 @@ void mv::CompilationDescriptor::addToGroup(const std::string& group, const std::
         g_elem.set<std::vector<std::string>>(recurrence, rec_v);
     }
 
-    if (!hasAttr(elem))
+    if (elemIsGroup)
     {
-        if (elemIsGroup)
+        if (!hasAttr(elem))
             addGroup(elem);
-        else
-            addPass(elem);
     }
+    else
+    {
+        if (!g_elem.hasAttr(elem))
+        {
+            Element p = Element(elem);
+            p.set<bool>("isGroup", false);
+            g_elem.set<Element>(elem, p);
+        }
+    }
+}
+
+void mv::CompilationDescriptor::remove(const std::string& group, const std::string& elem, const std::string& recurrence)
+{
+    if (!hasAttr(group))
+        throw ArgumentError(*this, "CompilationDescriptor", "invalid", "Trying to remove pass from a non-existent group (" + group + ")");
+    
+    Element& g_elem = get<Element>(group);
+
+    if (!g_elem.hasAttr(recurrence))
+        throw RuntimeError(*this, recurrence + " recurrence is not present in group " + group);
+
+    // Remove element from list of passes.
+    auto& rec_v = g_elem.get<std::vector<std::string>>(recurrence);
+    
+    auto it = std::find(rec_v.begin(), rec_v.end(), elem);
+    if (it == rec_v.end())
+        throw RuntimeError(*this, "Unable to find element (" + elem + ") in group (" + group + ")");
+    
+    rec_v.erase(it);
+
+    // Remove pass attribute from the group (if elem happens to be a pass).
+    if (g_elem.hasAttr(elem))
+        g_elem.erase(elem);
+}
+
+void mv::CompilationDescriptor::remove(const std::string& group, const std::string& elem)
+{
+    if (!hasAttr(group))
+        throw ArgumentError(*this, "CompilationDescriptor", "invalid", "Trying to remove element from a non-existent group (" + group + ")");
+
+    Element& g_elem = get<Element>(group);
+
+    if (g_elem.hasAttr("Singular"))
+        remove(group, elem, "Singular");
+
+    if (g_elem.hasAttr("Recurrent"))
+        remove(group, elem, "Recurrent");
+}
+
+void mv::CompilationDescriptor::remove(const std::string& group)
+{
+    if (!hasAttr(group))
+        throw ArgumentError(*this, "CompilationDescriptor", "invalid", "Trying to remove a non-existent group (" + group + ")");
+    
+    // Remove group attribute from descriptor.
+    erase(group);
+
+    // Remove entry from root group, if root group is present.
+    if (group != "root" && hasAttr("root"))
+    {
+        auto& root = get<Element>("root");
+        if (root.hasAttr(group))
+            root.erase(group);
+    }
+
+    for (auto g: groups_)
+    {
+        if (hasAttr(g))
+        {
+            // Find group in g's recurrence lists, and remove it
+            auto removeInRecurrenceGroup = [this, g, group](const std::string& recType) {
+                auto& elem = get<Element>(g);
+                if (elem.hasAttr(recType)) {
+                    auto& recList = elem.get<std::vector<std::string>>(recType);
+                    auto it = std::find(recList.begin(), recList.end(), group);
+                    if (it != recList.end())
+                        recList.erase(it);
+                }
+            };
+
+            removeInRecurrenceGroup("Singular");
+            removeInRecurrenceGroup("Recurrent");
+        }
+    }
+
+    // Remove group from list of groups
+    auto it = std::find(groups_.begin(), groups_.end(), group);
+    if (it != groups_.end()) {
+        groups_.erase(it);
+    }
+}
+
+void mv::CompilationDescriptor::clear()
+{
+    for (auto group: groups_) {
+        if (hasAttr(group)) {
+            erase(group);
+        }
+    }
+
+    groups_.clear();
+}
+
+size_t mv::CompilationDescriptor::getNumGroups()
+{
+    return groups_.size();
+}
+
+bool mv::CompilationDescriptor::rootGroupPresent()
+{
+    return hasAttr("root");
 }
 
 bool mv::CompilationDescriptor::isGroup(const std::string& elem)
 {
-    if (!hasAttr(elem))
-        throw ArgumentError(*this, "CompilationDescriptor", "invalid", "Non-existent element (" + elem + ")");
-
-    Element &e = get<Element>(elem);
-
-    return e.get<bool>("isGroup");
+    return std::find(groups_.begin(), groups_.end(), elem) != groups_.end();
 }
 
 void mv::CompilationDescriptor::setPassArg(const std::string& pass, const std::string& arg, const mv::Attribute& value)
 {
-    if (!hasAttr(pass))
-        throw ArgumentError(*this, "CompilationDescriptor", "invalid", "Trying to add arguments to a non-existent pass (" + pass + ")");
+    bool found = false;
 
-    Element& p = get<Element>(pass);
+    for (auto group: groups_)
+    {
+        if (hasAttr(group))
+        {
+            auto& g = get<Element>(group);
+            if (g.hasAttr(pass))
+            {
+                auto& p = g.get<Element>(pass);
+                p.set(arg, value);
+
+                if (!found)
+                    found = true;
+            }
+        }
+    }
+
+    if (!found)
+        throw RuntimeError(*this, "Trying to set arguments for a non-existent pass (" + pass + ")");
+}
+
+void mv::CompilationDescriptor::setPassArg(const std::string& group, const std::string& pass, const std::string& arg, const mv::Attribute& value)
+{
+    if (!hasAttr(group))
+        throw ArgumentError(*this, "CompilationDescriptor", "invalid", "Trying to set pass arguments for a non-existent group (" + group + ")");
+
+    Element& g = get<Element>(group);
+
+    if (!g.hasAttr(pass))
+        throw RuntimeError(*this, "Trying to set arguments for a non-existent pass (" + pass + ") in group (" + group + ")");
+
+    Element& p = g.get<Element>(pass);
     p.set(arg, value);
 }
 
-mv::Attribute mv::CompilationDescriptor::getPassArg(const std::string& pass, const std::string& arg)
+mv::Attribute mv::CompilationDescriptor::getPassArg(const std::string& group, const std::string& pass, const std::string& arg)
 {
-    if (!hasAttr(pass))
-        throw ArgumentError(*this, "CompilationDescriptor", "invalid", "Trying to get arguments from a non-existent pass (" + pass + ")");
+    if (!hasAttr(group))
+        throw ArgumentError(*this, "CompilationDescriptor", "invalid", "Trying to get arguments from a non-existent group (" + pass + ")");
 
-    Element& p = get<Element>(pass);
+    auto& g = get<Element>(group);
+    if (!g.hasAttr(pass))
+        throw RuntimeError(*this, "Trying to retrieve arguments for a non-existent pass (" + pass + ") in group (" + group + ")");
+
+    auto& p = g.get<Element>(pass);
+
+    if (!p.hasAttr(arg))
+        throw RuntimeError(*this, "Trying to retrieve arg value for a non-existent argument (" + arg + ")");
 
     return p.get(arg);
 }
 
-void mv::CompilationDescriptor::serializePassListInGroup(const std::string& group, std::vector<std::string> &serializedPasses)
+void mv::CompilationDescriptor::serializePassListInGroup(const std::string& group, std::vector<Element *>& serializedPasses)
 {
     if (!hasAttr(group))
         throw ArgumentError(*this, "CompilationDescriptor", "invalid", "Trying to serialize passes in a non-existent group (" + group + ")");
 
     Element &elem = get<mv::Element>(group);
 
-    std::vector<std::string> recurrentPasses;
+    std::vector<Element *> recurrentPasses;
     if (elem.hasAttr("Recurrent"))
     {
         std::vector<std::string> &recurrent_group = elem.get<std::vector<std::string>>("Recurrent");
-        for (auto g: recurrent_group)
+        for (auto rec_elem: recurrent_group)
         {
-            if (isGroup(g))
-                serializePassListInGroup(g, recurrentPasses);
+            if (isGroup(rec_elem))
+                serializePassListInGroup(rec_elem, recurrentPasses);
             else
-                recurrentPasses.push_back(g);
+            {
+                auto& pass = elem.get<Element>(rec_elem);
+                recurrentPasses.push_back(&pass);
+            }
         }
     }
 
     if (elem.hasAttr("Singular"))
     {
         std::vector<std::string> &singular_group = elem.get<std::vector<std::string>>("Singular");
-        for (auto g: singular_group)
+        for (auto sing_elem: singular_group)
         {
-            if (isGroup(g))
-                serializePassListInGroup(g, serializedPasses);
+            if (isGroup(sing_elem))
+                serializePassListInGroup(sing_elem, serializedPasses);
             else
-                serializedPasses.push_back(g);
+            {
+                auto& pass = elem.get<Element>(sing_elem);
+                serializedPasses.push_back(&pass);
+            }
 
             if (!recurrentPasses.empty())
                 serializedPasses.insert(serializedPasses.end(), recurrentPasses.begin(), recurrentPasses.end());
@@ -145,15 +274,20 @@ void mv::CompilationDescriptor::serializePassListInGroup(const std::string& grou
     }
 }
 
-std::vector<std::string> mv::CompilationDescriptor::serializePassList()
+std::vector<mv::Element> mv::CompilationDescriptor::serializePassList()
 {
 
     if (!hasAttr("root"))
         throw RuntimeError(*this, "Unable to find root group, cannot serialize passes in the descriptor.");
 
-    std::vector<std::string> serializedPasses;
+    std::vector<Element *> serialized;
 
-    serializePassListInGroup("root", serializedPasses);
+    serializePassListInGroup("root", serialized);
+
+    std::vector<Element> serializedPasses;
+    for (auto p: serialized) {
+        serializedPasses.push_back(*p);
+    }
 
     return serializedPasses;
 
--- tests/compiler/compilation_descriptor.cpp
+++ tests/compiler/compilation_descriptor.cpp
@@ -61,48 +61,69 @@ TEST(compilation_descriptor, bare)
     compDesc.addToGroup("testGroup1", "FuseScale", "Recurrent", false);
 
     compDesc.addGroup("testGroup2");
+    compDesc.addToGroup("testGroup2", "AllocateUnpopulatedTensors", "Singular", false);
     compDesc.addToGroup("testGroup2", "FuseBias", "Singular", false);
-    compDesc.addToGroup("testGroup2", "FuseScale", "Singular", false);
+    compDesc.addToGroup("testGroup2", "AllocateInputOutputTensors", "Singular", false);
     compDesc.addToGroup("testGroup2", "GenerateDot", "Singular", false);
 
     compDesc.addGroup("root");
     compDesc.addToGroup("root", "ConvolutionDilation", "Singular", false);
     compDesc.addToGroup("root", "testGroup1", "Recurrent", true);
 
-    std::vector<std::string> expectedPassList = { "ConvolutionDilation",
+    std::vector<std::string> expectedPassList = {
+                "ConvolutionDilation",
                 "FuseBatchNorm",
                 "FuseBias",
                 "FuseScale",
+                "AllocateUnpopulatedTensors",
                 "FuseBias",
-                "FuseScale",
+                "AllocateInputOutputTensors",
                 "GenerateDot",
                 "FuseBias",
                 "FuseScale" };
 
-    std::vector<std::string> passList = compDesc.serializePassList();
+    auto getPassListString = [](std::vector<mv::Element> inList) -> std::vector<std::string> {
+        std::vector<std::string> outList;
+        for (auto p: inList)
+            outList.push_back(p.getName());
 
-    ASSERT_EQ(expectedPassList, passList);
+        return outList;
+    };
 
-    // Test pass arguments
-    // String argument
+    std::vector<mv::Element> passList = compDesc.serializePassList();
+    std::vector<std::string> passListStr = getPassListString(passList);
+
+    ASSERT_EQ(expectedPassList, passListStr);
+
+    //// Test pass arguments
+    //// String argument
     std::string s1 = "/foo/bar";
     mv::Attribute p1 = s1;
     compDesc.setPassArg("GenerateDot", "p1", p1);
-    mv::Attribute r1 = compDesc.getPassArg("GenerateDot", "p1");
+    mv::Attribute r1 = compDesc.getPassArg("testGroup2", "GenerateDot", "p1");
     std::string p2 = r1.get<std::string>();
     ASSERT_EQ(p2, "/foo/bar");
+
+    // Retrieve pass argument from serialized pass list.
+    std::vector<mv::Element> plArg = compDesc.serializePassList();
+    for (auto p: plArg) {
+        if (p.getName() == "GenerateDot") {
+            std::string arg = p.get<std::string>("p1");
+            ASSERT_EQ(arg, s1);
+        }
+    }
 
     // Second argument to the same pass.
     mv::Attribute ia1 = 42;
     compDesc.setPassArg("GenerateDot", "ia1", ia1);
-    mv::Attribute ir = compDesc.getPassArg("GenerateDot", "ia1");
+    mv::Attribute ir = compDesc.getPassArg("testGroup2", "GenerateDot", "ia1");
     int ia2 = ir.get<int>();
     ASSERT_EQ(ia2, 42);
 
     // Double argument
     mv::Attribute da1 = 2.0;
     compDesc.setPassArg("ConvolutionDilation", "da1", da1);
-    mv::Attribute dr = compDesc.getPassArg("ConvolutionDilation", "da1");
+    mv::Attribute dr = compDesc.getPassArg("root", "ConvolutionDilation", "da1");
     double da2 = dr.get<double>();
     ASSERT_EQ(da2, 2.0);
 
@@ -110,8 +131,40 @@ TEST(compilation_descriptor, bare)
     std::vector<double> v1({1.0, 2.0, 3.0});
     mv::Attribute vda1 = v1;
     compDesc.setPassArg("ConvolutionDilation", "vda1", vda1);
-    mv::Attribute vda2 = compDesc.getPassArg("ConvolutionDilation", "vda1");
+    mv::Attribute vda2 = compDesc.getPassArg("root", "ConvolutionDilation", "vda1");
     auto vdr = vda2.get<std::vector<double>>();
     ASSERT_EQ(vdr, v1);
 
+    ////// Test group/pass removal //////
+    // Remove a pass/group within a group
+    compDesc.remove("testGroup2", "GenerateDot", "Singular");
+    std::vector<mv::Element> pl = compDesc.serializePassList();
+    std::vector<std::string> epl = {
+            "ConvolutionDilation",
+            "FuseBatchNorm",
+            "FuseBias",
+            "FuseScale",
+            "AllocateUnpopulatedTensors",
+            "FuseBias",
+            "AllocateInputOutputTensors",
+            "FuseBias",
+            "FuseScale" };
+    std::vector<std::string> pl_str = getPassListString(pl);
+    ASSERT_EQ(epl, pl_str);
+
+    // Remove entire group
+    compDesc.remove("testGroup2");
+    std::vector<mv::Element> pl2 = compDesc.serializePassList();
+    std::vector<std::string> epl2 = {
+            "ConvolutionDilation",
+            "FuseBatchNorm",
+            "FuseBias",
+            "FuseScale"
+        };
+    std::vector<std::string> pl2_str = getPassListString(pl2);
+    ASSERT_EQ(epl2, pl2_str);
+
+    // Clear entire descriptor
+    compDesc.clear();
+    ASSERT_EQ(compDesc.getNumGroups(), 0);
 }
