0ae378a95870753a40ab997be1d6f4fc913f0ffc
john.brady@intel.com <john.brady@intel.com>
2019-02-04 18:30:04 +0000

    started metis graph structure

--- src/pass/finalization/generate_workloads_pass.cpp
+++ src/pass/finalization/generate_workloads_pass.cpp
@@ -5,7 +5,10 @@
 #include "include/mcm/utils/custom_math.hpp"
 #include "include/mcm/utils/data_generator.hpp"
 #include "include/mcm/target/keembay/workloads.hpp"
+#include "include/mcm/graph/graph.hpp"
+
 #include <math.h>
+#include <metis.h>
 
 static void generateWorkloadsFcn(const mv::pass::PassEntry &, mv::ComputationModel &model, mv::TargetDescriptor &, mv::json::Object &, mv::json::Object &);
 
@@ -21,6 +24,133 @@ namespace mv
     }
 }
 
+
+struct MetisGraphStructure
+{
+    idx_t* xadj; // Indexes of starting points in adjacent array
+    idx_t* adjncy; // Adjacent vertices in consecutive index order
+
+    int m_nVertices;
+    int m_nEdges;
+    int m_xDim;
+    int m_yDim;
+
+    MetisGraphStructure(int nVertices, int nEdges, int xDim, int yDim) : m_nVertices(nVertices), m_nEdges(nEdges), m_xDim(xDim), m_yDim(yDim)  {
+        
+        xadj = new idx_t[nVertices + 1];
+        adjncy = new idx_t[2*nEdges];
+    }
+    
+    ~MetisGraphStructure() {
+        delete[] xadj;
+        delete[] adjncy;
+    } 
+};
+ 
+
+
+int gen_partion_graph(MetisGraphStructure& metisGraph) {
+    
+    std::vector<int> nodeNumbers  = mv::utils::generateSequence<int>(metisGraph.m_nVertices -1);
+    
+    // for (auto it = nodeNumbers.begin(); it != nodeNumbers.end(); it++) 
+    //     std::cout << *it << " " << std::endl; 
+    
+    int index = 0;
+    for (auto it = nodeNumbers.begin(); it != nodeNumbers.end(); it++) {
+
+        //int index = std::distance(nodeNumbers.begin(), it);
+        
+
+        //Node 0 special (top left)
+        if((*it%metisGraph.m_xDim == 0) && (*it == 0)) {
+            std::cout << "Left side Node - Node " << *it << " " << std::endl; 
+
+            metisGraph.adjncy[index] = *it + 1;
+            index++;
+            metisGraph.adjncy[index] = *it + (metisGraph.m_xDim); 
+            index++;
+        }
+  
+        //Intermediate left side node
+        if((*it%metisGraph.m_xDim == 0) && ((*it + metisGraph.m_xDim) < ((int)nodeNumbers.size() -1)) && ((*it) != 0)) {
+            std::cout << "Left side Node - Intermediate " << *it << " " << std::endl; 
+
+            metisGraph.adjncy[index] = *it - metisGraph.m_xDim;
+            index++;
+            metisGraph.adjncy[index] = *it + 1;
+            index++;
+            metisGraph.adjncy[index] = *it + (metisGraph.m_xDim); 
+            index++;
+        }
+
+        //End left side node
+        if((*it%metisGraph.m_xDim == 0) && ((*it + metisGraph.m_xDim) > ((int)nodeNumbers.size() -1)) && ((*it) != 0)) {
+            std::cout << "Left side Node - Intermediate " << *it << " " << std::endl; 
+
+            metisGraph.adjncy[index] = *it - metisGraph.m_xDim;
+            index++;
+            metisGraph.adjncy[index] = *it + 1;
+            index++;
+           
+        }
+
+        //Special node top right
+        if(((*it - 4)%metisGraph.m_xDim == 0) && ((*it - (*it -4)) == metisGraph.m_xDim -1) && ((*it-(metisGraph.m_xDim-1) == 0))) {
+            std::cout << "Special top right side Node " << *it << " " << std::endl; 
+
+            metisGraph.adjncy[index] = *it - 1;
+            index++;
+            metisGraph.adjncy[index] = *it + (metisGraph.m_xDim); 
+            index++;
+        }
+
+       //Intermediate right side node
+        if(((*it - 4)%metisGraph.m_xDim == 0) && ((*it - (*it -4)) == metisGraph.m_xDim -1)  && ((*it-(metisGraph.m_xDim-1) != 0))  && (*it %(nodeNumbers.size()-1) != 0)) {
+            std::cout << "Intermediate right side Node " << *it << " " << std::endl; 
+
+            metisGraph.adjncy[index] = *it - metisGraph.m_xDim;
+            index++;
+            metisGraph.adjncy[index] = *it - 1;
+            index++;
+            metisGraph.adjncy[index] = *it + (metisGraph.m_xDim); 
+            index++;
+        }
+
+        //Special node bottm right
+        if(((*it - 4)%metisGraph.m_xDim == 0) && ((*it - (*it -4)) == metisGraph.m_xDim -1) && (*it %(nodeNumbers.size()-1) == 0)) {
+            std::cout << "Special bottom right side Node " << *it << " " << std::endl; 
+
+            metisGraph.adjncy[index] = *it - (metisGraph.m_xDim); 
+            index++;
+            metisGraph.adjncy[index] = *it - 1;
+            index++;
+        }
+        
+        //Middle nodes
+        // if(((*it%metisGraph.m_xDim != 0) )) {
+        //     std::cout << "Middle node " << *it << " " << std::endl; 
+
+        //     metisGraph.adjncy[index] = *it - (metisGraph.m_xDim); 
+        //     index++;
+        //     metisGraph.adjncy[index] = *it - 1;
+        //     index++;
+        // }
+        
+    } 
+
+
+ for(int e = 0; e < 15; e++) {
+
+            std::cout << metisGraph.adjncy[e] << std::endl;
+        }
+     
+     return 0;
+    
+}
+
+    
+//Returns a number divided by 2 repeatly. Example maxSplitRange = 16 -> returns 16,8,4,2
 std::set<int> getSplitsFromRange(int maxSplitRange, int maxSplits = 50)
 {
     std::set<int> splits;
@@ -80,48 +210,67 @@ void generateWorkloadsFcn(const mv::pass::PassEntry &, mv::ComputationModel &mod
     int nClusters = 1;
     int nDPUxCluster = nDPU/nClusters;
     std::set<int> workloadsList;
+    std::pair <int,int> MPEMode (4, 4);
+
 
     for (auto opIt = om.getInput(); opIt != om.opEnd(); ++opIt)
     {
         if (opIt->getOpType() == "DPUTask")
         {
             std::cout << "Found DPUTask" << std::endl;
-
+ 
+            //Get output tensor
             auto outputTensor = opIt->getOutputTensor();
 
-            workloadsList = getNWorkloads(outputTensor, nDPUxCluster);
+            std::cout << "Tensor shape is " << outputTensor[0]->getShape().toString() << std::endl;
 
-            //Already began to calculate a pool of workloads as per PoC compiler but for ww09 
-            //Forcing number of workloads to be nDPU/nCluster round to nearest even number
-            auto nWorkloads = round(nDPUxCluster/2)*2; 
+            int tensorXDim = outputTensor[0]->get<mv::Shape>("shape")[0]; // why is dimension not full tensor size?
+            int tensorYDim = outputTensor[0]->get<mv::Shape>("shape")[1];
 
-            std::cout << "Number of workloads is " << nWorkloads << std::endl;
+            int numberTensorVertices = (tensorXDim  * tensorYDim) + 1;
+            int numberTensorEdges = (2 * tensorXDim * tensorYDim) - tensorXDim - tensorYDim + 2;
 
-            //Workload class
-            Workloads workloads(opIt->getName());
+            std::cout << "numberTensorVertices: " << numberTensorVertices << " " << "numberTensorEdges: " << numberTensorEdges << std::endl;
 
-            mv::Workload w1;   
-            w1.MinX = 0;
-            w1.MaxX = 15;
-            w1.MinY = 0;
-            w1.MaxY = 3;
-            w1.MinZ = 0;
-            w1.MaxZ = 15;
+            //Create Metis struct
+            MetisGraphStructure metisGraph(numberTensorVertices, numberTensorEdges, tensorXDim, tensorYDim);
 
-            mv::Workload w2;   
-            w2.MinX = 0;
-            w2.MaxX = 15;
-            w2.MinY = 0;
-            w2.MaxY = 3;
-            w2.MinZ = 0;
-            w2.MaxZ = 15;
+            gen_partion_graph(metisGraph); 
 
-            workloads.getWorkloads().push_back(w1);
-            workloads.getWorkloads().push_back(w2);
+
+            // workloadsList = getNWorkloads(outputTensor, nDPUxCluster);
+
+            // //Already began to calculate a pool of workloads as per PoC compiler but for ww09 
+            // //Forcing number of workloads to be nDPU/nCluster round to nearest even number
+            // auto nWorkloads = round(nDPUxCluster/2)*2; 
+
+            // std::cout << "Number of workloads is " << nWorkloads << std::endl;
+
+            // //Workload class
+            // Workloads workloads(opIt->getName());
+
+            // mv::Workload w1;   
+            // w1.MinX = 0;
+            // w1.MaxX = 15;
+            // w1.MinY = 0;
+            // w1.MaxY = 3;
+            // w1.MinZ = 0;
+            // w1.MaxZ = 15;
+
+            // mv::Workload w2;   
+            // w2.MinX = 0;
+            // w2.MaxX = 15;
+            // w2.MinY = 0;
+            // w2.MaxY = 3;
+            // w2.MinZ = 0;
+            // w2.MaxZ = 15;
+
+            // workloads.getWorkloads().push_back(w1);
+            // workloads.getWorkloads().push_back(w2);
             
-            opIt->set<mv::Workloads>("Workloads", workloads);
+            // opIt->set<mv::Workloads>("Workloads", workloads);
 
-            std::cout << opIt->toString();
+            // std::cout << opIt->toString();
 
         }
     }
