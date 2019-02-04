f74da5d7c669debcfb7b3af1a4d69ea214471aee
Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>
2019-02-04 11:40:36 -0800

    VPUNND-11: Refactor unit tests and examples to match compilation descriptor change
    All examples that use the compilation unit and pass arguments to
    the compilation descriptor have been refactored to use the new
    compilation descriptor.
    
    A default compilation descriptor added under config directory.
    This will need to be revised to match the pass list previously
    defined in the target descriptor.
    
    - IMPORTANT NOTES:
    	- GenerateCaffe pass is not included in the tests by default.
    	- Examples that require validation with GenerateCaffe are
    	  currently broken (will compile, but will encounter runtime
    	  errors).
    	- ScaleFission pass input argument parsing not tested.
    	- Python API doesn't import the compilation descriptor file.
    
    Signed-off-by: Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>

--- examples/standalone_softmax.cpp
+++ examples/standalone_softmax.cpp
@@ -16,17 +16,24 @@ int main()
     auto softmax = om.softmax(input);
     auto output = om.output(softmax);
 
+    unit.loadDefaultCompilationDescriptor();
+    mv::CompilationDescriptor &compDesc = unit.compilationDescriptor();
+
     std::string outputName = "test_softmax";
-    unit.compilationDescriptor()["GenerateBlob"]["fileName"] = std::string(outputName+".blob");
-    unit.compilationDescriptor()["GenerateBlob"]["enableFileOutput"] = true;
-    unit.compilationDescriptor()["GenerateBlob"]["enableRAMOutput"] = false;
-    unit.compilationDescriptor()["GenerateDot"]["output"] = std::string(outputName+".dot");
-    unit.compilationDescriptor()["GenerateDot"]["scope"] = std::string("OpControlModel");
-    unit.compilationDescriptor()["GenerateDot"]["content"] = std::string("full");
-    unit.compilationDescriptor()["GenerateDot"]["html"] = true;
-    unit.compilationDescriptor()["GenerateCaffe"]["outputPrototxt"] = std::string(outputName + ".prototxt");
-    unit.compilationDescriptor()["GenerateCaffe"]["outputCaffeModel"] = std::string(outputName + ".caffemodel");
-    unit.compilationDescriptor()["MarkHardwareOperations"]["disableHardware"] = true;
+    std::string blobName = outputName + ".blob";
+    compDesc.setPassArg("GenerateBlob", "fileName", blobName);
+    compDesc.setPassArg("GenerateBlob", "enableFileOutput", true);
+    compDesc.setPassArg("GenerateBlob", "enableRAMOutput", false);
+
+    compDesc.setPassArg("GenerateDot", "output", std::string("test_softmax.dot"));
+    compDesc.setPassArg("GenerateDot", "scope", std::string("OpControlModel"));
+    compDesc.setPassArg("GenerateDot", "content", std::string("full"));
+    compDesc.setPassArg("GenerateDot", "html", true);
+
+    compDesc.setPassArg("MarkHardwareOperations", "disableHardware", true);
+
+    // compDesc.setPassArg("GenerateCaffe", "outputPrototxt", std::string("test_softmax.prototxt"));
+    // compDesc.setPassArg("GenerateCaffe", "outputCaffeModel", std::string("test_softmax.caffemodel"));
 
     unit.loadTargetDescriptor(mv::Target::ma2480);
     unit.initialize();
