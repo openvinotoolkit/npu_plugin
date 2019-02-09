c474756b884857fe055de934ae374464dfb7eba3
Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>
2019-02-09 13:35:03 -0800

    VPUNND-14: Add method to create Element type attribute from simplified JSON typing
    Signed-off-by: Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>

--- src/base/attribute_def/mv_element.cpp
+++ src/base/attribute_def/mv_element.cpp
@@ -22,6 +22,14 @@ namespace mv
             return Element(v.get<json::Object>());
         }
 
+        static Attribute fromSimplifiedJSON(const json::Value& v)
+        {
+            if (v.valueType() != json::JSONType::Object)
+                throw AttributeError(v, "Unable to convert JSON value of type " + json::Value::typeName(v.valueType()) +
+                    " to mv::Element");
+            return Element(v.get<json::Object>(), true);
+        }
+
         static std::string toString(const Attribute& a)
         {
             return a.get<Element>().toString();
@@ -30,6 +38,7 @@ namespace mv
         MV_REGISTER_ATTR(Element)
             .setToJSONFunc(toJSON)
             .setFromJSONFunc(fromJSON)
+            .setFromSimplifiedJSONFunc(fromSimplifiedJSON)
             .setToStringFunc(toString);
 
     }
