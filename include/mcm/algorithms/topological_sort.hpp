e4a37beb83bffe507990a511c91980ab6bd678d3
Marco Mecchia <marco.mecchia@intel.com>
2019-03-27 17:24:20 +0000

    Implemented topological sort with maps of iterators.

--- include/mcm/algorithms/topological_sort.hpp
+++ include/mcm/algorithms/topological_sort.hpp
@@ -8,16 +8,24 @@
 
 namespace mv
 {
-
-    template <typename T_node, typename T_edge>
-    void visit(typename graph<T_node, T_edge>::node_list_iterator root, std::set<T_node>& unmarkedNodes, std::vector<typename graph<T_node, T_edge>::node_list_iterator>& toReturn, graph<T_node, T_edge>& g)
+    template <typename NodeIterator>
+    struct OpItComparatorTemplate2
     {
-        if(unmarkedNodes.find(*root) == unmarkedNodes.end())
+        bool operator()(NodeIterator lhs, NodeIterator rhs) const
+        {
+            return (*lhs) < (*rhs);
+        }
+    };
+
+    template <typename T_node, typename T_edge, typename OpIteratorComp>
+    void visit(typename graph<T_node, T_edge>::node_list_iterator root, std::set<typename graph<T_node, T_edge>::node_list_iterator, OpIteratorComp>& unmarkedNodes, std::vector<typename graph<T_node, T_edge>::node_list_iterator>& toReturn, graph<T_node, T_edge>& g)
+    {
+        if(unmarkedNodes.find(root) == unmarkedNodes.end())
             return;
         for(auto neighbour = root->leftmost_child(); neighbour != g.node_end(); ++neighbour)
             visit(neighbour, unmarkedNodes, toReturn, g);
 
-        unmarkedNodes.erase(*root);
+        unmarkedNodes.erase(root);
         toReturn.push_back(root);
     }
 
@@ -27,15 +35,14 @@ namespace mv
     {
         std::vector<typename graph<T_node, T_edge>::node_list_iterator> toReturn;
 
-        std::set<T_node> unmarkedNodes;
+        std::set<typename graph<T_node, T_edge>::node_list_iterator, OpItComparatorTemplate2<typename graph<T_node, T_edge>::node_list_iterator>> unmarkedNodes;
         for(auto node = g.node_begin(); node != g.node_end(); ++node)
-            unmarkedNodes.insert(*node);
+            unmarkedNodes.insert(node);
 
         while(!unmarkedNodes.empty())
         {
-            auto toVisitElement = unmarkedNodes.begin();
-            auto toVisit = g.node_find(*toVisitElement);
-            visit(toVisit, unmarkedNodes, toReturn, g);
+            auto toVisit = unmarkedNodes.begin();
+            visit(*toVisit, unmarkedNodes, toReturn, g);
         }
 
         std::reverse(toReturn.begin(), toReturn.end());
