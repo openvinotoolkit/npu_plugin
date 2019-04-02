ca9db38e58f20c7e531e05adefbced47e58ba4fd
Skvortsov, Vladimir <vladimir.skvortsov@intel.com>
2019-04-02 11:06:06 +0300

    Depthwise test corrected

--- tests/op/depthwiseConv2D.cpp
+++ tests/op/depthwiseConv2D.cpp
@@ -6,15 +6,16 @@ TEST(ops, depthconv2D)
 {
 
     mv::OpModel om("testModel");
-    auto input = om.input({32, 32, 1}, mv::DType("Float16"), mv::Order("CHW"));
-    std::vector<double> weightsData({1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f});
-    auto weights1 = om.constant(weightsData, {3, 3, 1, 1}, mv::DType("Float16"), mv::Order(mv::Order::getColMajorID(4)));
+    auto input = om.input({32, 32, 2}, mv::DType("Float16"), mv::Order("CHW"));
+    std::vector<double> weightsData({1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f,
+                                     8.0f, 7.0f, 6.0f, 5.0f, 4.0f, 3.0f, 2.0f, 1.0f, 0.0f});
+    auto weights1 = om.constant(weightsData, {3, 3, 2, 1}, mv::DType("Float16"), mv::Order(mv::Order::getColMajorID(4)));
     auto conv = om.depthwiseConv(input, weights1, {4, 4}, {1, 1, 1, 1});
     auto convOp = om.getSourceOp(conv);
     auto output = om.output(conv);
 
 
-    ASSERT_EQ(conv->getShape(), mv::Shape({8, 8, 1}));
+    ASSERT_EQ(conv->getShape(), mv::Shape({8, 8, 2}));
     ASSERT_EQ(convOp->getOpType(), "DepthwiseConv");
     ASSERT_EQ(convOp->attrsCount(), 5);
     auto s0 = convOp->get<std::array<unsigned short, 2>>("stride")[0];
