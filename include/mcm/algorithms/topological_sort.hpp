7f72272302f171f296bf5e58fe28d25ed3836a40
Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>
2019-04-07 11:43:02 -0700

    Perform lexicographical topological sort
    Signed-off-by: Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>

--- include/mcm/algorithms/topological_sort.hpp
+++ include/mcm/algorithms/topological_sort.hpp
@@ -18,13 +18,39 @@ namespace mv
         }
     };
 
+    template <typename NodeIterator>
+    struct OpItGreaterThanOperator
+    {
+        bool operator()(NodeIterator lhs, NodeIterator rhs) const
+        {
+            return !((*lhs) < (*rhs));
+        }
+    };
+
     template <typename T_node, typename T_edge, typename OpIteratorComp>
-    void visit(typename graph<T_node, T_edge>::node_list_iterator root, std::set<typename graph<T_node, T_edge>::node_list_iterator, OpIteratorComp>& unmarkedNodes, std::vector<typename graph<T_node, T_edge>::node_list_iterator>& toReturn, graph<T_node, T_edge>& g)
+    void visit(typename graph<T_node, T_edge>::node_list_iterator root,
+        std::set<typename graph<T_node, T_edge>::node_list_iterator,
+        OpIteratorComp>& unmarkedNodes,
+        std::vector<typename graph<T_node, T_edge>::node_list_iterator>& toReturn,
+        graph<T_node, T_edge>& g)
     {
         if(unmarkedNodes.find(root) == unmarkedNodes.end())
             return;
+
+        // lexicographical topological sort
+        std::vector<typename graph<T_node, T_edge>::node_list_iterator> sortedNbrs;
         for(auto neighbour = root->leftmost_child(); neighbour != g.node_end(); ++neighbour)
-            visit(neighbour, unmarkedNodes, toReturn, g);
+        {
+            sortedNbrs.push_back(neighbour);
+        }
+
+        OpItGreaterThanOperator<typename graph<T_node, T_edge>::node_list_iterator> op;
+        std::sort(sortedNbrs.begin(), sortedNbrs.end(), op);
+
+        for (auto nbr: sortedNbrs)
+        {
+            visit(nbr, unmarkedNodes, toReturn, g);
+        }
 
         unmarkedNodes.erase(root);
         toReturn.push_back(root);
