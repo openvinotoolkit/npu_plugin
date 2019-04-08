5279a371b9ed97f5c5524b78ef5aab3a8810be28
Stanislaw Maciag <stanislaw.maciag@intel.com>
2019-04-08 14:53:12 +0100

    Styling adjustment

--- include/mcm/algorithms/is_dag.hpp
+++ include/mcm/algorithms/is_dag.hpp
@@ -5,16 +5,22 @@
 #include <unordered_map>
 namespace mv
 {
-    // is_DAG() checks if the graph is acyclic (no cycles).
-    // Algorithm used is similar to the one in python networkX https://networkx.github.io/documentation/networkx-1.9/_modules/networkx/algorithms/dag.html#is_directed_acyclic_graph
-    // for each of the nodes of graph, using recursive DFS check for back edges (backedges get back to the node already explored)
+    /*  
+        is_DAG() checks if the graph is acyclic (no cycles).
+        Algorithm used is similar to the one in python networkX
+        https://networkx.github.io/documentation/networkx-1.9/_modules/networkx/algorithms/dag.html#is_directed_acyclic_graph
+        for each of the nodes of graph, using recursive DFS check for back edges
+        (backedges get back to the node already explored)
+    */
 
     template <typename T_node, typename T_edge>
     bool is_DAG(graph<T_node, T_edge>& g)
     {
-        // 'explored array' stores if the node is explored. recurStack stores the state of the stack (which node being processed)
-          std::unordered_map<int,bool> explored;
-          std::unordered_map<int,bool> recurStack;
+
+        // Stores if the node is explored
+        std::unordered_map<int,bool> explored;
+        // Stores the state of the stack (which node being processed)
+        std::unordered_map<int,bool> recurStack;
           
         for (auto it = g.node_begin(); it != g.node_end(); ++it)
         {
@@ -25,15 +31,20 @@ namespace mv
         for (auto it = g.node_begin(); it != g.node_end(); ++it)
             if (hasCycle(g, it, explored, recurStack))
                   return false;
+
         return true;
+        
     }
 
     // hasCycle returns if there are cycles by doing DFS
     template <typename T_node, typename T_edge>
-    bool hasCycle(graph<T_node, T_edge>&g, typename graph<T_node, T_edge>::node_list_iterator it, std::unordered_map<int, bool>& explored, std::unordered_map<int,bool>& recurStack)
+    bool hasCycle(graph<T_node, T_edge>&g, typename graph<T_node, T_edge>::node_list_iterator it,
+        std::unordered_map<int, bool>& explored, std::unordered_map<int,bool>& recurStack)
     {
+
         int64_t v = it->getID();
-        // below node_list_iterator object needed as the hasCycle method takes these objects only. so 'child iterator' gets masked as node_list_iterator and passed
+        // below node_list_iterator object needed as the hasCycle method takes these objects only.
+        // So 'child iterator' gets masked as node_list_iterator and passed
         typename graph<T_node, T_edge>::node_list_iterator it1;
 
         if(!explored[v])
@@ -55,11 +66,14 @@ namespace mv
                 return true;
 
         }
+
         // update recursion stack by removing the node (from the stack)
         recurStack[v] = false;
 
         return false;
+
     }
+    
 }
 
 #endif
