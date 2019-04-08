5e19fc3660613f25534e024489e38a6ad4bf8e10
tbartsok <tasos.bartsokas@intel.com>
2019-04-08 17:45:31 +0100

    Add quantization on matmul as POC

--- src/computation/op/def/matmul.cpp
+++ src/computation/op/def/matmul.cpp
@@ -39,16 +39,28 @@ namespace mv
                 
         static std::function<void(const std::vector<Data::TensorIterator>&, const std::map<std::string, Attribute>&, 
             std::vector<Tensor>&)> outputDefFcn =
-            [](const std::vector<Data::TensorIterator>& inputs, const std::map<std::string, Attribute>&, std::vector<Tensor>& outputs)
+            [](const std::vector<Data::TensorIterator>& inputs, const std::map<std::string, Attribute>& args, std::vector<Tensor>& outputs)
         {
 
-            outputs.push_back(mv::Tensor(":0", {inputs[0]->getShape()[0], inputs[1]->getShape()[1]}, inputs[0]->getDType(), inputs[0]->getOrder()));
+            auto zero_point = args.at("zero_point").get<std::vector<unsigned>>();
+            auto scale = args.at("scale").get<std::vector<double>>();
+            auto min = args.at("min").get<std::vector<double>>();
+            auto max = args.at("max").get<std::vector<double>>();
+            outputs.push_back(mv::Tensor(":0", {inputs[0]->getShape()[0], inputs[1]->getShape()[1]}, inputs[0]->getDType(), inputs[0]->getOrder(), zero_point, scale,
+                min, max));
 
         };
     
+        static std::vector<unsigned> empty_un;
+        static std::vector<double> empty_d;
+
         MV_REGISTER_OP(MatMul)
         .setInputs({"data0", "data1"})
         .setOutputs({"output"})
+        .setOptionalArg<std::vector<unsigned>>("zero_point", empty_un)
+        .setOptionalArg<std::vector<double>>("scale", empty_d)
+        .setOptionalArg<std::vector<double>>("min", empty_d)
+        .setOptionalArg<std::vector<double>>("max", empty_d)
         .setInputCheck(inputCheckFcn)
         .setOutputDef(outputDefFcn)
         .setTypeTrait({"executable", "exposed"});
