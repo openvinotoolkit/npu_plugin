7f4f83da5854d89d736f71ef51c2fd1cccbdeca5
tbartsok <tasos.bartsokas@intel.com>
2019-04-09 14:17:55 +0100

    Refresh googletests

--- tests/mcm_unit_tests.cpp
+++ tests/mcm_unit_tests.cpp
@@ -2,8 +2,7 @@
 
 int main(int argc, char **argv)
 {
-//    ::testing::InitGoogleTest(&argc, argv);
-    ::testing::GTEST_FLAG(filter)="quantization.case_conv";
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
 
