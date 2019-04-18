bf5dece2f918adc123cb7284cfcabc99bda57dba
Marco Mecchia <marco.mecchia@intel.com>
2019-04-18 15:03:55 +0100

    Fixed binary data missing serialization.

--- src/target/keembay/runtime_model/runtime_model.cpp
+++ src/target/keembay/runtime_model/runtime_model.cpp
@@ -645,7 +645,7 @@ void mv::RuntimeModel::buildGraphFile(ComputationModel& cm, mv::Element& compila
     {
         auto allocators = tensorIt->get<std::set<std::string>>("allocators");
         if (allocators.count("GraphFile") != 0)
-            buildBinaryDataT(cm, compilationDescriptor, *tensorIt);
+            graphFile_.binary_data.push_back(buildBinaryDataT(cm, compilationDescriptor, *tensorIt));
     }
 
 }
