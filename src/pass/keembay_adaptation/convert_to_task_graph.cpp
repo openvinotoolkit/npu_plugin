8747585325bfcbf3b08986aa99b049d684a913f2
Marco Mecchia <marco.mecchia@intel.com>
2019-05-07 19:28:34 +0100

    Added void PPETasks to Conv and Pooling.

--- src/pass/keembay_adaptation/convert_to_task_graph.cpp
+++ src/pass/keembay_adaptation/convert_to_task_graph.cpp
@@ -93,6 +93,10 @@ void convertOpsToTasksFcn(const mv::pass::PassEntry& , mv::ComputationModel& mod
                 }
             }
 
+            auto ppeFixedFunction = mv::PPEFixedFunction();
+            auto ppeTask = mv::PPETask(ppeFixedFunction);
+            dpuConvOp->set<mv::PPETask>("PPETask", ppeTask);
+
             storeSplitStrategy(om, opIt, dpuConvOp);
 
             adaptOutputDataFlow(om, opIt, dpuConv);
@@ -116,6 +120,10 @@ void convertOpsToTasksFcn(const mv::pass::PassEntry& , mv::ComputationModel& mod
             auto dpuPoolOp = om.getSourceOp(dpuPool);
             dpuPoolOp->set<unsigned>("opId", opId);
             dpuPoolOp->set<bool>("hasWeights", false);
+
+            auto ppeFixedFunction = mv::PPEFixedFunction();
+            auto ppeTask = mv::PPETask(ppeFixedFunction);
+            dpuPoolOp->set<mv::PPETask>("PPETask", ppeTask);
 
             storeSplitStrategy(om, opIt, dpuPoolOp);
 
