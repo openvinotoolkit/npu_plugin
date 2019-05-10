1b417c2adbb1a9ad71a1184c5b3e9366e7c78d4c
aleckey <alec.leckey@gmail.com>
2019-05-10 09:24:05 +0100

    Pushed initial version of individual regression tests
    Signed-off-by: aleckey <alec.leckey@gmail.com>

---
+++ tests/ci_tests/mobilenet_layer_tests/t_cong_mobilenet_dwconv6.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+sys.path.append("..")
+
+from test_helper import *
+
+
+net_folder = "network_csvs/"
+sch_folder = "POC_config_csvs/"
+root_file = "cong_mobilenet_dwconv6"
+
+model = generate_model(net_folder+root_file+"_MIG.csv")
+graphfile, s_result, _ = compile_graphFile(model, 1, 1, sch_folder+"SOK_POC.csv")
+t_result = execute_network(graphfile)
+validate_files(s_result, t_result)
