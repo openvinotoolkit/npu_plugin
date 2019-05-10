3adfdd6fac8311b2a3bb73e9f5247450013195e8
aleckey <alec.leckey@gmail.com>
2019-05-10 14:46:02 +0100

    Updated comments to switch between prod/poc runtimes
    Signed-off-by: aleckey <alec.leckey@gmail.com>

--- tests/ci_tests/test_helper.py
+++ tests/ci_tests/test_helper.py
@@ -14,8 +14,9 @@ MDK_ROOT = os.environ["MDK_HOME"]
 NZOO_ROOT = os.environ["NZOO_ROOT"]
 POC_ROOT = MDK_ROOT+'/projects/Fathom/'
 
-APP_ROOT = MDK_ROOT + "/testApps/keembay/tapeout_so/tc_keembay_ma2490/ts_nce_bm_blob/"  # PoC Runtime
-#APP_ROOT = MDK_ROOT + "/testApps/components/NeuralNet/008_demo_ncelib_LNN/conv_3l_16wi/" # Prod Runtime
+# which runtime do you want to use? (PoC/Production)
+#APP_ROOT = MDK_ROOT + "/testApps/keembay/tapeout_so/tc_keembay_ma2490/ts_nce_bm_blob/"  # PoC Runtime
+APP_ROOT = MDK_ROOT + "/testApps/components/NeuralNet/008_demo_ncelib_LNN/conv_3l_16wi/" # Prod Runtime
 
 PWD = os.path.dirname(os.path.realpath(__file__)) + "/"
 CWD = os.getcwd() + "/"
