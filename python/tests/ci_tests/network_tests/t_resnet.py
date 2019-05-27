099615049bfd9bee09991fbce834daadd8989bba
aleckey <alec.leckey@gmail.com>
2019-05-27 15:22:46 +0100

    Merge latest commits from Fathom suite of CI tests
    Signed-off-by: aleckey <alec.leckey@gmail.com>

--- python/tests/ci_tests/network_tests/t_resnet.py
+++ python/tests/ci_tests/network_tests/t_resnet.py
@@ -6,14 +6,16 @@ from test_helper import *
 
 skip()
 
+
 net_folder = "network_csvs/"
 sch_folder = "POC_config_csvs/"
 
 root_file = "test_resnet"
 
-model = generate_model(net_folder+root_file+"_MIG.csv", network=True)
-graphfile, _, _ = compile_graphFile(model, 4, 5, sch_folder+"ResNet50_POC.csv", emulator=False)
-_, s_result, _ = compile_graphFile(model, 1, 1, sch_folder+"simple_POC.csv", cmx=MAX_UINT32)
+p = os.path.dirname(os.path.realpath(__file__))
+
+model = generate_model(os.path.join(p,net_folder)+root_file+".py")
+graphfile, s_result, _ = compile_graphFile(model, 4, 5, os.path.join(p, "../../../config/resnet50_strategy.csv"))
 
 t_result = execute_network(graphfile)
 validate_files(s_result, t_result)
--- python/tests/ci_tests/test_helper.py
+++ python/tests/ci_tests/test_helper.py
@@ -8,6 +8,13 @@ MAX_UINT32 = 4294967295
 RESNET_CMX = 4*918
 CONG_CMX = 4096** 1024
 
+# Error Code Guide
+RESULT_SUCCESS  = 0
+FAIL_GENERAL    = 1
+FAIL_RESERVED   = 2
+FAIL_COMPILER   = 3  # Compiler fails to create graphfile or emulator files
+FAIL_RUNTIME    = 4  # Runtime fails to generate results file (NCE2Task_network_out.bin, output.dat)
+FAIL_VALIDATION = 5  # Correctness Error
 
 # Internal Defines
 MDK_ROOT = os.environ["MDK_HOME"]
@@ -25,24 +32,48 @@ CWD = os.getcwd() + "/"
 header = lambda x : "========= " + x + " ========="
 
 def skip():
-    if os.getenv('RUN_FAILED', '0') == '0':
+    if os.getenv('RUN_FAILED', '0') == '0' or os.getenv('RUN_FAILED', '0') == '':
         sys.exit(123)
 
-def generate_model(csv_file, network=False):   
-    print(header("Generate Model")) 
+def generate_model(csv_file, network=False, simplify=False, overwrite=True):
+    print(header("Generate Model"))
     sys.path.insert(0, NZOO_ROOT + '/tools/kmb_test_generation')
-    from run_testfile import main as genModel 
+    from run_testfile import main as genModel
 
     class arg():
-        def __init__(self, f, network):
+        def __init__(self, f, network, simplify=True):
             self.input_file=os.path.abspath(f)
             self.network=network
-            self.simplify=False
+            self.simplify=simplify
             self.output_name="graph"
 
-    a = arg(csv_file, network)
-    model = genModel(a)
-    
+    if (".py" in csv_file):
+        # This isn't a Csv file!
+        print("Python")
+        cmd = ["python3" , csv_file]
+        print(cmd)
+        code = subprocess.check_output(cmd)
+        print("\n\nPath:", code)
+
+        code = str(code.decode('ascii'))
+        print("not b",code)
+        m = re.search("FILE:(.+)", code)
+        if m:
+            found = m.group(1)
+            print("found:", found)
+            return found
+        else:
+            print("Did not detect string with prefix 'FILE' from .py")
+            sys.exit(7) # Unsupported
+
+    a = arg(csv_file, network, simplify=False)
+    if overwrite:
+        model = genModel(a)
+    else:
+        # re-use the existing model from previous run (due to random weights)
+        model = generate_model_path(a)
+
+    print ("Model: ", model)
     return model
 
 def checkExists(file, errorcode=1, aliases=[]):
@@ -60,8 +91,8 @@ def compile_graphFile(model, nClusters, nDPU, strategyFile, cmx=4*918, emulator=
         extra = " with Emulation"
     else:
         extra = ""
-    print(header("Compile" + extra)) 
-   
+    print(header("Compile" + extra))
+
     sys.path.insert(0, POC_ROOT+"src2/")
     from Fathom import main as genGraph
     from Models.EnumDeclarations import OperationMode, Parser
@@ -79,9 +110,9 @@ def compile_graphFile(model, nClusters, nDPU, strategyFile, cmx=4*918, emulator=
             ddr = 4*1024
             set_layout = [
                 (0, 1, 2, 3),
-                (0, 2, 1, 3), 
-                (0, 2, 3, 1), 
-                (0, 3, 2, 1), 
+                (0, 2, 1, 3),
+                (0, 2, 3, 1),
+                (0, 3, 2, 1),
                 (0, 1, 3, 2)
             ]
 
@@ -114,7 +145,7 @@ def compile_graphFile(model, nClusters, nDPU, strategyFile, cmx=4*918, emulator=
             self.verbose=False
             self.class_test_threshold=0.2
             self.kmb=True
-            self.blob_name= "vpu2.blob"
+            self.blob_name="vpu2.blob"
             self.outputs_name=None
             self.outputs_location=CWD+"output/"
             self.channel_swap=None
@@ -152,28 +183,24 @@ def compile_graphFile(model, nClusters, nDPU, strategyFile, cmx=4*918, emulator=
     genGraph = "out"
 
     graphFile = out_folder+"vpu2.blob"   # Ideally returned from Fathom
-    if (cpp==True):
-        graphFile_mcm = out_folder+"blob.bin"
     sim_result = out_folder+"expected_result_sim.dat"   # Ideally returned from Fathom
     # sim_result = out_folder+"../Fathom_simulation.npy"
     fw_result = out_folder+"../Fathom_expected.npy"
 
     if emulator:
-        checkExists(sim_result, 4)
-    checkExists(graphFile)
-    if (cpp==True):
-        return graphFile, sim_result, fw_result, graphFile_mcm
-    else:
-        return graphFile, sim_result, fw_result
+        checkExists(sim_result, FAIL_COMPILER)
+    checkExists(graphFile, FAIL_COMPILER)
+
+    return graphFile, sim_result, fw_result
 
 def execute_network(gf):
-    print(header("Execute Network")) 
-    checkExists(gf, 4)
+    print(header("Execute Network"))
+    checkExists(gf, FAIL_COMPILER)
 
     from shutil import copyfile
 
     # clean generated files
-    generated_files = ["blob.bin", "vpu2.blob", "input.dat", "NCE2Task_network_out.bin", "output.dat", "expected_result_sim.dat"]
+    generated_files = ["vpu2.blob", "input.dat", "NCE2Task_network_out.bin", "output.dat", "expected_result_sim.dat"]
     for file in generated_files:
         try:
             os.remove(APP_ROOT + file)
@@ -206,7 +233,7 @@ def execute_network(gf):
         sIP=' srvIP=iirfpga' + str(os.getenv('FPGA')) + ".ir.intel.com"
         sPort=""
 
-    command = "make run MV_SOC_REV=ma2490 MV_TOOLS_VERSION="+mvToolsV+" GRAPH_BIN=vpu2.blob GRAPH_BIN_PATH=. "+sPort + sIP 
+    command = "make run -j MV_SOC_REV=ma2490 MV_TOOLS_VERSION="+mvToolsV+" GRAPH_BIN=vpu2.blob GRAPH_BIN_PATH=. "+sPort + sIP
 
     print(">>>>>>>>>>>>", command)
     code = subprocess.run(command, shell=True, stderr=subprocess.STDOUT, cwd=APP_ROOT)
@@ -218,80 +245,32 @@ def execute_network(gf):
     result_file = APP_ROOT + "NCE2Task_network_out.bin"
     result_file_alt = [APP_ROOT + "NCE2Task0_out.bin", APP_ROOT + "output.dat"]  # Prod runtime uses "output.dat"
 
-    result_file = checkExists(result_file, 3, aliases=result_file_alt)
-
-    return result_file
-
-def execute_network_mcm(gf):
-    print(header("Execute Network")) 
-    checkExists(gf, 4)
-
-    from shutil import copyfile
-
-    # clean generated files
-    generated_files = ["vpu2.blob", "input.dat", "NCE2Task_network_out.bin", "output.dat", "expected_result_sim.dat"]
-    for file in generated_files:
-        try:
-            os.remove(APP_ROOT + file)
-        except FileNotFoundError:
-            pass
-
-    # directories - rmdir only deletes empty directories, it won't delete recursively
-    try:
-        from shutil import rmtree
-        rmtree(APP_ROOT + "output", ignore_errors=True)
-    except Exception:
-        pass
-
-    print("Copy:", gf, "to", APP_ROOT + "blob.bin")
-    copyfile(gf, APP_ROOT + "blob.bin")
-    print("Copy:", CWD+"output/input.dat", "to", APP_ROOT + "input.dat")
-    copyfile(CWD+"output/input.dat", APP_ROOT + "input.dat")
-    print("Copy:", CWD + "output/expected_result_sim.dat", "to", APP_ROOT + "expected_result_sim.dat")
-    copyfile(CWD + "output/expected_result_sim.dat", APP_ROOT + "expected_result_sim.dat")
-
-    moviSimPort=os.getenv('MOVISIM_PORT','30001')
-    mvToolsV=os.getenv('MV_TOOLS_VERSION','Latest_195458')
-
-    sPort = " srvPort=" + moviSimPort
-    sIP = ""
-    fpga = os.getenv('FPGA', 'None')
-    print("FPGA: \'", fpga,"\'", type(fpga))
-    if fpga != 'None' and fpga is not None:
-        sIP=' srvIP=iirfpga' + str(os.getenv('FPGA')) + ".ir.intel.com"
-        sPort=""
-
-    command = "make run MV_SOC_REV=ma2490 MV_TOOLS_VERSION="+mvToolsV+" GRAPH_BIN=blob.bin GRAPH_BIN_PATH=. "+sPort + sIP 
-
-    print(">>>>>>>>>>>>", command)
-    code = subprocess.run(command, shell=True, stderr=subprocess.STDOUT, cwd=APP_ROOT)
-
-    if code.returncode != 0:
-        print("Execution Failure")
-        exit(code.returncode)
-
-    result_file = APP_ROOT + "NCE2Task_network_out.bin"
-    result_file_alt = [APP_ROOT + "NCE2Task0_out.bin", APP_ROOT + "output.dat"]  # Prod runtime uses "output.dat"
-
-    result_file = checkExists(result_file, 3, aliases=result_file_alt)
+    result_file = checkExists(result_file, FAIL_RUNTIME, aliases=result_file_alt)
 
     return result_file
 
 def validate_files(ref, test):
 
-    checkExists(ref, 3)
-    checkExists(test, 4)
+    checkExists(ref, FAIL_COMPILER)
+    checkExists(test, FAIL_RUNTIME)
 
     result = True
 
-    print(header("Validate Result")) 
+    print(header("Validate Result"))
 
     command = "python3 "+POC_ROOT+"src2/Validate.py --reference "+ref+" --testdata "+ test + " --dtype u8"
 
     code = subprocess.run(command, shell=True, stderr=subprocess.STDOUT)
-    
+
     if code.returncode != 0:
         result = False
+
+    # dump first few lines of the results files
+    for f in [ref, test]:
+        print("Dump of ", f)
+        with open(f, "rb") as bf:
+            for i in range(0, 2):  # lines to dump
+                print("{:02X} ".format(i * 32), " ".join("{:02X}".format(x) for x in bf.read(32)))  # number of bytes to dump/line
 
     import zlib
     def crc(fileName):
@@ -304,8 +283,27 @@ def validate_files(ref, test):
     print("CRC RESULT: ", crc(test))
 
     if crc(ref) == crc(test):
-        # sys.exit(0)
-        return 0
+        sys.exit(RESULT_SUCCESS)
     else:
-        return 5
-        # sys.exit(5)
+        sys.exit(FAIL_VALIDATION)
+
+    return result
+
+
+def generate_model_path(args):
+    # loads the csv file and creates the model path
+    model_name = "/quantized_model.tflite"
+    if (args.simplify):
+        model_name = "/simplified_model.tflite"
+
+    folder_name = args.output_name
+    if not args.network:
+        import csv
+        with open(args.input_file, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                folder_name = row["Name"].strip()
+                break
+
+    model = NZOO_ROOT + "/internal/unit_tests/CompilerTestsKeembay/layers/" + folder_name + model_name
+    return model
