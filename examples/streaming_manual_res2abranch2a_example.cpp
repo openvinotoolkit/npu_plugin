b04e94cc5dca54c66125f45ae4e1534334423b9d
Eman Copty <eman.copty@intel.com>
2019-05-29 18:28:55 -0700

    Update params and input files

--- examples/streaming_manual_res2abranch2a_example.cpp
+++ examples/streaming_manual_res2abranch2a_example.cpp
@@ -27,7 +27,7 @@ int main()
 
     mv::CompilationUnit unit("parserModel");
     mv::OpModel& om = unit.model();
-    auto input0 = om.input({56,56,64,1}, mv::DType("UInt8"), mv::Order::getZMajorID(4), {{128},{0.007843137718737125},{-1.0},{1.0}}, "input#3");
+    auto input0 = om.input({56,56,64,1}, mv::DType("UInt8"), mv::Order::getZMajorID(4),  {{128},{0.007843137718737125},{-1.0},{1.0}}, "input#3");
 
     std::vector<int64_t> weightsData0 = read_weights_from_file<int64_t>(path + "/examples/data/res2a_branch2a_weights#1.dat");
     std::vector<mv::Data::TensorIterator> weights(NUM_SPLITS);
@@ -49,21 +49,21 @@ int main()
         std::copy(itrBegin, itrEnd, back_inserter(weightsData[i]));
         if (NUM_SPLITS > 1)
             name_suffix = std::to_string(i);
-        weights[i] = om.constantInt(weightsData[i],{1,1,64,64/NUM_SPLITS}, mv::DType("UInt8"), mv::Order::getZMajorID(4), {{129},{0.002591547090560198},{-0.3342282176017761},{0.3266163170337677}}, "res2a_branch2a_weights#1" + name_suffix);
-        convs[i] = om.conv(input0, weights[i],  {1, 1}, {0, 0, 0, 0}, 1, 1, {{128},{0.007843137718737125},{-1.003921627998352},{0.9960784316062927}}, "res2a_branch2a#4" + name_suffix);
+        weights[i] = om.constantInt(weightsData[i],{1,1,64,64/NUM_SPLITS}, mv::DType("UInt8"), mv::Order::getZMajorID(4), {{130},{0.0029303126502782106},{-0.38163304328918457},{0.3655967116355896}}, "res2a_branch2a_weights#1" + name_suffix);
+        convs[i] = om.conv(input0, weights[i],  {1, 1}, {0, 0, 0, 0}, 1, 1,  {{128},{0.007843137718737125},{-1.003921627998352},{0.9960784316062927}}, "res2a_branch2a#4" + name_suffix);
 
         biasData[i].reserve(biasSizePerSplit);
         itrBegin = biasWeightsData0.begin() + i * biasSizePerSplit;
         itrEnd = itrBegin + biasSizePerSplit;
         std::copy(itrBegin, itrEnd, back_inserter(biasData[i]));
-        biases[i] = om.constantInt(biasData[i],{64/NUM_SPLITS}, mv::DType("UInt8"), mv::Order::getColMajorID(1), {{0},{2.0325860532466322e-05},{-inf},{inf}}, "res2a_branch2a_bias#2weights" + name_suffix);
+        biases[i] = om.constantInt(biasData[i],{64/NUM_SPLITS}, mv::DType("UInt8"), mv::Order::getColMajorID(1),  {{0},{2.298284562129993e-05},{-inf},{inf}}, "res2a_branch2a_bias#2weights" + name_suffix);
         biasesOp[i] = om.bias(convs[i], biases[i], {{128},{0.007843137718737125},{-1.003921627998352},{0.9960784316062927}},  "res2a_branch2a_bias#2" + name_suffix);
     }
 
     if (NUM_SPLITS > 1)
     {
         //TODO need to quantize this layer but since it's not a DPU task for now I am passing the shift/mult
-        auto concat = om.concat(biasesOp, "C", {{128},{0.007843137718737125},{-1.003921627998352},{0.9960784316062927}, {23}, {21739}});
+        auto concat = om.concat(biasesOp, "C", {{128},{0.007843137718737125},{-1.003921627998352},{0.9960784316062927}, {23}, {24581}});
         auto output = om.output(concat);
     }
     else
