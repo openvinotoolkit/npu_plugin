d4da16ac9b99a599158406c28008a924660c57ad
john.brady@intel.com <john.brady@intel.com>
2019-06-03 15:16:10 +0100

    Removing strategy for workload generation

--- examples/dpu_conv_2_conv.cpp
+++ examples/dpu_conv_2_conv.cpp
@@ -14,55 +14,29 @@
 
 int main()
 {
-mv::Logger::setVerboseLevel(mv::VerboseLevel::Debug);
+    mv::Logger::setVerboseLevel(mv::VerboseLevel::Info);
     mv::CompilationUnit unit("testModel");
     mv::OpModel& om = unit.model();
 
-    auto input = om.input({7, 7, 64, 1}, mv::DType("UInt8"), mv::Order::getZMajorID(4), {{},{},{},{}}, "input#3");
-    std::vector<int64_t> weightsData = mv::utils::generateSequence<int64_t>(1*1*64*64);
-    auto weights = om.constantInt(weightsData, {1, 1, 64, 64}, mv::DType("UInt8"), mv::Order::getZMajorID(4), {{}, {}, {}, {}}, "res4a_branch2a_weights");
-    auto conv = om.conv(input, weights, {1, 1}, {0, 0, 0, 0}, 1, 1, {{},{},{},{}}, "res4a_branch2a#4");
+    auto input = om.input({16, 16, 15, 1}, mv::DType("UInt8"), mv::Order::getZMajorID(4), {{},{},{},{}}, "input#3");
 
-    std::vector<int64_t> biasWeightsData = mv::utils::generateSequence<int64_t>(64);
-    auto biasWeights = om.constantInt(biasWeightsData, {64}, mv::DType("UInt8"), mv::Order::getColMajorID(1));
+    std::vector<double> weightsData = mv::utils::generateSequence<double>(1*1*15*15);
+    auto weights = om.constant(weightsData, {1, 1, 15, 15}, mv::DType("Float16"), mv::Order("NCWH"));
+    auto conv = om.conv(input, weights, {1, 1}, {0, 0, 0, 0});
 
-    auto bias = om.bias(conv, biasWeights);
+    std::vector<double> weightsData1 = mv::utils::generateSequence<double>(1*1*15*15);
+    auto weights1 = om.constant(weightsData1, {1, 1, 15, 15}, mv::DType("Float16"), mv::Order("NCWH"));
+    auto conv1 = om.conv(conv, weights1, {1, 1}, {0, 0, 0, 0});
 
-    om.output(bias);
+    om.output(conv1);
 
     std::string compDescPath = mv::utils::projectRootPath() + "/config/compilation/debug_ma2490.json";
     unit.loadCompilationDescriptor(compDescPath);
-    mv::CompilationDescriptor &compDesc = unit.compilationDescriptor();
-    compDesc.setPassArg("GenerateDot", "scope", std::string("ControlModel"));
+    //compDesc.setPassArg("GenerateDot", "scope", std::string("ControlModel"));
 
     unit.loadTargetDescriptor(mv::Target::ma2490);
     unit.initialize();
-unit.run();
-
-
-    // mv::Logger::setVerboseLevel(mv::VerboseLevel::Info);
-    // mv::CompilationUnit unit("testModel");
-    // mv::OpModel& om = unit.model();
-
-    // auto input = om.input({16, 16, 15, 1}, mv::DType("UInt8"), mv::Order::getZMajorID(4), {{},{},{},{}}, "input#3");
-
-    // std::vector<double> weightsData = mv::utils::generateSequence<double>(1*1*15*15);
-    // auto weights = om.constant(weightsData, {1, 1, 15, 15}, mv::DType("Float16"), mv::Order("NCWH"));
-    // auto conv = om.conv(input, weights, {1, 1}, {0, 0, 0, 0});
-
-    // std::vector<double> weightsData1 = mv::utils::generateSequence<double>(1*1*15*15);
-    // auto weights1 = om.constant(weightsData1, {1, 1, 15, 15}, mv::DType("Float16"), mv::Order("NCWH"));
-    // auto conv1 = om.conv(conv, weights1, {1, 1}, {0, 0, 0, 0});
-
-    // om.output(conv1);
-
-    // std::string compDescPath = mv::utils::projectRootPath() + "/config/compilation/debug_ma2490.json";
-    // unit.loadCompilationDescriptor(compDescPath);
-    // //compDesc.setPassArg("GenerateDot", "scope", std::string("ControlModel"));
-
-    // unit.loadTargetDescriptor(mv::Target::ma2490);
-    // unit.initialize();
-    // unit.run();
+    unit.run();
 
     system("dot -Tpng original_model.dot -o original_model.png");
     system("dot -Tpng adapt_model.dot -o adapt_model.png");
