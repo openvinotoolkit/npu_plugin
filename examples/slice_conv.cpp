ef24cd523e70c429f31ffb7376f54277d8564574
Szilveszter Istvan <istvan.szilveszter@movidius.com>
2019-06-07 22:54:54 +0100

    fixing test

--- examples/slice_conv.cpp
+++ examples/slice_conv.cpp
@@ -21,8 +21,8 @@ template <typename T> std::vector<T> read_weights_from_file(std::string input_fi
 
 int main()
 {
-    mv::Logger::setVerboseLevel(mv::VerboseLevel::Warning);
-    mv::Logger::setVerboseLevel(mv::VerboseLevel::Info);
+//    mv::Logger::setVerboseLevel(mv::VerboseLevel::Warning);
+//    mv::Logger::setVerboseLevel(mv::VerboseLevel::Info);
     mv::CompilationUnit unit("parserModel");
     mv::OpModel& om = unit.model();
 
@@ -38,22 +38,22 @@ int main()
                             {0,0,0,0},
                             {16,16,16,1},
                             emptyQuantParams,
-                            "slice0_cmx");
+                            "slice0_cmx_");
     auto slice1 = om.slice(input,
                             {16,0,0,0},
                             {16,16,16,1},
                             emptyQuantParams,
-                            "slice1_cmx");
+                            "slice1_cmx_");
     std::vector<int64_t> weightsData = mv::utils::generateSequence<int64_t>(16*16);
     auto weights = om.constantInt(weightsData,{1,1,16,16},mv::DType("UInt8"),mv::Order::getZMajorID(4),emptyQuantParams,"weights");
-    auto conv0 = om.conv(slice0,weights,{1,1},{0,0,0,0},1,1,emptyQuantParams,"conv0");
-    auto conv1 = om.conv(slice1,weights,{1,1},{0,0,0,0},1,1,emptyQuantParams,"conv1");
+    auto conv0 = om.conv(slice0,weights,{1,1},{0,0,0,0},1,1,emptyQuantParams,"conv0_cmx_");
+    auto conv1 = om.conv(slice1,weights,{1,1},{0,0,0,0},1,1,emptyQuantParams,"conv1_cmx_");
 
     std::vector<mv::Data::TensorIterator> convs(2);
 
     convs[0] = conv0;
     convs[1] = conv1;
-    auto concat = om.concat(convs,"W",emptyQuantParams,"concat");
+    auto concat = om.concat(convs,"W",emptyQuantParams,"concat_ddr_");
     auto output = om.output(concat);
 
     std::string compDescPath = mv::utils::projectRootPath() + "/config/compilation/debug_ma2490_streaming.json";
