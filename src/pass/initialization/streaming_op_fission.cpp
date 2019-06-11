bea700da0992ec01ca9dd507a91b5ecc415ee881
Eman Copty <eman.copty@intel.com>
2019-06-11 16:42:43 +0100

    OpFission pass: move it after fuse passes, and fix issues
    - add splitting of bias
    - fix quant param setting for Concat
    - move pass after fuse passes in json file
    - remove un-needed pass from JSON

--- src/pass/initialization/streaming_op_fission.cpp
+++ src/pass/initialization/streaming_op_fission.cpp
@@ -8,6 +8,9 @@
 #include "include/mcm/algorithms/edge_exists.hpp"
 #include <algorithm>
 #include "include/mcm/logger/logger.hpp"
+#include "include/mcm/utils/custom_math.hpp"
+#include "include/mcm/utils/custom_strings.hpp"
+
 
 static void streamingOpFissionFcn(const mv::pass::PassEntry& pass, mv::ComputationModel& model, mv::TargetDescriptor&, mv::Element&, mv::json::Object&);
 
@@ -34,6 +37,7 @@ void streamingOpFissionFcn(const mv::pass::PassEntry& pass, mv::ComputationModel
     // original graph
     mv::OpModel om(model);
     mv::ControlModel cm(model);
+    mv::DataModel dm(model);
 
     // get ops to split and number of splits from descriptor
     auto globalParams = model.getGlobalConfigParams();
@@ -49,9 +53,14 @@ void streamingOpFissionFcn(const mv::pass::PassEntry& pass, mv::ComputationModel
     for (auto s: strategyList)
     {
         std::string nodeName = s.get<std::string>("name_filter") ;
-// TODO add heuristic to determine numsplits if no json
+        // TODO add heuristic to determine numsplits if no json
+        // TODO is the ops only for Conv? If not then we need cases for each Op
+        // or functions per Op type to allow copying passing its attribute on
+
         int32_t numWtSplits = s.get<int32_t>("weight_splits") ;
         auto opToFracture = om.getOp(nodeName);
+        auto opType = opToFracture->getOpType();
+        unsigned currentOpId = opToFracture->get<unsigned>("opId");
         std::cout << "IN STREAMING PASS: splitting op " << nodeName << " into " << numWtSplits << std::endl ;
         auto OpAttrDF = opToFracture->get("dilationFactor");
         auto OpAttrGrp = opToFracture->get("group");
@@ -74,64 +83,101 @@ void streamingOpFissionFcn(const mv::pass::PassEntry& pass, mv::ComputationModel
         int kWtSize = originalWeights.size()/numKernels;
         std::vector<mv::Data::TensorIterator> opsToJoin(numWtSplits) ;     // resulting ops after fission
 
-        int startIndex = 0;
-        int endIndex = 0;
-        for (int wi=0; wi<numWtSplits; wi++)
+        size_t startIndex = 0;
+        size_t endIndex = 0;
+        size_t biasStartIndex = 0;
+        size_t biasEndIndex = 0;
+        size_t numKernelsPerSplit = mv::ceil_division(numKernels, numWtSplits);
+        size_t remainingKernels = numKernels;
+        for (size_t wi=0; wi<numWtSplits; wi++)
         {
-            int subWtSize = originalWeights.size();
-            shapeArray[3]=numKernels/numWtSplits;
-            // for odd splits, first modulo wts tensors will be larger by 1 K-size
-            if (wi<numKernels % numWtSplits)
-            {
-                subWtSize = kWtSize * (numKernels/numWtSplits+1);
-                shapeArray[3]=numKernels/numWtSplits+1; 
-            }
-            // even splits or rest of odd wts tensors after modulus
-            else
-            {
-                subWtSize = kWtSize * (numKernels/numWtSplits);
-                shapeArray[3]=numKernels/numWtSplits;
-            }
+            size_t subWtSize = originalWeights.size();
+
+            //How many kernels in this sub-Op
+            numKernelsPerSplit = std::min(numKernelsPerSplit, remainingKernels);
+            remainingKernels -= numKernelsPerSplit;
+
+            shapeArray[3] = numKernelsPerSplit;
+            subWtSize = kWtSize * numKernelsPerSplit;
 
             startIndex = endIndex;
-            endIndex=startIndex+subWtSize ;
+            endIndex = startIndex + subWtSize ;
             if ( endIndex > originalWeights.size())
             {
                 endIndex = originalWeights.size();
-            } 
+            }
             std::cout << "In STREAMING PASS: wtssize beg,end = " << subWtSize << "   " << startIndex << "," << endIndex << std::endl ;
             std::vector<mv::DataElement>::const_iterator first = originalWeights.begin() + startIndex;
             std::vector<mv::DataElement>::const_iterator last = originalWeights.begin() + endIndex;
             std::vector<mv::DataElement> subWt(first, last);
-            std::string newName = nodeName+"_"+std::to_string(wi);
-            auto WtsAttrDType = originalWtsTensor->get("dType");
-            auto WtsAttrShape = originalWtsTensor->get("shape");
-// TODO generalize tensor shape, order (assumes wts N=shape(3) )
-            auto WtsAttrOrder = originalWtsTensor->get("order");
+
+            std::string newName = nodeName+std::to_string(wi);
+
+            // TODO generalize tensor shape, order (assumes wts N=shape(3) )
             mv::Data::TensorIterator weightsX ;
-            if (parentTensor->hasAttr("quantParams"))
+
+            if (originalWtsTensor->hasAttr("quantParams"))
             {
-                auto WtsAttrQPs = parentTensor->get("quantParams");
-                weightsX = om.constantDataElement(subWt, shapeArray, WtsAttrDType, WtsAttrOrder, WtsAttrQPs, newName+"wts");
+                auto WtsAttrQPs = originalWtsTensor->get("quantParams");
+                weightsX = om.constantDataElement(subWt, shapeArray, WtsAttrDType, WtsAttrOrder, WtsAttrQPs, newName+"weights#1");
             }
             else
             {
-                weightsX = om.constantDataElement(subWt, shapeArray,  WtsAttrDType, WtsAttrOrder, {{}, {}, {}, {}}, newName+"wts");
+                weightsX = om.constantDataElement(subWt, shapeArray,  WtsAttrDType, WtsAttrOrder, {{}, {}, {}, {}}, newName+"weights#1");
             }
+            auto originalOpId = om.getSourceOp(originalWtsTensor)->get<unsigned>("opId");
+            om.getSourceOp(weightsX)->set<unsigned>("opId", originalOpId);
             auto convX = om.conv(parentTensor, weightsX, OpAttrStride, OpAttrPad, OpAttrDF, OpAttrGrp, OpAttrQPs, newName);
 
             if (opToFracture->hasAttr("bias"))
             {
+                biasStartIndex = biasEndIndex;
+                biasEndIndex = biasStartIndex + numKernelsPerSplit;
+
                 auto biasTensorName = opToFracture->get<std::string>("bias");
-                om.addAttr(om.getSourceOp(convX), "bias", biasTensorName);
+                auto originalBiasTensor = dm.getTensor(biasTensorName);
+                auto oiginalBiasData = originalBiasTensor->getData();
+                if ( biasEndIndex > oiginalBiasData.size())
+                {
+                    biasEndIndex = oiginalBiasData.size();
+                }
+                std::vector<mv::DataElement>::const_iterator biasFirst = oiginalBiasData.begin() + biasStartIndex;
+                std::vector<mv::DataElement>::const_iterator biasLast = oiginalBiasData.begin() + biasEndIndex;
+                std::vector<mv::DataElement> subBiasData(biasFirst, biasLast);
+
+                std::string newBiasTensorName = mv::createBiasName(newName);
+                mv::Data::TensorIterator biasTensor;
+
+                mv::Data::TensorIterator biasTensorX;
+                if (originalBiasTensor->hasAttr("quantParams"))
+                {
+                    auto biasAttrQPs = originalBiasTensor->get("quantParams");
+                    biasTensorX = dm.defineTensor(mv::Tensor(newBiasTensorName, {numKernelsPerSplit}, originalBiasTensor->getDType(), originalBiasTensor->getOrder(), subBiasData, biasAttrQPs ));
+                }
+                else
+                {
+                    biasTensorX = dm.defineTensor(mv::Tensor(newBiasTensorName, {numKernelsPerSplit}, originalBiasTensor->getDType(), originalBiasTensor->getOrder(), subBiasData));
+                }
+
+                om.addAttr(om.getSourceOp(convX), "bias", biasTensorX->getName());
                 pass.log(mv::Logger::MessageType::Info, "Copied Bias attribute of Fractured conv " + opToFracture->getName() + " to " + convX->getName());
             }
-            
+
+            om.getSourceOp(convX)->set<unsigned>("opId", currentOpId);
+
             opsToJoin[wi] = convX ;
         }
-        auto subGraphOut = om.concat(opsToJoin);
-//       auto subGraphOut = om.concat(opsToJoin,"C", {{128},{0.007843137718737125},{-1.003921627998352},{0.9960784316062927}, {23}, {24581}});
-
+        mv::Data::TensorIterator subGraphOut;
+        if (opToFracture->hasAttr("quantParams"))
+        {
+            subGraphOut = om.concat(opsToJoin,"C", opToFracture->get("quantParams"));
+        }
+        else
+        {
+            subGraphOut = om.concat(opsToJoin,"C");
+        }
+        //TODO consider moving the pass till after "ComputeTensorQuantParams" then concat will get the quantization params for output tensor
+        om.getSourceOp(subGraphOut)->set<unsigned>("opId", currentOpId);
         // Create lists of children ops, and the input slot used into each
         std::vector<mv::Data::OpListIterator> opsToLink;
         std::vector<std::size_t> inputSlots;
