d2f3697fe09c16a3c9340325f880d819c1854566
askrebko <artemy.skrebkov@intel.com>
2019-06-12 00:39:02 +0300

    Update header with config options
      - To add a proper licence
      - To include vpu_plugin_config.hpp to avoid possible double definition

--- include/vpu/kmb_plugin_config.hpp
+++ include/vpu/kmb_plugin_config.hpp
@@ -1,6 +1,19 @@
-// Copyright (C) 2018-2019 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
 //
+// Copyright 2019 Intel Corporation.
+//
+// This software and the related documents are Intel copyrighted materials,
+// and your use of them is governed by the express license under which they
+// were provided to you (End User License Agreement for the Intel(R) Software
+// Development Products (Version May 2017)). Unless the License provides
+// otherwise, you may not use, modify, copy, publish, distribute, disclose or
+// transmit this software or the related documents without Intel's prior
+// written permission.
+//
+// This software and the related documents are provided as is, with no
+// express or implied warranties, other than those that are expressly
+// stated in the License.
+//
+
 
 /**
  * @brief A header that defines advanced related properties for VPU plugins.
@@ -11,20 +24,7 @@
 
 #pragma once
 
-#include <string>
-
-#include "ie_plugin_config.hpp"
-#include "ie_api.h"
-
-//
-// Common options
-//
-
-#define VPU_CONFIG_KEY(name) InferenceEngine::VPUConfigParams::_CONFIG_KEY(VPU_##name)
-#define VPU_CONFIG_VALUE(name) InferenceEngine::VPUConfigParams::VPU_##name
-
-#define DECLARE_VPU_CONFIG_KEY(name) DECLARE_CONFIG_KEY(VPU_##name)
-#define DECLARE_VPU_CONFIG_VALUE(name) DECLARE_CONFIG_VALUE(VPU_##name)
+#include <vpu/vpu_plugin_config.hpp>
 
 //
 // KMB plugin options
