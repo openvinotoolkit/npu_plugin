86a83b221f11f254ae91d3e77835a62482563c76
aleckey <alec.leckey@gmail.com>
2019-06-21 15:46:50 +0100

    Working version that builds/installs lemon locally

--- cmake-modules/FindLEMON.cmake
+++ cmake-modules/FindLEMON.cmake
@@ -38,12 +38,6 @@ IF(EXISTS "${LEMON_DIR}" AND NOT "${LEMON_DIR}" STREQUAL "")
   
   # SET(LEMON_INCLUDE_DIR ${LEMON_DIR})
   FIND_LIBRARY(LEMON_LIBRARY NAMES Lemon lemon emon libemon)
-
-  # locate Lemon libraries
-  IF(DEFINED LEMON_LIBRARY)
-    SET(LEMON_LIBRARIES ${LEMON_LIBRARY})
-  ENDIF()
-
   MESSAGE(STATUS "Found Lemon ${LEMON_VERSION} (include: ${LEMON_DIR})")
 ELSE()
   # Lemon not installed. Download external project and install. Mercurial required.
@@ -52,10 +46,10 @@ ELSE()
     message( FATAL_ERROR "Mercurial client (hg) requred to download Lemon sources. Please install 'Mercurial'. Exiting...")
   endif()
 
-  MESSAGE(STATUS "Lemon not found. Downloading...")
+  MESSAGE(STATUS "Lemon not found. Will download as External Project and install locally.")
   include(ExternalProject)
-  set(LEMON_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/contrib/lemon)
-  set(LEMON_INSTALL_DIR "../contrib/lemon/install")
+  set(LEMON_SOURCE_DIR "${PROJECT_SOURCE_DIR}/contrib/lemon")
+  set(LEMON_INSTALL_DIR "${PROJECT_SOURCE_DIR}/contrib/lemon/install")
   ExternalProject_Add(
       project_lemon
       HG_REPOSITORY     http://lemon.cs.elte.hu/hg/lemon 
@@ -65,6 +59,9 @@ ELSE()
       INSTALL_COMMAND   make DESTDIR=${LEMON_INSTALL_DIR} install
       VERBATIM
   )
+  SET(LEMON_INCLUDE_DIR "${LEMON_INSTALL_DIR}/usr/local/include")
+  SET(LEMON_LIBRARY ${LEMON_INSTALL_DIR}/usr/local/lib/libemon.a)
+
   SET(LEMON_FOUND TRUE)
 
 ENDIF()
