3604b1dbf3b2bdb57943701868592634f3035a94
Alexey Tarakanov <alexey.tarakanov@intel.com>
2019-07-10 16:10:52 +0300

    Remove deprecated API from Behavior Tests

--- tests/behavior/shared_tests_instances/plugin_tests/vpu_test_data.hpp
+++ tests/behavior/shared_tests_instances/plugin_tests/vpu_test_data.hpp
@@ -40,8 +40,6 @@ const std::vector<BehTestParams> withCorrectConfValues = {
     BEH_KMB.withConfig({ { KEY_VPU_COPY_OPTIMIZATION, NO } }),
     BEH_KMB.withConfig({ { KEY_VPU_LOG_LEVEL, LOG_DEBUG } }),
     BEH_KMB.withConfig({ { KEY_VPU_IGNORE_UNKNOWN_LAYERS, YES } }),
-    BEH_KMB.withConfig({ { KEY_VPU_INPUT_NORM, "0.5" } }),
-    BEH_KMB.withConfig({ { KEY_VPU_INPUT_BIAS, "0.1" } }),
     BEH_KMB.withConfig({ { KEY_VPU_HW_STAGES_OPTIMIZATION, YES } }),
     BEH_KMB.withConfig({ { KEY_VPU_NONE_LAYERS, "Tile" } }),
     BEH_KMB.withConfig({ { KEY_VPU_HW_ADAPTIVE_MODE, "YES" } }),
@@ -59,6 +57,5 @@ const std::vector<BehTestParams> withCorrectConfValuesNetworkOnly = {
 const BehTestParams withIncorrectConfValues[] = {
     BEH_KMB.withConfig({ { KEY_VPU_COPY_OPTIMIZATION, "ON" } }),
     BEH_KMB.withConfig({ { KEY_VPU_LOG_LEVEL, "VERBOSE" } }),
-    BEH_KMB.withConfig({ { KEY_VPU_INPUT_NORM, "0.0" } }),
     BEH_KMB.withConfig({ { KEY_VPU_IGNORE_UNKNOWN_LAYERS, "ON" } }),
 };
