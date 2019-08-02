2d03ce0c3671a1804d5fd8bc1d03a8c00a8503bb
Marco Mecchia <marco.mecchia@intel.com>
2019-08-02 12:56:57 +0100

    Removed obsolete POC condition

--- src/pass/keembay_adaptation/sparsity/sparsity_maps.cpp
+++ src/pass/keembay_adaptation/sparsity/sparsity_maps.cpp
@@ -190,8 +190,8 @@ static void generateSparsityMapsPopulatedTensorsFcn(const mv::pass::PassEntry& p
                 //Here only in the case of ZMajorConvolution
 
                 // POC ONLY CONDITION
-                if(!dpuTask->getInputTensor(0)->isSparse())
-                    continue;
+                //if(!dpuTask->getInputTensor(0)->isSparse())
+                    //continue;
 
                 auto weightsTensor = dpuTask->getInputTensor(1);
                 weightsTensor->setOrder(mv::Order("NHWC"));
