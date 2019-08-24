a288f024fac00bb357ea4027bc57335c22a81ce6
Eman Copty <eman.copty@intel.com>
2019-08-24 07:57:38 -0700

    Remove none useful debug print

--- src/target/keembay/koala_graph_scheduler.cpp
+++ src/target/keembay/koala_graph_scheduler.cpp
@@ -425,7 +425,7 @@ std::pair<int,std::vector<mv::koalaGraph::PEdge>> mv::KoalaGraphScheduler::calcu
         
         for (int k = 0; k < resInputToSource.edgeNo; k++) {
 
-            pass.log(mv::Logger::MessageType::Debug, shortestPathEdges[k]->info.name);
+            //pass.log(mv::Logger::MessageType::Debug, shortestPathEdges[k]->info.name);
 
             /*Add Fmax to the flow attribute of the edge*/
             auto edge = lookUpKoalaEdgebyName(shortestPathEdges[k]->info.name, this->edges_);
