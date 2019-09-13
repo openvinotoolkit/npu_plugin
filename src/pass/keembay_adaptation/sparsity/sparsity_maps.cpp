cb40d3b053869dc233e6412efe2e93ac758711a4
Marco Mecchia <marco.mecchia@intel.com>
2019-09-13 15:09:52 +0100

    Enforcing no activation sparsity in case of SOK.

--- src/pass/keembay_adaptation/sparsity/sparsity_maps.cpp
+++ src/pass/keembay_adaptation/sparsity/sparsity_maps.cpp
@@ -252,9 +252,18 @@ static void generateSparsityMapsUnpopulatedTensorsFcn(const mv::pass::PassEntry&
 
         bool activationSparsity = sink->hasAttr("activationSparsity") ? sink->get<bool>("activationSparsity") : false;
 
+        // Conditions for activation sparsity
+        // 1) The source of the activation tensor must be a DPUTask (Otherwise there's no ODU to write SM, SE)
+        // 2) The sink of the activation tensor must be a ZMajor Convolution, the only operation
+        //    with an IDU capable of handling sparsity data
+        // 3) Runtime doesn't support SOK and activation sparsity
         if(activationSparsity)
         {
-            if(!tensor->isPopulated() && source->getOpType() == "DPUTask" && sink->getOpType() == "DPUTask" && sink->get<std::string>("taskOp") == "Conv")
+            if(!tensor->isPopulated() &&
+                source->getOpType() == "DPUTask" &&
+                sink->getOpType() == "DPUTask" &&
+                sink->get<std::string>("taskOp") == "Conv" &&
+                sink->get<std::string>("splitStrategy") != "SplitOverK")
                 tensor->setSparse();
         }
         else
