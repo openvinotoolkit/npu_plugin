42bc2997fc5c6ca6a60cb3d115cd21e45b347c94
Marco Mecchia <marco.mecchia@intel.com>
2019-09-18 18:26:10 +0100

    Updated z_major_convolution example to include a UPA layer

--- example/z_major_convolution.cpp
+++ example/z_major_convolution.cpp
@@ -14,15 +14,17 @@ int main()
 
     mv::CompilationUnit unit("parserModel");
     mv::OpModel& om = unit.model();
-    auto input0 = om.input({12,12,32,1}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "input:0#3");
+    auto input0 = om.input({32,32,32,1}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "input:0#4");
 
     std::vector<double> weightsData0 = mv::utils::generateSequence<double> (1*1*32*64);
-    auto weights0 = om.constant(weightsData0,{1,1,32,64}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "output/Conv2D#1_weights#2");
-    auto conv0 = om.conv(input0, weights0, {1, 1}, {0, 0, 0, 0}, 1, 1, mv::DType("Float64"), {{0},{1.0},{-inf},{inf}}, "output/Conv2D:0#4");
+    auto weights0 = om.constant(weightsData0,{1,1,32,64}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "conv1/Conv2D#1_weights#2");
+    auto conv0 = om.conv(input0, weights0, {1, 1}, {0, 0, 0, 0}, 1, 1, mv::DType("Float64"), {{0},{1.0},{-inf},{inf}}, "conv1/Conv2D:0#5");
 
-    om.output(conv0);
+    auto identity0 = om.identity(conv0, mv::DType("Float64"), {{0},{1.0},{-inf},{inf}}, "output:0#6");
 
-    std::string compDescPath = "/home/tbartsok/Desktop/WORK/mcmCompiler/config/compilation/debug_ma2490.json";
+    om.output(identity0);
+
+    std::string compDescPath = "/home/mmecchia/WORK/mcmCompiler/config/compilation/debug_ma2490.json";
     unit.loadCompilationDescriptor(compDescPath);
 
     unit.loadTargetDescriptor(mv::Target::ma2490);
