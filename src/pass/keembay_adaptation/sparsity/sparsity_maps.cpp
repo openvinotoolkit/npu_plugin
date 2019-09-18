6a6a2764340595d79e878223228197b629a915e4
tbartsok <tasos.bartsokas@intel.com>
2019-09-18 12:02:26 +0100

    Fixes for dwise,ch_maj,maxpool mpe_mode max = 4

--- src/pass/keembay_adaptation/sparsity/sparsity_maps.cpp
+++ src/pass/keembay_adaptation/sparsity/sparsity_maps.cpp
@@ -41,25 +41,44 @@ mv::Data::TensorIterator createFakeSparsityMap(mv::OpModel om, mv::Data::OpListI
     return sparsityMap;
 }
 
-uint16_t getWindowSize(uint16_t kx, uint16_t sx)
+uint16_t getWindowSize(uint16_t kx, uint16_t sx, mv::DType dataType)
 {
     //Find max mpe where if calc window <= 32
     //return window size for the max mpe
     uint16_t windowSize, maxMpeWindowSize = 64;
     int mpe = 1;
 
-    //mpe in [1,2,4,8,16]
-    while(mpe <= 16)
+    if (dataType == mv::DType("UInt8"))
     {
-        if (sx <= kx)
-            windowSize = kx + sx * (mpe - 1);
-        else
-            windowSize = kx * mpe;
+        //mpe in [1,2,4,8,16] for uint8
+        while(mpe <= 16)
+        {
+            if (sx <= kx)
+                windowSize = kx + sx * (mpe - 1);
+            else
+                windowSize = kx * mpe;
 
-        if (windowSize <= 32)
-            maxMpeWindowSize = windowSize;
+            if (windowSize <= 32)
+                maxMpeWindowSize = windowSize;
 
-        mpe *= 2;
+            mpe *= 2;
+        }
+    }
+    else if (dataType == mv::DType("Float16"))
+    {
+        //mpe in [1,2,4] for float
+        while(mpe <= 4)
+        {
+            if (sx <= kx)
+                windowSize = kx + sx * (mpe - 1);
+            else
+                windowSize = kx * mpe;
+
+            if (windowSize <= 32)
+                maxMpeWindowSize = windowSize;
+
+            mpe *= 2;
+        }
     }
 
     return maxMpeWindowSize;
@@ -81,7 +100,7 @@ std::vector<int8_t> createBitPattern(uint16_t kernelW, uint16_t kernelH, uint16_
 
 // The sparsity maps relative to populated tensors have to be generated BEFORE the dma passes.
 // As they have to be DMAed into CMX.
-static void generateSparsityMapsPopulatedTensorsFcn(const mv::pass::PassEntry& pass, mv::ComputationModel& model, mv::TargetDescriptor&, mv::Element& passDesc, mv::Element&)
+static void generateSparsityMapsPopulatedTensorsFcn(const mv::pass::PassEntry& pass, mv::ComputationModel& model, mv::TargetDescriptor&, mv::Element&, mv::Element&)
 {
 
     MV_PROFILED_FUNCTION(MV_PROFILE_PASS)
@@ -131,7 +150,11 @@ static void generateSparsityMapsPopulatedTensorsFcn(const mv::pass::PassEntry& p
                     kernelH = weightsShape[mv::KERNEL_HEIGHT];
                 }
 
-                auto windowsSize = getWindowSize(kernelW, strides[0]);
+                mv::DType dataType = dpuTask->getInputTensor(0)->get<mv::DType>("dType");
+                if (!isPooling)
+                     dataType = dpuTask->getInputTensor(1)->get<mv::DType>("dType");
+
+                auto windowsSize = getWindowSize(kernelW, strides[0], dataType);
 
                 pass.log(mv::Logger::MessageType::Debug, "windowSize " + std::to_string(windowsSize));
                 pass.log(mv::Logger::MessageType::Debug, "OutputChannels " + std::to_string(outputChannels));
@@ -238,7 +261,7 @@ bool checkA0SOHSparsityBug(mv::Data::FlowListIterator flow)
 
 // Eltwise, being the hackiest operation ever, potentially can support sparsity input, sharing the IDU with ZMajorConv, but the runtime currently doesn't allow it.
 
-static void generateSparsityMapsUnpopulatedTensorsFcn(const mv::pass::PassEntry& pass, mv::ComputationModel& model, mv::TargetDescriptor&, mv::Element& passDesc, mv::Element&)
+static void generateSparsityMapsUnpopulatedTensorsFcn(const mv::pass::PassEntry& , mv::ComputationModel& model, mv::TargetDescriptor&, mv::Element& , mv::Element&)
 {
 
     MV_PROFILED_FUNCTION(MV_PROFILE_PASS)
