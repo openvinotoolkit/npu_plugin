751d276ed90877f84a2a38944aae221fdff352b1
john.brady@intel.com <john.brady@intel.com>
2019-09-20 18:58:11 +0100

    mobilenet passing up to the first eltwise, a lot of hacks in serialisation

--- src/target/keembay/runtime_model/runtime_model.cpp
+++ src/target/keembay/runtime_model/runtime_model.cpp
@@ -109,6 +109,23 @@ void mv::RuntimeModel::alignTensor(mv::ComputationModel& cm, std::unique_ptr<MVC
             tensorT->dimensions = std::vector<uint32_t>(dimensions.begin(), dimensions.end());
 }
 
+void mv::RuntimeModel::alignSubTensor(mv::ComputationModel& cm, std::unique_ptr<MVCNN::TensorReferenceT>& tensorT, mv::Tensor tensor, bool padFinalOutput)
+{
+        auto globalConfigParams = cm.getGlobalConfigParams();
+        int pad = globalConfigParams->hasAttr("VPU2ChannelPadding") ? globalConfigParams->get<int>("VPU2ChannelPadding") : 16;
+
+        std::vector<std::size_t> dimensions = tensor.getShape();
+        auto outputChannelsPadded = mv::round_up(dimensions[mv::IO_CHANNEL_DIMENSION], pad);
+        dimensions = {dimensions[mv::IO_WIDTH_DIMENSION], dimensions[mv::IO_HEIGHT_DIMENSION], outputChannelsPadded, dimensions[mv::IO_BATCH_DIMENSION]};
+        auto numericStrides = tensor.getOrder().computeByteStrides(mv::Shape(dimensions), tensor.getDType().getSizeInBits() / 8);
+        numericStrides.push_back(tensor.getDType().getSizeInBits() / 8);
+        std::reverse(dimensions.begin(), dimensions.end());
+        std::reverse(numericStrides.begin(), numericStrides.end());
+        tensorT->strides = numericStrides; // NOTE: Maybe directly bufferIt->computeStrides() in the future
+        if(padFinalOutput)
+            tensorT->dimensions = std::vector<uint32_t>(dimensions.begin(), dimensions.end());
+}
+
 MVCNN::DType mv::RuntimeModel::convertDtype(const mv::DType& dtype)
 {
     return dTypeMapping_.at(dtype.toString());
@@ -859,11 +876,31 @@ std::vector<std::unique_ptr<MVCNN::TaskT>> mv::RuntimeModel::buildNNDMATaskT(Com
                 //copying from CMX2DDR we need to crop down if tensor needed alignment in CMX
                 if (opIt->getInputTensor(0)->hasAttr("alignment"))
                 {
-                    alignTensor(cm, tmp->src, opIt->getInputTensor(0), padFinalOutput);
+                    if(opIt->getInputTensor()[0]->get<std::string>("splitStrategy") == "SplitOverH")
+                        alignSubTensor(cm, tmp->src, opIt->getInputTensor(0)->getSubTensor(i), padFinalOutput);
+
+                    else 
+                        alignTensor(cm, tmp->src, opIt->getInputTensor(0), padFinalOutput);
+                    
                 }
                 if (padFinalOutput && opIt->getOutputTensor(0)->hasAttr("alignment"))
                 {
-                    alignTensor(cm, tmp->dst, opIt->getOutputTensor(0), padFinalOutput);
+                    if(opIt->getInputTensor()[0]->get<std::string>("splitStrategy") == "SplitOverH") 
+                    {
+                        int pad = globalConfigParams->hasAttr("VPU2ChannelPadding") ? globalConfigParams->get<int>("VPU2ChannelPadding") : 16;
+                        alignSubTensor(cm, tmp->dst, opIt->getOutputTensor(0)->getSubTensor(i), padFinalOutput);
+                        std::vector<std::size_t> dimensions = opIt->getOutputTensor(0)->getShape();
+                        auto outputChannelsPadded = mv::round_up(dimensions[mv::IO_CHANNEL_DIMENSION], pad);
+                        dimensions = {dimensions[mv::IO_WIDTH_DIMENSION], dimensions[mv::IO_HEIGHT_DIMENSION], outputChannelsPadded, dimensions[mv::IO_BATCH_DIMENSION]};
+
+                        auto numericStrides = opIt->getOutputTensor(0)->getOrder().computeByteStrides(mv::Shape(dimensions), opIt->getOutputTensor(0)->getDType().getSizeInBits() / 8);
+                        numericStrides.push_back(opIt->getOutputTensor(0)->getDType().getSizeInBits() / 8);
+                        std::reverse(numericStrides.begin(), numericStrides.end());
+                        tmp->dst->strides = numericStrides;
+                    }
+
+                    else 
+                        alignTensor(cm, tmp->dst, opIt->getOutputTensor(0), padFinalOutput);
                 }
             }
 
@@ -1135,18 +1172,61 @@ std::unique_ptr<MVCNN::NCEInvariantFieldsT> mv::RuntimeModel::buildNCEInvariantF
 
         int pad = globalConfigParams->hasAttr("VPU2ChannelPadding") ? globalConfigParams->get<int>("VPU2ChannelPadding") : 16;
         auto opStrategy = opIt->get<std::string>("splitStrategy");
+
         if (opStrategy == "HKSwitch"|| opStrategy == "SplitOverK")
             pad = pad * numberClusters;
         std::vector<std::size_t> dimensions = parentOutputTensor->getShape();
         auto outputChannelsPadded = mv::round_up(dimensions[mv::IO_CHANNEL_DIMENSION], pad);
         dimensions = {dimensions[mv::IO_WIDTH_DIMENSION], dimensions[mv::IO_HEIGHT_DIMENSION], outputChannelsPadded, dimensions[mv::IO_BATCH_DIMENSION]};
         auto numericStrides = parentOutputTensor->getOrder().computeByteStrides(mv::Shape(dimensions), parentOutputTensor->getDType().getSizeInBits() / 8);
+
+        if (opStrategy == "SplitOverH") 
+        {
+
+            numericStrides = parentOutputTensor->getSubTensor(clusterId).getOrder().computeByteStrides(mv::Shape({
+                parentOutputTensor->getSubTensor(clusterId).getShape()[IO_WIDTH_DIMENSION],parentOutputTensor->getSubTensor(clusterId).getShape()[IO_HEIGHT_DIMENSION],
+                 mv::round_up(parentOutputTensor->getSubTensor(clusterId).getShape()[IO_CHANNEL_DIMENSION], pad), parentOutputTensor->getSubTensor(clusterId).getShape()[IO_BATCH_DIMENSION]})
+            , parentOutputTensor->getSubTensor(clusterId).getDType().getSizeInBits() / 8);
+
+            auto paddOutput = globalConfigParams->hasAttr("PadOutput") ? globalConfigParams->get<bool>("PadOutput") : false;
+            alignTensor(cm, toBuild->parent_output_tensor, opIt->getOutputTensor(0), paddOutput);
+        }
+
         numericStrides.push_back(parentOutputTensor->getDType().getSizeInBits() / 8);
         std::reverse(dimensions.begin(), dimensions.end());
         std::reverse(numericStrides.begin(), numericStrides.end());
         toBuild->output_data->strides = numericStrides;
-        toBuild->parent_output_tensor->dimensions = numericStrides;
         toBuild->parent_output_tensor->dimensions = std::vector<uint32_t>(dimensions.begin(), dimensions.end());
+    }
+    if (parentInputTensor->hasAttr("alignment"))
+    {
+        auto opStrategy = opIt->get<std::string>("splitStrategy");
+        auto globalConfigParams = cm.getGlobalConfigParams();
+        std::vector<std::size_t> dimensions = parentInputTensor->getShape();
+        auto numericStrides = parentInputTensor->getOrder().computeByteStrides(mv::Shape(dimensions), parentInputTensor->getDType().getSizeInBits() / 8);
+        int pad = globalConfigParams->hasAttr("VPU2ChannelPadding") ? globalConfigParams->get<int>("VPU2ChannelPadding") : 16;
+
+        if (opStrategy == "SplitOverH") 
+        {
+            std::vector<std::size_t> new_dimensions = {
+                parentInputTensor->getSubTensor(clusterId).getShape()[IO_WIDTH_DIMENSION],parentInputTensor->getSubTensor(clusterId).getShape()[IO_HEIGHT_DIMENSION],
+                 mv::round_up(parentInputTensor->getSubTensor(clusterId).getShape()[IO_CHANNEL_DIMENSION], pad), parentInputTensor->getSubTensor(clusterId).getShape()[IO_BATCH_DIMENSION]};
+
+            numericStrides = parentInputTensor->getSubTensor(clusterId).getOrder().computeByteStrides(mv::Shape(new_dimensions)
+            , parentInputTensor->getSubTensor(clusterId).getDType().getSizeInBits() / 8);
+
+            auto paddOutput = globalConfigParams->hasAttr("PadOutput") ? globalConfigParams->get<bool>("PadOutput") : false;
+            alignTensor(cm, toBuild->parent_input_tensor, opIt->getInputTensor(0), paddOutput);
+            
+            numericStrides.push_back(parentOutputTensor->getDType().getSizeInBits() / 8);
+        
+            
+            std::reverse(new_dimensions.begin(), new_dimensions.end());
+
+            std::reverse(numericStrides.begin(), numericStrides.end());
+            toBuild->input_data->dimensions = std::vector<uint32_t>(new_dimensions.begin(), new_dimensions.end());
+            toBuild->input_data->strides = numericStrides;
+        }
     }
     if (opIt->get<bool>("multiCast"))
     {
