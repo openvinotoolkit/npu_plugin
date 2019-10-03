5fdb55bc94d28d7732cbc1ed00a46e65413432c3
Ashley DeFlumere <ashley.deflumere@intel.com>
2019-10-03 14:32:02 +0100

    Enable saving of input and weights sparsity strategy to descriptor element
    Remove debugging dot generation from Strategy Manager

--- src/pass/optimization/strategy_manager.cpp
+++ src/pass/optimization/strategy_manager.cpp
@@ -450,7 +450,7 @@ void StrategyManager::saveMetaStrategy(std::vector<MetaGraph::edge_list_iterator
     std::vector<mv::Element> streamingStrategyElements = convertStreamingStrategyToElement(allStrategies, globalParams);
     std::vector<mv::Element> multiClusterStrategyElements = convertClusteringStrategyToElement(allStrategies, globalParams);
     std::vector<mv::Element> locationStrategyElements = convertLocationStrategyToElement(allStrategies);
-    //std::vector<mv::Element> sparsityStrategyElements = convertSparsityStrategyToElement(allStrategies);
+    std::vector<mv::Element> sparsityStrategyElements = convertSparsityStrategyToElement(allStrategies);
 
     if (enableSaveStrategyToDescriptor)
     {
@@ -458,7 +458,7 @@ void StrategyManager::saveMetaStrategy(std::vector<MetaGraph::edge_list_iterator
         auto compDesc = model_.getGlobalConfigParams();
         compDesc->set("streaming_strategy", streamingStrategyElements);
         compDesc->set("split_strategy", multiClusterStrategyElements);
-        //compDesc->set("sparsity_strategy", sparsityStrategyElements);
+        compDesc->set("sparsity_strategy", sparsityStrategyElements);
     }
 
     if (enableSaveStrategyToJsonFile)
@@ -481,15 +481,15 @@ void StrategyManager::saveMetaStrategy(std::vector<MetaGraph::edge_list_iterator
         SSA.set("streaming_strategy",streamingStrategyElements);
         CSA.set("split_strategy",multiClusterStrategyElements);
         LSA.set("tensor_placement_override",locationStrategyElements);
-        //SpSA.set("sparsity_strategy",sparsityStrategyElements);
+        SpSA.set("sparsity_strategy",sparsityStrategyElements);
         auto jsonSStrategy = SSA.toJSON(true);
         auto jsonCStrategy = CSA.toJSON(true);
         auto jsonLStrategy = LSA.toJSON(true);
-        //auto jsonSpStrategy = SpSA.toJSON(true);
+        auto jsonSpStrategy = SpSA.toJSON(true);
         jsonOutputFile << jsonSStrategy.stringifyPretty() << "," << std::endl;
         jsonOutputFile << jsonCStrategy.stringifyPretty() << "," << std::endl;
-        jsonOutputFile << jsonLStrategy.stringifyPretty()  << endl; //<< "," << std::endl;
-        //jsonOutputFile << jsonSpStrategy.stringifyPretty() << std::endl;
+        jsonOutputFile << jsonLStrategy.stringifyPretty()  << "," << std::endl;
+        jsonOutputFile << jsonSpStrategy.stringifyPretty() << std::endl;
 
         jsonOutputFile.close();
     }
@@ -767,8 +767,8 @@ void StrategyManager::recursiveCriticalPath(typename graph<mv::Op, mv::DataFlow>
     //TODO add check here for whether we've hit the same pivot (needed for nested parallelism)
     //TODO recurse if we haven't all hit the same pivot (into the nested parallel branch)
         next_modelSource.push_back(model_child);
-        writeDot(optimizationGraph, true); //debug
-        cout << "Dot at " << (*model_child).getName() << " has " << optimizationGraph.node_size() << " nodes" << endl;
+        //writeDot(optimizationGraph, true); //debug
+        //cout << "Dot at " << (*model_child).getName() << " has " << optimizationGraph.node_size() << " nodes" << endl;
     }
 
     //If child counter is 1, we were in a linear section, just add the subgraph to the metaGraph
