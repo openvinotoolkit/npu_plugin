ba3c7c8dd0fdd5a966903c1d04bae8c80905007e
Eman Copty <eman.copty@intel.com>
2019-10-10 15:53:51 -0700

    Enable softmax with strategy optimizer

--- example/softmax_only.cpp
+++ example/softmax_only.cpp
@@ -14,12 +14,14 @@ int main()
 
     mv::CompilationUnit unit("parserModel");
     mv::OpModel& om = unit.model();
-    auto input0 = om.input({1000,1,1,1}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "input:0#4");
-
-    auto softmax0 = om.softmax(input0);
+    auto input0 = om.input({1,1,1000,1}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "input:0#4");
+    //auto input0 = om.input({1,21,1917,1}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "input:0#4");
+    std::string axis = "C";
+    //std::string axis = "H";
+    auto softmax0 = om.softmax(input0, axis);
     om.output(softmax0);
 
-    std::string compDescPath = path + "/config/compilation/debug_ma2490.json";
+    std::string compDescPath = path + "/config/compilation/release_kmb.json";
     unit.loadCompilationDescriptor(compDescPath);
 
     unit.loadTargetDescriptor(mv::Target::ma2490);
