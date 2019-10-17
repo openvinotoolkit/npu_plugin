dbdd5bc53c2493a8deef2cd4ad8357092e2b08dc
Eman Copty <eman.copty@intel.com>
2019-10-17 13:19:37 -0700

    Fix cmx slice size in blob
    - we need total size of CMX not the size used by MaxCut

--- src/target/kmb/runtime_model/runtime_model.cpp
+++ src/target/kmb/runtime_model/runtime_model.cpp
@@ -503,7 +503,7 @@ std::unique_ptr<MVCNN::ResourcesT> mv::RuntimeModel::buildResourcesT(Computation
     setIfPresent<uint32_t, int>(toBuild->nce2_blocks, *globalConfigurationParams, "Number_of_DPUs");
     setIfPresent<uint32_t, int>(toBuild->upa_shared_cmx, *globalConfigurationParams, "UPASharedCMX");
     uint32_t nn_cmx_per_slice;
-    setIfPresent<uint32_t, unsigned>(nn_cmx_per_slice, *globalConfigurationParams, "cmx");
+    setIfPresent<uint32_t, unsigned>(nn_cmx_per_slice, *globalConfigurationParams, "totalCmx");
     toBuild->nn_cmx_per_slice = nn_cmx_per_slice;
     setIfPresent<uint32_t, unsigned>(toBuild->nn_cmx_slice_amount, *globalConfigurationParams, "clusters");
     setIfPresent<uint32_t, int>(toBuild->ddr_scratch, *globalConfigurationParams, "DDRScratch");
