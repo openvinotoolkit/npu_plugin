26a5acf9defc63fd692837dbb3bb423ba3eead2e
Ilya Lavrenov <ilya.lavrenov@intel.com>
2019-10-28 21:50:51 +0300

    CMAKE: compilation with updatde developer package

--- cmake/features.cmake
+++ cmake/features.cmake
@@ -7,8 +7,12 @@ include (options)
 # Enable MCM compiler by default
 if(ARM OR AARCH64)
 	ie_option(ENABLE_MCM_COMPILER "Enable mcm compiler" OFF)
+	ie_option(ENABLE_HDDL2 "Enable HDDL2 Plugin" OFF)
 else()
 	ie_option(ENABLE_MCM_COMPILER "Enable mcm compiler" ON)
+	ie_option(ENABLE_HDDL2 "Enable HDDL2 Plugin" ON)
 endif()
 
 ie_option(ENABLE_VPUAL "Enable VPUAL" ON)
+
+ie_option(ENABLE_KMB_SAMPLES "Enable KMB samples" ON)
