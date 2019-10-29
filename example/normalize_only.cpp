331ea08643a0d744a2da1ab43c5c5912efea4d9b
Henry Wang <henry.h.wang@intel.com>
2019-10-29 08:54:49 +0000

    fixed normalize and proposal to compile and use proper params

--- example/normalize_only.cpp
+++ example/normalize_only.cpp
@@ -14,7 +14,7 @@ int main()
 
     mv::CompilationUnit unit("parserModel");
     mv::OpModel& om = unit.model();
-    auto input0 = om.input({16,16,32,1}, mv::DType("Float16"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "input:0#1");
+    auto input0 = om.input({112,112,32,1}, mv::DType("Float16"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "input:0#1");
 
     std::string weightsPath = path + "/example/normalize_only/normalize.weights";
     std::vector<double> scaleWeights0;
@@ -26,10 +26,10 @@ int main()
     }
     fs.close();
 
-    auto scales0 = om.constant(scaleWeights0,{1,1,32,1}, mv::DType("Float16"), mv::Order::getZMajorID(4), {{0},{1.1524552064656746e-05},{-inf},{inf}}, "scale_weights#0");
+    auto scales0 = om.constant(scaleWeights0,{1,1,32,1}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.1524552064656746e-05},{-inf},{inf}}, "scale_weights#0");
     
     double eps = 0.001; 
-    auto normalize0 = om.normalize(input0, scales0, eps);
+    auto normalize0 = om.normalize(input0, scales0, eps, 0, 0, mv::DType("Float16"));
 
     om.output(normalize0);
 
--- example/proposal_only.cpp
+++ example/proposal_only.cpp
@@ -1,16 +1,20 @@
 //This file is the parsed network which is created through python.
 #include "include/mcm/compiler/compilation_unit.hpp"
 #include "include/mcm/utils/data_generator.hpp"
-#include "meta/include/mcm/op_model.hpp"
+#include "build/meta/include/mcm/op_model.hpp"
 #include "include/mcm/utils/hardware_tests.hpp"
+
 #include "iostream"
 #include "fstream"
+
 int main()
 {
     std::string path = std::getenv("MCM_HOME");
     double inf = std::numeric_limits<double>::infinity();
+
     mv::CompilationUnit unit("parserModel");
     mv::OpModel& om = unit.model();
+
     // Define Params
     int base_size = 256;
     int pre_nms_topn = 2147483647;
@@ -26,17 +30,25 @@ int main()
     float box_coordinate_scale = 10.0;
     std::string framework = "TENSORFLOW";
     bool for_deformable = false;
+    std::vector<double> imageInfoData({224.0,224.0,1.0});
+    std::vector<double> scaleData({0.25,0.5,1.0,2.0});
+    std::vector<double> ratioData({0.5,1.0,2.0});
     // Define tensors
-    //std::vector<double> clsPredData = mv::utils::generateSequence<double> (1*30*40*40);
-    std::vector<double> bboxPredData = mv::utils::generateSequence<double> (1*60*40*40);
-    std::vector<double> imageInfoData({800.,600.,1.,1.});
-    std::vector<double> scaleData({4.0,6.0,9.0,16.0,24.0,32.0});
-    std::vector<double> ratioData({2.67});
-    auto cls_pred = om.input({1,30,40,40}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "cls_pred0");
-    auto bbox_pred = om.constant(bboxPredData, {1,60,40,40}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "bbox_pred0");
+    std::string bboxPredPath = path + "/example/proposal_only/proposal.in2";
+    std::vector<double> bboxPredData;
+    std::fstream fs;
+    double data;
+    fs.open(bboxPredPath, std::fstream::in);
+    while( fs >> data ) {
+        bboxPredData.push_back(data);
+    }
+    fs.close();
+    auto cls_pred = om.input({14,14,24,1}, mv::DType("Float16"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "cls_pred0");
+    auto bbox_pred = om.constant(bboxPredData,{14,14,48,1}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "bbox_pred0");
     auto im_info = om.constant(imageInfoData, {1,1,1,imageInfoData.size()}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "im_info0");
     auto scale = om.constant(scaleData, {1,1,1,scaleData.size()}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "scale0");
     auto ratio = om.constant(ratioData, {1,1,1,ratioData.size()}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "ratio0");
+printf("3\n");
     // Build inputs vector
     std::vector<mv::Data::TensorIterator> inputs;
     inputs.push_back(cls_pred);
@@ -47,9 +59,12 @@ int main()
     // Build Model
     auto proposal0 = om.proposal(inputs, base_size, pre_nms_topn, post_nms_topn, nms_thresh, feat_stride, min_size, pre_nms_thresh, clip_before_nms, clip_after_nms, normalize, box_size_scale, box_coordinate_scale, framework, for_deformable, mv::DType("Float16"));
     om.output(proposal0);
+printf("2\n");
     std::string compDescPath = path + "/config/compilation/release_kmb.json";
     unit.loadCompilationDescriptor(compDescPath);
     unit.loadTargetDescriptor(mv::Target::ma2490);
     unit.initialize();
+printf("1\n");
     unit.run();
+printf("0\n");
 }
