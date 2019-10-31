bbc9dbf4b0554109930c446e9803a35f900ae247
Skvortsov, Vladimir <vladimir.skvortsov@intel.com>
2019-10-31 15:35:15 +0300

    MobilenetV2 post training quantization scripts corrected

--- scripts/post_training_quantization/run_post_trainig.sh
+++ scripts/post_training_quantization/run_post_trainig.sh
@@ -1,4 +1,4 @@
-if [ -z "$1" -o -z "$2"  -o -z "$3"  -o -z "$4" -o -z "$5" ]
+if [ -z "$1" -o -z "$2"  -o -z "$3"  -o -z "$4" ]
 then
 echo "You have not set some arguments"
 echo ""
@@ -10,14 +10,16 @@ echo "Usage:"
 echo ""
 echo "PYTHONPATH env variable should contain the paths to inference engine/python_api and dldt/model-optimizer"
 echo ""
-echo "run_post_trainig.sh <path to post trainig tool> <path to original model> <path to dataset> <path to dataset annotation file> <path to post training config(json)> [<path to accuracy checker config(yml)>] "
+echo "run_post_trainig.sh <path to post trainig tool> <path to original model> <path to dataset> <path to post training config(json)> [<path to accuracy checker config(yml)>] "
 echo ""
-echo "The resulting IR should be in the "results" directory of <post_trainig_json> file directory"
+echo "The resulting IR should be in the 'results' directory of <post training config(json)> directory"
 echo ""
 echo ""
-echo "Example:"
+echo "Examples:"
 echo ""
-echo "./run_post_trainig.sh ../post-training-compression-tool/ ../model-zoo-models-public/classification/resnet/v1/50/pytorch/resnet_v1_50_v1.0.1.onnx ../../Datasets/ImageNet ../../Datasets/ImageNet/val.txt resnet-50_pytorch/resnet-50-pytorch_int8.json"
+echo "./run_post_trainig.sh ../post-training-compression-tool/ ../model-zoo-models-public/classification/resnet/v1/50/pytorch/resnet_v1_50_v1.0.1.onnx ../../Datasets/ImageNet resnet-50_pytorch/resnet-50-pytorch_int8.json"
+echo "or"
+echo "./run_post_trainig.sh ../post-training-compression-tool/ ../model-zoo-models-public/classification/resnet/v1/50/pytorch/resnet_v1_50_v1.0.1.onnx ../../Datasets/ImageNet resnet-50_pytorch/resnet-50-pytorch_int8_yml.json resnet-50-pytorch_int8.yml"
 echo ""
 echo ""
 exit
@@ -29,34 +31,23 @@ original_model=`readlink -f $2`
 original_model=${original_model//\//\\\/}
 dataset_path=`readlink -f $3`
 dataset_path=${dataset_path//\//\\\/}
-dataset_annotation=`readlink -f $4`
-dataset_annotation=${dataset_annotation//\//\\\/}
-post_trainig_json=`readlink -f $5`
+post_trainig_json=`readlink -f $4`
 post_trainig_json_dir=`dirname $post_trainig_json`
 
-if [ -n "$6" ]
+if [ -n "$5" ]
 then 
-accuracy_checker_config=`readlink -f $6`
+accuracy_checker_config=`readlink -f $5`
 fi
 
 cd $post_trainig_json_dir
 sed "s/<ORIGINAL_MODEL>/$original_model/" $post_trainig_json > tmp.json
 sed -i "s/<DATASET_PATH>/$dataset_path/" tmp.json
-sed -i "s/<DATASET_ANNOTATION_FILE>/${dataset_annotation}/" tmp.json
 
 if [ -f "$accuracy_checker_config" ]
 then
 sed -i "s/<ACCURACY_CHECKER_CONFIG>/tmp.yml/" tmp.json
 sed "s/<DATASET_PATH>/$dataset_path/" $accuracy_checker_config > tmp.yml
-sed -i "s/<DATASET_ANNOTATION_FILE>/${dataset_annotation}/" tmp.yml
-fi
-
-echo ""
-echo ""
-python3 $post_trainig_tool/main.py -c tmp.json
-
-if [[ -z "$6" || ( -z "$7" && -f "$accuracy_checker_config" ) ]]
-then
-rm tmp.json
-rm tmp.yml
+python3 $post_trainig_tool/main.py -c tmp.json $6 $7 $8
+else
+python3 $post_trainig_tool/main.py -c tmp.json $5 $6 $7
 fi
