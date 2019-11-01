2bf701fa1a3ed32b43e51fca757281b803104edb
Paul S. Durley <paul.s.durley@intel.com>
2019-11-01 12:41:51 -0700

    Fix output shape for DetectionOutput

--- src/computation/op/def/detection_output.cpp
+++ src/computation/op/def/detection_output.cpp
@@ -8,7 +8,7 @@ namespace mv
 
         static std::function<std::pair<bool, std::size_t>(const std::vector<Data::TensorIterator>&,
             const std::map<std::string, Attribute>&, std::string&)> inputCheckFcn =
-            [](const std::vector<Data::TensorIterator>& inputs, const std::map<std::string, Attribute>&,
+            [](const std::vector<Data::TensorIterator>& inputs, const std::map<std::string, Attribute>& args,
             std::string& errMsg) -> std::pair<bool, std::size_t>
         {
 
@@ -17,6 +17,15 @@ namespace mv
             {
                 std::stringstream err;
                 err << "Incorrect number of inputs (must be 3): " << inputs.size();
+                errMsg = err.str();
+                return {false, 0};
+            }
+
+            auto keep_top_k = args.at("keep_top_k").get<int64_t>();
+            if (keep_top_k < 0)
+            {
+                std::stringstream err;
+                err << "keep_top_k must be a valid positive number.";
                 errMsg = err.str();
                 return {false, 0};
             }
@@ -40,10 +49,15 @@ namespace mv
             if(dTypeToUse == mv::DType("Default"))
                 dTypeToUse = inputs[0]->getDType();
 
+            // Calculate output shape
+            auto keep_top_k = args.at("keep_top_k").get<int64_t>();
+            auto max_detections = keep_top_k;
+            outputShape = {7,max_detections,1,1};
+
             if (args.at("quantParams").get<mv::QuantizationParams>().isEmpty())
-                outputs.push_back(mv::Tensor(":0",  inputShape, dTypeToUse, outputOrder));
+                outputs.push_back(mv::Tensor(":0",  outputShape, dTypeToUse, outputOrder));
             else
-                outputs.push_back(mv::Tensor(":0",  inputShape, dTypeToUse, outputOrder, args.at("quantParams").get<mv::QuantizationParams>()));
+                outputs.push_back(mv::Tensor(":0",  outputShape, dTypeToUse, outputOrder, args.at("quantParams").get<mv::QuantizationParams>()));
 
         };
     }
