b3ed57bc028c7233f2977655827b4907ef82799e
Henry Wang <henry.h.wang@intel.com>
2019-11-04 17:35:03 +0000

    changed normalize to load weights file in 'proper' way

--- example/normalize_only/normalize_only.cpp
+++ example/normalize_only/normalize_only.cpp
@@ -11,26 +11,26 @@ int main()
     std::string path = std::getenv("MCM_HOME");
     mv::CompilationUnit unit("parserModel");
     mv::OpModel& om = unit.model();
-    auto input0 = om.input({112,112,32,1}, mv::DType("Float16"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "input:0#1");
+    auto input0 = om.input({112,112,32,1}, mv::DType("Float16"), mv::Order::getZMajorID(4), {{0},{1.0},{-inf},{inf}}, "input0");
+    std::vector<uint16_t> weightsData(32);
 
-    std::string weightsPath = path + "/example/normalize_only/normalize.weights";
-    std::vector<double> scaleWeights0;
-    double weight;
-    std::fstream fs;
-    fs.open(weightsPath, std::fstream::in);
-    while( fs >> weight ) {
-        scaleWeights0.push_back(weight);
-    }
-    fs.close();
+    //Load weights tensor from file
+    std::string  weights_filename(path + "/example/normalize_only/normalize.in2");
+    std::ifstream w_file;
+    w_file.open(weights_filename, std::fstream::in | std::fstream::binary);
+    w_file.read((char*)(weightsData.data()), 32 * sizeof(uint16_t));
+    std::vector<int64_t> weightsData_converted(32);
+    for(unsigned i = 0; i < weightsData.size(); ++i)
+        weightsData_converted[i] = weightsData[i];
 
-    auto scales0 = om.constant(scaleWeights0,{1,1,32,1}, mv::DType("Float64"), mv::Order::getZMajorID(4), {{0},{1.1524552064656746e-05},{-inf},{inf}}, "scale_weights#0");
+    auto weights0 = om.constantInt(weightsData_converted,{1,1,32,1}, mv::DType("Float16"), mv::Order::getZMajorID(4), {{0},{1.1524552064656746e-05},{-inf},{inf}}, "weights0");
     
     double eps = 0.001; 
-    auto normalize0 = om.normalize(input0, scales0, eps, 0, 0, mv::DType("Float16"));
+    auto normalize0 = om.normalize(input0, weights0, eps, 0, 0, mv::DType("Float16"));
 
     om.output(normalize0);
 
-    std::string compDescPath = path + "/config/compilation/release_kmb_MC-Prefetch2.json";
+    std::string compDescPath = path + "/config/compilation/release_kmb.json";
     unit.loadCompilationDescriptor(compDescPath);
     unit.loadTargetDescriptor(mv::Target::ma2490);
     unit.initialize();
