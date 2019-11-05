73ec899721efe10e2513c3dd0cf462cfa16aceca
Henry Wang <henry.h.wang@intel.com>
2019-11-05 10:43:42 +0000

    fixed script to work with new directories, commented out tests that won't be finished for 1.1.2, removed excess binary

--- tests/layer/build_script.sh
+++ tests/layer/build_script.sh
@@ -1,12 +1,12 @@
 set -x
 set -e
 TESTNAME=$1
-cp ${TESTNAME}_only/${TESTNAME}.in ${INFERENCE_MANAGER_DEMO_HOME}
-cp ${TESTNAME}_only/${TESTNAME}.out ${INFERENCE_MANAGER_DEMO_HOME}
-cd ../build
+cp ${TESTNAME}/${TESTNAME}.in ${INFERENCE_MANAGER_DEMO_HOME}
+cp ${TESTNAME}/${TESTNAME}.out ${INFERENCE_MANAGER_DEMO_HOME}
+cd ../../build
 cmake ..
 make -j8
-cd example
-./${TESTNAME}_only
+cd tests/layer
+./${TESTNAME}
 cd output
 cp mcm.blob ${INFERENCE_MANAGER_DEMO_HOME}/${TESTNAME}.blob
