a80032b7950d529fe9740c44a13a612d17a777d0
aleckey <alec.leckey@gmail.com>
2019-11-09 22:19:41 +0000

    Removed unused parsing function

--- python/tools/post_process.py
+++ python/tools/post_process.py
@@ -6,12 +6,12 @@ import cv2
 import ast
 import re
 
-# Changes the order of channels in an input image. Taken from original Fathom project
+# Changes the order of channels of an input tensor
 #
 # command:
 #   python3 post_process.py --file <path to results file> --shape n,c,h,w --zmajor
 
-def convert_image(file_path, shape, datatype=np.uint8, zmajor=True):
+def convert_output(file_path, shape, datatype=np.uint8, zmajor=True):
 
     new_shape = [int(shape[0]),
                  int(shape[1]),
@@ -30,16 +30,9 @@ def convert_image(file_path, shape, datatype=np.uint8, zmajor=True):
     fp.close
 
 
-def arg_as_list(s):
-    v = ast.literal_eval(s)
-    if type(v) is not list:
-        raise argparse.ArgumentTypeError("Argument \"%s\" is not a list" % (s))
-    return v
-
-
 def main():
-    parser = argparse.ArgumentParser(description='Convert the image used to a format suitable for KMB.')
-    parser.add_argument('--file', type=str, required=True, help='an image to test the model')
+    parser = argparse.ArgumentParser(description='Convert result file to format suitable for KMB.')
+    parser.add_argument('--file', type=str, required=True, help='path to output-0.bin')
     parser.add_argument('--shape', type=str)
     parser.add_argument('--zmajor', action='store_true')
 
@@ -47,7 +40,7 @@ def main():
 
     image_shape = args.shape.split(',')
 
-    convert_image(args.file, image_shape, np.uint8)
+    convert_output(args.file, image_shape, np.uint8)
     
 if __name__ == "__main__":
     main()
