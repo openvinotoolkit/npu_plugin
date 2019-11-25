8aff42de8894784918450cad3de19ede8b0c79ec
Paul S. Durley <paul.s.durley@intel.com>
2019-11-25 14:37:13 -0800

    Add unittest for eltwise_fp16 replacement pass

---
+++ tests/layer/eltwise_fp16/eltwise_replacement.cpp
@@ -0,0 +1,40 @@
+#include "include/mcm/compiler/compilation_unit.hpp"
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+
+    mv::CompilationUnit unit("EltwiseReplacement");
+    mv::OpModel& om = unit.model();
+
+    auto inputShape = mv::Shape({16,16,32,1});
+
+    // Quantize zeropoints & scales
+    auto in_qp0 = mv::QuantizationParams({{0}, {0.003921568627451}, {},{}});
+    auto in_qp1 = mv::QuantizationParams({{0}, {0.003921568627451 * 2}, {},{}});
+    auto out_qp = mv::QuantizationParams({{0}, {1.0}, {},{}});
+
+    // Input
+    auto input0 = om.input(inputShape, mv::DType("UInt8"), mv::Order::getZMajorID(4), {{0},{0.003921568627451},{},{}}, "input0");
+
+    // Identity MaxPools
+    auto identity_maxPool0 = om.maxPool(input0, {1,1}, {1,1}, {0,0,0,0}, true, "", "floor", mv::DType("UInt8"), in_qp0, "identity_maxpool0");
+    auto identity_maxPool1 = om.maxPool(input0, {1,1}, {1,1}, {0,0,0,0}, true, "", "floor", mv::DType("UInt8"), in_qp1, "identity_maxpool1");
+
+    // Eltwise, to be replaced with eltwiseFP16 in replacement pass
+    auto eltwise0 = om.eltwise({identity_maxPool0, identity_maxPool1}, "Add", mv::DType("Float16"), out_qp, "eltwise0");
+
+    // Re-quantize output
+    auto requantize0 = om.quantize(eltwise0, mv::DType("UInt8"), out_qp, "requantize0");
+
+    // Output
+    auto output0 = om.output(requantize0);
+
+    std::string compDescPath = mv::utils::projectRootPath() + "/config/compilation/release_kmb_MC-Prefetch1.json";
+    unit.loadCompilationDescriptor(compDescPath);
+    unit.loadTargetDescriptor(mv::Target::ma2490);
+    unit.initialize();
+    unit.run();
+
+}
