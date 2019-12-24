979d4ebe717c4ef223803b0b0c0fd9ba8875490d
tbartsok <tasos.bartsokas@intel.com>
2019-12-24 16:27:47 +0000

    size of vector fix

--- src/pass/kmb_adaptation/streaming/spatial_split_streaming.cpp
+++ src/pass/kmb_adaptation/streaming/spatial_split_streaming.cpp
@@ -215,7 +215,7 @@ std::tuple<mv::Data::TensorIterator, mv::Data::TensorIterator,mv::Data::TensorIt
             if (kernelTensor->hasAttr("quantParams"))
             {
                 auto sliceQuantParams = kernelTensor->get<mv::QuantizationParams>("quantParams");
-                if (kernelTensor->get<mv::QuantizationParams>("quantParams").getScale().size())
+                if (kernelTensor->get<mv::QuantizationParams>("quantParams").getScale().size() > 1)
                 {
                     std::size_t outputChannelsofSlice, starting_point;
                     if (op->getOpType() == "Conv")
