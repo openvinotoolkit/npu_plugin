af438f503b1fee275dd01dc8c656571dc9be6bbc
Vladislav Vinogradov <vlad.vinogradov@intel.com>
2020-01-14 11:36:02 +0300

    Extend test utils
    * Integer versions for `fillUniform`, `genBlobUniform` and `makeScalarBlob`
    * New `makeSingleValueBlob` function.
    * Reduce code duplication.
    * Use `checked_cast` instead of `static_cast` to check range.

--- tests/functional/new/test_model/kmb_test_utils.cpp
+++ tests/functional/new/test_model/kmb_test_utils.cpp
@@ -17,10 +17,24 @@
 #include "kmb_test_utils.hpp"
 
 #include <precision_utils.h>
-
 #include <blob_factory.hpp>
 #include <blob_transform.hpp>
 #include <single_layer_common.hpp>
+
+#include <vpu/utils/checked_cast.hpp>
+
+namespace vpu {
+
+template <typename OutT, typename InT>
+typename std::enable_if<
+        std::is_floating_point<OutT>::value && std::is_integral<InT>::value,
+    OutT>::type checked_cast(InT value) {
+    // TODO: check range
+    // TODO: move to DLDT repository
+    return static_cast<OutT>(value);
+}
+
+}  // namespace vpu
 
 namespace {
 
@@ -32,7 +46,7 @@ void fill_(const Blob::Ptr& blob, std::default_random_engine& rd, Distribution& 
     IE_ASSERT(outPtr != nullptr);
 
     std::generate_n(outPtr, blob->size(), [&]() {
-        return static_cast<T>(dist(rd));
+        return vpu::checked_cast<T>(dist(rd));
     });
 }
 template <typename T, class Distribution, class ConvertOp>
@@ -47,40 +61,49 @@ void fill_(const Blob::Ptr& blob, std::default_random_engine& rd, Distribution& 
     });
 }
 
-}  // namespace
-
-void fillUniform(const Blob::Ptr& blob, std::default_random_engine& rd, float min, float max) {
+template <typename T>
+void fillUniform_(const Blob::Ptr& blob, std::default_random_engine& rd, T min, T max) {
     IE_ASSERT(blob != nullptr);
 
     switch (blob->getTensorDesc().getPrecision()) {
     case Precision::FP32: {
-        std::uniform_real_distribution<float> dist(min, max);
+        std::uniform_real_distribution<float> dist(vpu::checked_cast<float>(min), vpu::checked_cast<float>(max));
         fill_<float>(blob, rd, dist);
         break;
     }
     case Precision::FP16: {
-        std::uniform_real_distribution<float> dist(min, max);
+        std::uniform_real_distribution<float> dist(vpu::checked_cast<float>(min), vpu::checked_cast<float>(max));
         fill_<ie_fp16>(blob, rd, dist, PrecisionUtils::f32tof16);
         break;
     }
     case Precision::I32: {
-        std::uniform_int_distribution<int32_t> dist(static_cast<int32_t>(min), static_cast<int32_t>(max));
+        std::uniform_int_distribution<int32_t> dist(vpu::checked_cast<int32_t>(min), vpu::checked_cast<int32_t>(max));
         fill_<int32_t>(blob, rd, dist);
         break;
     }
     case Precision::U8: {
-        std::uniform_int_distribution<uint8_t> dist(static_cast<uint8_t>(max), static_cast<uint8_t>(min));
+        std::uniform_int_distribution<uint8_t> dist(vpu::checked_cast<uint8_t>(max), vpu::checked_cast<uint8_t>(min));
         fill_<uint8_t>(blob, rd, dist);
         break;
     }
     case Precision::I8: {
-        std::uniform_int_distribution<int8_t> dist(static_cast<int8_t>(max), static_cast<int8_t>(min));
+        std::uniform_int_distribution<int8_t> dist(vpu::checked_cast<int8_t>(max), vpu::checked_cast<int8_t>(min));
         fill_<int8_t>(blob, rd, dist);
         break;
     }
     default:
         THROW_IE_EXCEPTION << "Unsupported precision " << blob->getTensorDesc().getPrecision();
     }
+}
+
+}  // namespace
+
+void fillUniform(const Blob::Ptr& blob, std::default_random_engine& rd, float min, float max) {
+    fillUniform_(blob, rd, min, max);
+}
+
+void fillUniform(const Blob::Ptr& blob, std::default_random_engine& rd, int min, int max) {
+    fillUniform_(blob, rd, min, max);
 }
 
 void fillNormal(const Blob::Ptr& blob, std::default_random_engine& rd, float mean, float stddev) {
@@ -114,13 +137,26 @@ void fillNormal(const Blob::Ptr& blob, std::default_random_engine& rd, float mea
     }
 }
 
-Blob::Ptr genBlobUniform(const TensorDesc& desc, std::default_random_engine& rd, float min, float max) {
+namespace {
+
+template <typename T>
+Blob::Ptr genBlobUniform_(const TensorDesc& desc, std::default_random_engine& rd, T min, T max) {
     const auto blob = make_blob_with_precision(desc);
     blob->allocate();
 
     fillUniform(blob, rd, min, max);
 
     return blob;
+}
+
+}  // namespace
+
+Blob::Ptr genBlobUniform(const TensorDesc& desc, std::default_random_engine& rd, float min, float max) {
+    return genBlobUniform_(desc, rd, min, max);
+}
+
+Blob::Ptr genBlobUniform(const TensorDesc& desc, std::default_random_engine& rd, int min, int max) {
+    return genBlobUniform_(desc, rd, min, max);
 }
 
 Blob::Ptr genBlobNormal(const TensorDesc& desc, std::default_random_engine& rd, float mean, float stddev) {
@@ -132,48 +168,78 @@ Blob::Ptr genBlobNormal(const TensorDesc& desc, std::default_random_engine& rd, 
     return blob;
 }
 
-Blob::Ptr makeScalarBlob(float val, const Precision& precision, size_t numDims) {
-    const auto dims = SizeVector(numDims, 1);
-    const auto outDesc = TensorDesc(precision, dims, TensorDesc::getLayoutByDims(dims));
-    const auto out = make_blob_with_precision(outDesc);
-    out->allocate();
+namespace {
 
-    switch (precision) {
+template <typename T>
+Blob::Ptr makeSingleValueBlob_(const TensorDesc& desc, T val) {
+    const auto blob = make_blob_with_precision(desc);
+    blob->allocate();
+
+    switch (desc.getPrecision()) {
     case Precision::FP32: {
-        const auto outPtr = out->buffer().as<float*>();
+        const auto outPtr = blob->buffer().as<float*>();
         IE_ASSERT(outPtr != nullptr);
-        *outPtr = val;
+        std::fill_n(outPtr, blob->size(), vpu::checked_cast<float>(val));
         break;
     }
     case Precision::FP16: {
-        const auto outPtr = out->buffer().as<ie_fp16*>();
+        const auto outPtr = blob->buffer().as<ie_fp16*>();
         IE_ASSERT(outPtr != nullptr);
-        *outPtr = PrecisionUtils::f32tof16(val);
+        std::fill_n(outPtr, blob->size(), PrecisionUtils::f32tof16(vpu::checked_cast<float>(val)));
         break;
     }
     case Precision::I32: {
-        const auto outPtr = out->buffer().as<int32_t*>();
+        const auto outPtr = blob->buffer().as<int32_t*>();
         IE_ASSERT(outPtr != nullptr);
-        *outPtr = static_cast<int32_t>(val);
+        std::fill_n(outPtr, blob->size(), vpu::checked_cast<int32_t>(val));
         break;
     }
     case Precision::U8: {
-        const auto outPtr = out->buffer().as<uint8_t*>();
+        const auto outPtr = blob->buffer().as<uint8_t*>();
         IE_ASSERT(outPtr != nullptr);
-        *outPtr = static_cast<uint8_t>(val);
+        std::fill_n(outPtr, blob->size(), vpu::checked_cast<uint8_t>(val));
         break;
     }
     case Precision::I8: {
-        const auto outPtr = out->buffer().as<int8_t*>();
+        const auto outPtr = blob->buffer().as<int8_t*>();
         IE_ASSERT(outPtr != nullptr);
-        *outPtr = static_cast<int8_t>(val);
+        std::fill_n(outPtr, blob->size(), vpu::checked_cast<int8_t>(val));
         break;
     }
     default:
-        THROW_IE_EXCEPTION << "Unsupported precision " << precision;
+        THROW_IE_EXCEPTION << "Unsupported precision " << desc.getPrecision();
     }
 
-    return out;
+    return blob;
+}
+
+}  // namespace
+
+Blob::Ptr makeSingleValueBlob(const TensorDesc& desc, float val) {
+    return makeSingleValueBlob_(desc, val);
+}
+
+Blob::Ptr makeSingleValueBlob(const TensorDesc& desc, int val) {
+    return makeSingleValueBlob_(desc, val);
+}
+
+namespace {
+
+template <typename T>
+Blob::Ptr makeScalarBlob_(T val, const Precision& precision, size_t numDims) {
+    const auto dims = SizeVector(numDims, 1);
+    const auto desc = TensorDesc(precision, dims, TensorDesc::getLayoutByDims(dims));
+    return makeSingleValueBlob(desc, val);
+}
+
+}  // namespace
+
+Blob::Ptr makeScalarBlob(float val, const Precision& precision, size_t numDims) {
+    return makeScalarBlob_(val, precision, numDims);
+}
+
+Blob::Ptr makeScalarBlob(int val, const Precision& precision, size_t numDims) {
+    return makeScalarBlob_(val, precision, numDims);
 }
 
 Blob::Ptr toFP32(const Blob::Ptr& in) {
