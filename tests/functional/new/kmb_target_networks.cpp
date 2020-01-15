9b5410b877c266697f93e91459b941bd5298a2a3
Skvortsov, Vladimir <vladimir.skvortsov@intel.com>
2020-01-15 09:21:20 +0300

    Tests for sparse mobilenet and resnet networks added

--- tests/functional/new/kmb_target_networks.cpp
+++ tests/functional/new/kmb_target_networks.cpp
@@ -19,16 +19,25 @@
 // Fails on IE to mcmCompiler parsing stage with message
 // C++ exception with description "quant_model/resnet_v1_50/block1/unit_3/bottleneck_v1/addQuantize Eltwise
 // should has FakeQuantize on inputs
-TEST_F(KmbNetworkTest, DISABLED_ResNet_50_v1_tf_int8_sparse) {
+TEST_F(KmbNetworkTest, DISABLED_ResNet_50_v1_tf_int8_sparse_v2) {  // 60.4% sparsity
     runClassifyNetworkTest(
         "KMB_models/INT8/public/sparse/ResNet-50-tf/resnetv1-int8-sparse-v2-tf-0001",
+        "224x224/cat3.bmp",
+        1, 5.0f);
+}
+// Fails on IE to mcmCompiler parsing stage with message
+// C++ exception with description "quant_model/resnet_v1_50/block1/unit_3/bottleneck_v1/addQuantize Eltwise
+// should has FakeQuantize on inputs
+TEST_F(KmbNetworkTest, DISABLED_ResNet_50_v1_tf_int8_sparse_v1) {  // 28.4% sparsity
+    runClassifyNetworkTest(
+        "KMB_models/INT8/public/sparse/ResNet-50-tf/resnetv1-int8-sparse-v1-tf-0001",
         "224x224/cat3.bmp",
         1, 5.0f);
 }
 // Fails on mcmCompiler compilation stage with message
 // C++ exception with description "Caught std::runtime_error during unit run:
 // Populated tensor with DType Int32 with out of bound value -9223372036854775808
-TEST_F(KmbNetworkTest, DISABLED_ResNet_50_v1_onnx_int8_sparse) {
+TEST_F(KmbNetworkTest, DISABLED_ResNet_50_v1_onnx_int8_sparse_v2) {
     runClassifyNetworkTest(
         "KMB_models/INT8/public/sparse/ResNet-50-onnx/resnet50-int8-sparse-v2",
         "224x224/cat3.bmp",
@@ -37,7 +46,7 @@ TEST_F(KmbNetworkTest, DISABLED_ResNet_50_v1_onnx_int8_sparse) {
 // Fails on mcmCompiler compilation stage with message
 // C++ exception with description "Caught std::runtime_error during unit run:
 // Populated tensor with DType Int32 with out of bound value -4315556704
-TEST_F(KmbNetworkTest, DISABLED_MobileNet_v2_onnx_int8_sparse) {
+TEST_F(KmbNetworkTest, DISABLED_MobileNet_v2_onnx_int8_sparse_v2) {
     runClassifyNetworkTest(
         "KMB_models/INT8/public/sparse/MoblieNet-v2-onnx/mobilenetv2-int8-sparse-v2",
         "224x224/cat3.bmp",
@@ -91,9 +100,15 @@ TEST_F(KmbNetworkTest, GoogLeNet_v1_tf_int8_sparse) {
         "224x224/cat3.bmp",
         1, 5.0f);
 }
-TEST_F(KmbNetworkTest, MobileNet_v2_tf_int8_sparse) {
+TEST_F(KmbNetworkTest, MobileNet_v2_tf_int8_sparse_v2) {  // 59.3% sparsity
     runClassifyNetworkTest(
         "KMB_models/INT8/public/sparse/MoblieNet-v2-tf/mobilenetv2-int8-sparse-v2-tf-0001",
+        "224x224/cat3.bmp",
+        1, 5.0f);
+}
+TEST_F(KmbNetworkTest, MobileNet_v2_tf_int8_sparse_v1) {  // 30.8% sparsity
+    runClassifyNetworkTest(
+        "KMB_models/INT8/public/sparse/MoblieNet-v2-tf/mobilenetv2-int8-sparse-v1-tf-0001",
         "224x224/cat3.bmp",
         1, 5.0f);
 }
