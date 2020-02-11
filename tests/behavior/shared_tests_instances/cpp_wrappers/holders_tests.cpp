5801b248799bfdb04ede3704cb2be40c3485283b
Lavrenov, Ilya <ilya.lavrenov@intel.com>
2020-02-11 16:31:25 +0000

    Il/cppholders

--- tests/behavior/shared_tests_instances/cpp_wrappers/holders_tests.cpp
+++ tests/behavior/shared_tests_instances/cpp_wrappers/holders_tests.cpp
@@ -1,18 +1,22 @@
-// Copyright (C) 2018-2019 Intel Corporation
+// Copyright (C) 2018-2020 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 
 #include "holders_tests.hpp"
 
+#include <string>
+#include <vector>
+
 INSTANTIATE_TEST_CASE_P(DISABLED_ReleaseOrderTests, CPP_HoldersTests,
-    testing::ValuesIn(std::vector<std::vector<int>> {
-        // 0 - plugin
-        // 1 - executable_network
-        // 2 - infer_request
-        {0, 1, 2},
-        {0, 2, 1},
-        {1, 0, 2},
-        {1, 2, 0},
-        {2, 0, 1},
-        {2, 1, 0},
-    }));
+    testing::Combine(testing::ValuesIn(std::vector<std::vector<int>> {
+                         // 0 - plugin
+                         // 1 - executable_network
+                         // 2 - infer_request
+                         {0, 1, 2},
+                         {0, 2, 1},
+                         {1, 0, 2},
+                         {1, 2, 0},
+                         {2, 0, 1},
+                         {2, 1, 0},
+                     }),
+        testing::Values(std::string("KMB"))));
