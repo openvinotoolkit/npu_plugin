cb761c50d86218e76ba0da326eda012242a0a306
Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>
2020-02-23 20:20:15 -0800

    Fix build with graph comparator
    Signed-off-by: Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>

--- tests/unit/runtime_model/runtime_model_test.cpp
+++ tests/unit/runtime_model/runtime_model_test.cpp
@@ -2,7 +2,7 @@
 #include "include/mcm/base/attribute.hpp"
 #include "include/mcm/base/attribute_registry.hpp"
 #include "include/mcm/target/kmb/runtime_model/runtime_model.hpp"
-#include "include/graph_comparator/graph_comparator.hpp"
+#include "tools/graph_comparator/include/graph_comparator/graph_comparator.hpp"
 
 TEST(runtime_model, graphfile_basic_test)
 {
