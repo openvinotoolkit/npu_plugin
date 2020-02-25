593ab1a5fceb36cc78522b3c9c4da4ae7f7a34be
Mikhail Pedus <mikhail.pedus@intel.com>
2020-02-25 13:21:04 +0300

    Disable falied test

--- tests/functional/new/kmb_target_networks_tests.cpp
+++ tests/functional/new/kmb_target_networks_tests.cpp
@@ -309,7 +309,8 @@ TEST_F(KmbClassifyNetworkTest, INT8_Dense_Caffe_IRv7_SqueezeNet_1_1) {
         1, 0.05f);
 }
 
-TEST_F(KmbClassifyNetworkTest, INT8_Dense_PyTorch_IRv7_SqueezeNet_1_1) {
+// [Track number: S#27976]
+TEST_F(KmbClassifyNetworkTest, DISABLED_INT8_Dense_PyTorch_IRv7_SqueezeNet_1_1) {
     runTest(
         TestNetworkDesc("KMB_models/INT8/public/squeezenet1_1_pytorch/squeezenet1_1_pytorch_uint8_int8_weights_pertensor.xml")
             .setUserInputPresision("input", Precision::U8)
