f9a27d14e4eb084e60112165a58eb42985604c57
john.brady@intel.com <john.brady@intel.com>
2020-03-11 19:14:17 +0000

    adding test that causes slow TR

--- tests/layer/conv_batch/conv_batch.cpp
+++ tests/layer/conv_batch/conv_batch.cpp
@@ -8,12 +8,12 @@ int main()
     mv::OpModel& om = unit.model();
 
     // Input is in WHCN, for use with conv_batching.in and conv_batching.out
-    auto input0 = om.input({2,1,16,10}, mv::DType("UInt8"), mv::Order::getZMajorID(4),  {{0},{1},{},{}}, "input#170");
-    std::vector<int64_t> weightsData0 = mv::utils::generateSequence<int64_t> (8, 1, 0);
-    std::vector<int64_t> weightsData1 = mv::utils::generateSequence<int64_t> (8, 3, 0);
+    auto input0 = om.input({7,7,1024,100}, mv::DType("UInt8"), mv::Order::getZMajorID(4),  {{0},{1},{},{}}, "input#170");
+    std::vector<int64_t> weightsData0 = mv::utils::generateSequence<int64_t> (512, 1, 0);
+    std::vector<int64_t> weightsData1 = mv::utils::generateSequence<int64_t> (512, 3, 0);
     weightsData0.insert(weightsData0.end(), weightsData1.begin(), weightsData1.end());
 
-    auto weights0 = om.constantInt(weightsData0,{1,1,16,1}, mv::DType("Int8"), mv::Order::getZMajorID(4), {{0},{1.0},{},{}});
+    auto weights0 = om.constantInt(weightsData0,{1,1,1024,1}, mv::DType("Int8"), mv::Order::getZMajorID(4), {{0},{1.0},{},{}});
     auto conv0 = om.conv(input0, weights0, {1, 1}, {0, 0, 0, 0}, 1, 1,  mv::DType("Int8"),{{0},{1},{},{}} , "conv");
     om.output(conv0);
 
