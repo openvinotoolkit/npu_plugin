bdc0e001a0416c11105fca548cef573f6b58049d
john.brady@intel.com <john.brady@intel.com>
2020-03-11 13:43:14 +0000

    added batch unit test

--- tests/layer/conv_batch/conv_batch.cpp
+++ tests/layer/conv_batch/conv_batch.cpp
@@ -7,15 +7,8 @@ int main()
     mv::CompilationUnit unit("ConvBatchModel");
     mv::OpModel& om = unit.model();
 
-    //Input for use with conv_batching_large.in and conv_batching_large.out, which will test streaming
-    // auto input0 = om.input({7,7,1024,20}, mv::DType("UInt8"), mv::Order::getZMajorID(4),  {{0},{1},{},{}}, "input#170");
-
-    // std::vector<int64_t> weightsData0 = mv::utils::generateSequence<int64_t> (512, -1, 0);
-    // std::vector<int64_t> weightsData1 = mv::utils::generateSequence<int64_t> (512, 1, 0);
-
-    
     // Input is in WHCN, for use with conv_batching.in and conv_batching.out
-    auto input0 = om.input({2,1,16,2}, mv::DType("UInt8"), mv::Order::getZMajorID(4),  {{0},{1},{},{}}, "input#170");
+    auto input0 = om.input({2,1,16,10}, mv::DType("UInt8"), mv::Order::getZMajorID(4),  {{0},{1},{},{}}, "input#170");
     std::vector<int64_t> weightsData0 = mv::utils::generateSequence<int64_t> (8, 1, 0);
     std::vector<int64_t> weightsData1 = mv::utils::generateSequence<int64_t> (8, 3, 0);
     weightsData0.insert(weightsData0.end(), weightsData1.begin(), weightsData1.end());
