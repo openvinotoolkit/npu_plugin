1edac32811e97dd896d9e6221f49d0e069dbee87
Sergey Shumihin <sergey.shumihin@intel.com>
2020-03-17 21:58:27 +0300

    [HDDL2] Enable HDDL2 Plugin by default

--- cmake/features.cmake
+++ cmake/features.cmake
@@ -6,11 +6,9 @@ include(options)
 
 ie_dependent_option(ENABLE_MCM_COMPILER "Enable mcmCompiler" ON "NOT CMAKE_CROSSCOMPILING OR DEFINED MCM_COMPILER_EXPORT_FILE" OFF)
 
-# TODO: enable by default when linker issues is fixed
-ie_dependent_option (ENABLE_HDDL2 "Enable HDDL2 Plugin" OFF "NOT ARM;NOT AARCH64" OFF)
-
-# TODO: Switch to off when CI will use real device
-ie_option(ENABLE_HDDL2_SIMULATOR "Enable XLink emulator for HDDL2 Plugin" ON)
+ie_dependent_option (ENABLE_HDDL2 "Enable HDDL2 Plugin" ON "NOT ARM;NOT AARCH64" OFF)
+ie_option(ENABLE_HDDL2_TESTS "Enable Unit and Functional tests for HDDL2 Plugin" OFF)
+ie_option(ENABLE_HDDL2_SIMULATOR "Enable XLink emulator for HDDL2 Plugin" OFF)
 
 ie_option(ENABLE_KMB_SAMPLES "Enable KMB samples" ON)
 
