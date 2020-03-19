d1195518ad9eb8332b812afe9d14952c2c57546d
Paul S. Durley <paul.s.durley@intel.com>
2020-03-19 14:09:23 -0700

    Change implicit op locations based on non-implicit ops

--- src/pass/kmb_adaptation/update_implicit_layers_quatization_params.cpp
+++ src/pass/kmb_adaptation/update_implicit_layers_quatization_params.cpp
@@ -59,14 +59,39 @@ void updateImplicitLayersLocationParamsFcn(const mv::pass::PassEntry& , mv::Comp
     {
          std::string opType = opIt->getOpType();
 
-        if (opType ==  "Slice" || opType ==  "Crop" ||
-            opType == "ImplicitReshape" || opType == "ImplicitPermute")
+        if (opType ==  "Slice" || opType ==  "Crop")
         {
             auto input = opIt->getInputTensor(0);
             auto output = opIt->getOutputTensor(0);
 
             auto inputMemoryLocation = opIt->getInputTensor(0)->get<mv::Tensor::MemoryLocation>("Location");
             opIt->getOutputTensor(0)->set<mv::Tensor::MemoryLocation>("Location", inputMemoryLocation);
+        }
+
+        if (opType == "ImplicitReshape" || opType == "ImplicitPermute")
+        {
+            auto input = opIt->getInputTensor(0);
+            // Recursively search for non-implicit input op
+            auto inputOp = om.getSourceOp(input);
+            while(!(inputOp->hasTypeTrait("executable") || inputOp->hasTypeTrait("exposed")))
+            {
+                input = inputOp->getInputTensor(0);
+                inputOp = om.getSourceOp(input);
+            }
+            // Recursively search for non-implicit output op
+            auto outputOp = opIt.leftmostOutput().sink();
+            while(!(outputOp->hasTypeTrait("executable") || outputOp->hasTypeTrait("exposed")))
+            {
+                outputOp = outputOp.leftmostOutput().sink();
+            }
+            // If to/from DDR, pick DDR as location; else use inputTensor location
+            auto inputOpMemoryLocation = inputOp->getOutputTensor(0)->get<mv::Tensor::MemoryLocation>("Location");
+            auto outputOpMemoryLocation = outputOp->getInputTensor(0)->get<mv::Tensor::MemoryLocation>("Location");
+            auto newMemoryLocation = ((inputOpMemoryLocation == mv::Tensor::MemoryLocation::DDR) ||
+                                       outputOpMemoryLocation == mv::Tensor::MemoryLocation::DDR)
+                    ? mv::Tensor::MemoryLocation::DDR
+                    : opIt->getInputTensor(0)->get<mv::Tensor::MemoryLocation>("Location");
+            opIt->getOutputTensor(0)->set<mv::Tensor::MemoryLocation>("Location", newMemoryLocation);
         }
 
     }
