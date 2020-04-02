4568da949391fc801178cf9c9baa39c5a7c3aa52
Cristian Druta <cristian.druta@intel.com>
2020-04-02 17:49:49 +0300

    debug information in dot files , dilation is 1

--- tests/layer/conv_dilated/conv_dilated.cpp
+++ tests/layer/conv_dilated/conv_dilated.cpp
@@ -9,13 +9,15 @@ int main()
 
     auto input0 = om.input({16,16,16,1}, mv::DType("UInt8"), mv::Order::getZMajorID(4),  {{0},{1},{},{}}, "input#170");
 
-    std::vector<int64_t> weightsData0 = mv::utils::generateSequence<int64_t> (8, 1, 0);
-    std::vector<int64_t> weightsData1 = mv::utils::generateSequence<int64_t> (8, -1, 0);
+    std::vector<int64_t> weightsData0 = mv::utils::generateSequence<int64_t> (16*16*9/2, 1, 0);
+    std::vector<int64_t> weightsData1 = mv::utils::generateSequence<int64_t> (16*16*9/2, -1, 0);
     weightsData0.insert(weightsData0.end(), weightsData1.begin(), weightsData1.end());
 //auto weights0 = om.constantInt(weightsData0,{1,1,16,1}, mv::DType("Int8"), mv::Order::getZMajorID(4), {{0},{1.0},{},{}});//
-    auto weights0 = om.constantInt(weightsData0,{3,3,16,1}, mv::DType("Int8"), mv::Order::getZMajorID(4), {{0},{1.0},{},{}});
+    auto weights0 = om.constantInt(weightsData0,{3,3,16,16}, mv::DType("Int8"), mv::Order::getZMajorID(4), {{0},{1.0},{},{}});
     //the 2 is dilation factor
-    auto conv0 = om.conv(input0, weights0, {1, 1}, {0, 0, 0, 0}, 2, 1,  mv::DType("Int8"),{{0},{1},{},{}} , "conv");
+    //auto conv0 = om.conv(input0, weights0, {1, 1}, {0, 0, 0, 0}, 2, 1,  mv::DType("Int8"),{{0},{1},{},{}} , "conv");
+    //dilation 1
+    auto conv0 = om.conv(input0, weights0, {1, 1}, {0, 0, 0, 0}, 1, 1,  mv::DType("Int8"),{{0},{1},{},{}} , "conv");
 
     std::string compDescPath = mv::utils::projectRootPath() + "/config/compilation/release_kmb_MC-Prefetch1.json";
     unit.loadCompilationDescriptor(compDescPath);
