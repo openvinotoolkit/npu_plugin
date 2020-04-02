42194b521d50adfa3cffd23c66a2d6136d4ea7d8
tbartsok <tasos.bartsokas@intel.com>
2020-04-02 15:30:20 +0100

    Remove printing messages from Info Label

--- src/logger/logger.cpp
+++ src/logger/logger.cpp
@@ -1,14 +1,14 @@
 #include "include/mcm/logger/logger.hpp"
 #include "include/mcm/base/printable.hpp"
 
-void mv::Logger::InfoLog(const std::string& senderName, char* format, ...) {
+void mv::Logger::DebugLog(const std::string& senderName, char* format, ...) {
     va_list args;
     va_start(args, format);
     char buffer[256];
     std::string logString;
     vsnprintf(buffer, 256, format, args);
     logString = buffer;
-    mv::Logger::log(mv::Logger::MessageType::Info, senderName, logString);
+    mv::Logger::log(mv::Logger::MessageType::Debug, senderName, logString);
     va_end(args);
 }
 
