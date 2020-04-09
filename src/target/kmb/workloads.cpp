7be4592f53d3b5a90533cdddbd7ea19616f45450
Paul S. Durley <paul.s.durley@intel.com>
2020-04-09 07:47:11 -0700

    Align split sizes to 8 for SOH

--- src/target/kmb/workloads.cpp
+++ src/target/kmb/workloads.cpp
@@ -792,7 +792,7 @@ namespace mv {
     };
 
     static SplitSliceVariant splitSliceSymmetric(unsigned W, unsigned H, unsigned N,
-                                                 bool split_over_h, bool split_over_w)
+                                                 bool split_over_h, bool split_over_w, bool is_sparse)
     {
         SplitVariant best_variant = getBestSplitSymmetric(W, H, N, split_over_h, split_over_w);
         double& cost_estimate = best_variant.cost_estimate;
@@ -810,6 +810,11 @@ namespace mv {
 
         unsigned dx = std::ceil(static_cast<double>(W) / X);
         unsigned dy = std::ceil(static_cast<double>(H) / Y);
+
+        if (is_sparse)
+        {
+            dy = std::ceil(static_cast<double>(dy)/8) * 8;
+        }
 
         SplitSliceList slice_list; // empty
         for (unsigned x=0; x * dx < W; x++)
@@ -1093,7 +1098,7 @@ int mv::Workloads::partitionTensorWithRectangleHeuristic(const mv::DPUModeList& 
                                                              + ", reduced_width="  + std::to_string(reduced_shape.W));
 
     SplitSliceVariant slicing_variant = splitSliceSymmetric(reduced_shape.W, reduced_shape.H, nWorkloads,
-                                                            split_over_h, split_over_w);
+                                                            split_over_h, split_over_w, isSparse_);
     if (!split_symmetric)
     {
         SplitSliceVariant slicing_variant_2 = splitSliceNonSymmetric(reduced_shape.W, reduced_shape.H, nWorkloads,
