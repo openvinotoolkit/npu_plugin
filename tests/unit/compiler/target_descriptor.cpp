08169557b048cf3676ab88cbc4e1d03b2d15c20f
Rob Earhart <robert.earhart@intel.com>
2020-04-17 18:49:28 +0000

    Add THB target descriptor

--- tests/unit/compiler/target_descriptor.cpp
+++ tests/unit/compiler/target_descriptor.cpp
@@ -3,29 +3,29 @@
 #include "include/mcm/utils/env_loader.hpp"
 #include <cstdlib>
 
-TEST(target_descriptor, load_from_file)
+class TargetDescriptorTest : public testing::TestWithParam<std::tuple<mv::Target, const char*>> {};
+
+TEST_P(TargetDescriptorTest, load_from_file)
 {
 
-    std::string descPath = mv::utils::projectRootPath() + std::string("/config/target/release_kmb.json");
+    std::string descPath = mv::utils::projectRootPath() + std::string("/config/target/") + std::get<1>(GetParam());
     mv::TargetDescriptor desc;
     ASSERT_TRUE(desc.load(descPath));
-    ASSERT_EQ(desc.getTarget(), mv::Target::ma2490);
+    ASSERT_EQ(desc.getTarget(), std::get<0>(GetParam()));
 
-}  
-
-class TargetDescriptorTest : public testing::TestWithParam<mv::Target> {};
+}
 
 TEST_P(TargetDescriptorTest, compose)
 {
 
     mv::TargetDescriptor desc;
     
-    desc.setTarget(GetParam());
+    desc.setTarget(std::get<0>(GetParam()));
     desc.setDType(mv::DType("Float16"));
 
     desc.defineOp("Conv");
 
-    ASSERT_EQ(desc.getTarget(), GetParam());
+    ASSERT_EQ(desc.getTarget(), std::get<0>(GetParam()));
     ASSERT_EQ(desc.getDType(), mv::DType("Float16"));
     ASSERT_TRUE(desc.opSupported("Conv"));
     ASSERT_FALSE(desc.opSupported("UndefinedOp"));
@@ -34,4 +34,5 @@ TEST_P(TargetDescriptorTest, compose)
 
 INSTANTIATE_TEST_SUITE_P(Targets,
                          TargetDescriptorTest,
-                         testing::Values(mv::Target::ma2490, mv::Target::ma3100));
+                         testing::Values(std::make_tuple(mv::Target::ma2490, "release_kmb.json"),
+                                         std::make_tuple(mv::Target::ma3100, "release_thb.json")));
