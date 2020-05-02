f2c19a4f66af6b66023a4cf7bd76effe54daad46
Eman Copty <eman.copty@intel.com>
2020-05-01 22:32:03 -0700

    Don't use dilationFactor when streaming conv
    In dilation pass reset dilation factor to 1
    Otherwise When creating streamedConvs if the dilationFactor is set
    the output size will be wrongly sized based on dilationFactor.
    At this stage the kernel size is already fixed and

--- src/pass/kmb_adaptation/streaming/spatial_split_streaming.cpp
+++ src/pass/kmb_adaptation/streaming/spatial_split_streaming.cpp
@@ -537,7 +537,7 @@ std::tuple<mv::Data::TensorIterator, mv::Data::TensorIterator,mv::Data::TensorIt
                                 op->getInputTensor(1),
                                 kernelStride,
                                 currentPad,
-                                op->get<unsigned>("dilationFactor"),
+                                1, //this is already taken care of, dont want to change kernel size again
                                 op->get<mv::DType>("dType"),
                                 op->get<mv::QuantizationParams>("quantParams"),
                                 streamingOpName);
@@ -547,7 +547,7 @@ std::tuple<mv::Data::TensorIterator, mv::Data::TensorIterator,mv::Data::TensorIt
                                 op->getInputTensor(1),
                                 kernelStride,
                                 currentPad,
-                                op->get<unsigned>("dilationFactor"),
+                                1, //this is already taken care of, dont want to change kernel size again
                                 op->get<unsigned>("group"),
                                 op->get<mv::DType>("dType"),
                                 op->get<mv::QuantizationParams>("quantParams"),
