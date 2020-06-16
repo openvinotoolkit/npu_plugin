bb0054eeb707d20884bef079f1ad7b027d94cbc7
Artem Bolshakov <abolshak@intel.com>
2020-06-16 16:27:34 -0400

    First attempt at fp16full, plus games with dots.

---
+++ tests/layer/conv_fp16full/conv_fp16full.cpp
@@ -0,0 +1,29 @@
+#include "include/mcm/compiler/compilation_unit.hpp"
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+
+    mv::CompilationUnit unit("Fp16Full");
+    mv::OpModel& om = unit.model();
+
+    //Input full of -0.5s
+    auto input0 = om.input({16,16,16,1}, mv::DType("Float16"), mv::Order::getZMajorID(4),  {{0},{1.0},{},{}}, "input#170");
+
+    std::vector<double> weightsData0 = mv::utils::generateSequence<double> (32*16*3*3, 1.0, 0.0); 
+    //Wights first input channel of output channel full of 1s
+    auto weights0 = om.constant(weightsData0,{3,3,16,32}, mv::DType("Float16"), mv::Order::getZMajorID(4), {{0},{1.0},{},{}});
+    auto conv0 = om.conv(input0, weights0, {1, 1}, {0, 0, 0, 0}, 1, 1,  mv::DType("Float16"),{{0},{1.0},{},{}} , "conv");
+    om.output(conv0);
+
+    std::string compDescPath = mv::utils::projectRootPath() + "/config/compilation/release_kmb.json";
+    unit.loadCompilationDescriptor(compDescPath);
+    mv::CompilationDescriptor &compDesc = unit.compilationDescriptor();
+
+    unit.loadTargetDescriptor(mv::Target::ma2490);
+    unit.initialize();
+    unit.run();
+
+    return 0;
+}
