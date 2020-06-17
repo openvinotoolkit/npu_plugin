6756db9449cadc7cbf574d31eebd0a98c7df5be5
Artem Bolshakov <abolshak@intel.com>
2020-06-17 17:01:29 -0400

    conv_fp16full seems to compile.

--- tests/layer/conv_fp16full/conv_fp16full.cpp
+++ tests/layer/conv_fp16full/conv_fp16full.cpp
@@ -11,9 +11,9 @@ int main()
     //Input full of -0.5s
     auto input0 = om.input({16,16,16,1}, mv::DType("Float16"), mv::Order::getZMajorID(4),  {{0},{1.0},{},{}}, "input#170");
 
-    std::vector<double> weightsData0 = mv::utils::generateSequence<double> (32*16*3*3, 1.0, 0.0); 
+    std::vector<int64_t> weightsData0 = mv::utils::generateSequence<int64_t> (32*16*3*3, 1, 0); 
     //Wights first input channel of output channel full of 1s
-    auto weights0 = om.constant(weightsData0,{3,3,16,32}, mv::DType("Float16"), mv::Order::getZMajorID(4), {{0},{1.0},{},{}});
+    auto weights0 = om.constantInt(weightsData0,{3,3,16,32}, mv::DType("Float16"), mv::Order::getZMajorID(4), {{0},{1.0},{},{}});
     auto conv0 = om.conv(input0, weights0, {1, 1}, {0, 0, 0, 0}, 1, 1,  mv::DType("Float16"),{{0},{1.0},{},{}} , "conv");
     om.output(conv0);
 
