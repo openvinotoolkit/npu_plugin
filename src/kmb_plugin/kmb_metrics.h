d0c73b4a487d072f42840a9a426c643a8a625fb9
Lavrenov, Ilya <ilya.lavrenov@intel.com>
2020-06-23 11:24:53 +0000

    Remove usage of deprecated Data ctor

--- src/kmb_plugin/kmb_metrics.h
+++ src/kmb_plugin/kmb_metrics.h
@@ -59,10 +59,10 @@ private:
     const std::vector<std::string> _optimizationCapabilities = {METRIC_VALUE(INT8)};
 
     // Metric to provide a hint for a range for number of async infer requests. (bottom bound, upper bound, step)
-    const std::tuple<uint32_t, uint32_t, uint32_t> _rangeForAsyncInferRequests = {4, 10, 1};
+    const std::tuple<uint32_t, uint32_t, uint32_t> _rangeForAsyncInferRequests {4u, 10u, 1u};
 
     // Metric to provide information about a range for streams.(bottom bound, upper bound)
-    const std::tuple<uint32_t, uint32_t> _rangeForStreams = {1, 4};
+    const std::tuple<uint32_t, uint32_t> _rangeForStreams {1u, 4u};
 };
 
 }  // namespace KmbPlugin
