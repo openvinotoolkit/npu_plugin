db48be88600f1248568aef199d4e5ae27177dccd
Artem Bolshakov <abolshak@intel.com>
2020-06-25 11:36:41 -0400

    Fixed library inclusion issue, and removed defunct conv_fp16full unit test.

--- src/pass/adaptation/remove_passes.cpp
+++ src/pass/adaptation/remove_passes.cpp
@@ -1,7 +1,10 @@
 #include "include/mcm/pass/pass_registry.hpp"
 #include "include/mcm/op_model.hpp"
 #include "include/mcm/computation/model/data_model.hpp"
+#include "include/mcm/utils/warning_manager.hpp"
 #include "include/mcm/pass/pass_utils.hpp"
+#include <functional>
+#include <string>
 
 void removeSimpleOp(mv::Data::OpListIterator &opIt, mv::ComputationModel &model, std::string opType);
 void removeShapeRelevant(mv::Data::OpListIterator &opIt, mv::ComputationModel &model, std::string opType);
@@ -26,6 +29,8 @@ void removeOpsFcn(const mv::pass::PassEntry& pass, mv::ComputationModel& model,
                        mv::TargetDescriptor&, mv::Element&, mv::Element&)
 {
     pass.log(mv::Logger::MessageType::Debug, "Removal passes are starting");
+
+    mv::OpModel om(model);
     std::unordered_map<std::string, std::function<void(mv::Data::OpListIterator &, mv::ComputationModel& , std::string &)>> remTaskMap =
                                     {{"Identity", removeSimpleOp},
                                     {"Dropout", removeSimpleOp},
