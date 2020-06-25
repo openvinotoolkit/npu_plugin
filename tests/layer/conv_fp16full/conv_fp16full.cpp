e5e18964ea452dcc5d806a9b0f2a918df7c6080f
Artem Bolshakov <abolshak@intel.com>
2020-06-25 08:18:38 -0400

    Almost done; now to rename files and save the correct .out

--- tests/layer/conv_fp16full/conv_fp16full.cpp
+++ tests/layer/conv_fp16full/conv_fp16full.cpp
@@ -9,18 +9,18 @@ int main()
     mv::OpModel& om = unit.model();
 
     //Input full of -0.5s
-    auto input0 = om.input({16,16,16,1}, mv::DType("Float16"), mv::Order::getZMajorID(4),  {{0},{1.0},{},{}}, "input#170");
+    auto input0 = om.input({16,16,16,1}, mv::DType("UInt8"), mv::Order::getZMajorID(4),  {{0},{1.0/255.0},{},{}}, "input#170");
 
-    std::vector<int64_t> weightsData0 = mv::utils::generateSequence<int64_t> (32*16*3*3, 1, 0); 
+    std::vector<int64_t> weightsData0 = mv::utils::generateSequence<int64_t> (32*16*3*3, 255, 0); 
     //Wights first input channel of output channel full of 1s
-    auto weights0 = om.constantInt(weightsData0,{3,3,16,32}, mv::DType("Float16"), mv::Order::getZMajorID(4), {{0},{1.0},{},{}});
-    auto conv0 = om.conv(input0, weights0, {1, 1}, {0, 0, 0, 0}, 1, 1,  mv::DType("Float16"),{{0},{1.0},{},{}} , "conv");
+    auto weights0 = om.constantInt(weightsData0,{3,3,16,32}, mv::DType("UInt8"), mv::Order::getZMajorID(4), {{0},{1.0/255.0},{},{}});
+    auto conv0 = om.conv(input0, weights0, {1, 1}, {0, 0, 0, 0}, 1, 1,  mv::DType("UInt8"),{{0},{144.0/255.0},{},{}} , "conv");
     om.output(conv0);
 
     std::string compDescPath = mv::utils::projectRootPath() + "/config/compilation/release_kmb.json";
     unit.loadCompilationDescriptor(compDescPath);
     mv::CompilationDescriptor &compDesc = unit.compilationDescriptor();
-    unit.compilationDescriptor().remove("adapt", "PostTrainingQuantize"); //So that we really use fp16
+//    unit.compilationDescriptor().remove("adapt", "PostTrainingQuantize"); //So that we really use fp16
 
     unit.loadTargetDescriptor(mv::Target::ma2490);
     unit.initialize();
