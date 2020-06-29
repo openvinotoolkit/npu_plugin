da93b6f5fa5f67d96a8dc15433eef8e7f267f58f
Losev, Sergey E <sergey.e.losev@intel.com>
2020-06-29 20:45:07 +0300

    VPUSMM allocator for aarch64 executor

--- tests_deprecated/unit/kmb_plugin/kmb_infer_request_tests.cpp
+++ tests_deprecated/unit/kmb_plugin/kmb_infer_request_tests.cpp
@@ -23,6 +23,7 @@
 #include "kmb_infer_request.h"
 #include "kmb_private_config.hpp"
 #include "kmb_native_allocator.h"
+#include "kmb_vpusmm_allocator.h"
 
 using namespace ::testing;
 using namespace vpu::KmbPlugin;
@@ -52,7 +53,11 @@ protected:
 
 class MockExecutor : public KmbExecutor {
 public:
+#if defined(__arm__) || defined(__aarch64__)
+    MockExecutor(const KmbConfig& config): KmbExecutor(config, std::make_shared<KmbVpusmmAllocator>()) {}
+#else
     MockExecutor(const KmbConfig& config): KmbExecutor(config, std::make_shared<KmbNativeAllocator>()) {}
+#endif
 
     MOCK_METHOD0(deallocateGraph, void());
     MOCK_METHOD1(allocateGraph, void(const std::vector<char>&));
