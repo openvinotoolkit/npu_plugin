5e1033b314c50fb2d0a29c00752cbded1ba17a14
Talamanov, Anatoliy <anatoliy.talamanov@intel.com>
2020-07-09 18:48:52 +0000

    Add test for permute layer

---
+++ tests_deprecated/functional/new/test_model/kmb_test_permute_def.hpp
@@ -0,0 +1,42 @@
+//
+// Copyright 2019 Intel Corporation.
+//
+// This software and the related documents are Intel copyrighted materials,
+// and your use of them is governed by the express license under which they
+// were provided to you (End User License Agreement for the Intel(R) Software
+// Development Products (Version May 2017)). Unless the License provides
+// otherwise, you may not use, modify, copy, publish, distribute, disclose or
+// transmit this software or the related documents without Intel's prior
+// written permission.
+//
+// This software and the related documents are provided as is, with no
+// express or implied warranties, other than those that are expressly
+// stated in the License.
+//
+
+#pragma once
+
+#include "kmb_test_model.hpp"
+#include "kmb_test_utils.hpp"
+
+struct PermuteLayerDef final {
+    TestNetwork& testNet;
+    std::string name;
+    PortInfo inputPort;
+    PortInfo constPort;
+
+    PermuteLayerDef(TestNetwork& testNet, std::string name)
+        : testNet(testNet), name(std::move(name)) {}
+
+    PermuteLayerDef& input(const std::string& layerName, size_t index = 0) {
+        inputPort = PortInfo(layerName, index);
+        return *this;
+    }
+
+    PermuteLayerDef& order(const std::string& layerName, size_t index = 0) {
+        constPort = PortInfo(layerName, index);
+        return *this;
+    }
+
+    TestNetwork& build();
+};
