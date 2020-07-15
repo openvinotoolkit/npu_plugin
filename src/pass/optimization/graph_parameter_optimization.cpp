d870a7fb31a4a4854e4f43f811f31b8b3f942667
tbartsok <tasos.bartsokas@intel.com>
2020-07-15 18:48:22 +0100

    remove dwise dilation, as it does not support idu sparsity

--- src/pass/optimization/graph_parameter_optimization.cpp
+++ src/pass/optimization/graph_parameter_optimization.cpp
@@ -800,7 +800,7 @@ namespace mv
             // In these cases parent output sparsity does matter, but child input sparsity must be true
             bool requiresCompilerActivationSparsity(Op& op)
             {
-                if (((op.getOpType() == "Conv") or (op.getOpType() == "DepthwiseConv"))
+                if (((op.getOpType() == "Conv"))
                          and (op.hasAttr("DilatedSubConv") and op.get<bool>("DilatedSubConv")))
                     return true;
 
