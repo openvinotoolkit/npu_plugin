962325a4b879a288e3c9b64bd67a763ab89f03c0
Somsikov, Andrey <andrey.somsikov@intel.com>
2020-07-16 17:47:46 +0300

    Check for time tool before use in compile tests

--- tests/functional/compile/test_compile.py
+++ tests/functional/compile/test_compile.py
@@ -19,7 +19,8 @@ def run(args, log=None, verbose=True):
         log = logging.getLogger()
     log_out = log.info if verbose else log.debug
 
-    log_out(f'========== cmd: {" ".join(args)}')  # pylint: disable=logging-format-interpolation,logging-fstring-interpolation
+    log_out(  # pylint: disable=logging-fstring-interpolation
+        f'========== cmd: {" ".join(args)}')
 
     proc = subprocess.Popen(args,
                             stdout=subprocess.PIPE,
@@ -42,20 +43,34 @@ def run(args, log=None, verbose=True):
     return proc.returncode, ''.join(output)
 
 
+def measured_run(args, **kwargs):
+    """ Run command and measure peak memry used
+    """
+    if os.path.exists('/usr/bin/time'):
+        with tempfile.NamedTemporaryFile() as time_file:
+            returncode, output = run([
+                '/usr/bin/time',
+                '--format=%M',
+                f'--output={time_file.name}',
+                '--quiet'] + args, **kwargs)
+            peak_memory = open(time_file.name).read().strip()
+        return returncode, output, peak_memory
+    returncode, output = run(args, **kwargs)
+    return returncode, output, None
+
+
 def test_compile(request, param_ir):
     """ Test network can be compiled
     """
     out = os.path.splitext(os.path.join(param_ir.output_dir, param_ir.model))[0] + ".blob"
     os.makedirs(os.path.split(out)[0])
-    with tempfile.NamedTemporaryFile() as time_file:
-        returncode, output = run(
-            ['/usr/bin/time', '--format=%M', f'--output={time_file.name}', '--quiet',
-             param_ir.compiler_tool,
-             '-d=KMB',
-             f'-m={os.path.join(param_ir.models_dir, param_ir.model)}',
-             f'-o={out}'])
-        print(output)
-        peak_memory = open(time_file.name).read().strip()
-        request.node.peak_memory = peak_memory
-        print(f'Peak memory consumption {peak_memory}Kb')
-        assert returncode == 0, f'Command exited with non-zero status {returncode}'
+    returncode, output, peak_memory = measured_run([
+        param_ir.compiler_tool,
+        '-d=KMB',
+        f'-m={os.path.join(param_ir.models_dir, param_ir.model)}',
+        f'-o={out}'
+    ])
+    print(output)
+    request.node.peak_memory = peak_memory
+    print(f'Peak memory consumption {peak_memory}Kb')
+    assert returncode == 0, f'Command exited with non-zero status {returncode}'
