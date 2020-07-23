ac6c7ca0289d9698c59329ce1d1a5f2e1bb2f929
Eman Copty <eman.copty@intel.com>
2020-07-22 17:50:52 -0700

    revert changes to unit test

--- tests/layer/conv_uint8_demo/conv_uint8_demo.cpp
+++ tests/layer/conv_uint8_demo/conv_uint8_demo.cpp
@@ -9,12 +9,12 @@ int main()
     mv::OpModel& om = unit.model();
 
     //Input full of -0.5s
-    auto input0 = om.input({119,89,256,1}, mv::DType("UInt8"), mv::Order::getZMajorID(4),  {{0},{1.0/255.0},{},{}}, "input#170");
+    auto input0 = om.input({16,16,16,1}, mv::DType("UInt8"), mv::Order::getZMajorID(4),  {{0},{1.0/255.0},{},{}}, "input#170");
 
-    std::vector<int64_t> weightsData0 = mv::utils::generateSequence<int64_t> (128*256*3*3, 255, 0);
+    std::vector<int64_t> weightsData0 = mv::utils::generateSequence<int64_t> (32*16*3*3, 255, 0); 
     //Wights first input channel of output channel full of 1s
-    auto weights0 = om.constantInt(weightsData0,{3,3,256,128}, mv::DType("UInt8"), mv::Order::getZMajorID(4), {{0},{1.0/255.0},{},{}});
-    auto conv0 = om.conv(input0, weights0, {1, 1}, {2,2,2,2}, 2, 1,  mv::DType("UInt8"),{{0},{144.0/255.0},{},{}} , "conv");
+    auto weights0 = om.constantInt(weightsData0,{3,3,16,32}, mv::DType("UInt8"), mv::Order::getZMajorID(4), {{0},{1.0/255.0},{},{}});
+    auto conv0 = om.conv(input0, weights0, {1, 1}, {0, 0, 0, 0}, 1, 1,  mv::DType("UInt8"),{{0},{144.0/255.0},{},{}} , "conv");
     om.output(conv0);
 
     std::string compDescPath = mv::utils::projectRootPath() + "/config/compilation/release_kmb.json";
