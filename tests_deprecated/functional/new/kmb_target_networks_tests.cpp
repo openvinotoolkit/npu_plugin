ee58bed34adbb3a090c001ccf9b1655546e584d1
Andrey Markelov <andrey.markelov@intel.com>
2020-07-30 14:17:15 +0300

    Add custom compilation descriptor for RetinaFace network

--- tests_deprecated/functional/new/kmb_target_networks_tests.cpp
+++ tests_deprecated/functional/new/kmb_target_networks_tests.cpp
@@ -694,11 +694,11 @@ TEST_F(KmbClassifyNetworkTest, precommit_vgg16_caffe_dense_int8_IRv10_from_fp32)
 }
 
 TEST_F(KmbRetinaFaceNetworkTest, precommit_retinaface_mobilenetv2_0_25_modified) {
-    // SKIP_ON("KMB", "HDDL2", "VPU");  // "compile error"
     runTest(
             TestNetworkDesc("KMB_models/INT8/private/retinaface-mobilenetv2-0.25-modified/retinaface-mobilenetv2-0.25-modified.xml")
                     .setUserInputPrecision("input", Precision::U8)
-                    .setUserInputLayout("input", Layout::NHWC),
+                    .setUserInputLayout("input", Layout::NHWC)
+                    .setCompileConfig({{"VPU_COMPILER_COMPILATION_DESCRIPTOR", "release_kmb_retinaface"}}),
             "data",
             TestImageDesc("224x224/cat3.bmp", false));
 }
