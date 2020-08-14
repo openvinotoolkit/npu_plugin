169395f602e343105565b8d4912165362c62c499
Andrey Markelov <andrey.markelov@intel.com>
2020-08-14 18:22:39 +0300

    remove matchElementsByLexicographicalOrder function

--- src/frontend_mcm/include/mcm_network_description.hpp
+++ src/frontend_mcm/include/mcm_network_description.hpp
@@ -54,8 +54,6 @@ private:
     std::shared_ptr<vpu::Logger> _logger;
 
     vpux::DataMap matchElementsByName(const vpux::DataMap& actualDeviceData, const std::vector<std::string>& names);
-    vpux::DataMap matchElementsByLexicographicalOrder(
-        const vpux::DataMap& actualDeviceData, const std::vector<std::string>& names);
     vpux::DataMap createDeviceMapWithCorrectNames(
         const vpux::DataMap& actualDeviceData, const std::vector<std::string>& names);
 };
--- src/frontend_mcm/src/mcm_network_description.cpp
+++ src/frontend_mcm/src/mcm_network_description.cpp
@@ -135,31 +135,12 @@ vpux::DataMap MCMNetworkDescription::matchElementsByName(
     return updatedMap;
 }
 
-vpux::DataMap MCMNetworkDescription::matchElementsByLexicographicalOrder(
-    const vpux::DataMap& actualDeviceData, const std::vector<std::string>& names) {
-    _logger->debug("MCMNetworkDescription::matchElementsByLexicographicalOrder started.");
-    vpux::DataMap updatedMap;
-
-    std::size_t curMatchPos = 0;
-    for (const auto& data : actualDeviceData) {
-        auto name = names[curMatchPos];
-        updatedMap.insert({name, data.second});
-        _logger->debug("Matched \'%s\' with \'%s'\\n", name, data.first);
-        curMatchPos++;
-    }
-
-    _logger->debug("MCMNetworkDescription::matchElementsByLexicographicalOrder finished.");
-    return updatedMap;
-}
-
 vpux::DataMap MCMNetworkDescription::createDeviceMapWithCorrectNames(
     const vpux::DataMap& actualDeviceData, const std::vector<std::string>& names) {
     vpux::DataMap updatedMap;
 
     updatedMap = matchElementsByName(actualDeviceData, names);
-    if (updatedMap.empty()) {
-        updatedMap = matchElementsByLexicographicalOrder(actualDeviceData, names);
-    }
+    IE_ASSERT(updatedMap.size() == actualDeviceData.size());
 
     return updatedMap;
 }
