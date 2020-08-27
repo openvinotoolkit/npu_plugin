8caac63161ec0305facfb9941d13e9dd5a6a88b4
mznamens <mikhail.znamenskiy@intel.com>
2020-08-27 09:02:16 +0300

    Move/rename HDDL2RemoteBlob performance tests

--- tests_deprecated/unit/hddl2_tests/hddl2_remote_blob.cpp
+++ tests_deprecated/unit/hddl2_tests/hddl2_remote_blob.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright 2019 Intel Corporation.
+// Copyright 2020 Intel Corporation.
 //
 // This software and the related documents are Intel copyrighted materials,
 // and your use of them is governed by the express license under which they
@@ -14,8 +14,6 @@
 // stated in the License.
 //
 
-#include <chrono>
-
 #include "hddl2_remote_blob.h"
 
 #include <gtest/gtest.h>
@@ -27,6 +25,7 @@
 #include "hddl2_params.hpp"
 #include "helper_remote_context.h"
 #include "memory_usage.h"
+
 using namespace vpu::HDDL2Plugin;
 namespace IE = InferenceEngine;
 
@@ -175,37 +174,6 @@ TEST_F(HDDL2_RemoteBlob_UnitTests, CascadeROIBlobCorrect) {
     ASSERT_NO_THROW(blobDataAfter.assign(bDataAfter, bDataAfter + bSizeAfter));
     ASSERT_TRUE(blobDataBefore == blobDataAfter);
     ASSERT_TRUE(remoteBlobPtr->deallocate());
-}
-
-//------------------------------------------------------------------------------
-//      class HDDL2_RemoteBlob_UnitTests - check performance
-//------------------------------------------------------------------------------
-TEST_F(HDDL2_RemoteBlob_UnitTests, createRemoteBlobPerformance) {
-    const size_t BLOBS_COUNT = 1000000;
-    auto start_time = std::chrono::steady_clock::now();
-    for (size_t cur_blob = 0; cur_blob < BLOBS_COUNT; ++cur_blob) {
-        IE::RemoteBlob::Ptr remoteBlobPtr = remoteContextPtr->CreateBlob(tensorDesc, blobParamMap);
-    }
-    auto end_time = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end_time - start_time;
-
-    const double MAX_SPENT_TIME = 1.0;
-    ASSERT_LE(elapsed_seconds.count(), MAX_SPENT_TIME);
-}
-
-TEST_F(HDDL2_RemoteBlob_UnitTests, createROIBlobPerformance) {
-    IE::ROI roi {0, 2, 2, 221, 221};
-    const size_t BLOBS_COUNT = 1000000;
-    IE::RemoteBlob::Ptr remoteBlobPtr = remoteContextPtr->CreateBlob(tensorDesc, blobParamMap);
-    auto start_time = std::chrono::steady_clock::now();
-    for (size_t cur_blob = 0; cur_blob < BLOBS_COUNT; ++cur_blob) {
-        IE::RemoteBlob::Ptr remoteROIBlobPtr = std::static_pointer_cast <IE::RemoteBlob> (remoteBlobPtr->createROI(roi));
-    }
-    auto end_time = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end_time - start_time;
-
-    const double MAX_SPENT_TIME = 0.5;
-    ASSERT_LE(elapsed_seconds.count(), MAX_SPENT_TIME);
 }
 
 //------------------------------------------------------------------------------
