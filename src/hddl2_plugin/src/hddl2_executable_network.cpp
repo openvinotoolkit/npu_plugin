03cf5d070df4e959776583a5a2af11a0cf6ca48d
Skrebkov, Artemy <artemy.skrebkov@intel.com>
2020-09-03 19:12:02 +0000

    Enable ngraph parser for HDDL2

--- src/hddl2_plugin/src/hddl2_executable_network.cpp
+++ src/hddl2_plugin/src/hddl2_executable_network.cpp
@@ -19,11 +19,17 @@
 #include <hddl2_executable_network.h>
 #include <hddl2_infer_request.h>
 #include <hddl2_metrics.h>
+#include <net_pass.h>
 
+#include <convert_function_to_cnn_network.hpp>
 #include <fstream>
+#include <generic_ie.hpp>
 #include <memory>
 #include <string>
 #include <threading/ie_executor_manager.hpp>
+#include <transformations/convert_opset1_to_legacy/convert_opset1_to_legacy.hpp>
+#include <transformations/convert_opset1_to_legacy/convert_prior_to_ie_prior.hpp>
+#include <transformations/convert_opset2_to_opset1/convert_opset2_to_opset1.hpp>
 #include <vector>
 #include <vpux_compiler.hpp>
 
@@ -46,7 +52,45 @@ ExecutableNetwork::ExecutableNetwork(const vpu::HDDL2Config& config)
 ExecutableNetwork::ExecutableNetwork(
     IE::ICNNNetwork& network, const vpu::HDDL2Config& config, const IE::RemoteContext::Ptr& ieContext)
     : ExecutableNetwork(config) {
-    _networkPtr = _compiler->compile(network, _config);
+    // FIXME: This is a copy-paste from kmb_executable_network.cpp
+    // should be fixed after switching to VPUX completely
+    const bool kmb_use_ngraph = (NULL != getenv("KMB_USE_NGRAPH_PARSER"));
+    if (kmb_use_ngraph || _config.useNGraphParser()) {
+        if (const auto func = network.getFunction()) {
+            _logger->info("Using NGraph parser");
+            IE::InputsDataMap inputsInfo;
+            network.getInputsInfo(inputsInfo);
+
+            IE::OutputsDataMap outputsInfo;
+            network.getOutputsInfo(outputsInfo);
+
+            _networkPtr = _compiler->compile(func, network.getName(), inputsInfo, outputsInfo, _config);
+        } else {
+            _logger->warning("Failed to read NGraph network");
+        }
+    } else {
+        _logger->info("NGraph parser disabled");
+        _logger->info("Using CNNNetwork parser");
+        // HACK: convert nGraph to old CNNNetwork to fix LP transformations
+
+        std::shared_ptr<IE::ICNNNetwork> convertedNetwork;
+        auto actualNetwork = &network;
+
+        if (network.getFunction()) {
+            auto nGraphFunc = network.getFunction();
+            // Disable shape inference (WA for generic operations)
+            ::ngraph::op::GenericIE::DisableReshape noReshape(nGraphFunc);
+
+            // Note: instead of running all Conversion Transformations you can make up your own transformation pipeline
+            ngraph::pass::ConvertPriorBox().run_on_function(nGraphFunc);
+            ngraph::pass::ConvertOpSet2ToOpSet1().run_on_function(nGraphFunc);
+            ngraph::pass::ConvertOpSet1ToLegacy().run_on_function(nGraphFunc);
+            convertedNetwork = InferenceEngine::details::convertFunctionToICNNNetwork(nGraphFunc, network, true);
+            actualNetwork = convertedNetwork.get();
+        }
+
+        _networkPtr = _compiler->compile(*actualNetwork, _config);
+    }
     _executorPtr = vpux::HDDL2::HDDL2Executor::prepareExecutor(_networkPtr, config, ieContext);
 }
 
