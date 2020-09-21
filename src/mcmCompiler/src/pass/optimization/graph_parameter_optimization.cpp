de66d6b36f51a807beb13fc97319dc20253e2891
tbartsok <tasos.bartsokas@intel.com>
2020-09-21 17:06:44 +0100

    Decrease the number of outputs for networks that have multiple ones, so the model cutter can work.

--- src/mcmCompiler/src/pass/optimization/graph_parameter_optimization.cpp
+++ src/mcmCompiler/src/pass/optimization/graph_parameter_optimization.cpp
@@ -1825,8 +1825,9 @@ namespace mv
                         }
                     }
                 }
-                else if (childOpType == "Concat")
+                else if (childOpType == "Concat" )
                 {
+                    //NOTE: This is not correct for the spilling concats, as there the strategies can change with the dma...
                     std::pair<std::string, std::string> possibleCombination(parentClustering, childClustering);
                     for (auto restrictedCombination: incompatibleStrategiesChildConcat)
                     {
