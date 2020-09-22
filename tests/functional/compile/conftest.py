28f40cf5699c9534e717b4669cc1585cfb48a232
askrebko <artemy.skrebkov@intel.com>
2020-09-22 11:56:34 +0300

    Switch to ww34 models for compile and infer tests

--- tests/functional/compile/conftest.py
+++ tests/functional/compile/conftest.py
@@ -29,22 +29,22 @@ from py.xml import html  # pylint: disable=no-name-in-module,import-error
 # Keem Bay KPI models list
 # find <models path> -wholename "*/FP16-INT8/*.xml"
 KMB_KPI_MODELS = [
-    'vd_kmb_models_intel_ww22.tar.bz2/yolo-v2-tiny-ava-0001/tf/FP16-INT8/yolo-v2-tiny-ava-0001.xml',
-    'vd_kmb_models_intel_ww22.tar.bz2/yolo-v2-tiny-ava-sparse-30-0001/tf/FP16-INT8/yolo-v2-tiny-ava-sparse-30-0001.xml',
-    'vd_kmb_models_intel_ww22.tar.bz2/yolo-v2-tiny-ava-sparse-60-0001/tf/FP16-INT8/yolo-v2-tiny-ava-sparse-60-0001.xml',
-    'vd_kmb_models_public_ww22.tar.bz2/resnet-50-pytorch/caffe2/FP16-INT8/resnet-50-pytorch.xml',
-    'vd_kmb_models_public_ww22.tar.bz2/mobilenet-v2/caffe2/FP16-INT8/mobilenet-v2.xml',
-    'vd_kmb_models_public_ww22.tar.bz2/googlenet-v1/tf/FP16-INT8/googlenet-v1.xml',
-    'vd_kmb_models_public_ww22.tar.bz2/googlenet-v3/tf/FP16-INT8/googlenet-v3.xml',
-    'vd_kmb_models_public_ww22.tar.bz2/squeezenet1.1/caffe2/FP16-INT8/squeezenet1.1.xml',
+    'vd-kmb-models-kpi-ww34.tar.bz2/yolo-v2-tiny-ava-0001/tf/FP16-INT8/yolo-v2-tiny-ava-0001.xml',
+    'vd-kmb-models-kpi-ww34.tar.bz2/yolo-v2-tiny-ava-sparse-30-0001/tf/FP16-INT8/yolo-v2-tiny-ava-sparse-30-0001.xml',
+    'vd-kmb-models-kpi-ww34.tar.bz2/yolo-v2-tiny-ava-sparse-60-0001/tf/FP16-INT8/yolo-v2-tiny-ava-sparse-60-0001.xml',
+    'vd-kmb-models-kpi-ww34.tar.bz2/resnet-50-pytorch/caffe2/FP16-INT8/resnet-50-pytorch.xml',
+    'vd-kmb-models-kpi-ww34.tar.bz2/mobilenet-v2/caffe2/FP16-INT8/mobilenet-v2.xml',
+    'vd-kmb-models-kpi-ww34.tar.bz2/googlenet-v1/tf/FP16-INT8/googlenet-v1.xml',
+    'vd-kmb-models-kpi-ww34.tar.bz2/googlenet-v3/tf/FP16-INT8/googlenet-v3.xml',
+    'vd-kmb-models-kpi-ww34.tar.bz2/squeezenet1.1/caffe2/FP16-INT8/squeezenet1.1.xml',
 
-    {'marks': pytest.mark.xfail, 'path': 'vd_kmb_models_intel_ww22.tar.bz2/faster-rcnn-resnet101-coco-sparse-60-0001/tf/FP16-INT8/faster-rcnn-resnet101-coco-sparse-60-0001.xml'},
-    {'path': 'vd_kmb_models_public_ww22.tar.bz2/ssd512/caffe/FP16-INT8/ssd512.xml'},
+    {'marks': pytest.mark.xfail, 'path': 'vd-kmb-models-kpi-ww34.tar.bz2/faster-rcnn-resnet101-coco-sparse-60-0001/tf/FP16-INT8/faster-rcnn-resnet101-coco-sparse-60-0001.xml'},
+    {'path': 'vd-kmb-models-kpi-ww34.tar.bz2/ssd512/caffe/FP16-INT8/ssd512.xml'},
     {'marks': pytest.mark.xfail,
-     'path': 'vd_kmb_models_intel_ww22.tar.bz2/icnet-camvid-ava-0001/tf/FP16-INT8/icnet-camvid-ava-0001.xml'},
-    {'path': 'vd_kmb_models_intel_ww22.tar.bz2/yolo-v2-ava-0001/tf/FP16-INT8/yolo-v2-ava-0001.xml'},
-    {'path': 'vd_kmb_models_intel_ww22.tar.bz2/yolo-v2-ava-sparse-35-0001/tf/FP16-INT8/yolo-v2-ava-sparse-35-0001.xml'},
-    {'path': 'vd_kmb_models_intel_ww22.tar.bz2/yolo-v2-ava-sparse-70-0001/tf/FP16-INT8/yolo-v2-ava-sparse-70-0001.xml'},
+     'path': 'vd-kmb-models-kpi-ww34.tar.bz2/icnet-camvid-ava-0001/tf/FP16-INT8/icnet-camvid-ava-0001.xml'},
+    {'path': 'vd-kmb-models-kpi-ww34.tar.bz2/yolo-v2-ava-0001/tf/FP16-INT8/yolo-v2-ava-0001.xml'},
+    {'path': 'vd-kmb-models-kpi-ww34.tar.bz2/yolo-v2-ava-sparse-35-0001/tf/FP16-INT8/yolo-v2-ava-sparse-35-0001.xml'},
+    {'path': 'vd-kmb-models-kpi-ww34.tar.bz2/yolo-v2-ava-sparse-70-0001/tf/FP16-INT8/yolo-v2-ava-sparse-70-0001.xml'},
 ]
 
 
