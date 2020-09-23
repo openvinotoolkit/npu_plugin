dd910a38152721efe784179bc3676bb98e298841
john.brady@intel.com <john.brady@intel.com>
2020-09-23 10:13:43 +0100

    more specific rules for disabling layer 79 for SOH in ACLnet

--- src/mcmCompiler/src/pass/optimization/graph_parameter_optimization.cpp
+++ src/mcmCompiler/src/pass/optimization/graph_parameter_optimization.cpp
@@ -95,7 +95,7 @@ namespace mv
                 SparsitySpilling,
                 PipelineNotPossible,
                 DeConvSubConvSOKHeight,
-                SpiltOverHWithOneInputChannel,
+                SpiltOverHForLayer79InACLNet,
                 UpaHKSwitch,
                 Unknown
             };
@@ -126,7 +126,7 @@ namespace mv
                 {FailCause::SparsitySpilling, "SparsitySpilling"},
                 {FailCause::PipelineNotPossible, "PipelinedNotPossible"},
                 {FailCause::DeConvSubConvSOKHeight, "DeConvSubConvSOKHeight"},
-                {FailCause::SpiltOverHWithOneInputChannel, "SpiltOverHWithOneInputChannel"},
+                {FailCause::SpiltOverHForLayer79InACLNet, "SpiltOverHForLayer79InACLNet"},
                 {FailCause::UpaHKSwitch, "UpaHKSwitch"},
                 {FailCause::Unknown, "Unknown"}
             };
@@ -1014,8 +1014,11 @@ namespace mv
                 
                 // This is intended to be a temporary workaround for ACLnet, layer '79', which does work with SOH
                 // It has not been root caused to the compiler or runtime but as of now the compiler logic seems OK
-                if (clustering == "SplitOverH" && op.getOpType() == "Conv" && !isChanMajor && op.getInputTensor()[0]->getShape()[mv::IO_CHANNEL_DIMENSION] == 1)
-                    return FailCause::SpiltOverHWithOneInputChannel;
+                if (clustering == "SplitOverH" && op.getOpType() == "Conv" && !isChanMajor && op.getInputTensor()[0]->getShape()[mv::IO_CHANNEL_DIMENSION] == 1 && 
+                    op.getInputTensor()[0]->getShape()[mv::IO_WIDTH_DIMENSION] == 100 && op.getInputTensor()[0]->getShape()[mv::IO_HEIGHT_DIMENSION] == 64 &&
+                    op.getOutputTensor()[0]->getShape()[mv::IO_CHANNEL_DIMENSION] == 64 && op.getInputTensor()[0]->getShape()[mv::IO_WIDTH_DIMENSION] == 100 && 
+                    op.getInputTensor()[0]->getShape()[mv::IO_HEIGHT_DIMENSION] == 64)
+                    return FailCause::SpiltOverHForLayer79InACLNet;
 
                 return FailCause::Pass; //good strategy
             }
