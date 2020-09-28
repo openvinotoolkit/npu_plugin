9d74cff36df968f10726cc064aad68e2c43cfe7a
Vladislav Vinogradov <vlad.vinogradov@intel.com>
2020-09-28 14:46:58 +0300

    Duplicate some options from IE to allow override
    * `ENABLE_TESTS`
    * `ENABLE_LTO`
    * `ENABLE_CPPLINT`
    * `ENABLE_CLANG_FORMAT`

--- cmake/features.cmake
+++ cmake/features.cmake
@@ -4,6 +4,26 @@
 
 include(options)
 
+if(NOT ENABLE_TESTS)
+    set(ENABLE_TESTS OFF)
+endif()
+ie_dependent_option(ENABLE_TESTS "Unit, behavior and functional tests" ${ENABLE_TESTS} "TRUE" OFF)
+
+if(NOT ENABLE_LTO)
+    set(ENABLE_LTO OFF)
+endif()
+ie_dependent_option(ENABLE_LTO "Enable Link Time Optimization" ${ENABLE_LTO} "LINUX OR WIN32;NOT CMAKE_CROSSCOMPILING" OFF)
+
+if(NOT ENABLE_CPPLINT)
+    set(ENABLE_CPPLINT OFF)
+endif()
+ie_dependent_option(ENABLE_CPPLINT "Enable cpplint checks during the build" ${ENABLE_CPPLINT} "UNIX;NOT ANDROID" OFF)
+
+if(NOT ENABLE_CLANG_FORMAT)
+    set(ENABLE_CLANG_FORMAT OFF)
+endif()
+ie_dependent_option(ENABLE_CLANG_FORMAT "Enable clang-format checks during the build" ${ENABLE_CLANG_FORMAT} "TRUE" OFF)
+
 ie_dependent_option(ENABLE_KMB_SAMPLES "Enable KMB samples" ON "AARCH64" OFF)
 
 ie_dependent_option(ENABLE_HDDL2 "Enable HDDL2 Plugin" ON "X86_64" OFF)
