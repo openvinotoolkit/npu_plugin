26def4022f583e2d6109e6caed824e0067670e25
Vladislav Vinogradov <vlad.vinogradov@intel.com>
2020-09-28 14:48:51 +0300

    Add `WIN_STRICT` mode to `enable_warnings_as_errors`
    It turns on "warnings as error" on MSVC and disables warnings checks
    in 3rd-party components using experimental external MSVC feature.

--- cmake/compile_options.cmake
+++ cmake/compile_options.cmake
@@ -21,20 +21,28 @@ macro(replace_compile_visibility_options)
 endmacro()
 
 function(enable_warnings_as_errors TARGET_NAME)
-    if(MSVC)
-        # TODO: find a way to disable warnings checks for SYSTEM includes
-        # if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "19.14")
-        #     target_compile_options(${TARGET_NAME}
-        #         PRIVATE
-        #             /WX /Wall
-        #     )
-        # endif()
+    cmake_parse_arguments(WARNIGS "WIN_STRICT" "" "" ${ARGN})
 
+    if(MSVC)
         # Enforce standards conformance on MSVC
         target_compile_options(${TARGET_NAME}
             PRIVATE
                 /permissive-
         )
+
+        if(WARNIGS_WIN_STRICT)
+            # Use W3 instead of Wall, since W4 introduces some hard-to-fix warnings
+            target_compile_options(${TARGET_NAME}
+                PRIVATE
+                    /WX /W3
+            )
+
+            # Disable 3rd-party components warnings
+            target_compile_options(${TARGET_NAME}
+                PRIVATE
+                    /experimental:external /external:anglebrackets /external:W0
+            )
+        endif()
     else()
         target_compile_options(${TARGET_NAME}
             PRIVATE
