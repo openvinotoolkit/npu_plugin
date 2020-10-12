778acea1262c0e7ed418b53fb856c442cff162e7
mznamens <mikhail.znamenskiy@intel.com>
2020-10-12 10:56:41 +0300

    Add fix for Export() network

--- src/hddl2_plugin/include/hddl2_executable_network.h
+++ src/hddl2_plugin/include/hddl2_executable_network.h
@@ -42,10 +42,11 @@ public:
     InferenceEngine::InferRequestInternal::Ptr CreateInferRequestImpl(
         const InferenceEngine::InputsDataMap networkInputs,
         const InferenceEngine::OutputsDataMap networkOutputs) override;
-    void Export(std::ostream& model) override;
+    void ExportImpl(std::ostream& model) override;
 
     using InferenceEngine::ExecutableNetworkInternal::Export;
     void Export(const std::string& modelFileName) override;
+    void Export(std::ostream& networkModel) override { ExportImpl(networkModel); }
 
     void CreateInferRequest(InferenceEngine::IInferRequest::Ptr& asyncRequest) override;
 
