32bafc27b8f65a008536805c36fd1a8b2b476f15
aleckey <alexander.j.leckey@intel.com>
2020-10-26 21:31:27 +0000

    Add emulator calling code

--- src/mcmCompiler/include/mcm/compiler/compilation_unit.hpp
+++ src/mcmCompiler/include/mcm/compiler/compilation_unit.hpp
@@ -22,6 +22,7 @@ namespace mv
 
         static const std::string ma2490DefCompDescPath_;
         static const std::string ma3100DefCompDescPath_;
+        static const std::string ma2490EmulatorCompDescPath_;
         static const std::string compositionalModelRecordingsPath_;
         static const unsigned jsonParserBufferLength_ = 256;
 
@@ -33,6 +34,9 @@ namespace mv
         TargetDescriptor targetDescriptor_;
         CompilationDescriptor compDescriptor_;
         bool preCompiled_;
+
+        void deepCopy(mv::OpModel& copyTo);
+        void generateExpectedResults();
 
     public:
 
