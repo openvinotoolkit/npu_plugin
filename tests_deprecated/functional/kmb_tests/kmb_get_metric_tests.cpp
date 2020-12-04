e99371077e135a54c7062e759dfa4c51b44357d5
Losev, Sergey E <sergey.e.losev@intel.com>
2020-12-04 08:37:06 +0000

    Implement getDevice for VpualEngineBackend

--- tests_deprecated/functional/kmb_tests/kmb_get_metric_tests.cpp
+++ tests_deprecated/functional/kmb_tests/kmb_get_metric_tests.cpp
@@ -16,30 +16,61 @@
 
 #include "vpu_layers_tests.hpp"
 
+// xlink is used in this test to determine the number of available VPUs
+#if defined(__arm__) || defined(__aarch64__)
+#include <mvMacros.h>
+#include <xlink_uapi.h>
+#endif
+
 using namespace ::testing;
 using namespace InferenceEngine;
 using namespace details;
 
 using GetMetricTest = vpuLayersTests;
 
+static size_t getVpuCount() {
+    size_t vpuCount = 0;
+#if defined(__arm__) || defined(__aarch64__)
+    xlink_error initResult = xlink_initialize();
+    if (initResult != X_LINK_SUCCESS) {
+        return 0;
+    }
+
+    constexpr size_t MAX_DEVICES = 8;
+    uint32_t devIds[MAX_DEVICES];
+    uint32_t devCount = 0;
+    xlink_error getDevResult = xlink_get_device_list(devIds, &devCount);
+    if (getDevResult != X_LINK_SUCCESS) {
+        return 0;
+    }
+
+    constexpr uint32_t INTERFACE_TYPE_SELECTOR = 0x7000000;
+    for (size_t devIdx = 0; devIdx < devCount; devIdx++) {
+        if ((devIds[devIdx] & INTERFACE_TYPE_SELECTOR) == 0) {
+            vpuCount++;
+        }
+    }
+#endif
+    return vpuCount;
+}
+
 TEST_F(GetMetricTest, getAvailableDevices) {
     // [Track number: S#34628]
-    if (deviceName == "HDDL2" || deviceName == "VPUX") SKIP() << "No x86 device on CI";
+#if defined(__arm__) || defined(__aarch64__)
+    bool runningOnARM = true;
+#else
+    bool runningOnARM = false;
+#endif
+    if (deviceName == "HDDL2" || (deviceName == "VPUX" && !runningOnARM)) SKIP() << "No x86 device on CI";
     std::vector<std::string> kmbSupportedMetrics = core->GetMetric(deviceName, METRIC_KEY(SUPPORTED_METRICS));
     std::vector<std::string>::const_iterator kmbAvalableDevMetricIter =
         std::find(kmbSupportedMetrics.begin(), kmbSupportedMetrics.end(), METRIC_KEY(AVAILABLE_DEVICES));
     ASSERT_NE(kmbAvalableDevMetricIter, kmbSupportedMetrics.end());
 
     std::vector<std::string> kmbDeviceIds = core->GetMetric(deviceName, METRIC_KEY(AVAILABLE_DEVICES));
-#if defined(__arm__) || defined(__aarch64__)
-    bool runningOnARM = true;
-#else
-    bool runningOnARM = false;
-#endif
     if (runningOnARM) {
         ASSERT_FALSE(kmbDeviceIds.empty());
-        // FIXME: 4 for THB
-        ASSERT_EQ(kmbDeviceIds.size(), 1);
+        ASSERT_EQ(kmbDeviceIds.size(), getVpuCount());
         ASSERT_NE(kmbDeviceIds.begin()->find("VPU"), std::string::npos);
 
         std::cout << "Found available KMB devices: " << std::endl;
