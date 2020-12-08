69396e512379bf80143a288796c520c0e52f0210
john.brady@intel.com <john.brady@intel.com>
2020-12-08 14:41:51 +0000

    baseline

--- src/mcmCompiler/src/pass/optimization/graph_parameter_optimization.cpp
+++ src/mcmCompiler/src/pass/optimization/graph_parameter_optimization.cpp
@@ -846,35 +846,35 @@ namespace mv
                 // NOTE: This is a temporary workaround till we are able to identify the chains before graph
                 // optimizer and control the cmx percentage that we want the weigths to receive described in
                 // https://jira.devtools.intel.com/browse/CVS-43222
-                {
-                    if (op.getOpType() == "Conv")
-                    {
-                        if (op.getInputTensor()[0]->getShape() == mv::Shape({13,13,512,1}) &&
-                            op.getInputTensor()[1]->getShape() == mv::Shape({3,3,512,1024}) &&
-                            op.getOutputTensor()[0]->getShape() == mv::Shape({13,13,1024,1}))
-                        {
-                            if(globalEnablePipelining && streamShape["K"] != 8)
-                                return FailCause::cmxConcatDecision;
-                        }
+                // {
+                //     if (op.getOpType() == "Conv")
+                //     {
+                //         if (op.getInputTensor()[0]->getShape() == mv::Shape({13,13,512,1}) &&
+                //             op.getInputTensor()[1]->getShape() == mv::Shape({3,3,512,1024}) &&
+                //             op.getOutputTensor()[0]->getShape() == mv::Shape({13,13,1024,1}))
+                //         {
+                //             if(globalEnablePipelining && streamShape["K"] != 8)
+                //                 return FailCause::cmxConcatDecision;
+                //         }
 
-                        if (op.getInputTensor()[0]->getShape() == mv::Shape({13,13,1024,1}) &&
-                            op.getInputTensor()[1]->getShape() == mv::Shape({3,3,1024,1024}) &&
-                            op.getOutputTensor()[0]->getShape() == mv::Shape({13,13,1024,1}))
-                        {
-                            if(globalEnablePipelining && streamShape["K"] != 8)
-                                return FailCause::cmxConcatDecision;
-                        }
+                //         if (op.getInputTensor()[0]->getShape() == mv::Shape({13,13,1024,1}) &&
+                //             op.getInputTensor()[1]->getShape() == mv::Shape({3,3,1024,1024}) &&
+                //             op.getOutputTensor()[0]->getShape() == mv::Shape({13,13,1024,1}))
+                //         {
+                //             if(globalEnablePipelining && streamShape["K"] != 8)
+                //                 return FailCause::cmxConcatDecision;
+                //         }
 
-                        if (op.getInputTensor()[0]->getShape() == mv::Shape({1,1,2048,1}) &&
-                            op.getInputTensor()[1]->getShape() == mv::Shape({1,1,2048,1000}) &&
-                            op.getOutputTensor()[0]->getShape() == mv::Shape({1,1,1000,1}))
-                        {
-                            if(globalEnablePipelining && streamShape["K"] != 4 && streamShape["K"] != 2)
-                                return FailCause::cmxConcatDecision;
-                        }
-                    }
+                //         if (op.getInputTensor()[0]->getShape() == mv::Shape({1,1,2048,1}) &&
+                //             op.getInputTensor()[1]->getShape() == mv::Shape({1,1,2048,1000}) &&
+                //             op.getOutputTensor()[0]->getShape() == mv::Shape({1,1,1000,1}))
+                //         {
+                //             if(globalEnablePipelining && streamShape["K"] != 4 && streamShape["K"] != 2)
+                //                 return FailCause::cmxConcatDecision;
+                //         }
+                //     }
 
-                }
+                // }
 
                 // A proper decision on CMX concat for explicit concat or eltwise streaming cannot
                 // be made with the information on hand. Will not optimize strategies for these.
@@ -2348,7 +2348,7 @@ namespace mv
                 //Note: it is possible to change this to an && condition, but it alters the pipeline staging
                 //For simplicity we first do a 2 stage pipeline of weights read and compute, assuming
                 //input and output activations are in / will stay in cmx
-                if(spilling || parentSpilling)
+                if(parentSpilling)
                     return false;
 
                 if(clustering == "SplitOverH" || clustering == "HKSwitch")
