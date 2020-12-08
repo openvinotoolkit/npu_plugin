52bb43698f464b883104855e9c514deba6d5c86e
Pavel Volkovskiy <pavel.volkovskiy@intel.com>
2020-12-08 17:09:18 +0300

    Disable Interp Layer and Yolo V2 failing tests

--- tests/functional/shared_tests_instances/skip_tests_config.cpp
+++ tests/functional/shared_tests_instances/skip_tests_config.cpp
@@ -33,8 +33,16 @@ std::vector<std::string> disabledTestPatterns() {
         // [Track number: S#43484]
         ".*KmbMaxMinLayerTest.*",
 
-	// Need to create openvino releases/2020/vpux/2021/2 branch with #3350 pull request to fix test names
-	".*IEClassBasicTestP_smoke/IEClassBasicTestP.*",
-	".*IEClassGetMetricTest_nightly/IEClassGetMetricTest.*",
+        // Need to create openvino releases/2020/vpux/2021/2 branch with #3350 pull request to fix test names
+        ".*IEClassBasicTestP_smoke/IEClassBasicTestP.*",
+        ".*IEClassGetMetricTest_nightly/IEClassGetMetricTest.*",
+
+        // "Unable to deduce parameter 'align' for 'Interp' layer. Name is: 'interpolation', parameter is: 'align_corners'"
+        ".*KmbInterpLayerTests.EqualWithCPU/1",
+        ".*KmbInterpLayerTests.EqualWithCPU/3",
+        ".*KmbInterpLayerTests.EqualWithCPU/5",
+
+        // Windows Only: "LpScheduler - RuntimeError: input is not a DAG"
+        ".*precommit_yolo_v2_ava_0001_tf_dense_int8_IRv10_from_fp32",
     };
 }
