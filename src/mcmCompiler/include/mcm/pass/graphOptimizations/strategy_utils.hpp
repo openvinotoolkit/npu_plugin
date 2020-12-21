95ad44a4641fc1d664d9d4582fb79824dbabe354
Ashley DeFlumere <ashley.deflumere@intel.com>
2020-12-21 17:41:58 +0000

    Move memory size functions from graph optimizer to a utility file to be used elsewhere

---
+++ src/mcmCompiler/include/mcm/pass/graphOptimizations/strategy_utils.hpp
@@ -0,0 +1,19 @@
+#ifndef STRATEGY_UTILS_HPP
+#define STRATEGY_UTILS_HPP
+
+#include "include/mcm/computation/model/control_model.hpp"
+#include "include/mcm/computation/model/data_model.hpp"
+#include "include/mcm/op_model.hpp"
+
+namespace mv
+{
+    std::size_t realTensorSize(const mv::Data::TensorIterator tensorToSize, const mv::Shape& streamingPool, bool isCMConv);
+    std::size_t activationTensorSize(mv::Op& op, const mv::Data::TensorIterator tensorToSize, std::string clustering, const mv::Shape& streamingPool, 
+                                            bool isCMConv, int totalClusters, bool isInput, bool dilation = false);
+    std::size_t alignedWeightsSize(const mv::Data::TensorIterator tensorToSize, const Shape& streamConfig, std::string clustering, int totalClusters);
+    std::tuple<std::size_t,std::size_t,std::size_t> memorySize(mv::Op& op, int totalClusters, bool enableChannelMajorConv, std::string clustering, 
+                                            bool inputActivationSparsity, bool outputActivationSparsity, bool weightsSparsity, const Shape& streamConfig,
+                                            bool fakeSparsity, bool spilling = false, bool parentSpilling = true);
+}
+
+#endif
