9958543a40319695fa217891a3cf4ac081180451
Mateusz Darecki <mateusz.darecki@intel.com>
2021-01-11 09:23:54 +0100

    Fix int limit compilation issue with MSVC

--- src/mcmCompiler/include/mcm/target/kmb/ppe_fixed_function.hpp
+++ src/mcmCompiler/include/mcm/target/kmb/ppe_fixed_function.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <array>
+#include <climits>
 #include "include/mcm/target/kmb/ppe_layer_type.hpp"
 
 namespace mv
@@ -16,7 +17,7 @@ namespace mv
             uint8_t reluShift_;
             std::vector<PPELayerType> layers_;
         public:
-            PPEFixedFunction(int8_t lRelumult = 1, uint8_t lRelushift = 0, int lowClamp = -2147483648, int highClamp = 2147483647);
+            PPEFixedFunction(int8_t lRelumult = 1, uint8_t lRelushift = 0, int lowClamp = INT_MIN, int highClamp = INT_MAX);
 
             int getLowClamp() const;
             int getHighClamp() const ;
