7c24f6660cdb89daa65ba9c92728d3e66af504c6
john.brady@intel.com <john.brady@intel.com>
2021-01-20 18:53:07 +0000

    first pass at refactor

---
+++ src/mcmCompiler/src/pass/kmb_adaptation/streaming.cpp
@@ -0,0 +1,30 @@
+#include "include/mcm/pass/pass_registry.hpp"
+#include "include/mcm/op_model.hpp"
+#include "include/mcm/computation/model/control_model.hpp"
+#include "include/mcm/computation/model/data_model.hpp"
+#include "include/mcm/pass/pass_utils.hpp"
+#include "include/mcm/pass/graphOptimizations/streaming_performace.hpp"
+
+void streamingForPerformanceFnc(const mv::pass::PassEntry& pass, mv::ComputationModel& model,
+                                                 mv::TargetDescriptor&, mv::Element&, mv::Element&);
+
+namespace mv {
+namespace pass {
+MV_REGISTER_PASS(streamingForPerformance)
+        .setFunc(streamingForPerformanceFnc)
+        .setDescription("");
+}
+} 
+
+
+
+void streamingForPerformanceFnc(const mv::pass::PassEntry& pass, mv::ComputationModel& model,
+                                                 mv::TargetDescriptor&, mv::Element& passDesc, mv::Element&) {
+mv::OpModel om(model);
+mv::StreamingPerformance streamingPerformance(model,om);
+
+streamingPerformance.evaluateStreamingOverKStrategies();
+
+// Step 2: Get the min weights per cluster in a chain
+//getMinWeightsPerClusterSizePerChain(chainSubgraphs, pass, model, weightsPerClusterPerOp, fptr);
+}
