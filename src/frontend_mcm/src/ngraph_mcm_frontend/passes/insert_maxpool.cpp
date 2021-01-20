c954d1a1e4341a87ee8d47ea007fa80e842c387d
Losev, Sergey E <sergey.e.losev@intel.com>
2021-01-20 20:19:54 +0300

    Apply clang formatter

--- src/frontend_mcm/src/ngraph_mcm_frontend/passes/insert_maxpool.cpp
+++ src/frontend_mcm/src/ngraph_mcm_frontend/passes/insert_maxpool.cpp
@@ -14,54 +14,55 @@
 // stated in the License.
 //
 
-// clang-format off
-
-#include <details/ie_exception.hpp>
+// clang-format on
 #include "ngraph_mcm_frontend/passes/insert_maxpool.hpp"
+#include <details/ie_exception.hpp>
 
 #include <memory>
-#include <ngraph/op/fake_quantize.hpp>
-#include <ngraph/op/constant.hpp>
-#include <ngraph/type/element_type.hpp>
 #include <ngraph/op/concat.hpp>
-#include <ngraph/op/variadic_split.hpp>
+#include <ngraph/op/constant.hpp>
+#include <ngraph/op/fake_quantize.hpp>
 #include <ngraph/op/max_pool.hpp>
+#include <ngraph/op/variadic_split.hpp>
+#include <ngraph/type/element_type.hpp>
 
 #include "ngraph_mcm_frontend/quantization_helpers.hpp"
 
 namespace {
-    bool hasVarSplitParent(const std::shared_ptr<ngraph::Node>& fq_node) {
-        auto input_values = fq_node->input_values();
-        auto first_input = input_values.at(0).get_node_shared_ptr();
-        return std::dynamic_pointer_cast<ngraph::op::v1::VariadicSplit>(first_input) != nullptr;
-    }
-
-    void insertPooling(const std::shared_ptr<ngraph::Node>& fq_node) {
-        auto input_values = fq_node->input_values();
-        auto strides = ngraph::Strides{1, 1};
-        auto pads_begin = ngraph::Shape{0, 0};
-        auto pads_end = ngraph::Shape{0, 0};
-        auto kernel = ngraph::Shape{1, 1};
-        auto rounding_mode = ngraph::op::RoundingType::FLOOR;
-        auto auto_pad = ngraph::op::PadType::EXPLICIT;
-        auto max_pool = std::make_shared<ngraph::op::v1::MaxPool>(input_values.at(0).get_node_shared_ptr()->output(0),
-            strides, pads_begin, pads_end, kernel, rounding_mode, auto_pad);
-
-        auto input_low = input_values.at(1).get_node_shared_ptr();
-        auto input_high = input_values.at(2).get_node_shared_ptr();
-        auto output_low = input_values.at(3).get_node_shared_ptr();
-        auto output_high = input_values.at(4).get_node_shared_ptr();
-
-        auto old_fq = std::dynamic_pointer_cast<ngraph::op::FakeQuantize>(fq_node);
-        if (old_fq == nullptr) {
-            throw std::runtime_error("ForceFP16Split::insertPooling: failed to cast fake quantize pointer");
-        }
-        auto levels = old_fq->get_levels();
-        auto new_fq = std::make_shared<ngraph::op::FakeQuantize>(max_pool->output(0),
-            input_low, input_high, output_low, output_high, levels);
-        ngraph::replace_node(fq_node, new_fq);
-    }
+bool hasVarSplitParent(const std::shared_ptr<ngraph::Node>& fq_node) {
+    const auto input_values = fq_node->input_values();
+    const auto first_input = input_values.at(0).get_node_shared_ptr();
+    return std::dynamic_pointer_cast<ngraph::op::v1::VariadicSplit>(first_input) != nullptr;
 }
+
+void insertPooling(const std::shared_ptr<ngraph::Node>& fq_node) {
+    const auto input_values = fq_node->input_values();
+    const auto strides = ngraph::Strides{1, 1};
+    const auto pads_begin = ngraph::Shape{0, 0};
+    const auto pads_end = ngraph::Shape{0, 0};
+    const auto kernel = ngraph::Shape{1, 1};
+    const auto rounding_mode = ngraph::op::RoundingType::FLOOR;
+    const auto auto_pad = ngraph::op::PadType::EXPLICIT;
+    const auto max_pool =
+            std::make_shared<ngraph::op::v1::MaxPool>(input_values.at(0).get_node_shared_ptr()->output(0), strides,
+                                                      pads_begin, pads_end, kernel, rounding_mode, auto_pad);
+
+    const auto input_low = input_values.at(1).get_node_shared_ptr();
+    const auto input_high = input_values.at(2).get_node_shared_ptr();
+    const auto output_low = input_values.at(3).get_node_shared_ptr();
+    const auto output_high = input_values.at(4).get_node_shared_ptr();
+
+    const auto old_fq = std::dynamic_pointer_cast<ngraph::op::FakeQuantize>(fq_node);
+    // this check must never fail since all non-FQ nodes have already been filtered out at this point
+    if (old_fq == nullptr) {
+        THROW_IE_EXCEPTION << "ForceFP16Split::insertPooling: failed to cast fake quantize pointer";
+    }
+    auto levels = old_fq->get_levels();
+    auto new_fq = std::make_shared<ngraph::op::FakeQuantize>(max_pool->output(0), input_low, input_high, output_low,
+                                                             output_high, levels);
+    ngraph::replace_node(fq_node, new_fq);
+}
+}  // namespace
 
 bool InsertMaxPool::run_on_node(std::shared_ptr<ngraph::Node> node) {
     // replace { VarSplit -> FQ -> Concat } with { VarSplit -> MaxPool -> FQ -> Concat }
@@ -80,5 +81,3 @@ bool InsertMaxPool::run_on_node(std::shared_ptr<ngraph::Node> node) {
     }
     return false;
 }
-
-// clang-format on
