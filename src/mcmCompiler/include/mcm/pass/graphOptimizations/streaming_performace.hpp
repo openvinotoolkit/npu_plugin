2f28d1772d017fbf1aa6a2d72da5a75e131e66d3
john.brady@intel.com <john.brady@intel.com>
2021-01-21 16:43:49 +0000

    class for k streaming complete

--- src/mcmCompiler/include/mcm/pass/graphOptimizations/streaming_performace.hpp
+++ src/mcmCompiler/include/mcm/pass/graphOptimizations/streaming_performace.hpp
@@ -23,27 +23,32 @@ private:
     std::vector<mv::Element> multiClusterStrategyList_;
     std::vector<mv::Element> tensorMemoryLocation_;
     std::vector<mv::Element> newStrategies_;
-    bool enableChannelMajorConv_;
-    size_t nClusters_;
+    const bool enableChannelMajorConv_;
+    const size_t nClusters_;
     FILE* fptr_ = nullptr;
     const size_t minWeightsPerClusterPerChainConstant_ = 66560;  
-    std::map<std::string, size_t> minOutputChannels_ = {
+    const std::map<std::string, size_t> minOutputChannels_ = {
             {"SplitOverK", 64}, {"Clustering", 16}, {"SplitOverH", 16}, {"HKSwitch", 16}};
 
     std::map<size_t, size_t> calculateMininumWeightsSizePerClusterPerChain();
-    size_t calculateperClusterWeightsSize(mv::Op& op, const mv::Attribute& clustering, bool weightsSparsity, const mv::Shape& streamConfig);
-    std::tuple<std::vector<mv::Element>, mv::Attribute, bool> getGraphOptimizerAssignedStategies(std::string opName);
-    std::pair<size_t, double> calculatefullWeightsSizeForOpandOptimalKStreaming(std::string multiclusterStrategy,
-                                                                   size_t weightsPerClusterforOp,
-                                                                   size_t minWeightsPerClusterPerChain,
-                                                                   bool isKStreaming, int numberOfkStreams
-                                                                   );
 
-    void writeStatsToFile(unsigned chainID, std::string opName, int kStreaming, int hStreaming,
-                      std::string multiclusterStrategy, size_t fullweightsSize, size_t alignedFullOutputChannels,
-                      size_t weightsPerClusterPerOp, size_t minWeightsPerClusterPerChain,
-                      double optimalNumberOfKStreams, double maxpossibleStreams, double newKStreams);
-    void evaluateAndAssignStrategies();
+    size_t calculateperClusterWeightsSize(mv::Op& op, const mv::Attribute& clustering, const bool weightsSparsity,
+                                          const mv::Shape& streamConfig);
+
+    std::tuple<std::vector<mv::Element>, mv::Attribute, bool> getGraphOptimizerAssignedStategies(const std::string opName);
+
+    std::pair<size_t, double> calculatefullWeightsSizeForOpandOptimalKStreaming(const std::string multiclusterStrategy,
+                                                                                const size_t weightsPerClusterforOp,
+                                                                                const size_t minWeightsPerClusterPerChain,
+                                                                                const bool isKStreaming,
+                                                                                const int numberOfkStreams);
+
+    void writeStatsToFile(const unsigned chainID, const std::string opName, const int kStreaming, const int hStreaming,
+                          const std::string multiclusterStrategy,const size_t fullweightsSize, const size_t alignedFullOutputChannels,
+                          const size_t weightsPerClusterPerOp, size_t minWeightsPerClusterPerChain,
+                          const double optimalNumberOfKStreams,const double maxpossibleStreams,const double newKStreams);
+
+    void evaluateGraphOptimizerAssignedKStreamingStrategies();
     void assignNewSrategies();
 };
 }  // namespace mv
