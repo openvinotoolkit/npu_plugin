7df0734e00068ee40de5ce2e201a3ab280582b0a
Somsikov, Andrey <andrey.somsikov@intel.com>
2021-01-27 10:17:10 +0000

    Merge branch 'unmute_icnet' into 'releases/vpux/2021/2'
    Unmute icnet model in KPI tests
    
    See merge request inference-engine/kmb-plugin!1984
    
    (cherry picked from commit fd9916215d015ce74d7e7cb6f43df37182e7ad40)
    
    eefb9dc0 Unmute icnet model in KPI tests

--- tests/functional/compile/conftest.py
+++ tests/functional/compile/conftest.py
@@ -40,8 +40,7 @@ KMB_KPI_MODELS = [
 
     {'marks': pytest.mark.xfail, 'path': 'vd-kmb-models-kpi-ww50.tar.bz2/faster-rcnn-resnet101-coco-sparse-60-0001/tf/FP16-INT8/faster-rcnn-resnet101-coco-sparse-60-0001.xml'},
     {'path': 'vd-kmb-models-kpi-ww50.tar.bz2/ssd512/caffe/FP16-INT8/ssd512.xml'},
-    {'marks': pytest.mark.xfail,
-     'path': 'vd-kmb-models-kpi-ww50.tar.bz2/icnet-camvid-ava-0001/tf/FP16-INT8/icnet-camvid-ava-0001.xml'},
+    {'path': 'vd-kmb-models-kpi-ww50.tar.bz2/icnet-camvid-ava-0001/tf/FP16-INT8/icnet-camvid-ava-0001.xml'},
     {'path': 'vd-kmb-models-kpi-ww50.tar.bz2/yolo-v2-ava-0001/tf/FP16-INT8/yolo-v2-ava-0001.xml'},
     {'path': 'vd-kmb-models-kpi-ww50.tar.bz2/yolo-v2-ava-sparse-35-0001/tf/FP16-INT8/yolo-v2-ava-sparse-35-0001.xml'},
     {'path': 'vd-kmb-models-kpi-ww50.tar.bz2/yolo-v2-ava-sparse-70-0001/tf/FP16-INT8/yolo-v2-ava-sparse-70-0001.xml'},
