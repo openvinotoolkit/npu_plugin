3bfcd01b143d74d1fa3058afab2bf7f3479e08d1
Michal Gwizdz <michal.gwizdz@intel.com>
2021-02-04 13:55:23 +0100

    revert change

--- src/mcmCompiler/src/pass/lp_scheduler/lp_scheduler_pass.cpp
+++ src/mcmCompiler/src/pass/lp_scheduler/lp_scheduler_pass.cpp
@@ -369,6 +369,7 @@ void LpSchedulerPass(const mv::pass::PassEntry& pass,
   //////////////////////////////////////////////////////////////////////////////
 
 
+
   ///////////////Save Schedule for DDR Address Generation///////////////////////
   if (passDesc.hasAttr("ddr_address_generation") &&
         passDesc.get<bool>("ddr_address_generation")) {
