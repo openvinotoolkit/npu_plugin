84c0abdca49035f3f25f4253f603de52f12e5835
john.brady@intel.com <john.brady@intel.com>
2021-02-25 17:23:03 +0000

    added stream over h pass to the class

--- src/mcmCompiler/src/pass/kmb_adaptation/streaming.cpp
+++ src/mcmCompiler/src/pass/kmb_adaptation/streaming.cpp
@@ -17,7 +17,26 @@ MV_REGISTER_PASS(StreamingForPerformance).setFunc(streamingForPerformanceFnc).se
 void streamingForPerformanceFnc(const mv::pass::PassEntry& pass, mv::ComputationModel& model, mv::TargetDescriptor&,
                                 mv::Element& passDesc, mv::Element&) {
     mv::OpModel om(model);
-    mv::StreamingPerformance streamingPerformance(om);
+
+    int maxHStreams = 1000;
+    if (passDesc.hasAttr("maxHStreams"))
+        maxHStreams = passDesc.get<int>("maxHStreams");
+    mv::StreamingPerformance streamingPerformance(om, maxHStreams);
 
     streamingPerformance.increaseStreamingOverKforPerformance();
+
+    // Note: The idea of this pass is to increase streaming over the height dimension in specific cases
+    // to increase performance. Specifically, we consider DPU tasks (convs, dw, maxpool) that have their
+    // input tensor in DDR. Performance increase results because smaller DMA of input slices leads to
+    // earlier start to compute, and the resulting smaller pieces are often easier for the scheduler
+    // to schedule efficiently.
+    //
+    // Side Note: There are several reasons an input tensor might be in DDR, it could be the network
+    // input, or a spilled activation due to tensor size or need to change clustering strategy. In this
+    // pass we don't care why the tensor is in DDR, we just use the GO pass' prediction for where the
+    // tensor will be located. We skip extra streams in the case that the GO can't predict tensor location
+    // such as after an explicit concat (could be CMXed later). For simplicity, we also only consider ops
+    // that were already streaming over H, but this pass could be extended to consider non-streaming ops.
+
+    streamingPerformance.increaseStreamingOverHforPerformance(pass);
 }
