0c4e653fa566fc2b78e83a40fc1309fdeec4d3fb
john.brady@intel.com <john.brady@intel.com>
2021-02-27 20:12:07 +0000

    bug fix and remove maxpool WA

--- src/mcmCompiler/src/pass/optimization/graph_parameter_optimization.cpp
+++ src/mcmCompiler/src/pass/optimization/graph_parameter_optimization.cpp
@@ -1088,9 +1088,6 @@ namespace mv
                     (streamShape["K"]  * streamShape["H"]) > 1 && spilling)
                     return FailCause::SpiltOverHWithStreamOverK;
 
-                if (clustering == "SplitOverH" && op.getOpType() == "MaxPool" &&
-                    (streamShape["K"]  * streamShape["H"]) > 1 && spilling)
-                    return FailCause::SpiltOverHWithStreamOverK;
                 //NOTE: This is not a HACK!!! if an operation is assigned with streamOverH + SplitOverH
                 //and we concatenate on cmx the data are going to have a strange format...so nothing can be done later, so spill...
                 /*For example, let's think a convolution with splitOverH and streams = 2, the tensor will be splitted to
