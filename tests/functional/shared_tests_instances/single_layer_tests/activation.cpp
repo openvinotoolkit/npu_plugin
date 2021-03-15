d780458e07a58f0b711947e230280b7917ed446a
Bakalin, Andrew <andrew.bakalin@intel.com>
2021-03-15 11:35:26 +0300

    Exp layer & tests

--- tests/functional/shared_tests_instances/single_layer_tests/activation.cpp
+++ tests/functional/shared_tests_instances/single_layer_tests/activation.cpp
@@ -25,7 +25,8 @@ std::set<ngraph::helpers::ActivationTypes> supportedTypesMCM {
     ngraph::helpers::RoundHalfAwayFromZero,
     ngraph::helpers::Erf,
     ngraph::helpers::Gelu,
-    ngraph::helpers::Ceiling
+    ngraph::helpers::Ceiling,
+    ngraph::helpers::Exp,
 };
 
 std::set<ngraph::helpers::ActivationTypes> supportedTypesMLIR {
@@ -102,6 +103,7 @@ const std::map<ActivationTypes, std::vector<std::vector<float>>> activationTypes
     {Ceiling,  {{1.0f}}},
     {Erf,      {{1.0f}}},
     {Gelu,     {{1.0f}}},
+    {Exp,      {{1.0f}}},
     {Swish,    {{1.0f}}},
     {Negative, {{1.0f}}},
     {RoundHalfToEven,       {}},
