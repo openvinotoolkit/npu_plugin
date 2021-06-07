b19c07249aaadf8731b0e0fac12b8c3d1e26b628
Dirlea, Iosif-Ovidiu <iosif-ovidiu.dirlea@intel.com>
2021-06-07 21:22:42 +0000

    EISW-9846 : Introduce check for height kernel size when splitting over H

--- src/mcmCompiler/src/pass/kmb_adaptation/splitting_across_clusters.cpp
+++ src/mcmCompiler/src/pass/kmb_adaptation/splitting_across_clusters.cpp
@@ -213,9 +213,7 @@ void subTensorsGen(mv::ComputationModel& model, const tensorSet& tensors, unsign
         for (auto& tensor : tensors)
         {
             int success;
-            auto needs_sparse = (tensor->hasAttr("needs_sparse")) ? tensor->get<bool>("needs_sparse") : false;
-            auto needs_splits_aligned = (tensor->hasAttr("needs_splits_aligned")) ? tensor->get<bool>("needs_splits_aligned") : false;
-            mv::Workloads Tensor(tensor->getName(), tensor->getShape(), needs_sparse | needs_splits_aligned);
+            mv::Workloads Tensor(tensor->getName(), tensor->getShape());
             std::vector<mv::Workload> subTensors;
 
             if (tensor->get<std::string>("splitStrategy") == "SplitOverH")
@@ -377,9 +375,7 @@ void subTensorsGen(mv::ComputationModel& model, const tensorSet& tensors, unsign
         for (auto& tensor : tensors)
         {
             int success;
-            auto needs_sparse = (tensor->hasAttr("needs_sparse")) ? tensor->get<bool>("needs_sparse") : false;
-            auto needs_splits_aligned = (tensor->hasAttr("needs_splits_aligned")) ? tensor->get<bool>("needs_splits_aligned") : false;
-            mv::Workloads Tensor(tensor->getName(), tensor->getShape(), needs_sparse | needs_splits_aligned);
+            mv::Workloads Tensor(tensor->getName(), tensor->getShape());
             std::vector<mv::Workload> subTensors;
 
             if (std::find(activationSegmentableStrategies.cbegin(), activationSegmentableStrategies.cend(),
