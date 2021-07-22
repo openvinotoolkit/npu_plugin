49f9dc8f2af44c317887de78550475700d2c1bf3
Eugene Smirnov <eugene.smirnov@intel.com>
2021-07-23 01:15:51 +0300

    first modification in table generation by adding act-shaves types and scheme entries

---
+++ src/vpux_compiler/src/movitools/movitools.cpp
@@ -0,0 +1,29 @@
+#include "vpux/compiler/movitools/movitools.h"
+#include "vpux/utils/core/error.hpp"
+#include <mlir/Support/DebugStringHelper.h>
+
+#include <llvm/ADT/SmallString.h>
+#include <llvm/Support/Process.h>
+#include <llvm/Support/Path.h>
+
+using namespace llvm;  // NOLINT
+
+namespace vpux {
+namespace movitools {
+
+std::string getMoviToolsDir() {
+    const auto toolsDir = llvm::sys::Process::GetEnv("MV_TOOLS_DIR");
+    VPUX_THROW_UNLESS(toolsDir.hasValue(), "MV_TOOLS_DIR env var must be set");
+
+    const auto toolsVer = llvm::sys::Process::GetEnv("MV_TOOLS_VERSION");
+    VPUX_THROW_UNLESS(toolsVer.hasValue(), "MV_TOOLS_VERSION env var must be set");
+
+    SmallString<128> finalToolsDir(toolsDir.getValue());
+    sys::path::append(finalToolsDir, toolsVer.getValue());
+    VPUX_THROW_UNLESS(sys::fs::is_directory(finalToolsDir), "{0} is not a directory", finalToolsDir.str());
+
+    return finalToolsDir.str().str();
+}
+
+}  // namespace movitools
+}  // namespace vpux
