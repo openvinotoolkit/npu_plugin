9fdb13477d15b6aa1e06f1c019ca9c7b6aeca704
Kurin, Maxim <maxim.kurin@intel.com>
2021-08-10 15:03:28 +0000

    [MLIR] Change behavior of FakeQuantize 4D reshape

--- src/vpux_compiler/src/dialect/IE/pipelines.cpp
+++ src/vpux_compiler/src/dialect/IE/pipelines.cpp
@@ -26,8 +26,8 @@ using namespace vpux;
 void vpux::IE::buildAdjustForVPUPipeline(mlir::OpPassManager& pm, Logger log) {
     pm.addPass(IE::createConvertTile2PerAxisTilePass(log));
     pm.addPass(IE::createConvertPrecisionToFP16Pass(log));
-    pm.addPass(IE::createConvertConv1DToConv2DPass(log));
     pm.addPass(IE::createConvertShapeTo4DPass(log));
+    pm.addPass(IE::createConvertConv1DToConv2DPass(log));
     pm.addPass(IE::createConvertPaddingsToFloorModePass(log));
     pm.addPass(IE::createResolveStridedSlicePass(log));
     pm.addPass(IE::createFusePostOpsPass(log));
