5005c1d065cecbc9515b5cf4fe50c601ff7d8d30
Sergey Shumihin <sergey.shumihin@intel.com>
2021-08-23 16:20:24 +0300

    [CiD] Add zero compiler interfaces

---
+++ src/zero_compiler_adapter/include/ngraph_transformations.h
@@ -0,0 +1,45 @@
+//
+// Copyright Intel Corporation.
+//
+// LEGAL NOTICE: Your use of this software and any required dependent software
+// (the "Software Package") is subject to the terms and conditions of
+// the Intel(R) OpenVINO(TM) Distribution License for the Software Package,
+// which may also include notices, disclaimers, or license terms for
+// third party or open source software included in or with the Software Package,
+// and your use indicates your acceptance of all such terms. Please refer
+// to the "third-party-programs.txt" or other similarly-named text file
+// included with the Software Package for additional details.
+//
+
+#pragma once
+#include <vector>
+#include "zero_compiler_adapter.h"
+
+namespace vpux {
+namespace zeroCompilerAdapter {
+
+/**
+ * @brief Contain all required transformation on nGraph function
+ */
+namespace ngraphTransformations {
+
+/**
+ * @brief Perform lowering if it's required.
+ * @warning If conversion is not possible, this function will just skip this step
+ */
+void applyLoweringPasses(const std::shared_ptr<ngraph::Function>& netGraph, Opset opsetVersion);
+
+/**
+ * @brief Check, can we compile and run ngraph function, if only specific opset version supported
+ * @param opsetVersion Version of opset, which is supported
+ */
+bool isFunctionSupported(const std::shared_ptr<ngraph::Function>& netGraph, Opset opsetVersion);
+
+/**
+ * @brief Serialize ngraph function to IR
+ */
+IR serializeToIR(const std::shared_ptr<ngraph::Function>& netGraph);
+
+}  // namespace ngraphTransformations
+}  // namespace zeroCompilerAdapter
+}  // namespace vpux
---
+++ src/zero_compiler_adapter/include/zero_compiler_adapter.h
@@ -0,0 +1,48 @@
+//
+// Copyright Intel Corporation.
+//
+// LEGAL NOTICE: Your use of this software and any required dependent software
+// (the "Software Package") is subject to the terms and conditions of
+// the Intel(R) OpenVINO(TM) Distribution License for the Software Package,
+// which may also include notices, disclaimers, or license terms for
+// third party or open source software included in or with the Software Package,
+// and your use indicates your acceptance of all such terms. Please refer
+// to the "third-party-programs.txt" or other similarly-named text file
+// included with the Software Package for additional details.
+//
+
+#pragma once
+#include "icompiler_adapter.h"
+#include "vpux_compiler.hpp"
+
+namespace vpux {
+namespace zeroCompilerAdapter {
+
+/**
+ * @brief Adapter for Compiler in driver
+ * @details Wrap compiler in driver calls and do preliminary actions (like opset conversion)
+ */
+class ZeroCompilerAdapter final : public ICompiler {
+public:
+    ZeroCompilerAdapter();
+    std::shared_ptr<INetworkDescription> compile(const std::shared_ptr<ngraph::Function>& func,
+                                                 const std::string& netName,
+                                                 const InferenceEngine::InputsDataMap& inputsInfo,
+                                                 const InferenceEngine::OutputsDataMap& outputsInfo,
+                                                 const VPUXConfig& config) override;
+
+    InferenceEngine::QueryNetworkResult query(const InferenceEngine::CNNNetwork& network,
+                                              const VPUXConfig& config) override;
+
+    std::shared_ptr<vpux::INetworkDescription> parse(const std::vector<char>& network, const VPUXConfig& config,
+                                                     const std::string& netName) final;
+
+private:
+    /**
+     * @brief Separate externals calls to separate class
+     */
+    ICompiler_Adapter::Ptr apiAdapter;
+};
+
+}  // namespace zeroCompilerAdapter
+}  // namespace vpux
