8c1d01512eb56450de1a26a8e356bf151c592259
Efode, Irina <irina.efode@intel.com>
2021-08-25 11:42:58 +0000

    [IE TESTS] Behavior2.0 -> InferRequest()

--- tests/functional/shared_tests_instances/behavior/infer_request/multithreading.cpp
+++ tests/functional/shared_tests_instances/behavior/infer_request/multithreading.cpp
@@ -4,21 +4,16 @@
 
 #include <vector>
 
-#include "behavior/infer_request_callback.hpp"
+#include "behavior/infer_request/multithreading.hpp"
 #include "ie_plugin_config.hpp"
 
 using namespace BehaviorTestsDefinitions;
 namespace {
-const std::vector<InferenceEngine::Precision> netPrecisions = {
-        InferenceEngine::Precision::FP32
-};
-
 const std::vector<std::map<std::string, std::string>> configs = {};
 
-INSTANTIATE_TEST_SUITE_P(smoke_BehaviorTests, CallbackTests,
+INSTANTIATE_TEST_SUITE_P(smoke_BehaviorTests, InferRequestMultithreadingTests,
         ::testing::Combine(
-            ::testing::ValuesIn(netPrecisions),
             ::testing::Values(CommonTestUtils::DEVICE_KEEMBAY),
             ::testing::ValuesIn(configs)),
-        CallbackTests::getTestCaseName);
+    InferRequestMultithreadingTests::getTestCaseName);
 }  // namespace
