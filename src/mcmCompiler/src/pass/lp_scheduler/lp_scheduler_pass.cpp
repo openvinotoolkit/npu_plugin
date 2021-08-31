53ce0bea593df1c12af5ca39deaef8c03ac0e514
john.brady@intel.com <john.brady@intel.com>
2021-08-31 19:55:55 +0100

    test

--- src/mcmCompiler/src/pass/lp_scheduler/lp_scheduler_pass.cpp
+++ src/mcmCompiler/src/pass/lp_scheduler/lp_scheduler_pass.cpp
@@ -282,7 +282,10 @@ void LpSchedulerPass(const mv::pass::PassEntry& pass,
 
       cmx_address_alloc(scheduled_op);
 
-      if (!scheduled_op.is_original_op()) { has_any_dynamic_spill_ops = true; }
+      if (!scheduled_op.is_original_op()) {
+         has_any_dynamic_spill_ops = true; 
+         std::cout << "Needs spilling" << std::endl;
+         }
 
       //////////////////////////////////debug///////////////////////////////////
       if (fptr) {
