943d93d030c82c3ceca2c6325a83820a43ad2cfa
Alexander Perepelkin <alexander.perepelkin@intel.com>
2021-08-31 11:54:09 +0300

    Enable zero debug mode

--- src/vpux_plugin/src/vpux_plugin.cpp
+++ src/vpux_plugin/src/vpux_plugin.cpp
@@ -67,7 +67,7 @@ static VPUXConfig mergePluginAndNetworkConfigs(const VPUXConfig& pluginConfig,
 // FIXME: Remove NDEBUG, once #-15235 is fixed
 //
 static const std::vector<std::string> backendRegistry = {
-#if defined(_WIN32) || defined(_WIN64) || (defined(__linux__) && defined(__x86_64__) && defined(NDEBUG))
+#if defined(_WIN32) || defined(_WIN64) || (defined(__linux__) && defined(__x86_64__))
         "zero_backend",
 #endif
 #if defined(__arm__) || defined(__aarch64__)
