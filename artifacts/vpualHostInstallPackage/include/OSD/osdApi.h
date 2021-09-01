e15b35b91eb6d106a3ab18039dcadad6fb16b838
Znamenskiy, Mikhail <mikhail.znamenskiy@intel.com>
2021-09-01 12:43:18 +0000

    Merge branch 'mvds-ci-bkc_candidates-kmb-21ww36.2-plugin' into 'releases/2021/4_vpux_ww35'
    Artifacts for Gate 1.5 tests for Movidius 21ww36.2 - KMB
    
    See merge request inference-engine/kmb-plugin!3178
    
    (cherry picked from commit 6c61c30d7f357399981ad3f2e4db624c07135a74)
    
    a94d34e0 Artifacts for Gate 1.5 tests for Movidius 21ww36.2 - KMB release

--- artifacts/vpualHostInstallPackage/include/OSD/osdApi.h
+++ artifacts/vpualHostInstallPackage/include/OSD/osdApi.h
@@ -14,22 +14,22 @@ extern "C" {
 
 void OsdListInit(OsdList *lst, OsdAddr  base, uint32_t size);
 
-void OsdAlphaBmp(OsdList *lst, OsdBmpDesc  *d);
-void OsdAlphaCol(OsdList *lst, OsdMaskDesc *d);
+void OsdAlphaBmp(OsdList *lst, OsdBmpDesc  *d, uint32_t device_id = 0);
+void OsdAlphaCol(OsdList *lst, OsdMaskDesc *d, uint32_t device_id = 0);
 void OsdPoly    (OsdList *lst, OsdPolyDesc *d);
 void OsdMosaic  (OsdList *lst, OsdMosDesc  *d);
 void OsdFrame   (OsdList *lst, OsdFrmDesc  *d);
 void OsdBox     (OsdList *lst, OsdBoxDesc  *d);
 
-void OsdDraw    (OsdList *lst, OsdBuff *dest, uint32_t svuNo);
-void OsdWait    (uint32_t svuNo);
+void OsdDraw    (OsdList *lst, OsdBuff *dest, uint32_t svuNo, uint32_t device_id = 0);
+void OsdWait    (uint32_t svuNo, uint32_t device_id = 0);
 
 uint32_t OsdCheck(OsdList *lst, OsdBuff *dest);
 
 //could have multiple instances, svuNo is the "handle"
-uint32_t OsdOpen (uint32_t svuNo);
-uint32_t OsdClose(uint32_t svuNo);
-uint32_t OsdFlag (uint32_t svuNo, uint32_t flag, uint32_t val);
+uint32_t OsdOpen (uint32_t svuNo, uint32_t device_id = 0);
+uint32_t OsdClose(uint32_t svuNo, uint32_t device_id = 0);
+uint32_t OsdFlag (uint32_t svuNo, uint32_t flag, uint32_t val, uint32_t device_id = 0);
 
 #ifdef __cplusplus
 } //extern "C"
