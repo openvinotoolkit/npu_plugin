3edfe00e51856679297d256af3c606a6f0ba9c2a
Maxim Kurin <maxim.kurin@intel.com>
2021-09-06 21:25:55 +0300

    Enable Softmax layers with relevant input shapes

--- tests/functional/shared_tests_instances/single_layer_tests/softmax.cpp
+++ tests/functional/shared_tests_instances/single_layer_tests/softmax.cpp
@@ -89,10 +89,14 @@ const std::vector<size_t> axis2D = {
     0, 1
 };
 
+const std::vector<InferenceEngine::Precision> inOutPrecision = {
+    InferenceEngine::Precision::FP32,
+};
+
 const auto params2D = testing::Combine(
     testing::ValuesIn(netPrecisions),
-    testing::Values(InferenceEngine::Precision::UNSPECIFIED),
-    testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+    testing::ValuesIn(inOutPrecision),
+    testing::ValuesIn(inOutPrecision),
     testing::ValuesIn(inLayouts2D),
     testing::Values(InferenceEngine::Layout::ANY),
     testing::ValuesIn(inShapes2D),
@@ -109,18 +113,26 @@ INSTANTIATE_TEST_CASE_P(
 );
 
 const std::vector<InferenceEngine::SizeVector> inShapes4D = {
-    InferenceEngine::SizeVector {1, 100, 1, 1},
-    InferenceEngine::SizeVector {1, 3, 4, 3},
-    InferenceEngine::SizeVector {2, 3, 4, 5},
+    {1, 2, 204, 62},
+    {1, 12, 2, 1444},
+    {1, 2, 72, 10},
+    {1, 4, 1, 1},
+    {1, 1000, 1, 1},
+    {300, 21, 1, 1}
+};
+
+const std::vector<InferenceEngine::Layout> layouts4D = {
+    InferenceEngine::Layout::NCHW,
+    InferenceEngine::Layout::NHWC,
 };
 
 const std::vector<size_t> axis4D = {0, 1, 2, 3};
 
 const auto params4D = testing::Combine(
     testing::ValuesIn(netPrecisions),
-    testing::Values(InferenceEngine::Precision::UNSPECIFIED),
-    testing::Values(InferenceEngine::Precision::UNSPECIFIED),
-    testing::Values(InferenceEngine::Layout::NCHW),
+    testing::ValuesIn(inOutPrecision),
+    testing::ValuesIn(inOutPrecision),
+    testing::ValuesIn(layouts4D),
     testing::Values(InferenceEngine::Layout::ANY),
     testing::ValuesIn(inShapes4D),
     testing::ValuesIn(axis4D),
@@ -132,69 +144,6 @@ INSTANTIATE_TEST_CASE_P(
     smoke_SoftMax4D,
     KmbSoftMaxLayerTest,
     params4D,
-    SoftMaxLayerTest::getTestCaseName
-);
-
-// internal tests
-
-const std::vector<InferenceEngine::Precision> inPrecisionsInt = {
-    InferenceEngine::Precision::U8,
-    // TODO: nGraph testing framework doesn't support FP16
-//    InferenceEngine::Precision::FP16,
-    InferenceEngine::Precision::FP32,
-};
-
-const std::vector<InferenceEngine::Precision> outPrecisionsInt = {
-    // TODO: nGraph testing framework doesn't support FP16
-//    InferenceEngine::Precision::FP16,
-    InferenceEngine::Precision::FP32,
-};
-
-const auto params2DInt = testing::Combine(
-    testing::ValuesIn(netPrecisions),
-    testing::ValuesIn(inPrecisionsInt),
-    testing::ValuesIn(outPrecisionsInt),
-    testing::ValuesIn(inLayouts2D),
-    testing::Values(InferenceEngine::Layout::ANY),
-    testing::ValuesIn(inShapes2D),
-    testing::ValuesIn(axis2D),
-    testing::Values(LayerTestsUtils::testPlatformTargetDevice),
-    testing::Values(std::map<std::string, std::string>())
-);
-
-INSTANTIATE_TEST_CASE_P(
-    internal_SoftMax2D,
-    KmbSoftMaxLayerTest,
-    params2DInt,
-    SoftMaxLayerTest::getTestCaseName
-);
-
-const std::vector<InferenceEngine::SizeVector> inShapes4DInt = {
-    InferenceEngine::SizeVector {1, 3, 32, 32},
-    InferenceEngine::SizeVector {1, 3, 16, 16},
-};
-
-const std::vector<InferenceEngine::Layout> layouts4DInt = {
-    InferenceEngine::Layout::NCHW,
-    InferenceEngine::Layout::NHWC,
-};
-
-const auto params4DInt = testing::Combine(
-    testing::ValuesIn(netPrecisions),
-    testing::ValuesIn(inPrecisionsInt),
-    testing::ValuesIn(outPrecisionsInt),
-    testing::ValuesIn(layouts4DInt),
-    testing::ValuesIn(layouts4DInt),
-    testing::ValuesIn(inShapes4DInt),
-    testing::ValuesIn(axis4D),
-    testing::Values(LayerTestsUtils::testPlatformTargetDevice),
-    testing::Values(std::map<std::string, std::string>())
-);
-
-INSTANTIATE_TEST_CASE_P(
-    internal_SoftMax4D,
-    KmbSoftMaxLayerTest,
-    params4DInt,
     SoftMaxLayerTest::getTestCaseName
 );
 
