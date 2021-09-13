d83da7e288eae2ba587ba89abb61482a295d9040
john.brady@intel.com <john.brady@intel.com>
2021-09-13 14:09:47 +0100

    update

--- src/vpux_compiler/src/conversion/passes/convert_to_nce_ops.cpp
+++ src/vpux_compiler/src/conversion/passes/convert_to_nce_ops.cpp
@@ -193,6 +193,43 @@ private:
     Logger _log;
 };
 
+static mlir::Value alignchannelMajorWeightTensor(mlir::OpBuilder& builder, mlir::Location loc,
+                                                 const mlir::Value origFilter) {
+    const auto filterShape = getShape(origFilter);
+    const auto OC = filterShape[IE::Dims4D::Filter::OC];
+    const auto filtersPerInChan = filterShape[IE::Dims4D::Filter::IC];
+    const auto KY = filterShape[IE::Dims4D::Filter::KY];
+    const auto KX = filterShape[IE::Dims4D::Filter::KX];
+
+    const auto origFilterType = origFilter.getType().cast<mlir::ShapedType>();
+    const auto channelMajorConvAlignment = VPUIP::NCEInvariant::getChannelAlignment(origFilterType.getElementType());
+    const int64_t remainder = (filtersPerInChan * KY * KX) % channelMajorConvAlignment;
+    VPUX_THROW_UNLESS(remainder >= 0, "Channel alignment cannot be negative: {0}", remainder);
+    if (remainder == 0) {
+        // nothing to align
+        return origFilter;
+    }
+
+    auto weightsConst = origFilter.getDefiningOp<Const::DeclareOp>();
+    VPUX_THROW_UNLESS(weightsConst != nullptr, "Grouped convolution does not provide constant weights");
+
+    const int64_t alignment = channelMajorConvAlignment - remainder;
+    auto weightsContentAttr = weightsConst.contentAttr();
+    auto nchwWeightsContentAttr = weightsContentAttr.reorder(DimsOrder::NCHW);
+
+    auto flatWeightShape = Shape{OC, filtersPerInChan * KY * KX, 1, 1};
+    auto flatWeightsContentAttr = nchwWeightsContentAttr.reshape(flatWeightShape);
+    auto alignedWeightsContentAttr = flatWeightsContentAttr.padWithZero({0, 0, 0, 0}, {0, alignment, 0, 0});
+    auto nhwcWeightsContentAttr = alignedWeightsContentAttr.reorder(DimsOrder::NHWC);
+
+    auto alignedWeightShape = SmallVector<int64_t>{OC, filtersPerInChan * KY * KX + alignment, 1, 1};
+    const auto outAllocType = mlir::MemRefType::get(alignedWeightShape, origFilterType.getElementType());
+    const auto outAllocTypeNHWC = changeDimsOrder(outAllocType, DimsOrder::NHWC);
+    auto alignedWeightsOp = builder.create<Const::DeclareOp>(loc, outAllocTypeNHWC, nhwcWeightsContentAttr);
+
+    return alignedWeightsOp.output();
+}
+
 mlir::LogicalResult ConvRewrite::matchAndRewrite(IERT::ConvolutionOp origOp, mlir::PatternRewriter& rewriter) const {
     if (VPUIP::NCEInvariant::verifyOp(origOp, _log).failed()) {
         return matchFailed(rewriter, origOp, "Operation {0} does not satisfy the NCE invariant", origOp);
@@ -214,6 +251,7 @@ mlir::LogicalResult ConvRewrite::matchAndRewrite(IERT::ConvolutionOp origOp, mli
     //
 
     auto inputDPU = prepareTensorForDPU(rewriter, origOp->getLoc(), origOp.input());
+    auto alignedFilter = alignchannelMajorWeightTensor(rewriter, origOp->getLoc(), origOp.filter());
     auto filterDPU = prepareTensorForDPU(rewriter, origOp->getLoc(), origOp.filter());
 
     //
@@ -244,7 +282,7 @@ mlir::LogicalResult ConvRewrite::matchAndRewrite(IERT::ConvolutionOp origOp, mli
     auto outAllocOpCMX = rewriter.create<mlir::memref::AllocOp>(origOp->getLoc(), outTypeCMX);
 
     auto weightsTable = createWeightsTableTensor(rewriter, origOp->getLoc(), OC, inputDPU, outAllocOpCMX.memref(),
-                                                 filterDPU, origOp.bias(), nullptr);
+                                                 filterDPU, origOp.bias(), activationWindow);
 
     //
     // Create NCE per-cluster Operation
