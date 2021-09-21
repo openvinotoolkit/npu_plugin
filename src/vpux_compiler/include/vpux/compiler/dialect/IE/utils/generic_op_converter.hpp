53d0ebc66d062475b31f3afa3f24c66161675139
AndreiLupas <andrei.lupas@movidius.com>
2021-09-21 11:38:05 +0300

    move GenericOpConverter into a reusable header

---
+++ src/vpux_compiler/include/vpux/compiler/dialect/IE/utils/generic_op_converter.hpp
@@ -0,0 +1,72 @@
+//
+// Copyright Intel Corporation.
+//
+// LEGAL NOTICE: Your use of this software and any required dependent software
+// (the "Software Package") is subject to the terms and conditions of
+// the Intel(R) OpenVINO(TM) Distribution License for the Software Package,
+// which may also include notices, disclaimers, or license terms for
+// third party or open source software included in or with the Software Package,
+// and your use indicates your acceptance of all such terms. Please refer
+// to the "third-party-programs.txt" or other similarly-named text file
+// included with the Software Package for additional details.
+//
+
+#pragma once
+
+#include "vpux/compiler/dialect/IE/passes.hpp"
+
+#include "vpux/compiler/dialect/IE/ops.hpp"
+#include "vpux/compiler/utils/types.hpp"
+
+#include <mlir/Dialect/StandardOps/IR/Ops.h>
+#include <mlir/IR/BlockAndValueMapping.h>
+#include <mlir/IR/BuiltinOps.h>
+#include <mlir/IR/BuiltinTypes.h>
+#include <mlir/Transforms/DialectConversion.h>
+
+using namespace vpux;
+
+namespace {
+
+//
+// GenericOpConverter
+//
+
+class GenericOpConverter final : public mlir::ConversionPattern {
+public:
+    GenericOpConverter(mlir::TypeConverter& typeConverter, mlir::MLIRContext* ctx, Logger log)
+            : mlir::ConversionPattern(typeConverter, MatchAnyOpTypeTag{}, benefitHigh, ctx), _log(log) {
+    }
+
+public:
+    mlir::LogicalResult matchAndRewrite(mlir::Operation* origOp, ArrayRef<mlir::Value> operands,
+                                        mlir::ConversionPatternRewriter& rewriter) const final;
+
+private:
+    Logger _log;
+};
+
+mlir::LogicalResult GenericOpConverter::matchAndRewrite(mlir::Operation* origOp, ArrayRef<mlir::Value> operands,
+                                                        mlir::ConversionPatternRewriter& rewriter) const {
+    _log.trace("Process Operation '{0}'", origOp->getLoc());
+
+    auto* converter = getTypeConverter();
+    VPUX_THROW_UNLESS(converter != nullptr, "TypeConverter was not set");
+
+    const auto origOperands = origOp->getOperands();
+    VPUX_THROW_UNLESS(origOperands.size() == operands.size(), "Wrong operands size : {0}", operands.size());
+
+    mlir::BlockAndValueMapping mapper;
+    mapper.map(origOperands, operands);
+
+    auto* newOp = rewriter.clone(*origOp, mapper);
+    for (auto result : newOp->getResults()) {
+        result.setType(converter->convertType(result.getType()));
+    }
+
+    rewriter.replaceOp(origOp, newOp->getResults());
+
+    return mlir::success();
+}
+
+}  // namespace
--- src/vpux_compiler/src/dialect/IE/passes/convert_precision_to_i32.cpp
+++ src/vpux_compiler/src/dialect/IE/passes/convert_precision_to_i32.cpp
@@ -12,68 +12,13 @@
 //
 
 #include "vpux/compiler/dialect/IE/passes.hpp"
-
 #include "vpux/compiler/dialect/IE/ops.hpp"
-#include "vpux/compiler/utils/attributes.hpp"
+#include "vpux/compiler/dialect/IE/utils/generic_op_converter.hpp"
 #include "vpux/compiler/utils/types.hpp"
-
-#include "vpux/utils/IE/loop.hpp"
-#include "vpux/utils/core/checked_cast.hpp"
-#include "vpux/utils/core/error.hpp"
-#include "vpux/utils/core/format.hpp"
-#include "vpux/utils/core/numeric.hpp"
-#include "vpux/utils/core/range.hpp"
-
-#include <mlir/Dialect/StandardOps/IR/Ops.h>
-#include <mlir/IR/BlockAndValueMapping.h>
-#include <mlir/IR/BuiltinOps.h>
-#include <mlir/IR/BuiltinTypes.h>
-#include <mlir/Transforms/DialectConversion.h>
 
 using namespace vpux;
 
 namespace {
-
-//
-// GenericOpConverter
-//
-
-class GenericOpConverter final : public mlir::ConversionPattern {
-public:
-    GenericOpConverter(mlir::TypeConverter& typeConverter, mlir::MLIRContext* ctx, Logger log)
-            : mlir::ConversionPattern(typeConverter, MatchAnyOpTypeTag{}, benefitHigh, ctx), _log(log) {
-    }
-
-public:
-    mlir::LogicalResult matchAndRewrite(mlir::Operation* origOp, ArrayRef<mlir::Value> operands,
-                                        mlir::ConversionPatternRewriter& rewriter) const final;
-
-private:
-    Logger _log;
-};
-
-mlir::LogicalResult GenericOpConverter::matchAndRewrite(mlir::Operation* origOp, ArrayRef<mlir::Value> operands,
-                                                        mlir::ConversionPatternRewriter& rewriter) const {
-    _log.trace("Process Operation '{0}'", origOp->getLoc());
-
-    auto* converter = getTypeConverter();
-    VPUX_THROW_UNLESS(converter != nullptr, "TypeConverter was not set");
-
-    const auto origOperands = origOp->getOperands();
-    VPUX_THROW_UNLESS(origOperands.size() == operands.size(), "Wrong operands size : {0}", operands.size());
-
-    mlir::BlockAndValueMapping mapper;
-    mapper.map(origOperands, operands);
-
-    auto* newOp = rewriter.clone(*origOp, mapper);
-    for (auto result : newOp->getResults()) {
-        result.setType(converter->convertType(result.getType()));
-    }
-
-    rewriter.replaceOp(origOp, newOp->getResults());
-
-    return mlir::success();
-}
 
 //
 // ConvertPrecisionToI32Pass
