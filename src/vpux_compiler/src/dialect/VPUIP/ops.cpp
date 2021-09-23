39e7baec7d394f21b4e47d8faeb2cd5a4ec341a6
Vadim-Intel <curca.vadim@intel.com>
2021-09-23 10:38:29 +0300

    compilation PASS, inference FAIL

--- src/vpux_compiler/src/dialect/VPUIP/ops.cpp
+++ src/vpux_compiler/src/dialect/VPUIP/ops.cpp
@@ -223,7 +223,7 @@ void redirectOpInterfacesForIE(mlir::DialectRegistry& registry) {
     registry.addOpInterface<IE::HSwishOp, OpModelForSW<VPUIP::HSwishUPAOp>>();
     registry.addOpInterface<IE::MishOp, OpModelForSW<VPUIP::MishUPAOp>>();
     registry.addOpInterface<IE::ErfOp, OpModelForSW<VPUIP::ErfUPAOp>>();
-    // broadcast
+    registry.addOpInterface<IE::BroadcastOp, OpModelForSW<VPUIP::BroadcastUPAOp>>();
     registry.addOpInterface<IE::FloorOp, OpModelForSW<VPUIP::FloorUPAOp>>();
     registry.addOpInterface<IE::TanhOp, OpModelForSW<VPUIP::TanhUPAOp>>();
     registry.addOpInterface<IE::FakeQuantizeOp, OpModelForSW<VPUIP::FakeQuantizeUPAOp>>();
@@ -287,7 +287,7 @@ void redirectOpInterfacesForIERT(mlir::DialectRegistry& registry) {
     registry.addOpInterface<IERT::HSwishOp, OpModelForSW>();
     registry.addOpInterface<IERT::MishOp, OpModelForSW>();
     registry.addOpInterface<IERT::ErfOp, OpModelForSW>();
-    // broadcast
+    registry.addOpInterface<IERT::BroadcastOp, OpModelForSW>();
     registry.addOpInterface<IERT::FloorOp, OpModelForSW>();
     registry.addOpInterface<IERT::TanhOp, OpModelForSW>();
     registry.addOpInterface<IERT::QuantizeOp, OpModelForSW>();
