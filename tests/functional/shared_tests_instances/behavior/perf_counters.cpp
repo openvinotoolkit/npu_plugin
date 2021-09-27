1a93d7eb6e814d32a57d228e9ae9954459698474
Zamelin, Pavel <pavel.zamelin@intel.com>
2021-09-27 17:41:05 +0300

    upd VpuxPerfCountersTest SetUp, Skip

--- tests/functional/shared_tests_instances/behavior/perf_counters.cpp
+++ tests/functional/shared_tests_instances/behavior/perf_counters.cpp
@@ -4,53 +4,61 @@
 
 #include "behavior/perf_counters.hpp"
 #include "kmb_layer_test.hpp"
+#include "common/functions.h"
 
 using namespace LayerTestsUtils;
 
 namespace BehaviorTestsDefinitions {
 
 class VpuxPerfCountersTest :
-        public PerfCountersTest,
+        public PerfCountersTest, 
         virtual public KmbLayerTestsCommon {
+                
+    void SetUp() override {
+        std::tie(std::ignore, // Network precision
+                 KmbLayerTestsCommon::targetDevice,
+                 KmbLayerTestsCommon::configuration) = this->GetParam();
 
-        void TearDown() override 
-        {
-            KmbLayerTestsCommon::TearDown();     
+        KmbLayerTestsCommon::configuration.insert({InferenceEngine::PluginConfigParams::KEY_PERF_COUNT,
+                                                   InferenceEngine::PluginConfigParams::YES});
+
+        KmbLayerTestsCommon::function = ngraph::builder::subgraph::makeConvPoolRelu();
+    }
+
+    void SkipBeforeLoad() override {
+        const std::string backendName = getBackendName(*core);
+        const auto noDevice = backendName.empty();
+        if (noDevice) {
+            throw LayerTestsUtils::KmbSkipTestException("backend is empty (no device)");
         }
+    }
+
+    void TearDown() override {
+        KmbLayerTestsCommon::TearDown();
+    }
 };
 
 TEST_P(VpuxPerfCountersTest, NotEmptyWhenExecuted) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED()
-    // Set up config
-    KmbLayerTestsCommon::configuration.insert(PerfCountersTest::configuration.begin(),
-                                              PerfCountersTest::configuration.end());   
-    KmbLayerTestsCommon::configuration.insert({ InferenceEngine::PluginConfigParams::KEY_PERF_COUNT,
-                                                InferenceEngine::PluginConfigParams::YES });
-    KmbLayerTestsCommon::targetDevice = LayerTestsUtils::testPlatformTargetDevice;
-    // Set up ngrpah::Function
-    KmbLayerTestsCommon::function = ngraph::builder::subgraph::makeConvPoolRelu();
-    KmbLayerTestsCommon::Run(); 
-    std::map<std::string, InferenceEngine::InferenceEngineProfileInfo> perfMap;
-    perfMap = KmbLayerTestsCommon::inferRequest.GetPerformanceCounts();
-    ASSERT_NE(perfMap.size(), 0);
+    KmbLayerTestsCommon::Run();
+    if (KmbLayerTestsCommon::inferRequest) {
+        std::map<std::string, InferenceEngine::InferenceEngineProfileInfo> perfMap;
+        perfMap = KmbLayerTestsCommon::inferRequest.GetPerformanceCounts();
+        ASSERT_NE(perfMap.size(), 0);
+    }
 }
 
-}// namespace BehaviorTestsDefinitions
+}  // namespace BehaviorTestsDefinitions
 
 using namespace BehaviorTestsDefinitions;
-const LayerTestsUtils::KmbTestEnvConfig envConfig;
 
 namespace {
-    const std::vector<std::map<std::string, std::string>> configs = {
-            {}
-    };
+const std::vector<std::map<std::string, std::string>> configs = {{}};
 
-    INSTANTIATE_TEST_CASE_P(smoke_BehaviorTests, VpuxPerfCountersTest,
-                            ::testing::Combine(
-                                    ::testing::Values(InferenceEngine::Precision::FP32),
-                                    ::testing::Values(LayerTestsUtils::testPlatformTargetDevice),
-                                    ::testing::ValuesIn(configs)),
-                            VpuxPerfCountersTest::getTestCaseName);
+INSTANTIATE_TEST_CASE_P(smoke_BehaviorTests, VpuxPerfCountersTest,
+                        ::testing::Combine(::testing::Values(InferenceEngine::Precision::FP32),
+                                           ::testing::Values(LayerTestsUtils::testPlatformTargetDevice),
+                                           ::testing::ValuesIn(configs)),
+                        VpuxPerfCountersTest::getTestCaseName);
 
 }  // namespace
-
