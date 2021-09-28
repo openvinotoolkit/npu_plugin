6f7651d8f6ce28dafa0c9f550c3b6994db3fce5b
Vadim-Intel <curca.vadim@intel.com>
2021-09-28 15:22:51 +0300

    esthetic rework in inferReturnType Components

--- src/vpux_compiler/src/dialect/IE/ops/broadcast.cpp
+++ src/vpux_compiler/src/dialect/IE/ops/broadcast.cpp
@@ -35,26 +35,25 @@ mlir::FailureOr<SmallVector<int64_t>> extractIntVector(mlir::Location loc, const
     return errorAt(loc, "Parameter were not provided");
 }
 
-SmallVector<int64_t> get_result_shape_bidirectional(ArrayRef<int64_t> inShape, SmallVector<int64_t> targetShape) {
-    const auto target_padded_rank = std::max(inShape.size(), targetShape.size());
+SmallVector<int64_t> getResultShapeBidirectional(SmallVector<int64_t>& inShape, SmallVector<int64_t>& targetShape) {
+    const auto targetPaddedRank = std::max(inShape.size(), targetShape.size());
 
-    SmallVector<int64_t> resultShape(target_padded_rank);
-    SmallVector<int64_t> inShapeVec = to_small_vector(inShape);
+    SmallVector<int64_t> resultShape(targetPaddedRank);
 
-    while (inShapeVec.size() < target_padded_rank) {
-        inShapeVec.insert(inShapeVec.begin(), 1);
+    while (inShape.size() < targetPaddedRank) {
+        inShape.insert(inShape.begin(), 1);
     }
 
-    while (targetShape.size() < target_padded_rank) {
+    while (targetShape.size() < targetPaddedRank) {
         targetShape.insert(targetShape.begin(), 1);
     }
 
-    for (size_t i = 0; i < target_padded_rank; ++i) {
-        VPUX_THROW_UNLESS(inShapeVec[i] == 1 || targetShape[i] == 1 || inShapeVec[i] == targetShape[i],
-                          "Broadcast incorrect target shape. Expecting either 1 or {0}. Got {1}", inShapeVec[i],
-                          targetShape[i]);  // this check is duplicated in core/src/op/broadcast.cpp, to remove??
+    for (size_t i = 0; i < targetPaddedRank; ++i) {
+        VPUX_THROW_UNLESS(inShape[i] == 1 || targetShape[i] == 1 || inShape[i] == targetShape[i],
+                          "Broadcast incorrect target shape. Expecting either 1 or {0}. Got {1}", inShape[i],
+                          targetShape[i]);  // this check is duplicated in core/src/op/broadcast.cpp, remove this one??
 
-        resultShape[i] = std::max(inShapeVec[i], targetShape[i]);
+        resultShape[i] = std::max(inShape[i], targetShape[i]);
     }
 
     return resultShape;
@@ -71,19 +70,20 @@ mlir::LogicalResult vpux::IE::BroadcastOp::inferReturnTypeComponents(
         return mlir::failure();
     }
 
-    const auto inType = broadcast.input().getType().cast<mlir::ShapedType>();
-    const auto targetShape = extractIntVector(loc, broadcast.target_shape()).getValue();
-
+    auto inShape = to_small_vector(broadcast.input().getType().cast<mlir::ShapedType>().getShape());
+    auto targetShape = extractIntVector(loc, broadcast.target_shape()).getValue();
+    const auto inType = broadcast.input().getType().cast<mlir::ShapedType>().getElementType();
     const auto broadcastMode = broadcast.mode().getValue();
+
     SmallVector<int64_t> outShape;
 
     if (broadcastMode == IE::BroadcastType::NUMPY || broadcastMode == IE::BroadcastType::EXPLICIT) {
         outShape = targetShape;
     } else if (broadcastMode == IE::BroadcastType::BIDIRECTIONAL) {
-        outShape = get_result_shape_bidirectional(inType.getShape(), targetShape);
+        outShape = getResultShapeBidirectional(inShape, targetShape);
     }
 
-    inferredReturnShapes.emplace_back(outShape, inType.getElementType());
+    inferredReturnShapes.emplace_back(outShape, inType);
 
     return mlir::success();
 }
