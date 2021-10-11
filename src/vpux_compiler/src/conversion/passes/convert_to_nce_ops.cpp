9dd8c782f0fd2a397b9da8549b8a4ef7faca6798
Brady, John <john.brady@intel.com>
2021-10-11 20:39:31 +0000

    Update convert_to_nce_ops.cpp for maxpool bitbatternsize

--- src/vpux_compiler/src/conversion/passes/convert_to_nce_ops.cpp
+++ src/vpux_compiler/src/conversion/passes/convert_to_nce_ops.cpp
@@ -346,7 +346,7 @@ mlir::LogicalResult MaxPoolRewrite::matchAndRewrite(IERT::MaxPoolOp origOp, mlir
     const auto kernelStrides = parseIntArrayAttr<int64_t>(origOp.strides());
 
     const auto bitPatternSize =
-            VPUIP::NCESparsity::getBitPatternSize(kernelSize, kernelStrides[0], origInputType.getElementType(), IC);
+            VPUIP::NCESparsity::getBitPatternSize(kernelSize, kernelStrides[0], origInputType.getElementType(), 1);
 
     //
     // Prepare input for DPU
