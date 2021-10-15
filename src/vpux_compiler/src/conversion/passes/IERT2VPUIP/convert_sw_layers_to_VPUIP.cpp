a3bccdea43d2d22c8c9cc1a005442ae8e354e590
Eugene Smirnov <eugene.smirnov@intel.com>
2021-10-15 03:35:25 +0300

    added any software layer matcher

--- src/vpux_compiler/src/conversion/passes/IERT2VPUIP/convert_sw_layers_to_VPUIP.cpp
+++ src/vpux_compiler/src/conversion/passes/IERT2VPUIP/convert_sw_layers_to_VPUIP.cpp
@@ -38,6 +38,161 @@ private:
     void safeRunOnFunc() final;
 };
 
+// base trait - works for simplest SW kernels
+template <class T>
+class SW_Kernel_Args_Trait {
+    T _actual;
+public:
+    SW_Kernel_Args_Trait(T actual) : _actual(actual) {}
+    mlir::ValueRange inputs() const {
+        return {};
+        //return {_actual->input()};
+    }
+    mlir::ValueRange outputs() const {
+        return {};
+        //return {_actual->output()};
+    }
+    mlir::ValueRange args() const {
+        return {};
+          //  return mlir::ValueRange();
+    };
+};
+
+template <>
+class SW_Kernel_Args_Trait<mlir::Operation*> {
+    mlir::Operation *_actual;
+public:
+    SW_Kernel_Args_Trait(mlir::Operation * actual) : _actual(actual) {}
+    mlir::ValueRange inputs() const {
+        return {};
+       // static_assert(false, "Operation trait not needed");
+    }
+    mlir::ValueRange outputs() const {
+        return {};
+        //static_assert(false, "Operation trait not needed");
+    }
+    mlir::ValueRange args() const {
+        return {};
+        //static_assert(false, "Operation trait not needed");
+    };
+};
+
+class SW_Kernel_Inputs {
+public:
+    template <class T>
+    static mlir::ValueRange Invoke(T op) {
+        return SW_Kernel_Args_Trait<T>(op).inputs();
+    }
+};
+
+class SW_Kernel_Outputs {
+public:
+    template <class T>
+    static mlir::ValueRange Invoke(T op) {
+        return SW_Kernel_Args_Trait<T>(op).outputs();
+    }
+};
+
+class SW_Kernel_Args {
+public:
+    template <class T>
+    static mlir::ValueRange Invoke(T op) {
+        return SW_Kernel_Args_Trait<T>(op).args();
+    }
+};
+
+
+
+template <class TN, class ... TN_1>
+class run_for_type {
+public:
+    template <class Functor>
+    static auto findAndRun(mlir::Operation* op) -> decltype(Functor::template Invoke<TN*>(nullptr)) {
+        if (auto casted = mlir::dyn_cast_or_null<TN>(op)) {
+            return Functor::Invoke(casted);
+        }
+        return run_for_type<TN_1...>::template findAndRun<Functor>(op);
+    }
+};
+
+template <class T0>
+class run_for_type<T0> {
+public:
+    template <class Functor>
+    static auto findAndRun(mlir::Operation* op) -> decltype(Functor::template Invoke<T0*>(nullptr)) {
+        return Functor::Invoke(mlir::dyn_cast_or_null<T0>(op));
+    }
+};
+
+template <class ... TN>
+class supported_types {
+    class FindResult {
+    public:
+        template <class T>
+        static  bool Invoke(T op) {
+            return op != nullptr;
+        }
+    };
+public:
+    bool isSupported(mlir::Operation* origOp) const {
+        return run_for_type<TN ...>::template findAndRun<FindResult>(origOp);
+    }
+    ::mlir::ValueRange inputs(mlir::Operation* origOp) const {
+        return run_for_type<TN ...>::template findAndRun<SW_Kernel_Inputs>(origOp);
+    }
+    ::mlir::ValueRange outputs(mlir::Operation* origOp) const {
+        return run_for_type<TN ...>::template findAndRun<SW_Kernel_Outputs>(origOp);
+    }
+    ::mlir::ValueRange args(mlir::Operation* origOp) const {
+        return run_for_type<TN ...>::template findAndRun<SW_Kernel_Args>(origOp);
+    }
+};
+
+//
+// Any-SWLayerRewrite
+//
+
+
+class ANYSWLayerRewrite final : public mlir::RewritePattern {
+public:
+    ANYSWLayerRewrite(mlir::MLIRContext* ctx, Logger log)
+            : mlir::RewritePattern(MatchAnyOpTypeTag{}, mlir::PatternBenefit{1}, ctx), _log(log) {
+    }
+
+    using  sup_types = supported_types<
+            IERT::SigmoidOp,
+            IERT::SoftMaxOp>;
+
+
+    static const sup_types& operation() {
+        static const sup_types registeredOps;
+        return registeredOps;
+    }
+
+public:
+    mlir::LogicalResult match(mlir::Operation *op) const override {
+        if (operation().isSupported(op)) {
+            op->dump();
+            return mlir::success();
+        }
+        _log.error("unsupported sourceOP : {0}", op->getName());
+        return mlir::failure();
+    }
+
+    void rewrite(mlir::Operation *origOp, mlir::PatternRewriter &rewriter) const override {
+        rewriter.replaceOpWithNewOp<VPUIP::SW_Kernel>(origOp,
+            operation().inputs(origOp),
+            operation().outputs(origOp),
+            mlir::SymbolRefAttr(),  // TODO: add generation of built-in functions into trait
+            mlir::IntegerAttr(0), // tile 0
+            operation().args(origOp));
+    }
+
+private:
+    Logger _log;
+};
+
+
 void ConvertSWLayers2VPUIPPass::safeRunOnFunc() {
     auto& ctx = getContext();
 
@@ -52,19 +207,19 @@ void ConvertSWLayers2VPUIPPass::safeRunOnFunc() {
     target.addLegalOp<IERT::GenericReshapeOp, IERT::ImplicitReorderOp>();
     target.addLegalOp<IERT::TimestampOp>();
 
-    /*mlir::RewritePatternSet patterns(&ctx);
-    patterns.insert<CTCGreedyDecoderSeqLenRewrite>(&ctx, _log);
-    patterns.insert<LSTMCellRewrite>(&ctx, _log);
+    mlir::RewritePatternSet patterns(&ctx);
+    patterns.insert<ANYSWLayerRewrite>(&ctx, _log);
+    /*patterns.insert<LSTMCellRewrite>(&ctx, _log);
     patterns.insert<LSTMSequenceRewrite>(&ctx, _log);
     patterns.insert<FakeQuantizeRewrite>(&ctx, _log);
     patterns.insert<FullyConnectedRewrite>(&ctx, _log);
-    patterns.insert<RewriteConvolution>(&ctx, _log);
+    patterns.insert<RewriteConvolution>(&ctx, _log)*/
     populateWithGenerated(patterns);
 
     auto func = getFunction();
     if (mlir::failed(mlir::applyFullConversion(func, target, std::move(patterns)))) {
         signalPassFailure();
-    }*/
+    }
 }
 
 
--- tests/lit/conversion/passes/IERT2VPUIP/convert_softmax_to_sw_kernel_VPUIP.mlir
+++ tests/lit/conversion/passes/IERT2VPUIP/convert_softmax_to_sw_kernel_VPUIP.mlir
@@ -7,7 +7,7 @@ func @SingleSWLayer(%arg0: memref<1x1x1x1000xf16>, %arg1: memref<1x1x1x1000xf16>
     %0 = IERT.SoftMax {axisInd = 3} inputs(%arg0 : memref<1x1x1x1000xf16>) outputs(%arg1 : memref<1x1x1x1000xf16>) -> memref<1x1x1x1000xf16>
     return %0: memref<1x1x1x1000xf16>
 
-    // CHECK: [[VAR0:%.*]] = VPUIP.SoftMaxUPA
+    // CHECK: [[VAR0:%.*]] = VPUIP.SW.Kernel
     // CHECK-SAME:      axisInd = 3
     // CHECK-SAME:      inputs(%arg0 : memref<1x1x1x1000xf16>)
     // CHECK-SAME:      outputs(%arg1 : memref<1x1x1x1000xf16>)
