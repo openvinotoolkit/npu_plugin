71f2c72efe42913734f1fdcd0de3856358dc2a06
Bakalin, Andrew <andrew.bakalin@intel.com>
2021-10-19 12:01:39 +0300

    [ELF] Fix relocation and symbol's utility functions

--- src/vpux_elf/src/types/relocation_entry.cpp
+++ src/vpux_elf/src/types/relocation_entry.cpp
@@ -16,16 +16,16 @@
 using namespace elf;
 
 //! Extract symbol index from info
-Elf_Word elf64RSym(Elf_Xword info) {
+Elf_Word elf::elf64RSym(Elf_Xword info) {
     return info >> 32;
 }
 
 //! Extract relocation type from info
-Elf_Word elf64RType(Elf_Xword info) {
+Elf_Word elf::elf64RType(Elf_Xword info) {
     return static_cast<Elf_Word>(info);
 }
 
 //! Pack relocation type and symbol index into info
-Elf_Xword elf64RInfo(Elf_Word sym, Elf_Word type) {
+Elf_Xword elf::elf64RInfo(Elf_Word sym, Elf_Word type) {
     return (static_cast<Elf_Xword>(sym) << 32) + (static_cast<Elf_Xword>(type));
 }
