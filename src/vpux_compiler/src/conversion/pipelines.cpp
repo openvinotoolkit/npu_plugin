310d8f2aa99aa07b9c48cc72e5b64b0b69d40e7a
Eugene Smirnov <eugene.smirnov@intel.com>
2021-10-22 15:19:34 +0300

    added convert sw_kernel pass into lowering piepeline

--- src/vpux_compiler/src/conversion/pipelines.cpp
+++ src/vpux_compiler/src/conversion/pipelines.cpp
@@ -42,6 +42,7 @@ void vpux::buildLowerIE2IERTPipeline(mlir::OpPassManager& pm, Logger log) {
 void vpux::buildLowerIERT2VPUIPPipeline(mlir::OpPassManager& pm, Logger log) {
     const auto grc = getDefaultGreedyRewriteConfig();
 
+    pm.addPass(createConvertSWLayers2VPUIPPass(log));
     pm.addPass(createConvertLayers2VPUIPPass(log));
     pm.addPass(mlir::createCanonicalizerPass(grc));
     pm.addPass(createConvertDeclarations2VPUIPPass(log));
