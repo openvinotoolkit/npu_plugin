5043a8458433adf605357debe25ccebef2a45205
john.brady@intel.com <john.brady@intel.com>
2021-10-23 19:14:40 +0100

    fix build error

--- src/vpux_compiler/src/dialect/IE/passes/expand_activation_channels.cpp
+++ src/vpux_compiler/src/dialect/IE/passes/expand_activation_channels.cpp
@@ -16,6 +16,7 @@
 #include <vpux/compiler/utils/quantization.hpp>
 #include "vpux/compiler/core/layers.hpp"
 #include "vpux/compiler/dialect/IE/ops.hpp"
+#include "vpux/compiler/dialect/VPUIP/utils.hpp"
 #include "vpux/compiler/dialect/const/ops.hpp"
 #include "vpux/compiler/utils/attributes.hpp"
 #include "vpux/compiler/utils/error.hpp"
@@ -23,7 +24,6 @@
 #include "vpux/compiler/utils/types.hpp"
 #include "vpux/utils/core/func_ref.hpp"
 #include "vpux/utils/core/numeric.hpp"
-#include "vpux/compiler/dialect/VPUIP/utils.hpp"
 
 #include <mlir/Pass/PassManager.h>
 #include <mlir/Transforms/DialectConversion.h>
