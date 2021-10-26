5e7995e5c5c0531212cc6709a8daa4c073089158
Zamelin, Pavel <pavel.zamelin@intel.com>
2021-10-26 13:49:04 +0000

    Enable BehaviorTestsBasic tests

---
+++ tests/functional/shared_tests_instances/behavior/infer_request/perf_counters.cpp
@@ -0,0 +1,21 @@
+// Copyright (C) 2018-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <vector>
+
+#include "behavior/infer_request/perf_counters.hpp"
+#include "ie_plugin_config.hpp"
+
+using namespace BehaviorTestsDefinitions;
+namespace {
+const std::vector<std::map<std::string, std::string>> configs = {
+    {}
+};
+
+INSTANTIATE_TEST_SUITE_P(smoke_BehaviorTests, InferRequestPerfCountersTest,
+        ::testing::Combine(
+            ::testing::Values(CommonTestUtils::DEVICE_KEEMBAY),
+            ::testing::ValuesIn(configs)),
+        InferRequestPerfCountersTest::getTestCaseName);
+}  // namespace
--- tests/functional/shared_tests_instances/skip_tests_config.cpp
+++ tests/functional/shared_tests_instances/skip_tests_config.cpp
@@ -7,32 +7,71 @@
 #include <string>
 #include <vector>
 
+#include "functional_test_utils/plugin_cache.hpp"
+#include "common_test_utils/common_utils.hpp"
+#include "kmb_layer_test.hpp"
+#include "common/functions.h"
+
+
+const std::vector<std::string> DISABLED_TESTS = {
+    // TODO Tests failed due to starting infer on IA side
+    ".*CorrectConfigAPITests.*",
+
+    // ARM CPU Plugin is not available on Yocto
+    ".*IEClassLoadNetworkTest.*HETERO.*",
+    ".*IEClassLoadNetworkTest.*MULTI.*",
+
+    // Cannot detect vpu platform when it's not passed
+    // Skip tests on Yocto which passes device without platform
+    // [Track number: E#12774]
+    ".*IEClassLoadNetworkTest.LoadNetworkWithDeviceIDNoThrow.*",
+    ".*IEClassLoadNetworkTest.LoadNetworkWithBigDeviceIDThrows.*",
+    ".*IEClassLoadNetworkTest.LoadNetworkWithInvalidDeviceIDThrows.*",
+
+    // double free detected
+    // [Track number: S#27343]
+    ".*InferConfigInTests\\.CanInferWithConfig.*",
+    ".*InferConfigTests\\.withoutExclusiveAsyncRequests.*",
+    ".*InferConfigTests\\.canSetExclusiveAsyncRequests.*",
+
+    // TODO Add safe Softplus support
+    ".*ActivationLayerTest.*SoftPlus.*",
+
+    // TODO: Issue: 63469
+    ".*KmbConversionLayerTest.*ConvertLike.*"
+};
+
+const std::vector<std::string> TESTS_TO_SKIP_IF_NO_DEVICE_AVAILABLE = {
+    // Cannot run InferRequest tests without a device to infer to
+    ".*BehaviorTests.InferRequest*.*",
+};
+
+const bool IS_DEVICE_AVAILABLE = []() -> bool {
+    const auto corePtr = PluginCache::get().ie();
+    if (corePtr == nullptr) {
+        return false;
+    }
+    const auto backendName = getBackendName(*corePtr);
+    const auto noDevice = backendName.empty();
+    if (noDevice) {
+        std::cout << "backend is empty (no device)"
+                  << " - some tests might be skipped" << std::endl;
+    }
+    return !noDevice;
+}();
+
+const std::vector<std::string> ALL_DISABLED_TESTS_PATTERNS = []() {
+    auto allDisabledTestPatterns = DISABLED_TESTS;
+    if (!IS_DEVICE_AVAILABLE) {
+        allDisabledTestPatterns.insert(
+            allDisabledTestPatterns.end(),
+            TESTS_TO_SKIP_IF_NO_DEVICE_AVAILABLE.cbegin(),
+            TESTS_TO_SKIP_IF_NO_DEVICE_AVAILABLE.cend()
+        );
+    }
+    return allDisabledTestPatterns;
+}();
+
 std::vector<std::string> disabledTestPatterns() {
-    return {
-        // TODO Tests failed due to starting infer on IA side
-        ".*CorrectConfigAPITests.*",
-
-        // ARM CPU Plugin is not available on Yocto
-        ".*IEClassLoadNetworkTest.*HETERO.*",
-        ".*IEClassLoadNetworkTest.*MULTI.*",
-
-        // Cannot detect vpu platform when it's not passed
-        // Skip tests on Yocto which passes device without platform
-        // [Track number: E#12774]
-        ".*IEClassLoadNetworkTest.LoadNetworkWithDeviceIDNoThrow.*",
-        ".*IEClassLoadNetworkTest.LoadNetworkWithBigDeviceIDThrows.*",
-        ".*IEClassLoadNetworkTest.LoadNetworkWithInvalidDeviceIDThrows.*",
-
-        // double free detected
-        // [Track number: S#27343]
-        ".*InferConfigInTests\\.CanInferWithConfig.*",
-        ".*InferConfigTests\\.withoutExclusiveAsyncRequests.*",
-        ".*InferConfigTests\\.canSetExclusiveAsyncRequests.*",
-
-        // TODO Add safe Softplus support
-        ".*ActivationLayerTest.*SoftPlus.*",
-
-        // TODO: Issue: 63469
-        ".*KmbConversionLayerTest.*ConvertLike.*"
-    };
+    return ALL_DISABLED_TESTS_PATTERNS;
 }
