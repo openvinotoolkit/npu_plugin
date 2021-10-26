c5ec490c895d364bd46d7e42b3966764de6656ea
Vadim-Intel <curca.vadim@intel.com>
2021-10-26 17:27:49 +0300

    cleanup

--- sw_runtime_kernels/jtag_tests/app/layer_tests/test_icv/leon/tests/custom_cpp_sigmoid.cpp
+++ sw_runtime_kernels/jtag_tests/app/layer_tests/test_icv/leon/tests/custom_cpp_sigmoid.cpp
@@ -17,8 +17,8 @@ namespace ICV_TESTS_NAMESPACE(ICV_TESTS_PASTE2(ICV_TEST_SUITE_NAME, Sigmoid))
 {
 static constexpr std::initializer_list<SingleTest> sigmoid_test_list
 {
+    {{2, 2, 2}, {2, 2, 2}, orderZYX, FPE("sigmoid_fp16.elf"), {sw_params::Location::NN_CMX}},
     // {{2, 2, 2}, {2, 2, 2}, orderZYX, FPE("sigmoid.elf"), {/*{{224, 1, 1}, {1, 128, 1}, {0, 0, 0}, 3, 0}, */{0 /*axis*/, sw_params::Location::UPA_CMX /*mem type*/,}}},
-    {{2, 2, 1}, {2, 2, 1}, orderZYX, FPE("sigmoid_fp16.elf"), {1, sw_params::Location::NN_CMX}},
     // {{2, 2, 2}, {2, 2, 2}, orderZYX, FPE("softmax.elf"), {/*{{224, 1, 1}, {1, 128, 1}, {0, 0, 0}, 3, 0}, */{1 /*axis*/, sw_params::Location::UPA_CMX /*mem type*/,}}},
     // {{4, 4, 4}, {4, 4, 4}, orderZYX, FPE("softmax.elf"), {/*{{224, 1, 1}, {1, 128, 1}, {0, 0, 0}, 3, 0}, */{0 /*axis*/, sw_params::Location::NN_CMX/*mem type*/,}}},
     // {{4, 4, 4}, {4, 4, 4}, orderZYX, FPE("softmax.elf"), {/*{{224, 1, 1}, {1, 128, 1}, {0, 0, 0}, 3, 0}, */{1 /*axis*/, sw_params::Location::NN_CMX/*mem type*/,}}},
@@ -59,19 +59,14 @@ protected:
         paramContainer.resize(((int)sizeof(sw_params::SigmoidParams) + 7) / 8);
         CustomCppTests<fp16>::initData();
         const SingleTest* test = m_currentTest;
-        // int32_t ind[subspace::MAX_DIMS] = {0, };
-        // subspace::orderToIndices((t_D8StorageOrder)(test->storageOrder), ind);
-        // m_axis = ind[test->customLayerParams.layerParams[0]];
-        // m_temp = allocData<float>(m_inputTensor.memoryDims().dims[m_axis]);
+        int32_t ind[subspace::MAX_DIMS] = {0, };
+        subspace::orderToIndices((t_D8StorageOrder)(test->storageOrder), ind);
         m_sigmoidParams = reinterpret_cast<sw_params::SigmoidParams *>(paramContainer.data());
         *m_sigmoidParams = sw_params::SigmoidParams();
-        // m_sigmoidParams->axis = m_axis;
         m_params.paramData = reinterpret_cast<uint32_t*>(paramContainer.data());
         m_params.paramDataLen = paramContainer.size() * sizeof(uint64_t);
-        // m_requiredTensorLocation = static_cast<sw_params::Location>(test->customLayerParams.layerParams[1]);
+        m_requiredTensorLocation = static_cast<sw_params::Location>(test->customLayerParams.layerParams[0]);
         m_params.baseParamData = sw_params::ToBaseKernelParams(m_sigmoidParams);
-
-        // printf("%d", m_inputTensor.m_dims.dims[0]);
 
         // std::cout << "init data end" << std::endl;
     }
@@ -99,20 +94,20 @@ protected:
 
         rand_seed();
 
-        if (customData || 1)
+        if (customData)
         {
-            // static const float data[] = { -6.1601562, -10.28125  , -3.9277344 , -13.375     , -13.8046875 , -9.2421875 ,
-            //                               -6.6015625,  -2.3320312, -1.34375   ,  -1.7880859 ,  -3.6308594 ,  9.5       ,
-            //                                4.3984375,   6.9257812,  0.85058594,  11.53125   ,   0.26367188,  7.1640625 ,
-            //                              -10.0078125,  -7.9140625, -0.6176758 ,  -6.6679688 ,   4.9453125 , -3.6660156 ,
-            //                               -8.734375 ,  23.1875   ,  3.359375  ,  15.5234375 ,  -4.3242188 ,  3.0800781 ,
-            //                               -1.5019531,   4.015625 , -0.84521484,   0.61279297,   5.3554688 , -0.85546875, 4.265625 };
+            static const float data[] = { -6.1601562, -10.28125  , -3.9277344 , -13.375     , -13.8046875 , -9.2421875 ,
+                                          -6.6015625,  -2.3320312, -1.34375   ,  -1.7880859 ,  -3.6308594 ,  9.5       ,
+                                           4.3984375,   6.9257812,  0.85058594,  11.53125   ,   0.26367188,  7.1640625 ,
+                                         -10.0078125,  -7.9140625, -0.6176758 ,  -6.6679688 ,   4.9453125 , -3.6660156 ,
+                                          -8.734375 ,  23.1875   ,  3.359375  ,  15.5234375 ,  -4.3242188 ,  3.0800781 ,
+                                          -1.5019531,   4.015625 , -0.84521484,   0.61279297,   5.3554688 , -0.85546875, 4.265625 };
 
             // input
             int i = 0;
             m_inputTensor.forEach(false, [&](const MemoryDims& indices)
             {
-                m_inputTensor.at(indices) = f32Tof16(i++);
+                m_inputTensor.at(indices) = f32Tof16(data[i++]);
             });
         }
         else
@@ -136,47 +131,11 @@ protected:
     void generateReferenceData() override {
         m_inputTensor.forEach(false, [&](const MemoryDims& indices){
             float val = f16Tof32(m_inputTensor.at(indices));
-            float ref;
-            ref = val * -1.0f;
+            float ref = val * -1.0f;
             ref = 1.0f + expf(ref);
             ref = 1.0f / ref;
             m_referenceOutputTensor.at(indices) = f32Tof16(ref);
         });
-    //    const auto axis = m_axis;//decodeNegativeAxis(m_axis);
-
-    //     MemoryDims iDims = m_inputTensor.memoryDims();
-    //     MemoryDims ind = MemoryDims();
-    //     int totalSets = m_inputTensor.totalLines(m_axis);
-
-    //     for(int s = 0; s < totalSets; ++s)
-    //     {
-    //         ind.dims[m_axis] = 0;
-    //         float largest = f16Tof32(m_inputTensor.at(ind));
-    //         for (int i = 1; i < iDims.dims[m_axis]; ++i)
-    //         {
-    //             ind.dims[m_axis] = i;
-    //             float val = f16Tof32(m_inputTensor.at(ind));
-    //             largest = std::max(largest, val);
-    //         }
-
-    //         float sum = 0.f;
-    //         for (int i = 0; i < iDims.dims[m_axis]; ++i)
-    //         {
-    //             ind.dims[m_axis] = i;
-    //             float val = f16Tof32(m_inputTensor.at(ind));
-    //             m_temp[i] = pow(exp(1), val - largest)/*exp(val - largest)*/;
-    //             sum = sum + m_temp[i];
-    //         }
-
-    //         for (int i = 0; i < iDims.dims[m_axis]; ++i)
-    //         {
-    //             ind.dims[m_axis] = i;
-    //             float val = m_temp[i] / sum;
-    //             m_referenceOutputTensor.at(ind) = f32Tof16(val);
-    //         }
-
-    //         m_inputTensor.incrementLine(ind, m_axis);
-    //     }
     }
     virtual bool checkResult() override
         {
@@ -216,7 +175,6 @@ private:
 
     // Additional buffer to avoid convertion back and forth
     int m_axis;
-    // float* m_temp;
     std::vector<uint64_t> paramContainer;
     sw_params::SigmoidParams * m_sigmoidParams;
 //    Tensor<fp16> m_referenceTensor;
