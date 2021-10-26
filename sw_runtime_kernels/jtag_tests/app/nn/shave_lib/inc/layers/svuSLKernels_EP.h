1b84fbc0d599ae668b26d017cce34d7b60537eb4
Vadim-Intel <curca.vadim@intel.com>
2021-10-26 15:56:25 +0300

    first working/debug version

--- sw_runtime_kernels/jtag_tests/app/nn/shave_lib/inc/layers/svuSLKernels_EP.h
+++ sw_runtime_kernels/jtag_tests/app/nn/shave_lib/inc/layers/svuSLKernels_EP.h
@@ -139,6 +139,8 @@ extern uint32_t SLK_preEdsl;
 extern uint32_t SLK_preResample;
 extern uint32_t SLK_preSoftmax;
 extern uint32_t SLK_singleShaveSoftmax;
+extern uint32_t SLK_preSigmoid_fp16;
+extern uint32_t SLK_sigmoid_fp16;
 extern uint32_t SLK_preCorrelation;
 extern uint32_t SLK_preROIPooling;
 extern uint32_t SLK_preCTCDecoder;
