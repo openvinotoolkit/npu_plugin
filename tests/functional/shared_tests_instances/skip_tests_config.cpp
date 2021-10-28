fb3ef2a2917fcc10526e7c4f76f71dcfd9c0fdfd
pazamelin <pavel.zamelin@intel.com>
2021-10-28 16:34:31 +0300

    Config initialization of first call

--- tests/functional/shared_tests_instances/skip_tests_config.cpp
+++ tests/functional/shared_tests_instances/skip_tests_config.cpp
@@ -77,60 +77,60 @@ private:
     const BackendName _backend;
 };
 
-const auto ALL_DISABLED_TESTS_PATTERNS = []() {
-    //  Unconditionally disabled test patterns
-    std::vector<std::string> disabledTests = {
-        // TODO Tests failed due to starting infer on IA side
-        ".*CorrectConfigAPITests.*",
-
-        // ARM CPU Plugin is not available on Yocto
-        ".*IEClassLoadNetworkTest.*HETERO.*",
-        ".*IEClassLoadNetworkTest.*MULTI.*",
-
-        // Cannot detect vpu platform when it's not passed
-        // Skip tests on Yocto which passes device without platform
-        // [Track number: E#12774]
-        ".*IEClassLoadNetworkTest.LoadNetworkWithDeviceIDNoThrow.*",
-        ".*IEClassLoadNetworkTest.LoadNetworkWithBigDeviceIDThrows.*",
-        ".*IEClassLoadNetworkTest.LoadNetworkWithInvalidDeviceIDThrows.*",
-
-        // double free detected
-        // [Track number: S#27343]
-        ".*InferConfigInTests\\.CanInferWithConfig.*",
-        ".*InferConfigTests\\.withoutExclusiveAsyncRequests.*",
-        ".*InferConfigTests\\.canSetExclusiveAsyncRequests.*",
-
-        // TODO Add safe Softplus support
-        ".*ActivationLayerTest.*SoftPlus.*",
-
-        // TODO: Issue: 63469
-        ".*KmbConversionLayerTest.*ConvertLike.*"
-    };
-
-    // Conditional skip patterns
-    ConditionalSkipper skipper(disabledTests);
-    const static BackendName backendName;
-    
-    skipper.addPatterns(
-        backendName.isEmpty(),  
-        "backend is empty (no device)",
-        {
-            // Cannot run InferRequest tests without a device to infer to
-            ".*InferRequest.*",
-        }
-    );
-
-    skipper.addPatterns(
-        backendName.isZero(),  
-        "* CumSum layer is not supported by MTL platform *",
-        {
-            "*VpuxBehaviorTestsSetBlob*",
-        }
-    );
-    
-    return disabledTests;
-}( );
-
 std::vector<std::string> disabledTestPatterns() {
-    return ALL_DISABLED_TESTS_PATTERNS;
+    static const auto allDisabledTestPatterns = []() {
+        //  Unconditionally disabled test patterns
+        std::vector<std::string> disabledTests = {
+            // TODO Tests failed due to starting infer on IA side
+            ".*CorrectConfigAPITests.*",
+
+            // ARM CPU Plugin is not available on Yocto
+            ".*IEClassLoadNetworkTest.*HETERO.*",
+            ".*IEClassLoadNetworkTest.*MULTI.*",
+
+            // Cannot detect vpu platform when it's not passed
+            // Skip tests on Yocto which passes device without platform
+            // [Track number: E#12774]
+            ".*IEClassLoadNetworkTest.LoadNetworkWithDeviceIDNoThrow.*",
+            ".*IEClassLoadNetworkTest.LoadNetworkWithBigDeviceIDThrows.*",
+            ".*IEClassLoadNetworkTest.LoadNetworkWithInvalidDeviceIDThrows.*",
+
+            // double free detected
+            // [Track number: S#27343]
+            ".*InferConfigInTests\\.CanInferWithConfig.*",
+            ".*InferConfigTests\\.withoutExclusiveAsyncRequests.*",
+            ".*InferConfigTests\\.canSetExclusiveAsyncRequests.*",
+
+            // TODO Add safe Softplus support
+            ".*ActivationLayerTest.*SoftPlus.*",
+
+            // TODO: Issue: 63469
+            ".*KmbConversionLayerTest.*ConvertLike.*"
+        };
+
+        // Conditional skip patterns
+        ConditionalSkipper skipper(disabledTests);
+        const static BackendName backendName;
+    
+        skipper.addPatterns(
+            backendName.isEmpty(),  
+            "backend is empty (no device)",
+            {
+                // Cannot run InferRequest tests without a device to infer to
+                ".*InferRequest.*",
+            }
+        );
+
+        skipper.addPatterns(
+            backendName.isZero(),  
+            "* CumSum layer is not supported by MTL platform *",
+            {
+                "*VpuxBehaviorTestsSetBlob*",
+            }
+        );
+
+        return disabledTests;
+    }( );
+
+    return allDisabledTestPatterns;
 }
