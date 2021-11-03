28cbd5d4ed37ed77ff4e48b41e30a090b976bdfa
kshkileva <ksenia.shkileva@intel@com>
2021-11-03 14:46:03 +0300

    [MLIR] Support HW Quantize/Dequantize with Eltwise Add
    While Eltwise And is not supported on MTL, implement Quantize/Dequantize with Eltwise Add.
    Add extra QuantizeCast for Dequantize/Quantie case for dividing/multiplicating scales by 2.

--- src/vpux_compiler/src/dialect/IE/passes/convert_quantize_ops_to_eltwise.cpp
+++ src/vpux_compiler/src/dialect/IE/passes/convert_quantize_ops_to_eltwise.cpp
@@ -15,6 +15,7 @@
 
 #include "vpux/compiler/utils/logging.hpp"
 #include "vpux/compiler/utils/passes.hpp"
+#include "vpux/compiler/utils/quantization.hpp"
 #include "vpux/compiler/utils/rewriter.hpp"
 #include "vpux/compiler/utils/types.hpp"
 
@@ -23,35 +24,6 @@
 using namespace vpux;
 
 namespace {
-
-//
-// GenericConverter
-//
-
-template <class ConcreteOp>
-class GenericConverter final : public mlir::OpRewritePattern<ConcreteOp> {
-public:
-    GenericConverter(mlir::MLIRContext* ctx, Logger log)
-            : mlir::OpRewritePattern<ConcreteOp>(ctx, benefitLow), _log(log) {
-    }
-
-public:
-    mlir::LogicalResult matchAndRewrite(ConcreteOp originOp, mlir::PatternRewriter& rewriter) const final;
-
-private:
-    Logger _log;
-};
-
-template <class ConcreteOp>
-mlir::LogicalResult GenericConverter<ConcreteOp>::matchAndRewrite(ConcreteOp originOp,
-                                                                  mlir::PatternRewriter& rewriter) const {
-    const auto broadcastType =
-            vpux::IE::AutoBroadcastTypeAttr::get(this->getContext(), IE::AutoBroadcastType::NONE_OR_EXPLICIT);
-    rewriter.replaceOpWithNewOp<IE::AndOp>(originOp, originOp.getType(), originOp.input(), originOp.input(),
-                                           broadcastType, nullptr);
-
-    return mlir::success();
-}
 
 //
 // ConvertQuantizeOpsToEltwisePass
@@ -64,12 +36,104 @@ public:
         _log.setName(Base::getArgumentName());
     }
 
+public:
+    class DequantizeOpConverter;
+    class QuantizeOpConverter;
+
 private:
     void safeRunOnFunc() final;
 
 private:
     Logger _log;
 };
+
+//
+// DequantizeOpConverter
+//
+
+class ConvertQuantizeOpsToEltwisePass::DequantizeOpConverter final : public mlir::OpRewritePattern<IE::DequantizeOp> {
+public:
+    DequantizeOpConverter(mlir::MLIRContext* ctx, Logger log)
+            : mlir::OpRewritePattern<IE::DequantizeOp>(ctx), _log(log) {
+    }
+
+public:
+    mlir::LogicalResult matchAndRewrite(IE::DequantizeOp originOp, mlir::PatternRewriter& rewriter) const final;
+
+private:
+    Logger _log;
+};
+
+mlir::LogicalResult ConvertQuantizeOpsToEltwisePass::DequantizeOpConverter::matchAndRewrite(
+        IE::DequantizeOp originOp, mlir::PatternRewriter& rewriter) const {
+    const auto broadcastType =
+            vpux::IE::AutoBroadcastTypeAttr::get(getContext(), IE::AutoBroadcastType::NONE_OR_EXPLICIT);
+
+    auto inElemType = originOp.input().getType().cast<mlir::ShapedType>().getElementType();
+    auto uniformQInElemType = inElemType.dyn_cast<mlir::quant::UniformQuantizedType>();
+    const auto scale = uniformQInElemType.getScale();
+    // originQElemType = <u8:fp32, scale>
+    // newQElemType = <u8:fp32, scale / 2>
+    // Op -> originQElemType -> QuantizeCastOp -> newQElemType -> AddOp(output x2) -> result
+    const auto newScale = static_cast<double>(scale / 2.0);
+    const auto zeroPoint = uniformQInElemType.getZeroPoint();
+
+    auto qType = inElemType.dyn_cast<mlir::quant::QuantizedType>();
+    auto outQuantizeElemType = mlir::quant::UniformQuantizedType::get(
+            qType.getFlags(), qType.getStorageType(), qType.getExpressedType(), newScale, zeroPoint,
+            qType.getStorageTypeMin(), qType.getStorageTypeMax());
+
+    auto quantizeCastOp = rewriter.create<IE::QuantizeCastOp>(originOp.getLoc(), originOp.input(), outQuantizeElemType);
+
+    rewriter.replaceOpWithNewOp<IE::AddOp>(originOp, originOp.getType(), quantizeCastOp.getResult(),
+                                           quantizeCastOp.getResult(), broadcastType, nullptr);
+
+    return mlir::success();
+}
+
+//
+// QuantizeOpConverter
+//
+
+class ConvertQuantizeOpsToEltwisePass::QuantizeOpConverter final : public mlir::OpRewritePattern<IE::QuantizeOp> {
+public:
+    QuantizeOpConverter(mlir::MLIRContext* ctx, Logger log): mlir::OpRewritePattern<IE::QuantizeOp>(ctx), _log(log) {
+    }
+
+public:
+    mlir::LogicalResult matchAndRewrite(IE::QuantizeOp originOp, mlir::PatternRewriter& rewriter) const final;
+
+private:
+    Logger _log;
+};
+
+mlir::LogicalResult ConvertQuantizeOpsToEltwisePass::QuantizeOpConverter::matchAndRewrite(
+        IE::QuantizeOp originOp, mlir::PatternRewriter& rewriter) const {
+    const auto broadcastType =
+            vpux::IE::AutoBroadcastTypeAttr::get(getContext(), IE::AutoBroadcastType::NONE_OR_EXPLICIT);
+
+    auto outElemType = originOp.output().getType().cast<mlir::ShapedType>().getElementType();
+    auto uniformQOutElemType = outElemType.dyn_cast<mlir::quant::UniformQuantizedType>();
+    const auto scale = uniformQOutElemType.getScale();
+    // originQElemType = <u8:fp32, scale>
+    // newQElemType = <u8:fp32, scale * 2>
+    // Op -> AddOp(output x2) -> newQElemType -> QuantizeCastOp -> originQElemType -> result
+    const auto newScale = static_cast<double>(scale * 2.0);
+    const auto zeroPoint = uniformQOutElemType.getZeroPoint();
+
+    auto qType = outElemType.dyn_cast<mlir::quant::QuantizedType>();
+    auto quantizeElemType = mlir::quant::UniformQuantizedType::get(
+            qType.getFlags(), qType.getStorageType(), qType.getExpressedType(), newScale, zeroPoint,
+            qType.getStorageTypeMin(), qType.getStorageTypeMax());
+    auto newAddOutType = mlir::RankedTensorType::get(originOp.getType().getShape(), quantizeElemType);
+
+    auto addOp = rewriter.create<IE::AddOp>(originOp.getLoc(), newAddOutType, originOp.input(), originOp.input(),
+                                            broadcastType, nullptr);
+
+    rewriter.replaceOpWithNewOp<IE::QuantizeCastOp>(originOp, addOp.getResult(), outElemType);
+
+    return mlir::success();
+}
 
 void ConvertQuantizeOpsToEltwisePass::safeRunOnFunc() {
     auto& ctx = getContext();
@@ -84,11 +148,12 @@ void ConvertQuantizeOpsToEltwisePass::safeRunOnFunc() {
         auto inElemType = dequantizeOp.input().getType().cast<mlir::ShapedType>().getElementType();
         return inElemType.isa<mlir::quant::UniformQuantizedPerAxisType>();
     });
-    target.addLegalOp<IE::AndOp>();
+    target.addLegalOp<IE::AddOp>();
+    target.addLegalOp<IE::QuantizeCastOp>();
 
     mlir::RewritePatternSet patterns(&ctx);
-    patterns.insert<GenericConverter<IE::QuantizeOp>>(&ctx, _log);
-    patterns.insert<GenericConverter<IE::DequantizeOp>>(&ctx, _log);
+    patterns.insert<DequantizeOpConverter>(&ctx, _log);
+    patterns.insert<QuantizeOpConverter>(&ctx, _log);
 
     auto func = getFunction();
     if (mlir::failed(mlir::applyPartialConversion(func, target, std::move(patterns)))) {
--- tests/lit/dialect/IE/passes/convert_quantize_ops_to_eltwise.mlir
+++ tests/lit/dialect/IE/passes/convert_quantize_ops_to_eltwise.mlir
@@ -1,15 +1,20 @@
 // RUN: vpux-opt --split-input-file --convert-quantize-ops-to-eltwise  %s | FileCheck %s
 
-!qElemType = type !quant.uniform<u8:f32, 1.000000e+00>
+!qElemType0 = type !quant.uniform<u8:f32, 2.000000e+00>
+!qElemType1 = type !quant.uniform<u8:f32, 1.000000e+00>
+!qElemType2 = type !quant.uniform<u8:f32, 5.000000e-01>
 
 func @PerTensor(%arg0 : tensor<1x4xf32>) -> tensor<1x4xf32> {
-    %0 = IE.Quantize(%arg0) {dstElemType = !qElemType} : tensor<1x4xf32> -> tensor<1x4x!qElemType>
-    %1 = IE.Dequantize(%0) {dstElemType = f32} : tensor<1x4x!qElemType> -> tensor<1x4xf32>
-
-    // CHECK:  %0 = IE.And(%arg0, %arg0) {auto_broadcast = "NONE_OR_EXPLICIT"} : tensor<1x4xf32>, tensor<1x4xf32> -> tensor<1x4x!qElemType>
-    // CHECK:  %1 = IE.And(%0, %0) {auto_broadcast = "NONE_OR_EXPLICIT"} : tensor<1x4x!qElemType>, tensor<1x4x!qElemType> -> tensor<1x4xf32>
+    %0 = IE.Quantize(%arg0) {dstElemType = !qElemType1} : tensor<1x4xf32> -> tensor<1x4x!qElemType1>
+    %1 = IE.Dequantize(%0) {dstElemType = f32} : tensor<1x4x!qElemType1> -> tensor<1x4xf32>
 
     return %1 : tensor<1x4xf32>
+
+    // CHECK:  %[[VAL0:.*]] = IE.Add(%arg0, %arg0) {auto_broadcast = "NONE_OR_EXPLICIT"} : tensor<1x4xf32>, tensor<1x4xf32> -> tensor<1x4x!qElemType0>
+    // CHECK:  %[[VAL1:.*]] = IE.QuantizeCast(%[[VAL0]]) {dstElemType = !qElemType1} : tensor<1x4x!qElemType0> -> tensor<1x4x!qElemType1>
+    // CHECK:  %[[VAL2:.*]] = IE.QuantizeCast(%[[VAL1]]) {dstElemType = !qElemType2} : tensor<1x4x!qElemType1> -> tensor<1x4x!qElemType2>
+    // CHECK:  %[[VAL3:.*]] = IE.Add(%[[VAL2]], %[[VAL2]]) {auto_broadcast = "NONE_OR_EXPLICIT"} : tensor<1x4x!qElemType2>, tensor<1x4x!qElemType2> -> tensor<1x4xf32>
+    // CHECK:  return %[[VAL3]] : tensor<1x4xf32>
 }
 
 // -----
