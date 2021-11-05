8d6d16fe87b71a831f6b5f95b2c32cfbb992634d
kgan <kgan>
2021-11-05 17:16:06 +0800

    merge selectOp single layer test into eltwise.cpp

--- tests/functional/shared_tests_instances/single_layer_tests/eltwise.cpp
+++ tests/functional/shared_tests_instances/single_layer_tests/eltwise.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include "single_layer_tests/eltwise.hpp"
 #include "kmb_layer_test.hpp"
+#include "single_layer_tests/select.hpp"
+#include "common_test_utils/test_constants.hpp"
 
 namespace LayerTestsDefinitions {
 
@@ -131,6 +133,20 @@ TEST_P(KmbEltwiseLayerTest_MLIR, CompareWithRefs_SW) {
 TEST_P(KmbEltwiseLayerTest_MLIR, CompareWithRefs_HW) {
     useCompilerMLIR();
     setReferenceHardwareModeMLIR();
+    Run();
+}
+
+class KmbSelectLayerTest : public SelectLayerTest, virtual public LayerTestsUtils::KmbLayerTestsCommon {
+    void SkipBeforeValidate() override {
+        throw LayerTestsUtils::KmbSkipTestException("comparison fails");
+    }
+};
+
+class KmbSelectLayerTest_MLIR : public KmbSelectLayerTest {};
+
+TEST_P(KmbSelectLayerTest_MLIR, CompareWithRefs_HW) {
+    useCompilerMLIR();
+    setReferenceSoftwareModeMLIR();
     Run();
 }
 
@@ -322,3 +338,49 @@ INSTANTIATE_TEST_CASE_P(smoke_CompareWithRefs_Specific_subtract, KmbEltwiseLayer
                         KmbEltwiseLayerTest::getTestCaseName);
 
 }  // namespace
+
+using namespace LayerTestsDefinitions;
+
+namespace {
+const std::vector<InferenceEngine::Precision> inputPrecision = {
+    //InferenceEngine::Precision::I8
+    InferenceEngine::Precision::FP16,
+    //InferenceEngine::Precision::FP32 
+};
+
+const std::vector<std::vector<std::vector<size_t>>> noneShapes = {
+    {{1}, {1}, {1}},
+    {{8}, {8}, {8}},
+    {{4, 5}, {4, 5}, {4, 5}},
+    {{3, 4, 5}, {3, 4, 5}, {3, 4, 5}},
+    {{2, 3, 4, 5}, {2, 3, 4, 5}, {2, 3, 4, 5}},
+    {{2, 3, 4, 5, 6}, {2, 3, 4, 5, 6}, {2, 3, 4, 5, 6}}
+};
+
+const auto noneCases = ::testing::Combine(
+    ::testing::ValuesIn(noneShapes),
+    ::testing::ValuesIn(inputPrecision),
+
+    ::testing::Values(ngraph::op::AutoBroadcastSpec::NONE),
+    ::testing::Values(LayerTestsUtils::testPlatformTargetDevice)
+);
+
+const std::vector<std::vector<std::vector<size_t>>> numpyShapes = {
+    {{1}, {1}, {1}},
+    {{1}, {1}, {16}},
+    {{1}, {8}, {8}},
+    {{8}, {1}, {8}},
+    {{8}, {8}, {8}},
+    {{4, 1}, {1}, {4, 8}},
+    {{8, 1}, {8, 1}, {8, 1}},
+};
+
+const auto numpyCases = ::testing::Combine(
+    ::testing::ValuesIn(numpyShapes),
+    ::testing::ValuesIn(inputPrecision),
+    ::testing::Values(ngraph::op::AutoBroadcastSpec::NUMPY),
+    ::testing::Values(LayerTestsUtils::testPlatformTargetDevice)
+);
+
+INSTANTIATE_TEST_CASE_P(smoke_TestsSelectOp_none, KmbSelectLayerTest_MLIR, noneCases, KmbSelectLayerTest::getTestCaseName);
+}
