431c5c804cfcdf40ef567623c9e7f07ef4bd8f7b
Mateusz Darecki <mateusz.darecki@intel.com>
2021-11-07 23:48:21 +0100

    [SPILLING FRONTEND] During spilling when some operation is no longer active, move it to ready compute ops list

--- src/vpux_compiler/src/core/feasible_memory_scheduler.cpp
+++ src/vpux_compiler/src/core/feasible_memory_scheduler.cpp
@@ -501,6 +501,7 @@ void FeasibleMemoryScheduler::forceScheduleActiveOpEviction() {
     }
     for (auto notActive : noLongerActive) {
         _activeComputeOps.erase(notActive);
+        _readyComputeOps.insert(notActive);
     }
 
     // add with a spilled write state
