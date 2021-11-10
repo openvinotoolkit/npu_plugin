f871e09cc75137accee3bf766ad867f2cb8fdb9f
Smirnov, Eugene <eugene.smirnov@intel.com>
2021-11-10 10:36:50 +0300

    [MLIR] Add sigmoid kernel and update softmax

---
+++ sw_runtime_kernels/kernels/inc/param_sigmoid.h
@@ -0,0 +1,37 @@
+// {% copyright %}
+
+#pragma once
+
+#include <common_types.h>
+
+#ifdef __cplusplus
+namespace sw_params {
+#endif
+
+#pragma pack(push, 1)
+
+struct SigmoidParams {
+    struct MemRefData input;
+    struct MemRefData output;
+    int64_t axis;
+};
+
+#pragma pack (pop)
+
+inline struct BaseKernelParams ToBaseKernelParams(struct SigmoidParams * params) {
+    struct BaseKernelParams result;
+    result.numInputs = 1;
+    result.numOutputs = 1;
+#ifdef  __cplusplus
+    result.inputsOffset = reinterpret_cast<uint8_t*>(&(params->input)) - reinterpret_cast<uint8_t*>(params);
+    result.outputsOffset = reinterpret_cast<uint8_t*>(&(params->output)) - reinterpret_cast<uint8_t*>(params);
+#else
+    result.inputsOffset = (uint8_t*)(&(params->input)) - (uint8_t*)(params);
+    result.outputsOffset = (uint8_t*)(&(params->output)) - (uint8_t*)(params);
+#endif
+    return result;
+}
+
+#ifdef __cplusplus
+}  // namespace sw_params
+#endif
---
+++ sw_runtime_kernels/kernels/sigmoid_fp16.c
@@ -0,0 +1,30 @@
+// {% copyright %}
+
+#include <moviVectorTypes.h>
+#include <math.h>
+#include <param_sigmoid.h>
+
+void sigmoid_fp16(const struct SigmoidParams *lParams) {
+
+    half* p_act_data = (half*)(lParams->input.dataAddr); // 0x1F000000
+    half* p_act_out = (half*)(lParams->output.dataAddr); // 0x1F004000
+
+    int32_t *pDims     = (int32_t *)(lParams->input.dimsAddr);    // 0x1E000000 , dimsAddr is globally computed in  WIN_E
+    int64_t *pStrdides = (int64_t *)(lParams->input.stridesAddr); // 0x1E000000 , stridesAddr is globally computed in  WIN_E
+
+    int32_t nElements = 1;
+    int32_t i = 0;
+    half act = 0;
+
+    for (i = 0; i!= lParams->input.numDims; i++ ) {
+        // TODO: check overflow
+        nElements *=  pDims[i];
+    }
+
+    for (uint32_t e = 0; e < nElements; ++e) {
+        act = *p_act_data++ * -1.0f;
+        act = 1.0f + expf(act);
+        act = 1.0f / act;
+        *p_act_out++ = (half)act;
+    }
+}
