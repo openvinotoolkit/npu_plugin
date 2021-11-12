ef5c10125176b2a1ad1806e4ade504cb31757726
Eugene Smirnov <eugene.smirnov@intel.com>
2021-11-12 17:41:17 +0300

    codereview: removed unneeded AllocOp from legal operation set

--- src/vpux_compiler/src/conversion/passes/IERT2VPUIP/convert_layers_to_VPUIP.cpp
+++ src/vpux_compiler/src/conversion/passes/IERT2VPUIP/convert_layers_to_VPUIP.cpp
@@ -302,7 +302,6 @@ void ConvertLayers2VPUIPPass::safeRunOnFunc() {
     target.addLegalOp<IERT::SubViewOp, IERT::ConcatViewOp>();
     target.addLegalOp<IERT::GenericReshapeOp, IERT::PermuteCastOp>();
     target.addLegalOp<IERT::QuantizeCastOp>();
-    target.addLegalOp<mlir::memref::AllocOp>();
     target.addLegalOp<VPUIP::SW_KernelOp>();
     target.markOpRecursivelyLegal<VPUIP::SW_KernelOp>([&](mlir::Operation*) {
         return true;
