9de156821013aecd917d11128d975599b41346c6
Zoran Zomborat <zoran.zomborat@intel.com>
2021-11-15 14:58:20 +0200

    [EMU][VPUX]Extend converter list with explicit CPP routines for more complex cases

--- src/vpux_compiler/src/conversion/passes/IE2EMU/convert_layers_to_EMU.cpp
+++ src/vpux_compiler/src/conversion/passes/IE2EMU/convert_layers_to_EMU.cpp
@@ -24,6 +24,247 @@ using namespace vpux;
 namespace {
 
 //
+// SplitRewrite
+//
+
+class SplitRewrite final : public mlir::OpRewritePattern<IE::SplitOp> {
+public:
+    SplitRewrite(mlir::MLIRContext* ctx, Logger log): mlir::OpRewritePattern<IE::SplitOp>(ctx), _log(log) {
+    }
+
+public:
+    mlir::LogicalResult matchAndRewrite(IE::SplitOp origOp, mlir::PatternRewriter& rewriter) const final;
+
+private:
+    Logger _log;
+};
+
+mlir::LogicalResult SplitRewrite::matchAndRewrite(IE::SplitOp origOp, mlir::PatternRewriter& rewriter) const {
+    _log.trace("Found Split Operation '{0}'", origOp->getLoc());
+    rewriter.replaceOpWithNewOp<EMU::SplitUPAOp>(origOp, origOp.getResults().getTypes(), origOp.input(),
+                                                 origOp.axis_valueAttr(), origOp.num_splitsAttr());
+    _log.trace("Replaced with 'EMU.SplitUPAOp'");
+
+    return mlir::success();
+}
+
+//
+// CTCGreedyDecoderSeqLenRewrite
+//
+
+class CTCGreedyDecoderSeqLenRewrite final : public mlir::OpRewritePattern<IE::CTCGreedyDecoderSeqLenOp> {
+public:
+    CTCGreedyDecoderSeqLenRewrite(mlir::MLIRContext* ctx, Logger log)
+            : mlir::OpRewritePattern<IE::CTCGreedyDecoderSeqLenOp>(ctx), _log(log) {
+    }
+
+public:
+    mlir::LogicalResult matchAndRewrite(IE::CTCGreedyDecoderSeqLenOp origOp,
+                                        mlir::PatternRewriter& rewriter) const final;
+
+private:
+    Logger _log;
+};
+
+mlir::LogicalResult CTCGreedyDecoderSeqLenRewrite::matchAndRewrite(IE::CTCGreedyDecoderSeqLenOp origOp,
+                                                                   mlir::PatternRewriter& rewriter) const {
+    _log.trace("Found CTCGreedyDecoderSeqLen Operation '{0}'", origOp->getLoc());
+    auto resultTypes = origOp.getResults().getTypes();
+    rewriter.replaceOpWithNewOp<EMU::CTCGreedyDecoderSeqLenUPAOp>(origOp, resultTypes[0], resultTypes[1],
+                                                                  origOp.input(), origOp.sequenceLength(),
+                                                                  origOp.blankIndex(), origOp.mergeRepeatedAttr());
+    _log.trace("Replaced with 'EMU.CTCGreedyDecoderSeqLenOp'");
+
+    return mlir::success();
+}
+
+//
+// LSTMCellRewrite
+//
+
+class LSTMCellRewrite final : public mlir::OpRewritePattern<IE::LSTMCellOp> {
+public:
+    LSTMCellRewrite(mlir::MLIRContext* ctx, Logger log): mlir::OpRewritePattern<IE::LSTMCellOp>(ctx), _log(log) {
+    }
+
+public:
+    mlir::LogicalResult matchAndRewrite(IE::LSTMCellOp origOp, mlir::PatternRewriter& rewriter) const final;
+
+private:
+    Logger _log;
+};
+
+mlir::LogicalResult LSTMCellRewrite::matchAndRewrite(IE::LSTMCellOp origOp, mlir::PatternRewriter& rewriter) const {
+    _log.trace("Found LSTMCell Operation '{0}'", origOp->getLoc());
+    auto resultTypes = origOp.getResults().getTypes();
+    rewriter.replaceOpWithNewOp<EMU::LSTMCellUPAOp>(origOp, resultTypes[0], resultTypes[1], origOp.inputData(),
+                                                    origOp.initialHiddenState(), origOp.initialCellState(),
+                                                    origOp.weights(), origOp.biases());
+    _log.trace("Replaced with 'EMU.LSTMCellOp'");
+
+    return mlir::success();
+}
+
+//
+// LSTMSequenceRewrite
+//
+
+class LSTMSequenceRewrite final : public mlir::OpRewritePattern<IE::LSTMSequenceOp> {
+public:
+    LSTMSequenceRewrite(mlir::MLIRContext* ctx, Logger log)
+            : mlir::OpRewritePattern<IE::LSTMSequenceOp>(ctx), _log(log) {
+    }
+
+public:
+    mlir::LogicalResult matchAndRewrite(IE::LSTMSequenceOp origOp, mlir::PatternRewriter& rewriter) const final;
+
+private:
+    Logger _log;
+};
+
+mlir::LogicalResult LSTMSequenceRewrite::matchAndRewrite(IE::LSTMSequenceOp origOp,
+                                                         mlir::PatternRewriter& rewriter) const {
+    _log.trace("Found LSTMSequence Operation '{0}'", origOp->getLoc());
+    auto resultTypes = origOp.getResults().getTypes();
+    rewriter.replaceOpWithNewOp<EMU::LSTMSequenceUPAOp>(origOp, resultTypes[0], resultTypes[1], resultTypes[2],
+                                                        origOp.inputData(), origOp.initialHiddenState(),
+                                                        origOp.initialCellState(), origOp.weights(), origOp.biases(),
+                                                        origOp.sequenceLengthAttr(), origOp.directionAttr());
+    _log.trace("Replaced with 'EMU.LSTMSequenceOp'");
+
+    return mlir::success();
+}
+
+//
+// FakeQuantizeRewrite
+//
+
+class FakeQuantizeRewrite final : public mlir::OpRewritePattern<IE::FakeQuantizeOp> {
+public:
+    FakeQuantizeRewrite(mlir::MLIRContext* ctx, Logger log)
+            : mlir::OpRewritePattern<IE::FakeQuantizeOp>(ctx), _log(log) {
+    }
+
+public:
+    mlir::LogicalResult matchAndRewrite(IE::FakeQuantizeOp origOp, mlir::PatternRewriter& rewriter) const final;
+
+private:
+    Logger _log;
+};
+
+mlir::LogicalResult FakeQuantizeRewrite::matchAndRewrite(IE::FakeQuantizeOp origOp,
+                                                         mlir::PatternRewriter& rewriter) const {
+    _log.trace("Found FakeQuantize Operation '{0}'", origOp->getLoc());
+
+    auto inLowConst = origOp.input_low().getDefiningOp<Const::DeclareOp>();
+    auto inHighConst = origOp.input_high().getDefiningOp<Const::DeclareOp>();
+    auto outLowConst = origOp.output_low().getDefiningOp<Const::DeclareOp>();
+    auto outHighConst = origOp.output_high().getDefiningOp<Const::DeclareOp>();
+
+    if (inLowConst == nullptr || inHighConst == nullptr || outLowConst == nullptr || outHighConst == nullptr) {
+        return matchFailed(rewriter, origOp, "Got non constant parameters");
+    }
+
+    rewriter.replaceOpWithNewOp<EMU::FakeQuantizeUPAOp>(
+            origOp, origOp.output().getType(), origOp.input(), origOp.levels(), inLowConst.contentAttr(),
+            inHighConst.contentAttr(), outLowConst.contentAttr(), outHighConst.contentAttr());
+    _log.trace("Replaced with 'EMU.FakeQuantizeUPAOp'");
+    return mlir::success();
+}
+
+// Bias reshape
+
+mlir::FailureOr<mlir::Value> reshapeBias(mlir::PatternRewriter& rewriter, mlir::Value bias) {
+    auto biasConst = bias.getDefiningOp<Const::DeclareOp>();
+    if (biasConst == nullptr) {
+        return mlir::failure();
+    }
+    const auto origBiasType = bias.getType().cast<mlir::ShapedType>();
+    const auto origBiasShape = origBiasType.getShape();
+
+    const auto newBiasShape = ShapeRef({origBiasShape[1]});
+    const auto newBiasType = changeShape(origBiasType, newBiasShape);
+    const auto newBiasConstAttr = biasConst.contentAttr().reshape(newBiasShape);
+    return rewriter.replaceOpWithNewOp<Const::DeclareOp>(biasConst, newBiasType, newBiasConstAttr).output();
+}
+
+//
+// FullyConnectedRewrite
+//
+
+class FullyConnectedRewrite final : public mlir::OpRewritePattern<IE::FullyConnectedOp> {
+public:
+    FullyConnectedRewrite(mlir::MLIRContext* ctx, Logger log)
+            : mlir::OpRewritePattern<IE::FullyConnectedOp>(ctx), _log(log) {
+    }
+
+public:
+    mlir::LogicalResult matchAndRewrite(IE::FullyConnectedOp origOp, mlir::PatternRewriter& rewriter) const final;
+
+private:
+    Logger _log;
+};
+
+mlir::LogicalResult FullyConnectedRewrite::matchAndRewrite(IE::FullyConnectedOp origOp,
+                                                           mlir::PatternRewriter& rewriter) const {
+    _log.trace("Found FullyConnected Operation '{0}'", origOp->getLoc());
+
+    if (origOp.bias() == nullptr) {
+        rewriter.replaceOpWithNewOp<EMU::FullyConnectedUPAOp>(origOp, origOp.output().getType(), origOp.input(),
+                                                              origOp.weights(), nullptr);
+        return mlir::success();
+    }
+
+    auto newBias = reshapeBias(rewriter, origOp.bias());
+    if (mlir::failed(newBias)) {
+        return mlir::failure();
+    }
+    rewriter.replaceOpWithNewOp<EMU::FullyConnectedUPAOp>(origOp, origOp.output().getType(), origOp.input(),
+                                                          origOp.weights(), newBias.getValue());
+    _log.trace("Replaced with 'EMU.FullyConnectedUPAOp'");
+    return mlir::success();
+}
+
+//
+// ConvolutionRewrite
+//
+
+class ConvolutionRewrite final : public mlir::OpRewritePattern<IE::ConvolutionOp> {
+public:
+    ConvolutionRewrite(mlir::MLIRContext* ctx, Logger log): mlir::OpRewritePattern<IE::ConvolutionOp>(ctx), _log(log) {
+    }
+
+public:
+    mlir::LogicalResult matchAndRewrite(IE::ConvolutionOp origOp, mlir::PatternRewriter& rewriter) const final;
+
+private:
+    Logger _log;
+};
+
+mlir::LogicalResult ConvolutionRewrite::matchAndRewrite(IE::ConvolutionOp origOp,
+                                                        mlir::PatternRewriter& rewriter) const {
+    _log.trace("Found Convolution Operation '{0}'", origOp->getLoc());
+
+    const int64_t groups = 1;
+    if (origOp.bias() == nullptr) {
+        rewriter.replaceOpWithNewOp<EMU::ConvolutionUPAOp>(
+                origOp, origOp.output().getType(), origOp.input(), origOp.filter(), nullptr, origOp.strides(),
+                origOp.dilations(), origOp.pads_begin(), origOp.pads_end(), groups);
+        return mlir::success();
+    }
+
+    auto newBias = reshapeBias(rewriter, origOp.bias());
+    if (mlir::failed(newBias)) {
+        return mlir::failure();
+    }
+    rewriter.replaceOpWithNewOp<EMU::ConvolutionUPAOp>(
+            origOp, origOp.output().getType(), origOp.input(), origOp.filter(), newBias.getValue(), origOp.strides(),
+            origOp.dilations(), origOp.pads_begin(), origOp.pads_end(), groups);
+    _log.trace("Replaced with 'EMU.ConvolutionUPAOp'");
+    return mlir::success();
+}
+
+//
 // Generated
 //
 
@@ -54,6 +295,13 @@ void ConvertLayers2EMUPass::safeRunOnFunc() {
     target.addLegalOp<mlir::FuncOp, mlir::ReturnOp>();
 
     mlir::RewritePatternSet patterns(&ctx);
+    patterns.insert<SplitRewrite>(&ctx, _log);
+    patterns.insert<CTCGreedyDecoderSeqLenRewrite>(&ctx, _log);
+    patterns.insert<LSTMCellRewrite>(&ctx, _log);
+    patterns.insert<LSTMSequenceRewrite>(&ctx, _log);
+    patterns.insert<FakeQuantizeRewrite>(&ctx, _log);
+    patterns.insert<FullyConnectedRewrite>(&ctx, _log);
+    patterns.insert<ConvolutionRewrite>(&ctx, _log);
     populateWithGenerated(patterns);
 
     auto func = getFunction();
