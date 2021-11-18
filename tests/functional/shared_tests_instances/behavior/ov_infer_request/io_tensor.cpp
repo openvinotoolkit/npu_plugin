400bc4786da6a83b12441c0a78a1f276ef1f0a03
Pavel Zamelin <pavel.zamelin@intel.com>
2021-11-18 05:20:26 +0300

    Fix license

--- tests/functional/shared_tests_instances/behavior/ov_infer_request/io_tensor.cpp
+++ tests/functional/shared_tests_instances/behavior/ov_infer_request/io_tensor.cpp
@@ -1,5 +1,14 @@
-// Copyright (C) 2021 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2021 Intel Corporation.
+//
+// LEGAL NOTICE: Your use of this software and any required dependent software
+// (the "Software Package") is subject to the terms and conditions of
+// the Intel(R) OpenVINO(TM) Distribution License for the Software Package,
+// which may also include notices, disclaimers, or license terms for
+// third party or open source software included in or with the Software Package,
+// and your use indicates your acceptance of all such terms. Please refer
+// to the "third-party-programs.txt" or other similarly-named text file
+// included with the Software Package for additional details.
 //
 
 #include <vector>
@@ -39,7 +48,7 @@ INSTANTIATE_TEST_SUITE_P(smoke_Auto_BehaviorTests, OVInferRequestIOTensorTest,
                                 ::testing::ValuesIn(configs)),
                         OVInferRequestIOTensorTest::getTestCaseName);
 
-std::vector<ov::element::Type> prcs = {
+const std::vector<ov::element::Type> prcs = {
     ov::element::boolean,
     ov::element::bf16,
     ov::element::f16,
