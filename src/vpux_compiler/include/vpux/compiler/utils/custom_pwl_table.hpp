b424762ffb17023e501aeec79168e9cdeb1ffc4b
TofeniMarius <marius-daniel.tofeni@intel.com>
2021-11-23 09:14:11 +0200

    custom PWL and leakyRelu

---
+++ src/vpux_compiler/include/vpux/compiler/utils/custom_pwl_table.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <mlir/Dialect/Quant/QuantTypes.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include "vpux/compiler/utils/types.hpp"
+
+namespace vpux {
+
+struct PWLTableType {
+    std::string activation;
+    mlir::Type dtype;
+};
+
+struct PWLTableEntry {
+    std::vector<int> range;
+    std::vector<int> shift;
+    std::vector<int> bias;
+    std::pair<double, double> float_range;
+    int post_shift;
+};
+
+struct PWLTableHash {
+    std::size_t operator()(const PWLTableType& key) const {
+        const auto h1 = std::hash<std::string>()(key.activation);
+        const auto h2 = mlir::hash_value(key.dtype);
+
+        return h1 ^ h2;
+    }
+};
+
+struct PWLTableEq {
+    bool operator()(const PWLTableType& key1, const PWLTableType& key2) const {
+        return key1.activation == key2.activation && key1.dtype == key2.dtype;
+    }
+};
+
+typedef std::unordered_map<PWLTableType, std::vector<PWLTableEntry>, PWLTableHash, PWLTableEq> PWLTableMap;
+
+PWLTableMap* customPWLTable_leakyRelu();
+}  // namespace vpux
---
+++ src/vpux_compiler/src/utils/custom_pwl_table.cpp
@@ -0,0 +1,24 @@
+#include "vpux/compiler/utils/custom_pwl_table.hpp"
+
+namespace vpux {
+
+PWLTableMap* customPWLTable_leakyRelu() {
+    PWLTableType pwl_type;
+    pwl_type.activation = "LeakyRelu";
+    pwl_type.dtype = mlir::quant::QuantizedType();
+
+    std::vector<int> vecRange{-128, -109, -90, -72, -54, -36, -18, 0, 128};
+    std::vector<int> vecShift{1, -1, 0, 0, 0, -1, -1, -4};
+    std::vector<int> vecBias{-119, 44, -43, -31, -19, 18, 10, 0};
+    double range_min = -65504.0, range_max = 65504.0;
+    int post_shift = 4;
+    PWLTableEntry pwl_table = {vecRange, vecShift, vecBias, std::make_pair(range_min, range_max), post_shift};
+    std::vector<PWLTableEntry> vecTable;
+    vecTable.push_back(pwl_table);
+
+    PWLTableMap* pwlTables = new PWLTableMap();
+    pwlTables->insert({pwl_type, vecTable});
+    return pwlTables;
+}
+
+}  // namespace vpux
