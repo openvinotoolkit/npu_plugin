1dc4b29d44f27f04c61cda4f2bb5584428c819f8
johnbrad <john.brady@intel.com>
2021-11-24 11:05:41 +0000

    barrier scheduler

---
+++ src/vpux_compiler/include/vpux/compiler/core/feasible_schedule_generator.hpp
@@ -0,0 +1,140 @@
+//
+// Copyright Intel Corporation.
+//
+// LEGAL NOTICE: Your use of this software and any required dependent software
+// (the "Software Package") is subject to the terms and conditions of
+// the Intel(R) OpenVINO(TM) Distribution License for the Software Package,
+// which may also include notices, disclaimers, or license terms for
+// third party or open source software included in or with the Software Package,
+// and your use indicates your acceptance of all such terms. Please refer
+// to the "third-party-programs.txt" or other similarly-named text file
+// included with the Software Package for additional details.
+//
+#pragma once
+
+//#include "vpux/compiler/core/deps_info.hpp"
+
+#include "vpux/utils/core/func_ref.hpp"
+#include "vpux/utils/core/logger.hpp"
+#include "vpux/utils/core/small_vector.hpp"
+
+#include "vpux/compiler/core/attributes/strides.hpp"
+#include "vpux/compiler/dialect/IE/ops.hpp"
+#include "vpux/compiler/dialect/IERT/ops.hpp"
+#include "vpux/compiler/utils/attributes.hpp"
+#include "vpux/compiler/utils/error.hpp"
+
+#include <mlir/Dialect/Async/IR/Async.h>
+#include <mlir/IR/BuiltinOps.h>
+#include <mlir/IR/Operation.h>
+
+#include "vpux/utils/core/checked_cast.hpp"
+#include "vpux/utils/core/error.hpp"
+#include "vpux/utils/core/format.hpp"
+#include "vpux/utils/core/numeric.hpp"
+
+#include <mlir/Dialect/MemRef/IR/MemRef.h>
+#include <mlir/Dialect/StandardOps/IR/Ops.h>
+#include <mlir/IR/Value.h>
+#include <mlir/Transforms/DialectConversion.h>
+
+#include <llvm/ADT/BitVector.h>
+#include <llvm/ADT/DenseSet.h>
+
+#include "vpux/compiler/dialect/VPUIP/attributes/enums.hpp"
+#include "vpux/compiler/core/barrier_resource_state.hpp"
+#include "vpux/compiler/core/op_resource_state.hpp"
+#include "vpux/compiler/dialect/VPUIP/ops.hpp"
+
+namespace vpux {
+
+struct op_resource_state_t;
+class FeasibleScheduleGenerator {
+public:
+    
+    typedef size_t schedule_time_t;
+    struct heap_element_t {
+        heap_element_t(mlir::Operation* op = NULL, schedule_time_t t = 0UL): op_(op), time_(t) {
+        }
+
+        mlir::Operation* op_;
+        schedule_time_t time_;
+    };  // struct heap_element_t //
+
+    struct min_heap_ordering_t {
+        bool operator()(const heap_element_t& a, const heap_element_t& b) {
+            return a.time_ > b.time_;
+        }
+    };  // struct min_heap_ordering_t //
+
+    using delay_t = size_t;
+    using schedulable_ops_t = std::list<mlir::Operation*>;
+    typedef typename schedulable_ops_t::iterator schedulable_ops_iterator_t;
+    using processed_ops_t = std::set<mlir::Operation*>;
+    using schedule_heap_t = std::vector<heap_element_t>;
+    using operation_in_degree_t = std::map<mlir::Operation*, size_t>;
+    using priority_map_t = std::map<mlir::Operation*, size_t>;
+    using resource_utility_map_t = std::unordered_map<mlir::Operation*, unsigned> ;
+    resource_utility_map_t resource_utility_map_;
+   
+
+    FeasibleScheduleGenerator(mlir::MLIRContext* ctx, mlir::FuncOp func, const resource_state_t& rstate);
+    FeasibleScheduleGenerator(mlir::MLIRContext* ctx, mlir::FuncOp func);
+
+    bool operator==(const FeasibleScheduleGenerator& o) const;
+    bool reached_end() const;
+    void operator++();
+    bool next_schedulable_operation();
+    mlir::Operation*& operator*(); //should be const ?
+    size_t current_time() const;
+    const resource_state_t& resource_state() const;
+    void getAllBarriersProducersAndConsumers();
+    bool init(const resource_state_t& upper_bound);
+    void init_resource_state(const resource_state_t& start_state);
+    void compute_op_indegree(operation_in_degree_t& in_degree);
+    void add_to_candidate_set(mlir::Operation* op);
+    void compute_operation_priorities();
+    schedulable_ops_iterator_t find_schedulable_op();
+    void create_resource_utility_table_for_barrier_scheduling();
+    bool doesOpRunOnNCE(mlir::Operation* op);
+    unsigned countProducerConsumerTasks(mlir::Operation* op);
+    static SmallVector<mlir::Operation*> getConsumerOps(mlir::Operation* op);
+    static std::string printOpType(mlir::Operation* op);
+    void printInfo(mlir::FuncOp func);
+    bool is_valid_op(schedulable_ops_iterator_t itr) const;
+    void pushToHeap(const heap_element_t& elem);
+    heap_element_t popFromHeap();
+    void add_outgoing_operations_to_candidate_list(mlir::Operation* op);
+    void assignUniqueIds(mlir::FuncOp func);
+    static mlir::IntegerAttr getUniqueID(mlir::Operation* op);
+
+protected:
+    mlir::MLIRContext* _ctx;
+    mlir::FuncOp _func;
+    operation_in_degree_t in_degree_;
+    schedule_heap_t heap_;
+    schedule_time_t current_time_;
+    schedulable_ops_t candidates_;
+    resource_state_t resource_state_;
+    min_heap_ordering_t heap_ordering_;
+    mlir::Operation* schedulable_op_;
+    processed_ops_t processed_ops_;
+    priority_map_t priority_;
+    // outputs of the graph
+    llvm::DenseSet<mlir::Operation*> _outputOps;
+    // operation out-degree, number of outgoing edges
+    std::map<mlir::Operation*, size_t> _outDegreeTable;
+
+
+    //std::unordered_map<mlir::Operation*, size_t> _operationInDegree;
+    //std::unordered_map<mlir::Operation*, size_t> _operationOutDegree;
+    SmallVector<IERT::LayerOpInterface> _allTaskOps;
+    SmallVector<VPUIP::DeclareVirtualBarrierOp> _allBarrierOps;
+    static std::unordered_map<mlir::Operation*, SmallVector<mlir::Operation*>> barrierProducersMap;
+    static std::unordered_map<mlir::Operation*, SmallVector<mlir::Operation*>> barrierConsumersMap;
+    
+};
+
+}  // namespace vpux
+
+
---
+++ src/vpux_compiler/src/core/token_barrier_scheduler.cpp
@@ -0,0 +1,102 @@
+//
+// Copyright Intel Corporation.
+//
+// LEGAL NOTICE: Your use of this software and any required dependent software
+// (the "Software Package") is subject to the terms and conditions of
+// the Intel(R) OpenVINO(TM) Distribution License for the Software Package,
+// which may also include notices, disclaimers, or license terms for
+// third party or open source software included in or with the Software Package,
+// and your use indicates your acceptance of all such terms. Please refer
+// to the "third-party-programs.txt" or other similarly-named text file
+// included with the Software Package for additional details.
+//
+
+#include "vpux/compiler/core/token_barrier_scheduler.hpp"
+
+#include "vpux/compiler/utils/attributes.hpp"
+
+#include "vpux/utils/core/range.hpp"
+
+using namespace vpux;
+
+//
+// Constructor
+//
+
+TokenBasedBarrierScheduler::TokenBasedBarrierScheduler(mlir::MLIRContext* ctx, mlir::FuncOp func, int64_t numBarriers, int64_t slotCount,
+                                                       Logger log)
+        : _ctx(ctx), _func(func), barrierCount_(numBarriers), slotCount_(slotCount)  {
+}
+
+size_t TokenBasedBarrierScheduler::schedule() {
+    
+    size_t btask_count = 0UL;
+
+    // last associated barrier task associated with index //
+    barrier_association_table_t barrier_association;
+
+    Logger::global().error("STEP-0: Initialize the association table");
+    for (size_t barrierId = 1; barrierId <= barrierCount_; barrierId++) {
+        auto bitr = barrier_association.insert(std::make_pair(barrierId, barrier_transition_structure_t(*this)));
+        barrier_transition_structure_t& bstructure = (bitr.first)->second;
+        bstructure.init();
+    }
+    {
+    Logger::global().error("STEP-1: run the scheduler");
+    BarrierScheduleGenerator scheduler_begin(_ctx, _func, barrierCount_, slotCount_);
+    BarrierScheduleGenerator scheduler_end(_ctx, _func);
+    size_t scheduling_number = 0UL;
+
+    for (; scheduler_begin != scheduler_end; ++scheduler_begin) {
+
+    const BarrierScheduleGenerator::schedule_info_t& sinfo = *scheduler_begin;
+
+    Logger::global().error("Getting the schedule information sinfo from the Barrier_Schedule_Generator class");
+    Logger::global().error("The time is {0}, the Operation is {1} end time is {1}", sinfo.schedule_time_, FeasibleScheduleGenerator::getUniqueID(sinfo.op_));
+    Logger::global().error("The barrier index is {0}, , the slot cout is {1}", sinfo.barrier_index_, sinfo.slot_count_);
+    
+    
+    Logger::global().error("Now looking up the barrier association table for the barrier {0} and getting the barrier association table", sinfo.barrier_index_);
+
+    auto bitr = barrier_association.find(sinfo.barrier_index_);
+    assert(bitr != barrier_association.end());
+    barrier_transition_structure_t& bstructure = bitr->second;
+        
+    //Set scheduling number
+    Logger::global().error("Assigning scheduling number {0} to the Operation {1} ", scheduling_number, FeasibleScheduleGenerator::getUniqueID(sinfo.op_));
+    scheduling_number++;
+    // STEP-2: update barrier structure invariant //
+    bool new_barrier_task_created = bstructure.process_next_scheduled_op(sinfo);
+
+    if (new_barrier_task_created) { ++btask_count; }
+    }
+    }
+
+    // STEP-2.5: process trailing barrier control structures //
+     {
+        for (auto bitr=barrier_association.begin(); bitr!=barrier_association.end(); ++bitr) {
+          barrier_transition_structure_t &bstruct = bitr->second;
+          bstruct.close_barrier_producer_list();
+        }
+      }
+
+
+
+
+    for(auto& barrier: configureBarrierOpProducersMap)
+    {
+        Logger::global().error("Barrier ID {0} has the following producers",barrier.first->getAttr("id"));
+        for(auto op : barrier.second)
+            Logger::global().error("producer Op with ID {0} to barrier {1}", FeasibleScheduleGenerator::getUniqueID(op), barrier.first->getAttr("id"));
+    }
+
+    for(auto& barrier: configureBarrierOpConsumersMap)
+    {
+        Logger::global().error("Barrier ID {0} has the following consumers",barrier.first->getAttr("id"));
+        for(auto op : barrier.second)
+            Logger::global().error("consumer Op with ID {0} to barrier {1}", FeasibleScheduleGenerator::getUniqueID(op), barrier.first->getAttr("id"));
+    }
+
+    std::cout << "Done scheduling" << std::endl;
+
+}
