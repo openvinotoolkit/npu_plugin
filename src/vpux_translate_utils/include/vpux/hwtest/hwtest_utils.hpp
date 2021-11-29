b22a7cd5e0f0bd635ca1e0786229dacdca2253a9
Mikhail Pedus <mikhail.pedus@intel.com>
2021-11-29 14:45:03 +0300

    Add AvgPool hwtest with hw generated weights

--- src/vpux_translate_utils/include/vpux/hwtest/hwtest_utils.hpp
+++ src/vpux_translate_utils/include/vpux/hwtest/hwtest_utils.hpp
@@ -70,6 +70,8 @@ void buildMaxPool(const nb::TestCaseJsonDescriptor& testDesc, mlir::ModuleOp mod
                   Logger& log, mlir::Type input0Type, mlir::Type outputType);
 void buildAvgpoolWithDwConv(const nb::TestCaseJsonDescriptor& testDesc, mlir::ModuleOp module, mlir::OpBuilder builder,
                             Logger& log, mlir::Type inputType, mlir::Type outputType);
+void buildAvgpool(const nb::TestCaseJsonDescriptor& testDesc, mlir::ModuleOp module, mlir::OpBuilder builder,
+                  Logger& log, mlir::Type inputType, mlir::Type outputType);
 mlir::DenseElementsAttr splitWeightsOverC(mlir::DenseElementsAttr wt_vec, ArrayRef<int64_t> wt_shape, mlir::Type dtype,
                                           mlir::MLIRContext* ctx, size_t startC, size_t endC);
 template <typename T>
