e835563c4c2bbf13872090cc74f07d43345775ce
johnbrad <john.brady@intel.com>
2021-12-02 11:27:50 +0000

    removed setInsertionPoint() it is not required

--- src/vpux_compiler/include/vpux/compiler/core/token_barrier_scheduler.hpp
+++ src/vpux_compiler/include/vpux/compiler/core/token_barrier_scheduler.hpp
@@ -242,9 +242,8 @@ public:
              static size_t barrier_task_id=0UL;
 
              mlir::OpBuilder builder(_func.getBody());
-
-             builder.setInsertionPoint(sinfo.op_);
-             mlir::Operation* newBarrier = builder.create<VPURT::DeclareVirtualBarrierOp>(sinfo.op_->getLoc(), barrier_task_id); //Neds to be virtual.
+             auto newBarrier = builder.create<VPURT::DeclareVirtualBarrierOp>(sinfo.op_->getLoc(), barrier_task_id); //Neds to be virtual.
+           
 
              std::set<mlir::Operation*> newBarrierProducers{};
              std::set<mlir::Operation*> newBarrierConsumers{};            
