9c800508dd73f157e65745cd61bde47d82a0b190
Anna Verner <anna.verner@intel.com>
2021-12-02 11:21:00 +0300

    Fix arch check

--- src/vpux_compiler/src/dialect/VPUIP/ops/upa_quant_cast.cpp
+++ src/vpux_compiler/src/dialect/VPUIP/ops/upa_quant_cast.cpp
@@ -69,10 +69,10 @@ std::pair<VPUIP::BlobWriter::Vector<uint16_t>, VPUIP::BlobWriter::Vector<uint16_
 mlir::LogicalResult vpux::VPUIP::verifyOp(QuantCastUPAOp op) {
     const auto inType = op.input().getType().cast<mlir::MemRefType>().getElementType();
     const auto outType = op.output().getType().cast<mlir::MemRefType>().getElementType();
-    const auto arch = VPUIP::getArch(op.getOperation()->getParentOfType<mlir::ModuleOp>());
+    const auto arch = VPU::getArch(op.getOperation()->getParentOfType<mlir::ModuleOp>());
 
-    if (arch == VPU::ArchKind::KMB && (inType.isBF16() || outType.isBF16())) {
-        return errorAt(op, "Unsupported type (BF16) for KMB");
+    if (arch != VPU::ArchKind::MTL && (inType.isBF16() || outType.isBF16())) {
+        return errorAt(op, "BF16 is only supported by MTL");
     }
 
     if (!((inType.isF16() && outType.isa<mlir::quant::QuantizedType>()) ||
