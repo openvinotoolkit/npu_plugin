dce5452586727fd877e8865de45eb04ff9955e2d
Skvortsov, Vladimir <vladimir.skvortsov@intel.com>
2021-12-06 22:35:12 +0300

    Code cleaned

--- sw_runtime_kernels/kernels/common/subdir.mk
+++ sw_runtime_kernels/kernels/common/subdir.mk
@@ -3,5 +3,5 @@ include-dirs-shave-y += inc
 include-dirs-shave_nn-y += inc
 srcs-lrt-y += $(wildcard ./src/*.c*)
 srcs-shave-y += $(wildcard ./src/*.c*)
-srcs-shave_nn-y += $(wildcard ./src/*.c*)
+#srcs-shave_nn-y += $(wildcard ./src/*.c*)
 
