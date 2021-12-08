d85b073426e190a967c97e6d4e99b9acfe45549e
AndreiLupas <andrei.lupas@movidius.com>
2021-12-08 11:38:29 +0200

    I420->RGB test instance (just compiles OK)

--- tests/functional/shared_tests_instances/single_layer_tests/convert_color_yuv_to_rgb.cpp
+++ tests/functional/shared_tests_instances/single_layer_tests/convert_color_yuv_to_rgb.cpp
@@ -3,19 +3,22 @@
 //
 
 #include "single_layer_tests/convert_color_nv12.hpp"
+#include "single_layer_tests/convert_color_i420.hpp"
 #include "common_test_utils/test_constants.hpp"
 #include "kmb_layer_test.hpp"
 
 namespace LayerTestsDefinitions {
 
-class KmbConvertColorNV12LayerTest: public ConvertColorNV12LayerTest, virtual public LayerTestsUtils::KmbLayerTestsCommon {
-    // void ConfigureNetwork() override {
-    //    cnnNetwork.getInputsInfo().begin()->second->setLayout(InferenceEngine::Layout::NHWC);
-    //   cnnNetwork.getOutputsInfo().begin()->second->setLayout(InferenceEngine::Layout::NHWC);
-    // }
-};
+class KmbConvertColorNV12LayerTest: public ConvertColorNV12LayerTest, virtual public LayerTestsUtils::KmbLayerTestsCommon {};
 
 TEST_P(KmbConvertColorNV12LayerTest, CompareWithRefs_MLIR) {
+    useCompilerMLIR();
+    Run();
+}
+
+class KmbConvertColorI420LayerTest: public ConvertColorI420LayerTest, virtual public LayerTestsUtils::KmbLayerTestsCommon {};
+
+TEST_P(KmbConvertColorI420LayerTest, CompareWithRefs_MLIR) {
     useCompilerMLIR();
     Run();
 }
@@ -40,6 +43,18 @@ ov::element::Type dTypes[] = {
 INSTANTIATE_TEST_SUITE_P(
         smoke_ConvertColorNV12,
         KmbConvertColorNV12LayerTest,
+        testing::Combine(
+           testing::ValuesIn(inShapes),
+           testing::ValuesIn(dTypes),    // elem Type
+           testing::Values(true, false), // conv_to_RGB
+           testing::Values(true, false), // is_single_plane
+           testing::Values(LayerTestsUtils::testPlatformTargetDevice)),
+        KmbConvertColorNV12LayerTest::getTestCaseName
+);
+
+INSTANTIATE_TEST_SUITE_P(
+        smoke_ConvertColorI420,
+        KmbConvertColorI420LayerTest,
         testing::Combine(
            testing::ValuesIn(inShapes),
            testing::ValuesIn(dTypes),    // elem Type
