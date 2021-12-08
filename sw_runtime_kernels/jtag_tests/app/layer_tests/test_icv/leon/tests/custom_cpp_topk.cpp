ae26742fa631a8c4a70cf1e1236ea8e8ef667529
liuhao3 <hao4.liu@intel.com>
2021-12-08 14:31:06 +0800

    [Draft]Remove input KTensor
    Signed-off-by: liuhao3 <hao4.liu@intel.com>

--- sw_runtime_kernels/jtag_tests/app/layer_tests/test_icv/leon/tests/custom_cpp_topk.cpp
+++ sw_runtime_kernels/jtag_tests/app/layer_tests/test_icv/leon/tests/custom_cpp_topk.cpp
@@ -18,11 +18,9 @@ extern void*(shvNN0_topk);
 namespace ICV_TESTS_NAMESPACE(ICV_TESTS_PASTE2(ICV_TEST_SUITE_NAME, TopK)) {
 typedef int32_t Index;
 typedef t_D8StorageOrder StorageOrder;
-typedef std::initializer_list<int32_t> Dims;
-typedef std::initializer_list<int32_t> Gaps;
 
 static constexpr std::initializer_list<SingleTest> topk_test_list{
-    {{2, 2, 2}, {1, 2, 2}, orderZYX, FPE("topk.elf"), {{0/*axes*/,0/*mode=0,1(max,min)*/,2/*sort=0,1,2(none,value,index)*/,sw_params::Location::NN_CMX /*mem type*/,}}}, //int only
+    {{2, 2, 2}, {1, 2, 2}, orderZYX, FPE("topk.elf"), {{1,0/*axes*/,0/*mode=0,1(max,min)*/,2/*sort=0,1,2(none,value,index)*/,sw_params::Location::NN_CMX /*mem type*/,}}}, //int only
 };
 
 // pair of (value, index) used in sorting
@@ -76,14 +74,6 @@ protected:
         addLoop(m_testsLoop);
     }
     
-    void reverse(MemoryDims& dims, int m_dims) {
-        for (int l = 0,r = m_dims - 1; l < r; l++, r--) {
-            int32_t dim = dims.dims[l];
-            dims.dims[l] = dims.dims[r];
-            dims.dims[r] = dim;
-        }
-    }
-    
     void initData() override{
         printf("init data.\n");
         m_params = {0xFFFFFFFF, m_elfBuffer, 0, nullptr, MAX_LOCAL_PARAMS, 0, 0};
@@ -97,11 +87,9 @@ protected:
         const StorageOrder& storageOrder = m_currentTest->storageOrder;
         
         const TensorDims dims3In(dimIn.width,   dimIn.height,  dimIn.channels,  1);
-        const TensorDims dims3K(1, 1, 1, 1);
         const TensorDims dims3Out(dimOut.width, dimOut.height, dimOut.channels, 1);
 
         m_inputTensor.init(storageOrder, dims3In);
-        m_kTensor.init(storageOrder, dims3K);
         m_valueTensor.init(storageOrder, dims3Out);
         m_indexTensor.init(storageOrder, dims3Out);
         m_referenceValueTensor.init(storageOrder, dims3Out);
@@ -109,7 +97,6 @@ protected:
         
         
         allocBuffer(m_inputTensor);
-        allocBuffer(m_kTensor);
         allocBuffer(m_valueTensor);
         allocBuffer(m_indexTensor);
         allocBuffer(m_referenceValueTensor);
@@ -118,6 +105,14 @@ protected:
         const SingleTest* test = m_currentTest;
         int32_t ind[subspace::MAX_DIMS] = {0};
         subspace::orderToIndices((t_D8StorageOrder)(test->storageOrder), ind);
+        
+        m_k = ind[test->customLayerParams.layerParams[0]];
+        m_axis = ind[test->customLayerParams.layerParams[1]];
+        m_mode = ind[test->customLayerParams.layerParams[2]];
+        m_sort = ind[test->customLayerParams.layerParams[3]];
+        m_hasOutputValues = 1;
+        m_hasOutputIndices = 1;
+        
         m_TopKParams = reinterpret_cast<sw_params::TopKParams*>(paramContainer.data());
         *m_TopKParams = sw_params::TopKParams();
         m_params.paramData = reinterpret_cast<uint32_t*>(paramContainer.data());
@@ -125,12 +120,7 @@ protected:
         m_requiredTensorLocation = static_cast<sw_params::Location>(test->customLayerParams.layerParams[0]);
         m_params.baseParamData = sw_params::ToBaseKernelParams(m_TopKParams);
         
-        m_axis = ind[test->customLayerParams.layerParams[0]];
-        m_mode = ind[test->customLayerParams.layerParams[1]];
-        m_sort = ind[test->customLayerParams.layerParams[2]];
-        m_hasOutputValues = 1;
-        m_hasOutputIndices = 1;
-        
+        m_TopKParams->k = m_k;
         m_TopKParams->axis = m_axis;
         m_TopKParams->mode = m_mode;
         m_TopKParams->sort = m_sort;
@@ -141,7 +131,7 @@ protected:
     void initTestCase() override {
         printf("init test case.\n");
         m_currentTest = &m_testsLoop.value();
-        m_test_threshold = 0.0005f;
+        m_test_threshold = 0.01f;
     }
     
     void initParserRunner() override {
@@ -155,10 +145,6 @@ protected:
         Buffer inBuff;
         m_inputTensor.exportToBuffer(inBuff);
         customCppOp->addInputBuffer(inBuff, m_requiredTensorLocation);
-
-        Buffer kBuff;
-        m_kTensor.exportToBuffer(kBuff);
-        customCppOp->addInputBuffer(kBuff, m_requiredTensorLocation);
 
         Buffer valueBuff;
         m_valueTensor.exportToBuffer(valueBuff);
@@ -187,30 +173,34 @@ protected:
             float tmp = float(rand() % 1000) / 100 - 5.0f;
             m_inputTensor.at(indices) = f32Tof16(tmp);
         });
-
-        // input K
-        int32_t k = 1;
-        m_kTensor.forEach(false, [&](const MemoryDims& indices) {
-            m_kTensor.at(indices) = k;
-        });
-
-//        // reference
-//        generateReferenceData();
     }
     
     void generateReferenceData() override {
         printf("generate reference data.\n");
-        const auto k = 1;
+        const auto k = m_k;
         const auto mode = m_mode;
         const auto sort = m_sort;
-        const auto ndims = 3;
+        const auto ndims = m_inputTensor.ndims();
         const auto axis = m_axis; // Change axis due to layout
+        printf("k = %d\n", k);
+        printf("axis = %d\n", axis);
+        printf("mode = %d\n", mode);
+        printf("sort = %d\n", sort);
+        printf("ndims = %d\n", ndims);
         
         MemoryDims dims = m_inputTensor.memoryDims();
         const int n = dims.dims[axis];
         dims.dims[axis] = 1;
         
+        mvTensorAssert(k <= n);
+        mvTensorAssert(m_inputTensor.storageOrder() == m_referenceValueTensor.storageOrder());
+        mvTensorAssert(m_inputTensor.storageOrder() == m_referenceIndexTensor.storageOrder());
+        
         const int inputValuesAxisStep = m_inputTensor.memorySteps().dims[axis];
+        for (int i = 0; i < 3; i++) {
+            printf("m_inputTensor.memorySteps().dims[%d] = %d\n", i, m_inputTensor.memorySteps().dims[i]);
+        }
+
         const int refValuesAxisStep = m_referenceValueTensor.memorySteps().dims[axis];
         const int refIndicesAxisStep = m_referenceIndexTensor.memorySteps().dims[axis];
 
@@ -219,9 +209,13 @@ protected:
 
         dims.forEach(ndims, dims.dims, [&](const MemoryDims& id){
             const auto inputValuesData = &m_inputTensor.at(id);
-            for (int i = 0; i < n; ++i)
+            
+            for (int i = 0; i < n; ++i) {
                 temp[i] = Pair(inputValuesData[i * inputValuesAxisStep], i);
-                         
+                printf("inputValuesData[i * inputValuesAxisStep] = %f\n",
+                       f16Tof32(inputValuesData[i * inputValuesAxisStep]));
+            }
+            
             std::partial_sort(temp.begin(), temp.begin() + k, temp.begin() + n, compareValues);
             if (sort == 0) {
                 std::sort(temp.begin(), temp.begin() + k, compareIndices);
@@ -233,9 +227,11 @@ protected:
                 const auto& t = temp[i];
                 refValuesData[i * refValuesAxisStep] = t.first;
                 refIndicesData[i * refIndicesAxisStep] = t.second;
+                printf("refValuesData[%d] = %f, refIndicesData[%d] = %d\n", i * refValuesAxisStep, f16Tof32(refValuesData[i * refValuesAxisStep]), i * refIndicesAxisStep, refIndicesData[i * refIndicesAxisStep]);
             }
         });
     }
+    
     static CompareFunction modeComparison(int32_t mode)
     {
         switch (mode)
@@ -247,58 +243,62 @@ protected:
     }
     
     virtual bool checkResult() override{
+        printf("check result.\n");
         bool test_failed = false;
         
+        m_inputTensor.confirmBufferData();
+        m_valueTensor.confirmBufferData();
+        m_indexTensor.confirmBufferData();
         m_referenceValueTensor.confirmBufferData();
         
-//        saveMemoryToFile(reinterpret_cast<u32>(m_inputTensor.buffer()), m_inputTensor.bufferSize(), "input.bin");
-//        saveMemoryToFile(reinterpret_cast<u32>(m_valueTensor.buffer()), m_valueTensor.bufferSize(), "outvalue.bin");
-//        saveMemoryToFile(reinterpret_cast<u32>(m_referenceValueTensor.buffer()), m_referenceValueTensor.bufferSize(), "outref.bin");
+        saveMemoryToFile(reinterpret_cast<u32>(m_inputTensor.buffer()), m_inputTensor.bufferSize(), "input.bin");
+        saveMemoryToFile(reinterpret_cast<u32>(m_valueTensor.buffer()), m_valueTensor.bufferSize(), "outvalue.bin");
+        saveMemoryToFile(reinterpret_cast<u32>(m_referenceValueTensor.buffer()), m_referenceValueTensor.bufferSize(), "outref.bin");
+        saveMemoryToFile(reinterpret_cast<u32>(m_indexTensor.buffer()), m_indexTensor.bufferSize(), "outindex.bin");
         
         m_referenceValueTensor.forEach(true, [this, &test_failed](const MemoryDims& indices){
             const float gt_value    = f16Tof32(m_referenceValueTensor.at(indices));
-            float out_value   = (m_hasOutputValues == 1) ? f16Tof32(m_valueTensor.at(indices)) : 0.f;
-            const bool value_differ = (m_hasOutputValues == 1) && (out_value != gt_value);
-            
-            const Index gt_index    = m_referenceIndexTensor.at(indices);
-            const Index out_index   = (m_hasOutputIndices == 1) ? m_indexTensor.at(indices) : 0;
-            const bool index_differ = (m_hasOutputIndices == 1) && (out_index != gt_index);
-
+            float value = f16Tof32(m_valueTensor.at(indices));
+            float abs_diff = fabs(value - gt_value);
+            bool value_differ = !bool(abs_diff <= m_test_threshold);
+            const Index gt_index = m_referenceIndexTensor.at(indices);
+            const Index out_index = m_indexTensor.at(indices);
+            const bool index_differ = (out_index != gt_index);
             const bool differ = value_differ || index_differ;
             test_failed = test_failed || differ;
+            printf("m_valueTensor value = %f, gt_value = %f\n", value, gt_value);
 
-            if (m_hasOutputValues!=1)
-            {
-                int32_t setCoords[MAX_DIMS];
-                subspace::getCoord(out_index, m_inputTensor.memoryDims().dims, m_inputTensor.ndims(), setCoords);
-                MemoryDims indicesIn(setCoords, m_inputTensor.ndims());
-                out_value = m_inputTensor.at(indicesIn);
-            }
-
-            if (differ && GlobalData::doPrintDiffs)
-            {
-                char indices_str[64];
-                printf("DIFF [%s] %f %s %f, %ld %s %ld\n",
-                       ((m_hasOutputValues==1) ? m_valueTensor.indicesToString(indices, indices_str) : m_indexTensor.indicesToString(indices, indices_str)),
-                       out_value, value_differ ? "!=" : "==", gt_value,
-                       out_index, index_differ ? "!=" : "==", gt_index);
-            }
+//            if (m_hasOutputValues!=1)
+//            {
+//                int32_t setCoords[MAX_DIMS];
+//                subspace::getCoord(out_index, m_inputTensor.memoryDims().dims, m_inputTensor.ndims(), setCoords);
+//                MemoryDims indicesIn(setCoords, m_inputTensor.ndims());
+//                out_value = m_inputTensor.at(indicesIn);
+//            }
+//
+//            if (differ && GlobalData::doPrintDiffs)
+//            {
+//                char indices_str[64];
+//                printf("DIFF [%s] %f %s %f, %ld %s %ld\n",
+//                       ((m_hasOutputValues==1) ? m_valueTensor.indicesToString(indices, indices_str) : m_indexTensor.indicesToString(indices, indices_str)),
+//                       out_value, value_differ ? "!=" : "==", gt_value,
+//                       out_index, index_differ ? "!=" : "==", gt_index);
+//            }
         });
         return !test_failed;
     }
 private:
     ListIterator<SingleTest> m_testsLoop;
-    int m_ndims;
-    int m_axis;
-    int m_mode;
-    int m_sort;
+    int32_t m_k;
+    int32_t m_axis;
+    int32_t m_mode;
+    int32_t m_sort;
     int m_hasOutputValues;
     int m_hasOutputIndices;
     
     std::vector<uint64_t> paramContainer;
     sw_params::TopKParams* m_TopKParams;
     
-    Tensor<int32_t> m_kTensor;
     Tensor<fp16> m_valueTensor;
     Tensor<int32_t> m_indexTensor;
     
