baf816668c7c7e1f00ff399916706ebd57832572
Lei5 Chen <lei5.chen@intel.com>
2021-12-09 16:05:49 +0800

    Clean comments and format code.

--- sw_runtime_kernels/kernels/single_shave_topk.cpp
+++ sw_runtime_kernels/kernels/single_shave_topk.cpp
@@ -13,8 +13,7 @@ struct Pack {
 
 using ComparePackedFunc = bool (*)(const Pack& a, const Pack& b);
 
-struct PartialHeapSortPacked
-{
+struct PartialHeapSortPacked {
     Pack* m_items;
     int m_cnt;
     int m_fill;
@@ -37,15 +36,12 @@ struct PartialHeapSortPacked
         m_fill = m_cnt;
 
         int start = INDEX_PARENT(m_cnt - 1);
-        while (start >= 0)
-        {
+        while (start >= 0) {
             siftDown(start, -1, comparePacked);
             start--;
         }
-        for (int i = m_fill; i < n; ++i)
-        {
-            if (comparePacked(m_items[i], m_items[0]))
-            {
+        for (int i = m_fill; i < n; ++i) {
+            if (comparePacked(m_items[i], m_items[0])) {
                 m_items[0] = m_items[i];
                 siftDown(0, -1, comparePacked);
             }
@@ -55,11 +51,9 @@ struct PartialHeapSortPacked
     //full sort the array by putting smallest item to the end one by one.
     //after full sort, the heap is empty and array is in descending order
     int fullSort(ComparePackedFunc comparePacked) {
-        if (m_cnt > 1)
-        {
+        if (m_cnt > 1) {
             int N = m_cnt;
-            while (N > 1)
-            {
+            while (N > 1) {
                 std::swap(m_items[0], m_items[N-1]);
                 N --;
                 siftDown(0, N, comparePacked);
@@ -80,22 +74,18 @@ struct PartialHeapSortPacked
         int child0 = (2*root + 1);
         //the root element is going down along the heap-tree structure
         //and smaller child along its path will bubble-up
-        while (child0 < end)
-        {
+        while (child0 < end) {
             int child1 = child0 + 1;
             int swapx = root;
             Pack* pItemx = &itemx;
 
-            if (comparePacked(*pItemx, m_items[child0]))
-            {
+            if (comparePacked(*pItemx, m_items[child0])) {
                 swapx = child0;
                 pItemx = &m_items[child0];
             }
 
-            if (child1 < end)
-            {
-                if (comparePacked(*pItemx, m_items[child1]))
-                {
+            if (child1 < end) {
+                if (comparePacked(*pItemx, m_items[child1])) {
                     swapx = child1;
                     pItemx = &m_items[child1];
                 }
@@ -143,19 +133,6 @@ void singleShaveTopK(uint32_t lParamsAddr) {
 
     const TopKParams* lParams = reinterpret_cast<const TopKParams *>(lParamsAddr);
 
-    half* p_act_input_tmp = p_act_input;
-    half* p_act_value_tmp = p_act_value;
-    for (int i = 0; i < 2 * 3 * 4; i++) {
-        //*p_act_value_tmp = *p_act_input_tmp;
-        *p_act_value_tmp = 0;
-        if (i == 0) {
-            *p_act_value_tmp = 1234;
-        }
-        p_act_input_tmp++;
-        p_act_value_tmp++;
-    }
-
-    // Case dimension NCHW 4, 3, 2 (0,0,0)=1, (3,2,1)=24
     int32_t numInputDims = (int32_t)lParams->input.numDims;
     int32_t* pInputDims = (int32_t*)(lParams->input.dimsAddr);
     int64_t* pInputStrides = (int64_t*)(lParams->input.stridesAddr);
@@ -167,47 +144,6 @@ void singleShaveTopK(uint32_t lParamsAddr) {
     int32_t numIndexDims = (int32_t)lParams->index.numDims;
     int32_t* pIndexDims = (int32_t*)(lParams->index.dimsAddr);
     int64_t* pIndexStrides = (int64_t*)(lParams->index.stridesAddr);
-
-    p_act_index[0] = k;
-
-    // input dim number
-    p_act_index[1] = numInputDims; // 3
-    // input dims
-    p_act_index[2] = pInputDims[0]; // 4
-    p_act_index[3] = pInputDims[1]; // 3
-    p_act_index[4] = pInputDims[2]; // 2
-    p_act_index[5] = pInputDims[3]; // -1
-    // input strides
-    p_act_index[6] = pInputStrides[0] / CHAR_BIT; // 2
-    p_act_index[7] = pInputStrides[1] / CHAR_BIT; // 8
-    p_act_index[8] = pInputStrides[2] / CHAR_BIT;; // 24
-    p_act_index[9] = pInputStrides[3] / CHAR_BIT;; // -842150451 or 0
-
-//    // value dim number
-//    p_act_index[10] = numValueDims; // 3
-//    // value dims
-//    p_act_index[11] = pValueDims[0]; // 4
-//    p_act_index[12] = pValueDims[1]; // 3
-//    p_act_index[13] = pValueDims[2]; // 2
-//    p_act_index[14] = pValueDims[3]; // -1
-//    // value strides
-//    p_act_index[15] = pValueStrides[0] / CHAR_BIT; // 2
-//    p_act_index[16] = pValueStrides[1] / CHAR_BIT; // 8
-//    p_act_index[17] = pValueStrides[2] / CHAR_BIT; // 24
-//    p_act_index[18] = pValueStrides[3] / CHAR_BIT; // -842150451 or 0
-
-    // index dim number
-    p_act_index[10] = numIndexDims; // 3
-    // index dims
-    p_act_index[11] = pIndexDims[0]; // 4
-    p_act_index[12] = pIndexDims[1]; // 3
-    p_act_index[13] = pIndexDims[2]; // 2
-    p_act_index[14] = pIndexDims[3]; // -1
-    // index strides
-    p_act_index[15] = pIndexStrides[0] / CHAR_BIT; // 4
-    p_act_index[16] = pIndexStrides[1] / CHAR_BIT; // 16
-    p_act_index[17] = pIndexStrides[2] / CHAR_BIT; // 48
-    p_act_index[18] = pIndexStrides[3] / CHAR_BIT; // -842150451 or 0
 
     int32_t axis = 2;
     int32_t mode = 0; // max: 0, min: 1
@@ -221,7 +157,6 @@ void singleShaveTopK(uint32_t lParamsAddr) {
     }
 
     // calculate top K inner (axis = 0)
-    k = 1;
     if (axis == 0) {
 
         int numLines = 1;
@@ -310,7 +245,7 @@ void singleShaveTopK(uint32_t lParamsAddr) {
 
         int numLines = 1;
         for (int i = 0; i < numInputDims - 1; i++) {
-            numLines *= pInputDims[i]; // 12
+            numLines *= pInputDims[i];
         }
 
         int inputStride = pInputStrides[0] / CHAR_BIT;
