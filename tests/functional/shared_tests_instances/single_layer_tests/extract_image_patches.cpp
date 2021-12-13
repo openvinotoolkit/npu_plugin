5278eecfd55caf6113574db43becbc112e385758
Denisa Pap-Gherasim <denisa.pap-gherasim@intel.com>
2021-12-13 15:14:57 +0200

    added few more files for ExtractImagePatches layer

---
+++ tests/functional/shared_tests_instances/single_layer_tests/extract_image_patches.cpp
@@ -0,0 +1,12 @@
+// Copyright (C) Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "single_layer_tests/convolution.hpp"
+
+#include <vector>
+
+#include "common_test_utils/test_constants.hpp"
+#include "kmb_layer_test.hpp"
+
+// TODO
