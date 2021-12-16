05974d36aeb071074b11b8d3a12a6c45aef8ac07
Zhibin Li <zhibin.li@intel.com>
2021-12-16 17:10:03 +0800

    fix

--- sw_runtime_kernels/jtag_tests/app/layer_tests/test_icv/leon/tests/custom_cpp_elu.cpp
+++ sw_runtime_kernels/jtag_tests/app/layer_tests/test_icv/leon/tests/custom_cpp_elu.cpp
@@ -1,8 +1,8 @@
 // {% copyright %}
 
 #include <custom_cpp_tests.h>
-#include "mvSubspaces.h"
 #include "layers/param_custom_cpp.h"
+#include "mvSubspaces.h"
 
 #ifdef CONFIG_TARGET_SOC_3720
 __attribute__((aligned(1024)))
@@ -19,116 +19,98 @@ union Hex {
     int32_t i;
 };
 
-#define F_0_5 0x3f000000 // Hex representation of 0.5f
+#define F_0_5 0x3f000000  // Hex representation of 0.5f
 
-namespace ICV_TESTS_NAMESPACE(ICV_TESTS_PASTE2(ICV_TEST_SUITE_NAME, Elu))
-{
-static constexpr std::initializer_list<SingleTest> elu_test_list
-{
-    {{2, 2, 2}, {2, 2, 2}, orderZYX, FPE("elu.elf"), {{F_0_5 /*alpha*/, sw_params::Location::NN_CMX /*mem type*/,}}},
-};
+namespace ICV_TESTS_NAMESPACE(ICV_TESTS_PASTE2(ICV_TEST_SUITE_NAME, Elu)) {
+    static constexpr std::initializer_list<SingleTest> elu_test_list{
+            {{2, 2, 2},
+             {2, 2, 2},
+             orderZYX,
+             FPE("elu.elf"),
+             {{
+                     F_0_5 /*alpha*/,
+                     sw_params::Location::NN_CMX /*mem type*/,
+             }}},
+    };
 
-class CustomCppEluTest: public CustomCppTests<fp16> {
-public:
-    explicit CustomCppEluTest(): m_testsLoop(elu_test_list, "test") {}
-    virtual ~CustomCppEluTest() {}
-protected:
-    const char* suiteName() const override
-    {
-        return "CustomCppEluTest";
-    }
-    void userLoops() override
-    {
-        addLoop(m_testsLoop);
-    }
+    class CustomCppEluTest : public CustomCppTests<fp16> {
+    public:
+        explicit CustomCppEluTest(): m_testsLoop(elu_test_list, "test") {
+        }
+        virtual ~CustomCppEluTest() {
+        }
 
-    void initData() override {
+    protected:
+        const char* suiteName() const override {
+            return "CustomCppEluTest";
+        }
+        void userLoops() override {
+            addLoop(m_testsLoop);
+        }
 
-        m_params = {
-            0xFFFFFFFF,
-            m_elfBuffer,
-            0,
-            nullptr,
-            MAX_LOCAL_PARAMS,
-            0,
-            0
-        };
+        void initData() override {
+            m_params = {0xFFFFFFFF, m_elfBuffer, 0, nullptr, MAX_LOCAL_PARAMS, 0, 0};
 
-        paramContainer.resize(((int)sizeof(sw_params::EluParams) + 7) / 8);
-        CustomCppTests<fp16>::initData();
-        const SingleTest* test = m_currentTest;
-        const Hex alpha_hex = { .i = test->customLayerParams.layerParams[0] };
-	m_alpha = alpha_hex.f;
-        m_eluParams = reinterpret_cast<sw_params::EluParams *>(paramContainer.data());
-        *m_eluParams = sw_params::EluParams();
-        m_eluParams->alpha = m_alpha;
-        m_params.paramData = reinterpret_cast<uint32_t*>(paramContainer.data());
-        m_params.paramDataLen = paramContainer.size() * sizeof(uint64_t);
-        m_requiredTensorLocation = static_cast<sw_params::Location>(test->customLayerParams.layerParams[1]);
-        m_params.baseParamData = sw_params::ToBaseKernelParams(m_eluParams);
-    }
+            paramContainer.resize(((int)sizeof(sw_params::EluParams) + 7) / 8);
+            CustomCppTests<fp16>::initData();
+            const SingleTest* test = m_currentTest;
+            const Hex alpha_hex = {.i = test->customLayerParams.layerParams[0]};
+            m_alpha = alpha_hex.f;
+            m_eluParams = reinterpret_cast<sw_params::EluParams*>(paramContainer.data());
+            *m_eluParams = sw_params::EluParams();
+            m_eluParams->alpha = m_alpha;
+            m_params.paramData = reinterpret_cast<uint32_t*>(paramContainer.data());
+            m_params.paramDataLen = paramContainer.size() * sizeof(uint64_t);
+            m_requiredTensorLocation = static_cast<sw_params::Location>(test->customLayerParams.layerParams[1]);
+            m_params.baseParamData = sw_params::ToBaseKernelParams(m_eluParams);
+        }
 
-    void initTestCase() override {
-        m_currentTest = &m_testsLoop.value();
-        m_test_threshold = 0.0005f;
+        void initTestCase() override {
+            m_currentTest = &m_testsLoop.value();
+            m_test_threshold = 0.0005f;
+        }
 
-//        const StorageOrder& storageOrder = m_currentTest->storageOrder;
-//        const auto& dimIn = m_currentTest->inDim;
-//        const TensorDims dims3In(dimIn.width, dimIn.height, dimIn.channels, 1);
-//        m_refInputTensor.init(storageOrder, dims3In);
-//        allocBuffer(m_refInputTensor);
-    }
-
-    void generateInputData() override {
-        const auto customData = false;//m_testLoop.value().customData;
-
+        void generateInputData() override {
 #ifdef CONFIG_TARGET_SOC_3720
-        m_params.kernel  = reinterpret_cast<uint64_t>(sk_elu_fp16_3010xx_text);
+            m_params.kernel = reinterpret_cast<uint64_t>(sk_elu_fp16_3010xx_text);
 #else
-        m_params.kernel  = reinterpret_cast<uint64_t>(PREAMBLE_FUNC(elu_fp16));
+            m_params.kernel = reinterpret_cast<uint64_t>(PREAMBLE_FUNC(elu_fp16));
 #endif
 
-        rand_seed();
+            const int ndims = m_inputTensor.ndims();
+            m_inputTensor.forEach(false, [&](const MemoryDims& indices) {
+                int tmp = 1;
+                for (int i = 0; i < ndims; ++i)
+                    tmp *= (3 + (indices.dims[i] % 13));
+                int tmp2 = (tmp % 33) - 14;
+                fp16 val = f32Tof16((float)tmp2);
 
-        // set random seed
-        u64 ticks_for_seed = rtems_clock_get_uptime_nanoseconds();
-        srand(ticks_for_seed);
-
-        // input
-        m_inputTensor.forEach(false, [&](const MemoryDims& indices)
-        {
-            float tmp = float(rand() % 1000) / 100 - 5.0f;
-            m_inputTensor.at(indices) = f32Tof16(tmp);
-        });
-
-        // reference output
-        generateReferenceData();
-    }
-    void generateReferenceData() override {
-        m_inputTensor.forEach(false, [&](const MemoryDims& indices) {
-            float val = f16Tof32(m_inputTensor.at(indices));
-            float min = std::min(val, 0.0f);
-            float max = std::max(val, 0.0f);
-            float ref = max + 0.3f * (exp((double)min) - 1.0f);
-            m_referenceOutputTensor.at(indices) = f32Tof16(ref);
-        });
-
-    }
-    virtual bool checkResult() override
-        {
+                m_inputTensor.at(indices) = f32Tof16(val);
+            });
+            // reference output
+            generateReferenceData();
+        }
+        void generateReferenceData() override {
+            m_inputTensor.forEach(false, [&](const MemoryDims& indices) {
+                float val = f16Tof32(m_inputTensor.at(indices));
+                float min = std::min(val, 0.0f);
+                float max = std::max(val, 0.0f);
+                float ref = max + m_alpha * (exp((double)min) - 1.0f);
+                m_referenceOutputTensor.at(indices) = f32Tof16(ref);
+            });
+        }
+        virtual bool checkResult() override {
             m_outputTensor.confirmBufferData();
 
-//            // save output data
-//            if (save_to_file)
-//            {
-//                saveMemoryToFile(reinterpret_cast<u32>(m_outputTensor.buffer()), m_outputTensor.bufferSize(), "outMyriad.bin");
-//            }
+            // save output data
+            if (m_save_to_file) {
+                saveMemoryToFile(reinterpret_cast<u32>(m_outputTensor.buffer()), m_outputTensor.bufferSize(),
+                                 "outMyriad.bin");
+            }
 
             bool threshold_test_failed = false;
 
-            m_outputTensor.forEach(false, [&](const MemoryDims& indices)
-            {
-
+            m_outputTensor.forEach(false, [&](const MemoryDims& indices) {
                 float value = f16Tof32(m_outputTensor.at(indices));
                 float gt_value = f16Tof32(m_referenceOutputTensor.at(indices));
                 float abs_diff = fabs(value - gt_value);
@@ -136,24 +118,24 @@ protected:
 
                 threshold_test_failed |= differ;
 
-                if (differ && GlobalData::doPrintDiffs)
-                {
+                if (differ && GlobalData::doPrintDiffs) {
                     const TensorDims ti = m_outputTensor.toTensor(indices);
-                    printf("DIFF HWC [%d:%d:%d] %f %f %f\n", ti.height, ti.width, ti.channels, value, gt_value, abs_diff);
+                    printf("DIFF HWC [%d:%d:%d] %f %f %f\n", ti.height, ti.width, ti.channels, value, gt_value,
+                           abs_diff);
                 }
             });
 
             return !threshold_test_failed;
         }
-private:
-    ListIterator<SingleTest> m_testsLoop;
 
-    // Additional buffer to avoid convertion back and forth
-    float m_alpha;
-    std::vector<uint64_t> paramContainer;
-    sw_params::EluParams * m_eluParams;
-//    Tensor<fp16> m_referenceTensor;
-};
+    private:
+        ListIterator<SingleTest> m_testsLoop;
 
-ICV_TESTS_REGISTER_SUITE(CustomCppEluTest)
-}
+        // Additional buffer to avoid convertion back and forth
+        float m_alpha;
+        std::vector<uint64_t> paramContainer;
+        sw_params::EluParams* m_eluParams;
+    };
+
+    ICV_TESTS_REGISTER_SUITE(CustomCppEluTest)
+}  // namespace ICV_TESTS_NAMESPACE(ICV_TESTS_PASTE2(ICV_TEST_SUITE_NAME,Elu))
--- sw_runtime_kernels/kernels/elu_fp16.cpp
+++ sw_runtime_kernels/kernels/elu_fp16.cpp
@@ -7,15 +7,23 @@
 #endif
 #include <math.h>
 #include <param_elu.h>
-#include <moviVectorTypes.h>
 #ifdef CONFIG_TARGET_SOC_3720
 #include <dma_shave_nn.h>
 #else
 #include <dma_shave.h>
 #endif
 
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define intrinsic_vec(intrinsic, vin, vout) \
+    (vout)[0] = intrinsic((vin)[0]);        \
+    (vout)[1] = intrinsic((vin)[1]);        \
+    (vout)[2] = intrinsic((vin)[2]);        \
+    (vout)[3] = intrinsic((vin)[3]);        \
+    (vout)[4] = intrinsic((vin)[4]);        \
+    (vout)[5] = intrinsic((vin)[5]);        \
+    (vout)[6] = intrinsic((vin)[6]);        \
+    (vout)[7] = intrinsic((vin)[7]);
+
+#define exp2_vec(vin, vout) intrinsic_vec(__builtin_shave_sau_exp2_f16_l_r, vin, vout)
 
 using namespace sw_params;
 
@@ -25,32 +33,42 @@ namespace shave_lib {
 extern "C" {
 
 void elu_fp16(uint32_t lParamsAddr) {
+    const EluParams* lParams = (const EluParams*)lParamsAddr;
 
-    const EluParams * lParams = (const EluParams*)lParamsAddr;
+    half8* p_act_data = (half8*)(lParams->input.dataAddr);  // 0x1F000000
+    half8* p_act_out = (half8*)(lParams->output.dataAddr);  // 0x1F004000
 
-    half* p_act_data = (half*)(lParams->input.dataAddr); // 0x1F000000
-    half* p_act_out = (half*)(lParams->output.dataAddr); // 0x1F004000
-
-    const EluParams * layerParams = reinterpret_cast<const EluParams *>(lParams);
+    const EluParams* layerParams = reinterpret_cast<const EluParams*>(lParams);
     const half alpha = layerParams->alpha;
-    int32_t *pDims = (int32_t *)(lParams->input.dimsAddr);
+    int32_t* pDims = (int32_t*)(lParams->input.dimsAddr);
+
+    const half8 one = (half8)1.0f;
+    const half8 zero = (half8)0.0f;
+
+    const unsigned short inv_ln2 = 0x3dc6;
+    const half inv_ln2_h = *reinterpret_cast<const half*>(&inv_ln2);
+    const half8 vinv_ln2 = (half8)inv_ln2_h;
 
     int32_t nElements = 1;
     int32_t i = 0;
 
-    for (i = 0; i!= lParams->input.numDims; i++ ) {
+    for (i = 0; i != lParams->input.numDims; i++) {
         // TODO: where pointers patch should be???
         // TODO: check overflow
-        nElements *=  pDims[i];
+        nElements *= pDims[i];
     }
 
-    for (uint32_t e = 0; e < nElements; ++e) {
-        half min = MIN(*p_act_data, 0.0f);
-	half max = MAX(*p_act_data, 0.0f);
-        *p_act_out++ = max + 0.3f * (exp((double)min) - 1.0f);
-	p_act_data++;
+#pragma clang loop unroll_count(8)
+    for (i = 0; i < nElements; i++) {
+        half8 min = __builtin_shave_cmu_min_f16_rr_half8(p_act_data[i], zero);
+        half8 max = __builtin_shave_cmu_max_f16_rr_half8(p_act_data[i], zero);
+
+        half8 exp_x = min * vinv_ln2;
+        exp2_vec(exp_x, exp_x);
+
+        p_act_out[i] = max + alpha * (exp_x - one);
     }
 }
 }
-} // namespace shave_lib
-} // namespace nn
+}  // namespace shave_lib
+}  // namespace nn
