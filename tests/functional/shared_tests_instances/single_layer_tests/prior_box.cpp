e41fc1f5d8ab554e177f129f4c8f3db09ee3bac2
AndreiLupas <andrei.lupas@movidius.com>
2021-12-21 13:21:11 +0200

    GOLD: 2 test PASS

--- tests/functional/shared_tests_instances/single_layer_tests/prior_box.cpp
+++ tests/functional/shared_tests_instances/single_layer_tests/prior_box.cpp
@@ -26,7 +26,7 @@ TEST_P(KmbPriorBoxLayerTest, CompareWithRefs_MLIR) {
 
 using namespace LayerTestsDefinitions;
 
-namespace {  //openvino eg
+namespace {  // TEST_1 (openvino eg)
 
 const priorBoxSpecificParams param1 = {
    std::vector<float>{16.0},  // min_size
@@ -45,17 +45,54 @@ const priorBoxSpecificParams param1 = {
 };
 
 INSTANTIATE_TEST_SUITE_P(
-        smoke_PriorBox,
+        smoke_PriorBox1,
         KmbPriorBoxLayerTest,
         testing::Combine(
            testing::Values(param1),
+           testing::Values(InferenceEngine::Precision::FP16),
            testing::Values(InferenceEngine::Precision::UNSPECIFIED),
            testing::Values(InferenceEngine::Precision::UNSPECIFIED),
-           testing::Values(InferenceEngine::Precision::FP32),
            testing::Values(InferenceEngine::Layout::ANY),
            testing::Values(InferenceEngine::Layout::ANY),
            testing::Values(InferenceEngine::SizeVector{ 24,  42}), //output_size
            testing::Values(InferenceEngine::SizeVector{348, 672}), //image_size
+           testing::Values(LayerTestsUtils::testPlatformTargetDevice)),
+        KmbPriorBoxLayerTest::getTestCaseName
+);
+
+}  // namespace
+
+
+namespace { //TEST_2
+
+const priorBoxSpecificParams param2 = {
+   std::vector<float>{256.0}, // min_size
+   std::vector<float>{315.0}, // max_size
+   std::vector<float>{2.0},   // aspect_ratio
+   std::vector<float>{1.0},   // [density]
+   std::vector<float>{},      // [fixed_ratio]
+   std::vector<float>{},      // [fixed_size]
+   false,                     // clip
+   false,                     // flip
+   1.0,                       // step
+   0.0,                       // offset
+   std::vector<float>{},      // variance
+   false,                     // [scale_all_sizes]
+   false                      // min_max_aspect_ratios_order ?
+};
+
+INSTANTIATE_TEST_SUITE_P(
+        smoke_PriorBox2,
+        KmbPriorBoxLayerTest,
+        testing::Combine(
+           testing::Values(param2),
+           testing::Values(InferenceEngine::Precision::FP16),
+           testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+           testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+           testing::Values(InferenceEngine::Layout::ANY),
+           testing::Values(InferenceEngine::Layout::ANY),
+           testing::Values(InferenceEngine::SizeVector{300,300}), //output_size
+           testing::Values(InferenceEngine::SizeVector{ 32, 32}), //image_size
            testing::Values(LayerTestsUtils::testPlatformTargetDevice)),
         KmbPriorBoxLayerTest::getTestCaseName
 );
