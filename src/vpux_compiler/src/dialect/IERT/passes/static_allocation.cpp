59ce21fd45a668abdfc495e2eb46450a5304bdcb
syao-2015 <shaojun.yao@intel.com>
2021-12-28 11:45:32 +0800

    add barrier scheduler

--- src/vpux_compiler/src/dialect/IERT/passes/static_allocation.cpp
+++ src/vpux_compiler/src/dialect/IERT/passes/static_allocation.cpp
@@ -192,12 +192,6 @@ LinearScanHandler StaticAllocationPass::runLinearScan(mlir::FuncOp netFunc, IE::
         _log.trace("Process next task at '{0}'", curExecOp->getLoc());
         _log = _log.nest();
 
-        // TODO: remove temporary linearization
-        if (prevExecOp != nullptr) {
-            _log.trace("Add explicit dependency from '{0}' to '{1}'", prevExecOp->getLoc(), curExecOp->getLoc());
-            depsInfo.addDependency(prevExecOp, curExecOp);
-        }
-
         const auto usedBufs = liveRangeInfo.getUsedBuffers(curExecOp);
 
         allocNewBuffers(usedBufs);
