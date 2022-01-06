4abffe0930a40f1e8c6a0cd9c555887ec78ec875
syao-2015 <shaojun.yao@intel.com>
2022-01-06 16:11:18 +0800

    remove unique ID

--- src/vpux_compiler/src/core/feasible_barrier_generator.cpp
+++ src/vpux_compiler/src/core/feasible_barrier_generator.cpp
@@ -769,6 +769,12 @@ void FeasibleBarrierScheduler::cleanUpVirtualBarriers() {
     });
 }
 
+void FeasibleBarrierScheduler::clearUniqueID() {
+    _func->walk([](VPURT::TaskOp op) {
+        op->removeAttr(uniqueIdAttrName);
+    });
+}
+
 bool FeasibleBarrierScheduler::performRuntimeSimulation() {
     bool success = true;
     reorderIR();
