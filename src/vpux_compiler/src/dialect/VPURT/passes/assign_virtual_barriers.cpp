9dd18c074329f940549b0d32357a7f7601cb463b
johnbrad <john.brady@intel.com>
2022-01-06 13:55:05 +0000

    fix assign_virtual_barriers.cpp

--- src/vpux_compiler/src/dialect/VPURT/passes/assign_virtual_barriers.cpp
+++ src/vpux_compiler/src/dialect/VPURT/passes/assign_virtual_barriers.cpp
@@ -37,40 +37,6 @@ private:
     void safeRunOnFunc() final;
 };
 
-namespace {
-
-const EnumMap<VPU::ArchKind, int64_t> MAX_BARRIERS_PER_INFERENCE = {
-        {VPU::ArchKind::KMB, 64 / 2},  // half barries are used (runtime limitation)
-        {VPU::ArchKind::TBH, 64 / 2},  // half barries are used (runtime limitation)
-        {VPU::ArchKind::MTL, 64},      //
-        {VPU::ArchKind::LNL, 64},      //
-};
-
-int64_t getNumAvailableBarriers(mlir::Operation* parentOp) {
-    const auto arch = VPU::getArch(parentOp);
-
-    auto module = parentOp->getParentOfType<mlir::ModuleOp>();
-    auto nceResOp = IE::getAvailableExecutor(module, VPU::ExecutorKind::NCE);
-    VPUX_THROW_UNLESS(nceResOp != nullptr, "Failed to get NCE Executor information");
-
-    const auto numClusters = nceResOp.count();
-
-    const auto maxNumClustersForArch = VPU::getMaxDPUClusterNum(module);
-    VPUX_THROW_UNLESS(maxNumClustersForArch != 0, "Failed to get maxNumClustersForArch");
-
-    const auto barIt = MAX_BARRIERS_PER_INFERENCE.find(arch);
-    VPUX_THROW_WHEN(barIt == MAX_BARRIERS_PER_INFERENCE.end(), "Unsupported VPU architecture '{0}'", arch);
-
-    const auto maxBarriersPerInference = barIt->second;
-
-    const auto barriersPerCluster = maxBarriersPerInference / maxNumClustersForArch;
-    const auto maxNumBarriers = std::min(maxBarriersPerInference, barriersPerCluster * numClusters);
-
-    return maxNumBarriers;
-}
-
-}  // namespace
-
 void AssignVirtualBarriersPass::safeRunOnFunc() {
     auto& ctx = getContext();
     auto func = getFunction();
