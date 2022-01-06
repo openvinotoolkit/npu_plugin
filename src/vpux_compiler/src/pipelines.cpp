5eb59746cbd9306d1d582edda5fb9270707105ad
Li, Yingyue <yingyue.li@intel.com>
2022-01-06 14:49:08 +0800

    remove parallel copies for weight prefetching

--- src/vpux_compiler/src/pipelines.cpp
+++ src/vpux_compiler/src/pipelines.cpp
@@ -297,6 +297,7 @@ void vpux::buildDefaultHWModePipeline(mlir::OpPassManager& pm, const DefaultHWOp
     if (options.enableOptimizeCopies) {
         pm.addPass(IERT::createOptimizeCopiesPass(log));
         pm.addPass(IERT::createCopyOpHoistingPass(log));
+        pm.addPass(IERT::createOptimizeParallelCopiesPass(log));
     }
 
     if (options.enableProfiling && options.enableDPUProfiling) {
