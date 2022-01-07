c4d790dc6295698fd43979fd7a7f6cd6175f9555
AndreiLupas <andrei.lupas@movidius.com>
2022-01-07 12:11:15 +0200

    compiles OK

--- src/vpux_compiler/src/dialect/VPUIP/ops.cpp
+++ src/vpux_compiler/src/dialect/VPUIP/ops.cpp
@@ -427,6 +427,7 @@ void redirectOpInterfacesForIE(mlir::DialectRegistry& registry) {
     registry.addOpInterface<IE::FakeQuantizeOp, OpModelForSW<VPUIP::FakeQuantizeUPAOp>>();
     registry.addOpInterface<IE::GatherOp, OpModelForSW<VPUIP::GatherUPAOp>>();
     registry.addOpInterface<IE::ScatterNDUpdateOp, OpModelForSW<VPUIP::ScatterNDUpdateUPAOp>>();
+    registry.addOpInterface<IE::ScatterUpdateOp, OpModelForSW<VPUIP::ScatterUpdateUPAOp>>();
     registry.addOpInterface<IE::QuantizeOp, OpModelForSW<VPUIP::QuantCastUPAOp>>();
     registry.addOpInterface<IE::DequantizeOp, OpModelForSW<VPUIP::QuantCastUPAOp>>();
     registry.addOpInterface<IE::PReluOp, OpModelForSW<VPUIP::PReluUPAOp>>();
@@ -519,6 +520,7 @@ void redirectOpInterfacesForIERT(mlir::DialectRegistry& registry) {
     registry.addOpInterface<IERT::GatherOp, OpModelForSW>();
     registry.addOpInterface<IERT::GatherElementsOp, OpModelForSW>();
     registry.addOpInterface<IERT::ScatterNDUpdateOp, OpModelForSW>();
+    registry.addOpInterface<IERT::ScatterUpdateOp, OpModelForSW>();
     registry.addOpInterface<IERT::PReluOp, OpModelForSW>();
     registry.addOpInterface<IERT::LeakyReluOp, OpModelForSW>();
     registry.addOpInterface<IERT::DivideOp, OpModelForSW>();
