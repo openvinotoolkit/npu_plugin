6be75e8dfc572dfca5e85e97aaec007345a75149
syao-2015 <shaojun.yao@intel.com>
2022-01-07 15:51:28 +0800

    more refactoring

--- src/vpux_compiler/src/dialect/VPURT/passes/assign_virtual_barriers.cpp
+++ src/vpux_compiler/src/dialect/VPURT/passes/assign_virtual_barriers.cpp
@@ -52,7 +52,7 @@ void AssignVirtualBarriersPass::safeRunOnFunc() {
         barrierScheduler.generateScheduleWithBarriers(barrier_bound, numSlotsPerBarrierToUse);
         success = barrierScheduler.performRuntimeSimulation();
     }
-    barrierScheduler.clearUniqueIDAttribute();
+    barrierScheduler.clearTemporaryAttribute();
 
     if (!success) {
         VPUX_THROW("Barrier scheduling and/or runtime simulation was not suceessful");
