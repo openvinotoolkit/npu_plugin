18ac54e5d60fd6669d5306585f06a0eaaed50043
AndreiLupas <andrei.lupas@movidius.com>
2022-01-10 12:39:55 +0200

    GOLD: all tests with shapes ranks <=5 and axis=0 PASS (with some local VPUIP checks disabled)

--- tests/functional/shared_tests_instances/single_layer_tests/scatter_update.cpp
+++ tests/functional/shared_tests_instances/single_layer_tests/scatter_update.cpp
@@ -22,47 +22,26 @@ using namespace LayerTestsDefinitions;
 
 namespace {
 
-
-// map<inputShape, map<indicesShape, axis>>
-std::map<std::vector<size_t>, std::map<std::vector<size_t>, std::vector<int>>> axesShapeInShape {
-    {{10, 16, 12, 15}, {{{2, 2, 2}, {0, 1, 2, 3}}, {{2, 4}, {0, 1, 2, 3}}, {{8}, {0, 1, 2, 3}}}},
-    {{10, 9, 10, 9, 10}, {{{8}, {0, 1, 2, 3, 4}}, {{4, 2}, {0, 1, 2, 3, 4}}}},
-    {{10, 9, 10, 9, 10, 12}, {{{8}, {0, 1, 2, 3, 4, 5}}}},
-    {{10, 16, 12, 15}, {{{2, 4}, {0, 1, 2, 3}}, {{8}, {-1, -2, -3, -4}}}},
-    {{10, 9, 10, 9, 10}, {{{8}, {-3, -1, 0, 2, 4}}, {{4, 2}, {-2, 2}}}},
-};
 //indices should not be random value
 const std::vector<std::vector<int64_t>> idxValue = {
         {0, 2, 4, 6, 1, 3, 5, 7}
 };
 
-/*
-INSTANTIATE_TEST_SUITE_P(
-        smoke_ScatterUpdate,
-        KmbScatterUpdateLayerTest,
-        testing::Combine(
-           testing::ValuesIn(ScatterUpdateLayerTest::combineShapes(axesShapeInShape)),
-           testing::ValuesIn(idxValue),
-           testing::Values(InferenceEngine::Precision::FP16), // input prec
-           testing::Values(InferenceEngine::Precision::I32),  // indices prec
-           testing::Values(LayerTestsUtils::testPlatformTargetDevice)),
-        KmbScatterUpdateLayerTest::getTestCaseName
-);
-*/
-//==============================================================================
-const axisUpdateShapeInShape toyParam {
-    {10, 9, 10, 9, 10}, //in shape
-    { 8},               //indices shape
-    { 8, 9, 10, 9, 10}, //update shape
-     0                  //axis
+//inShape, indicesShape, updateShape, axis
+const std::vector<axisUpdateShapeInShape> toyParam {
+   {{10, 9, 10, 9, 10}, {8},     {8, 9, 10, 9, 10},  0}, //OK
+   {{10, 16, 12, 15},   {2,4},   {2, 4, 16, 12, 15}, 0}, //OK
+   {{10, 16, 12, 15},   {8},     {8, 16, 12, 15},    0}, //OK
+   {{10, 16, 12},       {2,2,2}, {2, 2, 2, 16, 12},  0}, //OK
+   {{10, 9, 10, 9},     {4,2},   {4, 2, 9, 10, 9},   0}, //OK
 };
 
 INSTANTIATE_TEST_SUITE_P(
         smoke_ScatterUpdateToy,
         KmbScatterUpdateLayerTest,
         testing::Combine(
-           testing::Values(toyParam),
-           testing::Values(std::vector<int64_t>{0, 2, 4, 6, 1, 3, 5, 7}), //indices values
+           testing::ValuesIn(toyParam),
+           testing::ValuesIn(idxValue),                       //indices values
            testing::Values(InferenceEngine::Precision::FP16), // input prec
            testing::Values(InferenceEngine::Precision::I32),  // indices prec
            testing::Values(LayerTestsUtils::testPlatformTargetDevice)),
