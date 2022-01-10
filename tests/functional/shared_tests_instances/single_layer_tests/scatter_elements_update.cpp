54b2a2afbc21ff794c69070ef7dc50a5381545be
AndreiLupas <andrei.lupas@movidius.com>
2022-01-10 15:53:07 +0200

    + dummy test (compiles OK)

---
+++ tests/functional/shared_tests_instances/single_layer_tests/scatter_elements_update.cpp
@@ -0,0 +1,48 @@
+// Copyright (C) Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "single_layer_tests/scatter_elements_update.hpp"
+#include "common_test_utils/test_constants.hpp"
+#include "kmb_layer_test.hpp"
+
+namespace LayerTestsDefinitions {
+
+class KmbScatterElementsUpdateLayerTest: public ScatterElementsUpdateLayerTest, virtual public LayerTestsUtils::KmbLayerTestsCommon {
+};
+
+TEST_P(KmbScatterElementsUpdateLayerTest, CompareWithRefs_MLIR) {
+    useCompilerMLIR();
+    Run();
+}
+
+}  // namespace LayerTestsDefinitions
+
+using namespace LayerTestsDefinitions;
+
+namespace {
+
+//indices should not be random value
+const std::vector<std::vector<size_t>> idxValue = {
+        {0, 2, 4, 6, 1, 3, 5, 7},
+        {1, 0, 4, 6, 2, 3, 7, 5}
+};
+
+//inShape, updateShape, axis
+const std::vector<axisShapeInShape> params {
+    {{10,12,15}, {1,2,4}, 0},
+};
+
+INSTANTIATE_TEST_SUITE_P(
+        smoke_ScatterElementsUpdate,
+        KmbScatterElementsUpdateLayerTest,
+        testing::Combine(
+           testing::ValuesIn(params),
+           testing::ValuesIn(idxValue),                       // indices values
+           testing::Values(InferenceEngine::Precision::FP16), // input prec
+           testing::Values(InferenceEngine::Precision::I32),  // indices prec
+           testing::Values(LayerTestsUtils::testPlatformTargetDevice)),
+        KmbScatterElementsUpdateLayerTest::getTestCaseName
+);
+
+}  // namespace
