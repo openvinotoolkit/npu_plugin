3d17b245c892f4613f8967385e45bd23703fa4cf
AndreiLupas <andrei.lupas@movidius.com>
2022-01-11 15:35:15 +0200

    more configs OK

--- tests/functional/shared_tests_instances/single_layer_tests/scatter_elements_update.cpp
+++ tests/functional/shared_tests_instances/single_layer_tests/scatter_elements_update.cpp
@@ -9,6 +9,13 @@
 namespace LayerTestsDefinitions {
 
 class KmbScatterElementsUpdateLayerTest: public ScatterElementsUpdateLayerTest, virtual public LayerTestsUtils::KmbLayerTestsCommon {
+    void SkipBeforeLoad() override {
+        const auto &params = std::get<0>(GetParam());
+        const auto  axis   = std::get<2>(params);
+        if (axis < 0) {
+            throw LayerTestsUtils::KmbSkipTestException("Runtime only supports positive axis, actual=" + std::to_string(axis));
+        }
+    }
 };
 
 TEST_P(KmbScatterElementsUpdateLayerTest, CompareWithRefs_MLIR) {
@@ -26,8 +33,25 @@ namespace {
 const std::vector<size_t> idxValue {0, 2, 4, 6, 1, 3, 5, 7};
 
 //inShape, updateShape, axis
-const std::vector<axisShapeInShape> params = {
-    {{10,12,15}, {1,2,4}, 0},
+const std::vector<axisShapeInShape> params =
+{
+    {{10,12,15},  {1,2,4},   0},
+    {{10,12,15},  {1,2,4},   1},
+    {{10,12,15},  {1,2,4},   2},
+
+    {{10,12,15},  {2,2,2},   0},
+    {{10,12,15},  {2,2,2},   1},
+    {{10,12,15},  {2,2,2},   2},
+
+    {{15,9,8,12}, {1,2,2,2}, 0},
+    {{15,9,8,12}, {1,2,2,2}, 1},
+    {{15,9,8,12}, {1,2,2,2}, 2},
+    {{15,9,8,12}, {1,2,2,2}, 3},
+
+    {{15,9,8,12}, {1,2,1,4}, 0},
+    {{15,9,8,12}, {1,2,1,4}, 1},
+    {{15,9,8,12}, {1,2,1,4}, 2},
+    {{15,9,8,12}, {1,2,1,4}, 3},
 };
 
 INSTANTIATE_TEST_SUITE_P(
