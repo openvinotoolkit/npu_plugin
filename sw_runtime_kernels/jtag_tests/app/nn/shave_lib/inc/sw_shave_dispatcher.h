2001fa06edbfcdccc8800d7bd79283bbac785fbc
Skvortsov, Vladimir <vladimir.skvortsov@intel.com>
2022-01-14 10:21:11 +0300

    37xx/upa_task_runner.cpp code cleaned

--- sw_runtime_kernels/jtag_tests/app/nn/shave_lib/inc/sw_shave_dispatcher.h
+++ sw_runtime_kernels/jtag_tests/app/nn/shave_lib/inc/sw_shave_dispatcher.h
@@ -7,6 +7,3 @@
 #   include "sw_shave_dispatcher_2490.h"
 #endif // CONFIG_TARGET_SOC_MA2490 || CONFIG_TARGET_SOC_MA2490_B0 || CONFIG_TARGET_SOC_3100
 
-#if (defined(CONFIG_TARGET_SOC_3600) || defined(CONFIG_TARGET_SOC_3710) || defined(CONFIG_TARGET_SOC_3720))
-#   include "sw_shave_dispatcher_3600.h"
-#endif // CONFIG_TARGET_SOC_3600 || CONFIG_TARGET_SOC_3710 || CONFIG_TARGET_SOC_3720
