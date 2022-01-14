9bda390e401b52fe6076803d1581854c5420cfe9
istvan.szilveszter@intel.com <istvan.szilveszter@intel.com>
2022-01-14 19:59:40 +0200

    more jit code

--- tools/vpux-binutils/vpux-jit/main.cpp
+++ tools/vpux-binutils/vpux-jit/main.cpp
@@ -12,8 +12,16 @@
 //
 
 #include <llvm/Support/CommandLine.h>
+#include <llvm/Support/SourceMgr.h>
 
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/Parser.h>
 #include <mlir/Support/FileUtilities.h>
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/Dialect.h"
+
+#include <vpux/compiler/init.hpp>
+#include "vpux/utils/core/logger.hpp"
 
 namespace {
 
@@ -37,9 +45,32 @@ llvm::cl::list<std::string> inputFiles("f", llvm::cl::value_desc("Input Files"),
 llvm::cl::opt<std::string> outputFile("o", llvm::cl::value_desc("filename"), llvm::cl::init("vpu.bin"),
                                       llvm::cl::desc("Output file name"));
 
-} // namespace
+llvm::cl::opt<bool> verbose("v", llvm::cl::desc("enable verbose execution"));
+
+vpux::Logger logger("VPUX-JIT", vpux::LogLevel::Warning);
+
+}  // namespace
+
 
 int main(int argc, char* argv[]) {
     llvm::cl::ParseCommandLineOptions(argc, argv);
+
+    std::string errorMessage;
+    auto mlirFile = mlir::openInputFile(mlirFilePath, &errorMessage);
+    if (!mlirFile) {
+        logger.error("{0}", errorMessage);
+    }
+
+    mlir::MLIRContext context;
+    mlir::DialectRegistry dialectRegistry;
+    vpux::registerDialects(dialectRegistry);
+    context.appendDialectRegistry(dialectRegistry);
+
+    llvm::SourceMgr sourceMgr;
+    sourceMgr.AddNewSourceBuffer(std::move(mlirFile), llvm::SMLoc());
+
+    auto module = mlir::OwningModuleRef(mlir::parseSourceFile(sourceMgr, &context));
+
+
 
 }
