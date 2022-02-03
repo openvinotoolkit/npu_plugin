a0fb15ebd332fe2393a088d0fac4b9c38766b442
johnbrad <john.brady@intel.com>
2022-02-03 08:47:35 +0000

    create soh

--- src/vpux_compiler/src/dialect/VPU/passes/multi-cluster-strategy-assignment.cpp
+++ src/vpux_compiler/src/dialect/VPU/passes/multi-cluster-strategy-assignment.cpp
@@ -51,6 +51,12 @@ mlir::LogicalResult ConvToMultiCluster::matchAndRewrite(VPU::NCEConvolutionOp or
     }
     const auto bodyBuilder = [origOp](mlir::OpBuilder& builder, mlir::Location loc, mlir::ValueRange newOperands) {
         mlir::BlockAndValueMapping mapper;
+
+        // Create NCEClusterTilingOp with SOH Distributed Tensor
+        auto mode = vpux::VPU::DistributionMode::segmented;
+        SmallVector<uint16_t, 4> numTiles = {1, 1, 4, 1};
+        auto numClusters = 4;
+
         mapper.map(origOp->getOperands(), newOperands);
         auto* newOp = builder.clone(*origOp, mapper);
         builder.create<VPU::YieldOp>(loc, newOp->getResults());
