0abbb5a8f97d47cedf0b869a276c9d5499ca1b08
Andrey Markelov <andrey.markelov@intel.com>
2022-02-08 09:37:11 +0300

    Fix types in getElemTypeInfo (#607)

--- src/vpux_compiler/src/dialect/IE/ops_interfaces.cpp
+++ src/vpux_compiler/src/dialect/IE/ops_interfaces.cpp
@@ -309,13 +309,13 @@ vpux::IE::LayerDataInfo<mlir::Type> vpux::IE::getElemTypeInfo(mlir::Operation* o
     SmallVector<mlir::Type> inputTypes;
     inputTypes.reserve(op->getNumOperands());
     for (const auto& val : op->getOperands()) {
-        inputTypes.push_back(val.getType().cast<mlir::MemRefType>().getElementType());
+        inputTypes.push_back(val.getType().cast<mlir::ShapedType>().getElementType());
     }
 
     SmallVector<mlir::Type> outputTypes;
     outputTypes.reserve(op->getNumResults());
     for (const auto& val : op->getResults()) {
-        outputTypes.push_back(val.getType().cast<mlir::MemRefType>().getElementType());
+        outputTypes.push_back(val.getType().cast<mlir::ShapedType>().getElementType());
     }
 
     return vpux::IE::LayerDataInfo<mlir::Type>(std::move(inputTypes), std::move(outputTypes));
