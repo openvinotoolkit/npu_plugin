ab8c5a31dc75eac19c66300e576e2b3ea9c97886
johnbrad <john.brady@intel.com>
2022-02-14 17:58:19 +0000

    switch off removing strategy

--- src/vpux_compiler/src/dialect/VPU/passes/multi-cluster-strategy-assignment.cpp
+++ src/vpux_compiler/src/dialect/VPU/passes/multi-cluster-strategy-assignment.cpp
@@ -265,7 +265,7 @@ void MultiClusterStrategyAssignmentPass::safeRunOnFunc() {
     auto func = getFunction();
     StrategyManager strategyManager(func, _log);
     strategyManager.computeOptimalMultiClusterStrategy();
-    strategyManager.removeStrategyAttribute();
+    //strategyManager.removeStrategyAttribute();
 
     auto& ctx = getContext();
     mlir::RewritePatternSet patterns(&ctx);
