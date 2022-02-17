837c7a42f7890991d60999fcfd2363415623d794
Mateusz Darecki <mateusz.darecki@intel.com>
2022-02-17 13:28:21 +0100

    EISW-28762: Control edge algorithm should differentiate between memory range user or producer (#555)
    * In scheduler control edge generation identify operation input and output buffers separately
    * Extend control edge disjoint interval algorithm with notion of interval producer and consumers
    * In static-allocation pass DDR resources need to take into account producers and consumers when generating control edges
    * Add LIT test for update in control edge generation logic

--- src/vpux_compiler/include/vpux/compiler/core/feasible_scheduler_utils.hpp
+++ src/vpux_compiler/include/vpux/compiler/core/feasible_scheduler_utils.hpp
@@ -20,16 +20,19 @@
 namespace vpux {
 
 struct ScheduledOpOneResource {
+    enum class EResRelation { PRODUCER = 0, CONSUMER = 1 };
+
     using OperationType = size_t;
     ScheduledOpOneResource(): _op(), _addressStart(), _addressEnd() {
     }
 
-    ScheduledOpOneResource(OperationType op, size_t start, size_t end)
-            : _op(op), _addressStart(start), _addressEnd(end) {
+    ScheduledOpOneResource(OperationType op, size_t start, size_t end,
+                           EResRelation resRelation = EResRelation::PRODUCER)
+            : _op(op), _addressStart(start), _addressEnd(end), _resRelation(resRelation) {
     }
 
     ScheduledOpOneResource(const ScheduledOpOneResource& o)
-            : _op(o._op), _addressStart(o._addressStart), _addressEnd(o._addressEnd) {
+            : _op(o._op), _addressStart(o._addressStart), _addressEnd(o._addressEnd), _resRelation(o._resRelation) {
     }
 
     const ScheduledOpOneResource& operator=(const ScheduledOpOneResource& o) {
@@ -43,9 +46,17 @@ struct ScheduledOpOneResource {
         return (_op == o._op) && (_addressStart == o._addressStart) && (_addressEnd == o._addressEnd);
     }
 
+    bool operator<(const ScheduledOpOneResource& o) const {
+        if (_addressStart == o._addressStart && _addressEnd == o._addressEnd) {
+            return _op < o._op;
+        }
+        return (_addressStart != o._addressStart) ? (_addressStart < o._addressStart) : (_addressEnd < o._addressEnd);
+    }
+
     OperationType _op;
     size_t _addressStart;
     size_t _addressEnd;
+    EResRelation _resRelation;
 };  // struct ScheduledOpOneResource //
 
 }  // namespace vpux
