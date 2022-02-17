8dbf63c12c0c178686fda55bf19d0d4c48e0f8b4
johnbrad <john.brady@intel.com>
2022-02-17 11:19:09 +0000

    revert changes to sw_runtime_kernels

--- sw_runtime_kernels/kernels/single_shave_softmax.cpp
+++ sw_runtime_kernels/kernels/single_shave_softmax.cpp
@@ -1,15 +1,4 @@
-//
-// Copyright 2022 Intel Corporation.
-//
-// LEGAL NOTICE: Your use of this software and any required dependent software
-// (the "Software Package") is subject to the terms and conditions of
-// the Intel(R) OpenVINO(TM) Distribution License for the Software Package,
-// which may also include notices, disclaimers, or license terms for
-// third party or open source software included in or with the Software Package,
-// and your use indicates your acceptance of all such terms. Please refer
-// to the "third-party-programs.txt" or other similarly-named text file
-// included with the Software Package for additional details.
-//
+// {% copyright %}
 
 #ifdef CONFIG_HAS_LRT_SRCS
 #include <nn_log.h>
