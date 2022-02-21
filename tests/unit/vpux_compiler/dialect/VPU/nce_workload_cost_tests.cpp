93e5d7766e0fd1c9e1b45e22b67e91f4c423a938
Liu Xiao <xiao.x.liu@intel.com>
2022-02-21 17:36:14 +0800

    Update unit test for vpunn
    Signed-off-by: Liu Xiao <xiao.x.liu@intel.com>

--- tests/unit/vpux_compiler/dialect/VPU/nce_workload_cost_tests.cpp
+++ tests/unit/vpux_compiler/dialect/VPU/nce_workload_cost_tests.cpp
@@ -13,7 +13,6 @@
 
 #include "vpux/compiler/core/tiling.hpp"
 #include "vpux/compiler/dialect/VPU/attributes.hpp"
-#include "vpux/compiler/dialect/VPU/ops.hpp"
 #include "vpux/compiler/dialect/VPUIP/dpu_tiler.hpp"
 #include "vpux/compiler/init.hpp"
 
@@ -26,94 +25,70 @@ namespace {
 
 constexpr int64_t numDPU = 5;
 constexpr int64_t maxSplitNum = 5;
+constexpr int64_t initDimensionValue = 16;
+constexpr int64_t maxDimensionValue = 64;
+constexpr int64_t testStep = 2;
 
-vpux::VPUIP::WorkloadCostParams buildWorkloadCost(vpux::VPU::NCEConvolutionOp convolutionOp,
-                                                  vpux::VPU::MPEMode mpeMode) {
-    const auto outputShape = vpux::getShape(convolutionOp.output());
-    const auto inputShape = vpux::getShape(convolutionOp.input());
-    const auto kernelStrides = vpux::parseIntArrayAttr<int64_t>(convolutionOp.strides());
-    const auto filterShape = convolutionOp.rawFilterShapeAttr() != nullptr
-                                     ? vpux::Shape(vpux::parseIntArrayAttr<int64_t>(convolutionOp.rawFilterShapeAttr()))
-                                     : vpux::getShape(convolutionOp.filter());
+struct NceOpTensorShape {
+    NceOpTensorShape(vpux::ShapeRef input, vpux::ShapeRef output): inputShape(input.raw()), outputShape(output.raw()) {
+    }
+    vpux::Shape inputShape;
+    vpux::Shape outputShape;
+};
 
+vpux::VPUIP::WorkloadCostParams buildWorkloadCost(const NceOpTensorShape& tensorShape, vpux::VPU::MPEMode mpeMode,
+                                                  mlir::MLIRContext* ctx) {
+    const auto outputShape = tensorShape.outputShape;
+    const auto inputShape = tensorShape.inputShape;
     vpux::VPUIP::WorkloadCostParams costParams;
-    costParams.dataType = convolutionOp.input().getType().template cast<mlir::RankedTensorType>().getElementType();
-    costParams.inputShape = inputShape.raw();
-    costParams.outputShape = outputShape.raw();
-    costParams.padInfo = vpux::VPU::toPadInfo(convolutionOp.pad());
-    costParams.kernelSize = {filterShape[vpux::Dims4D::Filter::KY], filterShape[vpux::Dims4D::Filter::KX]};
-    costParams.kernelStride = {kernelStrides[0], kernelStrides[1]};
+    costParams.dataType = mlir::Float16Type::get(ctx);
+    costParams.inputShape = tensorShape.inputShape;
+    costParams.outputShape = tensorShape.outputShape;
+    costParams.padInfo = vpux::PadInfo(0, 0, 0, 0);
+    costParams.kernelSize = {1, 1};
+    costParams.kernelStride = {1, 1};
     costParams.nceTaskType = vpux::VPUIP::NCETaskType::CONV;
     costParams.arch = vpux::VPU::ArchKind::KMB;
     costParams.mpeMode = mpeMode;
     costParams.numDPU = numDPU;
     return costParams;
 }
-
 }  // namespace
 
 TEST(MLIR_VPU_WorkloadCost, VPUNNCostInterface) {
-    mlir::DialectRegistry registry;
-    vpux::registerDialects(registry);
-
-    mlir::MLIRContext ctx(registry);
-
-    constexpr llvm::StringLiteral inputIR = R"(
-        #NHWC = affine_map<(d0, d1, d2, d3) -> (d0, d2, d3, d1)>
-        module @main {
-            func @main(%arg0: tensor<1x64x64x64xf16, {order = #NHWC}>) -> tensor<1x64x64x64xf16, {order = #NHWC}> {
-                %cst0 = const.Declare tensor<64x64x1x1xf16, {order = #NHWC}> =
-                    #const.Content<dense<1.000000e+00> : tensor<64x64x1x1xf16>, [#const.Reorder<#NHWC>]>
-
-                %0 = IE.Copy(%arg0) {out_mem_space = @CMX_NN} : tensor<1x64x64x64xf16, {order = #NHWC}>
-                    -> tensor<1x64x64x64xf16, {mem_space = @CMX_NN, order = #NHWC}>
-                %1 = IE.Copy(%cst0) {out_mem_space = @CMX_NN} : tensor<64x64x1x1xf16, {order = #NHWC}>
-                    -> tensor<64x64x1x1xf16, {mem_space = @CMX_NN, order = #NHWC}>
-                %2 = VPU.NCE.Convolution(%0, %1) {
-                        pad = {bottom = 0 : i64, left = 0 : i64, right = 0 : i64, top = 0 : i64},
-                        strides = [1, 1]
-                    } : tensor<1x64x64x64xf16, {mem_space = @CMX_NN, order = #NHWC}>, tensor<64x64x1x1xf16, {mem_space = @CMX_NN, order = #NHWC}>
-                    -> tensor<1x64x64x64xf16, {mem_space = @CMX_NN, order = #NHWC}>
-
-                %3 = IE.Copy(%2) : tensor<1x64x64x64xf16, {mem_space = @CMX_NN, order = #NHWC}>
-                    -> tensor<1x64x64x64xf16, {order = #NHWC}>
-
-                return %3 : tensor<1x64x64x64xf16, {order = #NHWC}>
-            }
-        }
-    )";
-
-    auto module = mlir::parseSourceString(inputIR, &ctx);
-    ASSERT_TRUE(module.get() != nullptr);
-
-    auto func = module.get().lookupSymbol<mlir::FuncOp>("main");
-    ASSERT_TRUE(func != nullptr);
+    mlir::MLIRContext ctx;
 
     llvm::SmallVector<vpux::VPU::MPEMode> mpeModeList{vpux::VPU::MPEMode::VECTOR_FP16, vpux::VPU::MPEMode::VECTOR,
                                                       vpux::VPU::MPEMode::MATRIX, vpux::VPU::MPEMode::CUBOID_4x16};
 
-    for (auto& op : func.getOps()) {
-        if (auto convolutionOp = mlir::dyn_cast<vpux::VPU::NCEConvolutionOp>(op)) {
-            for (auto& mpeMode : mpeModeList) {
-                auto costParams = buildWorkloadCost(convolutionOp, mpeMode);
-                const auto outputShape = vpux::getShape(convolutionOp.output());
-                vpux::VPUIP::DpuTiler dpuTiler(outputShape, mpeMode);
-                const auto& splitPool = dpuTiler.generateSplitNumberPool(numDPU, maxSplitNum);
+    llvm::SmallVector<NceOpTensorShape> testTensorLists;
+    for (int64_t h = initDimensionValue; h < maxDimensionValue; h *= testStep) {
+        for (int64_t w = initDimensionValue; w < maxDimensionValue; w *= testStep) {
+            for (int64_t c = initDimensionValue; c < maxDimensionValue; c *= testStep) {
+                NceOpTensorShape tensorShape(vpux::ShapeRef({1, c, h, w}), vpux::ShapeRef({1, c, h, w}));
+                testTensorLists.push_back(std::move(tensorShape));
+            }
+        }
+    }
+    for (auto& testTensor : testTensorLists) {
+        for (auto& mpeMode : mpeModeList) {
+            auto costParams = buildWorkloadCost(testTensor, mpeMode, &ctx);
+            vpux::VPUIP::DpuTiler dpuTiler(costParams.outputShape, mpeMode);
+            const auto& splitPool = dpuTiler.generateSplitNumberPool(numDPU, maxSplitNum);
 
-                vpux::Shape nTilesOnDim(outputShape.size(), 1);
-                const auto& outTilesWithSingleSplit = vpux::fillDividedTiles(nTilesOnDim, outputShape);
-                auto baseHardwareExecutionCost = dpuTiler.cost(outTilesWithSingleSplit, costParams);
+            vpux::Shape nTilesOnDim(costParams.outputShape.size(), 1);
+            const auto& outTilesWithSingleSplit = vpux::fillDividedTiles(nTilesOnDim, costParams.outputShape);
+            auto baseHardwareExecutionCost = dpuTiler.cost(outTilesWithSingleSplit, costParams);
 
-                dpuTiler.tileOverH(numDPU);
-                for (auto& splitNum : splitPool) {
-                    dpuTiler.tileOverZ(splitNum);
-                }
+            dpuTiler.tileOverH(numDPU);
+            for (auto& splitNum : splitPool) {
+                dpuTiler.tileOverZ(splitNum);
+            }
 
-                const auto& splitCandidates = dpuTiler.getSplitPool();
-                for (size_t idx = 0; idx < splitCandidates.size(); idx++) {
-                    auto hardwareExecutionCost = dpuTiler.cost(splitCandidates[idx], costParams);
-                    EXPECT_LE(hardwareExecutionCost, baseHardwareExecutionCost);
-                }
+            const auto& splitCandidates = dpuTiler.getSplitPool();
+            for (size_t idx = 0; idx < splitCandidates.size(); idx++) {
+                auto hardwareExecutionCost = dpuTiler.cost(splitCandidates[idx], costParams);
+                EXPECT_LE(hardwareExecutionCost, baseHardwareExecutionCost);
             }
         }
     }
