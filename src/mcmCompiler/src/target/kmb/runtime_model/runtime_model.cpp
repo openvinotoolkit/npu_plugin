7aaea3691c7f21b5e09b3803c1862dcd28402c9e
Guoliang Shi <guoliang.shi@intel.com>
2022-02-22 19:53:34 +0800

    [mcm] enable csram for thb (#719)

--- src/mcmCompiler/src/target/kmb/runtime_model/runtime_model.cpp
+++ src/mcmCompiler/src/target/kmb/runtime_model/runtime_model.cpp
@@ -1414,7 +1414,6 @@ std::unique_ptr<MVCNN::BinaryDataT> mv::RuntimeModel::buildBinaryDataT(Computati
         t.set<bool>("Compression", false);
     }
 
-    csramCacheable = false;
     toBuild->csram_cacheable = csramCacheable;
 
     return toBuild;
