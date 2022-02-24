bbe22a9d009fd14694a0331e74b42b3117ead3c5
Yana Bulina <yana.bulina@intel.com>
2022-02-24 17:07:40 +0300

    yolo_v3_tiny fixed (#721)

--- src/vpux_compiler/src/dialect/VPUIP/ops/upa_region_yolo.cpp
+++ src/vpux_compiler/src/dialect/VPUIP/ops/upa_region_yolo.cpp
@@ -47,6 +47,6 @@ void vpux::VPUIP::RegionYoloUPAOp::inferLayoutInfo(mlir::Operation* origOp, IE::
     if (regionYoloOp.do_softmax()) {
         IE::fillDefaultLayoutInfo(info);
     } else {
-        IERT::inferLayoutInfoSameInOutSpecificDimsOrder(info, {DimsOrder::NCHW, DimsOrder::NHWC});
+        IERT::inferLayoutInfoSameInOutSpecificDimsOrder(info, {DimsOrder::NCHW});
     }
 }
