999fe3370c594591ac938eacfff5bbccadbbbc66
Michal Gwizdz <michal.gwizdz@intel.com>
2022-03-02 15:12:18 +0000

    remove trailing whitespace

--- src/vpux_compiler/src/pipelines.cpp
+++ src/vpux_compiler/src/pipelines.cpp
@@ -340,7 +340,7 @@ void vpux::buildDefaultHWModePipeline(mlir::OpPassManager& pm, const DefaultHWOp
 
     pm.addPass(VPU::createCMXConcatPass(log));
     pm.addPass(mlir::createCanonicalizerPass(grc));
-    
+
     pm.addPass(VPU::createSplitNCEOpsOntoWorkloadsPass(log));
 
     // Lowering
