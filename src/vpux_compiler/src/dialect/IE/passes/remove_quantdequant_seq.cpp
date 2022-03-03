58bf6d7da33d3a308ccfd7bfda1f272443115865
Marek Senderski <marek.senderski@intel.com>
2022-03-03 04:21:11 +0100

    Remove quant./dequant. pairs between UPA

--- src/vpux_compiler/src/dialect/IE/passes/remove_quantdequant_seq.cpp
+++ src/vpux_compiler/src/dialect/IE/passes/remove_quantdequant_seq.cpp
@@ -11,6 +11,7 @@
 // included with the Software Package for additional details.
 //
 
+#include <vpux/compiler/dialect/VPUIP/nce_invariant.hpp>
 #include "vpux/compiler/dialect/IE/passes.hpp"
 
 using namespace vpux;
@@ -25,6 +26,52 @@ class RemoveQuantDequantSeqPass final : public IE::RemoveQuantDequantSeqBase<Rem
 public:
     explicit RemoveQuantDequantSeqPass(Logger log) {
         Base::initLogger(log, Base::getArgumentName());
+    }
+    bool isDPU(mlir::Operation* op) {
+        auto convOp = mlir::dyn_cast<IE::ConvolutionOp>(op);
+        if (convOp != nullptr) {
+            if (!VPUIP::NCEInvariant::verifyKernel(convOp, _log).failed()) {
+                return true;
+            }
+        }
+        auto grConvOp = mlir::dyn_cast<IE::GroupConvolutionOp>(op);
+        if (grConvOp != nullptr) {
+            if (!VPUIP::NCEInvariant::verifyKernel(grConvOp, _log).failed()) {
+                return true;
+            }
+        }
+        auto maxPoolOp = mlir::dyn_cast<IE::MaxPoolOp>(op);
+        if (maxPoolOp != nullptr) {
+            if (!VPUIP::NCEInvariant::verifyKernel(maxPoolOp, _log).failed()) {
+                return true;
+            }
+        }
+        auto andOp = mlir::dyn_cast<IE::AndOp>(op);
+        if (andOp != nullptr) {
+            if (!VPUIP::NCEInvariant::verifyKernel(andOp, _log).failed()) {
+                return true;
+            }
+        }
+        auto subtractOp = mlir::dyn_cast<IE::SubtractOp>(op);
+        if (subtractOp != nullptr) {
+            if (!VPUIP::NCEInvariant::verifyKernel(subtractOp, _log).failed()) {
+                return true;
+            }
+        }
+        auto multiplyOp = mlir::dyn_cast<IE::MultiplyOp>(op);
+        if (multiplyOp != nullptr) {
+            if (!VPUIP::NCEInvariant::verifyKernel(multiplyOp, _log).failed()) {
+                return true;
+            }
+        }
+        auto addOp = mlir::dyn_cast<IE::AddOp>(op);
+        if (addOp != nullptr) {
+            if (!VPUIP::NCEInvariant::verifyKernel(addOp, _log).failed()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 private:
@@ -44,6 +91,17 @@ void RemoveQuantDequantSeqPass::safeRunOnFunc() {
         if (dequantizeOp == nullptr) {
             return;
         }
+        if (!dequantizeOp.output().hasOneUse()) {
+            return;
+        }
+        if (isDPU(quantizeOp.input().getDefiningOp())) {
+            return;
+        }
+        if (isDPU(*dequantizeOp.output().getUsers().begin())) {
+            return;
+        }
+        // quantizeOp.input().getDefiningOp()->dump();
+        // dequantizeOp.output().getUsers().begin()->dump();
         dequantizeOp.replaceAllUsesWith(quantizeOp.input());
     });
 }  // namespace
