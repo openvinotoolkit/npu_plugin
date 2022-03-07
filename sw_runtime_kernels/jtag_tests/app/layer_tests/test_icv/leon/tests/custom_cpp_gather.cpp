3c66b06e4d3e08316bf5972d9f16b1c1b4203322
Lei5 Chen <lei5.chen@intel.com>
2022-03-07 16:00:02 +0800

    Add initial Gather operation.

---
+++ sw_runtime_kernels/jtag_tests/app/layer_tests/test_icv/leon/tests/custom_cpp_gather.cpp
@@ -0,0 +1,25 @@
+//
+// Copyright Intel Corporation.
+//
+// LEGAL NOTICE: Your use of this software and any required dependent software
+// (the "Software Package") is subject to the terms and conditions of
+// the Intel(R) OpenVINO(TM) Distribution License for the Software Package,
+// which may also include notices, disclaimers, or license terms for
+// third party or open source software included in or with the Software Package,
+// and your use indicates your acceptance of all such terms. Please refer
+// to the "third-party-programs.txt" or other similarly-named text file
+// included with the Software Package for additional details.
+//
+
+#include <custom_cpp_tests.h>
+#include "layers/param_custom_cpp.h"
+#include "mvSubspaces.h"
+
+#ifdef CONFIG_TARGET_SOC_3720
+__attribute__((aligned(1024)))
+#include "sk.singleShaveGather.3010xx.text.xdat"
+#else
+#include "svuSLKernels_EP.h"
+#endif
+
+#include "param_gather.h"
