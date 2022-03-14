750bf29e23bb7bf861341dd0a43dae74ef08c184
yli150 <victor.y.li@intel.com>
2022-03-14 06:20:46 -0400

    add debug code for ReshapeMemPermuteOpConverter

--- src/vpux_compiler/src/dialect/IE/passes/fuse_mem_permute_and_reshape.cpp
+++ src/vpux_compiler/src/dialect/IE/passes/fuse_mem_permute_and_reshape.cpp
@@ -51,6 +51,7 @@ public:
 
 public:
     class MemPermuteOpConverter;
+    class ReshapeMemPermuteOpConverter;
 
 private:
     void safeRunOnFunc() final;
@@ -90,15 +91,15 @@ mlir::LogicalResult FuseMemPermuteReshapePass::MemPermuteOpConverter::matchAndRe
 
     // MemPermute(nchw) -> PermuteCast(nchw)-> MemPermute(nhwc)
     const auto inOrder = DimsOrder::fromValue(firstMemPermute.input());
-    const auto inShape = getShape(firstMemPermute.input());
+    // const auto inShape = getShape(firstMemPermute.input());
 
     const auto outOrder = DimsOrder::fromValue(origOp.output());
-    const auto outShape = getShape(origOp.output());
+    // const auto outShape = getShape(origOp.output());
 
-	std::cout<<llvm::formatv("{0} {1}", firstMemPermute->getName(), firstMemPermute->getLoc()).str()<<std::endl;
+	// std::cout<<llvm::formatv("{0} {1}", firstMemPermute->getName(), firstMemPermute->getLoc()).str()<<std::endl;
 
-    std::cout<<llvm::formatv("Before inOrder {0} {1} outOrder {2} {3}", inOrder, inShape, outOrder, outShape).str()<<std::endl; 
- 	std::cout<<llvm::formatv("Before inOrder {0} {1} outOrder {2} {3}", inOrder, inShape, outOrder, outShape).str()<<std::endl;
+    // std::cout<<llvm::formatv("Before inOrder {0} {1} outOrder {2} {3}", inOrder, inShape, outOrder, outShape).str()<<std::endl; 
+ 	// std::cout<<llvm::formatv("Before inOrder {0} {1} outOrder {2} {3}", inOrder, inShape, outOrder, outShape).str()<<std::endl;
 
 
     // inOrder == nchw, outOrder == nhwc
@@ -121,15 +122,96 @@ mlir::LogicalResult FuseMemPermuteReshapePass::MemPermuteOpConverter::matchAndRe
 
     // auto memPermAttr = mlir::AffineMapAttr::get(getPermutationFromOrders(inOrder, DimsOrder::NHWC, origOp->getContext()));
 
- 	std::cout<<llvm::formatv("Passed inOrder {0} {1} outOrder {2} {3} {4}", inOrder, inShape, outOrder, outShape, origOp.dst_orderAttr()).str()<<std::endl;
- 	std::cout<<llvm::formatv("Passed memPermAttr {0}", memPermAttr).str()<<std::endl;
- 	std::cout<<llvm::formatv("Passed dstOrder {0}", dstOrder).str()<<std::endl;
+ 	// std::cout<<llvm::formatv("Passed inOrder {0} {1} outOrder {2} {3} {4}", inOrder, inShape, outOrder, outShape, origOp.dst_orderAttr()).str()<<std::endl;
+ 	// std::cout<<llvm::formatv("Passed memPermAttr {0}", memPermAttr).str()<<std::endl;
+ 	// std::cout<<llvm::formatv("Passed dstOrder {0}", dstOrder).str()<<std::endl;
 
 
     rewriter.replaceOpWithNewOp<IE::MemPermuteOp>(origOp, firstMemPermute.input(), dstOrder, memPermAttr);
     return mlir::success();
 
 }
+
+
+//
+// ReshapeMemPermuteOpConverter
+//
+
+class FuseMemPermuteReshapePass::ReshapeMemPermuteOpConverter final : public mlir::OpRewritePattern<IE::MemPermuteOp> {
+public:
+    ReshapeMemPermuteOpConverter(mlir::MLIRContext* ctx, Logger log)
+            : mlir::OpRewritePattern<IE::MemPermuteOp>(ctx), _log(log) {
+    }
+
+public:
+    mlir::LogicalResult matchAndRewrite(IE::MemPermuteOp origOp, mlir::PatternRewriter& rewriter) const final;
+
+private:
+    Logger _log;
+};
+
+mlir::LogicalResult FuseMemPermuteReshapePass::ReshapeMemPermuteOpConverter::matchAndRewrite(
+        IE::MemPermuteOp origOp, mlir::PatternRewriter& rewriter) const {
+
+    const auto ctx = rewriter.getContext();
+
+    // MemPermute -> Reshape -> MemPermute 
+    // Search reverse 
+    auto reshape = origOp.input().getDefiningOp<IE::AffineReshapeOp>();
+    if (reshape == nullptr) {
+        return mlir::failure();
+    }
+
+    auto firstMemPermute = reshape.input().getDefiningOp<IE::MemPermuteOp>();
+    if (firstMemPermute == nullptr) {
+        return mlir::failure();
+    }
+
+    // MemPermute(nchw) -> PermuteCast(nchw)-> MemPermute(nhwc)
+    const auto inOrder = DimsOrder::fromValue(firstMemPermute.input());
+    const auto inShape = getShape(firstMemPermute.input());
+
+    const auto outOrder = DimsOrder::fromValue(origOp.output());
+    const auto outShape = getShape(origOp.output());
+
+	std::cout<<llvm::formatv("ReshapeMemPermuteOpConverter {0} {1}", firstMemPermute->getName(), firstMemPermute->getLoc()).str()<<std::endl;
+    std::cout<<llvm::formatv("ReshapeMemPermuteOpConverter inOrder {0} {1} outOrder {2} {3}", inOrder, inShape, outOrder, outShape).str()<<std::endl; 
+ 	std::cout<<llvm::formatv("ReshapeMemPermuteOpConverter inOrder {0} {1} outOrder {2} {3}", inOrder, inShape, outOrder, outShape).str()<<std::endl;
+
+
+    // inOrder == nchw, outOrder == nhwc
+    if (inOrder != DimsOrder::NHWC || outOrder != DimsOrder::NHWC){
+        // should check mempermute instead of order 
+        return mlir::failure();
+    }
+
+
+    // //Before inOrder NHWC [1, 64, 48, 8] outOrder NCHW [1, 48, 8, 64]
+    // // create permutation <d0, d1, d2, d3> -> <d0, d2, d3, d1>
+    SmallVector<uint32_t> perm(4, 0);
+    perm[0] = 0;
+    perm[1] = 1;
+    perm[2] = 2;
+    perm[3] = 3;
+    auto memPermAttr = mlir::AffineMapAttr::get(mlir::AffineMap::getPermutationMap(perm, ctx));
+	std::cout<<llvm::formatv("Passed memPermAttr {0}", memPermAttr).str()<<std::endl;
+
+
+    // auto dstOrder = mlir::AffineMapAttr::get(DimsOrder::NCHW.toAffineMap(origOp.getContext()));
+
+    // auto memPermAttr = mlir::AffineMapAttr::get(getPermutationFromOrders(inOrder, DimsOrder::NHWC, origOp->getContext()));
+
+ 	// std::cout<<llvm::formatv("Passed inOrder {0} {1} outOrder {2} {3} {4}", inOrder, inShape, outOrder, outShape, origOp.dst_orderAttr()).str()<<std::endl;
+ 	// std::cout<<llvm::formatv("Passed memPermAttr {0}", memPermAttr).str()<<std::endl;
+ 	// std::cout<<llvm::formatv("Passed dstOrder {0}", dstOrder).str()<<std::endl;
+
+
+    // rewriter.replaceOpWithNewOp<IE::MemPermuteOp>(origOp, firstMemPermute.input(), dstOrder, memPermAttr);
+    return mlir::success();
+
+}
+
+
 
 void FuseMemPermuteReshapePass::safeRunOnFunc() {
     auto func = getFunction();
@@ -138,6 +220,7 @@ void FuseMemPermuteReshapePass::safeRunOnFunc() {
 
     mlir::RewritePatternSet patterns(&ctx);
     patterns.insert<MemPermuteOpConverter>(&ctx, _log);
+    patterns.insert<ReshapeMemPermuteOpConverter>(&ctx, _log);
 
     if (mlir::failed(applyPatternsAndFoldGreedily(func, std::move(patterns), getDefaultGreedyRewriteConfig()))) {
         signalPassFailure();
