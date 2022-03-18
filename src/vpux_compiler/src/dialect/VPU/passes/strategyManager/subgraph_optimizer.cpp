fffb0f45cb9e19b8cadd1484e061df1497bda0d3
mingqi <mingqi.hu@intel.com>
2022-03-18 17:03:28 +0800

    Add core logic for rollback

--- src/vpux_compiler/src/dialect/VPU/passes/strategyManager/subgraph_optimizer.cpp
+++ src/vpux_compiler/src/dialect/VPU/passes/strategyManager/subgraph_optimizer.cpp
@@ -38,6 +38,24 @@ bool SubgraphOptimizer::isKCompatible(mlir::Operation* op, bool allowHK){
     return false;
 }
 
+// @tbd decided by bdk5
+bool SubgraphOptimizer::canBeSOK(mlir::Operation* op){
+    if (mlir::isa<VPU::NCEConvolutionOp>(op) || mlir::isa<VPU::NCEDepthConvolutionOp>
+        || mlir::isa<VPU::NCEMaxPoolOp>(op)) {
+            return true;
+    }
+    return false;
+}
+
+bool SubgraphOptimizer::canBeHKSwitch(mlir::Operation* op){
+    // @todo according to op type: conv/dwConv/maxpool -> hk
+    if (mlir::isa<VPU::NCEConvolutionOp>(op) || mlir::isa<VPU::NCEDepthConvolutionOp>
+        || mlir::isa<VPU::NCEMaxPoolOp>(op)) {
+            return true;
+    }
+    return false;
+}
+
 void SubgraphOptimizer::skipSOH(mlir::Operation* op, bool allowHK){
     auto opID = op->getName().getStringRef();
     if (!_strategySkip.find(opID)){
@@ -76,7 +94,7 @@ bool SubgraphOptimizer::isSpillingFromStrategy(mlir::Operation* op){
     return false;
 }
 
-double bestCostOfKCompatible(mlir::Operation* op, bool allowHK){
+double SubgraphOptimizer::bestCostOfKCompatible(mlir::Operation* op, bool allowHK){
     double SOKCost = greedyCostOfLayer(op, splitOverKernel);
     double clusteringCost = greedyCostOfLayer(op, clustering);
     double HKSwitchCost = allowHK? greedyCostOfLayer(op, HKSwitch) : COST_MAX;
@@ -84,7 +102,7 @@ double bestCostOfKCompatible(mlir::Operation* op, bool allowHK){
 }
 
 // @warning I drop the allowHK flag for HCompatible
-double bestCostOfHCompatible(mlir::Operation* op){
+double SubgraphOptimizer::bestCostOfHCompatible(mlir::Operation* op){
     double SOHCost = greedyCostOfLayer(op, splitOverHeight);
     double SOHOverCost = greedyCostOfLayer(op, splitOverHeightOverlapped);
     return std::min({SOHCost, SOHOverCost});
@@ -95,22 +113,22 @@ double bestCostOfHCompatible(mlir::Operation* op){
 // better suited to SOH or SOK
 // @warning Currently we may not need this in vpux, as we didn't consider spilling 
 // in our simple cost model
-bool hasGreedyKCompatible(mlir::Operation* op){
+bool SubgraphOptimizer::hasGreedyKCompatible(mlir::Operation* op){
     auto hCost = bestCostOfHCompatible(op);
     auto kCost = bestCostOfKCompatible(op, true);
     return kCost < hCost ? true : false;
 }
 
-bool areChildrenKCompatible(mlir::Operation* op){
+bool SubgraphOptimizer::areChildrenKCompatible(mlir::Operation* op){
     for (auto child : op->getResult(0).getUsers()){
-        if (!isKCompatible(child)){
+        if (child->getAttr(multiClusterStrategy) && !isKCompatible(child)){
             return false;
         }
     }
     return true;
 }
 
-void setKCompatible(mlir::Operation* op, bool allowHK=true){
+void SubgraphOptimizer::setKCompatible(mlir::Operation* op, bool allowHK=true){
     if (allowHk){
         op->setAttr(multiClusterStrategy, mlir::StringAttr::get(origOp->getContext(), "HKSwitch"));
     }else if (greedyCostOfLayer(op, splitOverHeight) < greedyCostOfLayer(op, clustering)){
@@ -120,9 +138,95 @@ void setKCompatible(mlir::Operation* op, bool allowHK=true){
     }
 }
 
-void singleRollback(mlir::Operation* op){
+void SubgraphOptimizer::singleRollback(mlir::Operation* op){
     setKCompatible(op, true);
     skipSOH(op, true);
+}
+
+// @mcm The idea of this algorithm is to decide between spilling to change tensor split strategy,
+// rolling back SOH to some point where this strategy switch can happen in CMX (HKSwitch)
+// We move backwards through a topological sort of the Op Model
+// For each op, if it is a strategy spill we first decide what ops would need to change for a rollback
+// Consider a simple linear graph A (SOH or HK) -> B (SOH) -> C (SOH) -> D (SOK)
+// When we reach C, a SOH layer that must spill we process it:
+// 1. Add C to ops_to_change, mark it
+// 2. Add unmarked children to Q_c, if they have SOH or HKSwitch strategy and mark them
+//      ex: Q_c : remains empty
+// 3. If C cannnot take HKSwitch, add unmarked parents to Q_p and mark them
+//      ex: Q_p : B
+// 4. Continue processing elements from Q_p while not empty
+//      ex: pop B and process it from step 1 (Q_c will remain empty, Q_p: A)
+//          pop A and process it from step 1 (Q_c will remain empty, Q_p is empty)
+// 5. Continue processing elements from Q_c while not empty
+// 6. If cost cheaper to roll back, last HK-elligble op added to ops to change is HK,
+//    rest take best compatible (SOK, clus) strategy
+void SubgraphOptimizer::rollbackOnSubgraph(mlir::Operation* op){
+    double currentCost = 0.0;
+    double rollbackCost = 0.0;
+    std::queue<mlir::Operation*> parents;
+    std::queue<mlir::Operation*> children;
+    std::vector<mlir::Operation*> parentsToChange;
+    std::vector<mlir::Operation*> childrenToChange;
+    std::set<std::string> processedOps;
+    // @todo Need confirm that
+    // auto heuristicMultiplier = getMultiplier(opIt);
+
+    // @tbd forceRollback() need confirm
+
+    // Only process node with greedy strategy SOH
+    if (!isSpillingFromStrategy(op) || isKCompatible(op, true)) continue;
+    
+    // SOH -> KCompatible
+    parents.push_back(op);    
+    while(!parents.empty() || !children.empty()){
+        mlir::Operation *opIt= nullptr;
+        if (!parents.empty()){
+            opIt= parents.front();
+            parents.pop();
+            parentsToChange.push_back(opIt);
+            rollbackCost += bestCostOfKCompatible(opIt);
+        }else{
+            opIt= children.front();
+            children.pop();
+            childrenToChange.push_back(opIt);
+            rollbackCost += bestCostOfKCompatible(opIt);
+        }
+
+        // Had better record the cost for each layer and read it
+        currentCost+= greedyCostOfLayer(opIt, opIt->getAttr(multiClusterStrategy).cast<mlir::StringAttr>().getValue());
+        processedOps.insert(opIt->getName());
+
+        // Add children if they are still in SOH compatible
+        for (auto child : op->getResult(0).getUsers()){
+            if (processedOps.find(child->getName())) continue;
+            if (auto childStrategy= child->getAttr(multiClusterStrategy)){
+                auto childStrategy_ = childStrategy.cast<mlir::StringAttr>().getValue();
+                if (childStrategy_ == SplitOverKernel || childStrategy_ == HKSwitch){
+                    children.push(child);
+                }
+            } 
+        }
+
+        // We stop moving up the graph when we find nodes that could be HK switch points
+        // or nodes that are already in compatible strategies (SOK, Clus)
+        for (auto input : opIt.inputs()) {
+            auto parent = input.getDefiningOp();
+            if (auto parentStrategy= parent->getAttr(multiClusterStrategy)){
+                if ((canBeSOK(op) && isKCompatible(parent)) || (canBeHKSwitch(op) && !isKCompatible(parent)))
+                    continue;
+                parents.push(parent);
+            }
+        }
+    }
+
+    if (rollbackCost < currentCost){
+        for (auto op : parentsToChange){
+            setKCompatible(op, true);
+        }
+        for (auto op : childrenToChange){
+            setKCompatible(op, false);
+        }
+    }
 }
 
 // @mcm This function is the heart of what the StrategyManager and MetaGraph did for the
@@ -144,7 +248,7 @@ void SubgraphOptimizer::rollbackOrSpill(){
         if(isSpillingFromStrategy(op) && hasGreedyKCompatible(op) && areChildrenKCompatible(op)){
             singleRollback(op);
         }else{
-
+            rollbackOnSubgraph(op);
         }
 
     }
