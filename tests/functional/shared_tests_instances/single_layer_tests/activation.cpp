d65b3d4d51d490cd7fb2a0367ae3512728e04817
Daniil Kireev <daniil.kireev@intel.com>
2022-03-25 14:43:31 +0300

    Fix licenses for open sourcing, part10. tests/functional/, tests/libs/, tests/lit/ affected. (#922)

--- tests/functional/shared_tests_instances/single_layer_tests/activation.cpp
+++ tests/functional/shared_tests_instances/single_layer_tests/activation.cpp
@@ -1,4 +1,5 @@
-// Copyright Intel Corporation.
+//
+// Copyright (C) 2022 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 
--- tests/functional/shared_tests_instances/single_layer_tests/broadcast.cpp
+++ tests/functional/shared_tests_instances/single_layer_tests/broadcast.cpp
@@ -1,7 +1,6 @@
-
-// Copyright (C) Intel Corporation
 //
-// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: Apache 2.0
 //
 
 #include "single_layer_tests/broadcast.hpp"
